afaa59c00abd1070f6d53a0de82acbd97d8e0041
Tian Zhang <tzhang@us.ibm.com>
2012-03-02 15:52:58 -0800

    19772: Tian - convert Kmeans* to new style of junit test, i.e., -args - Tian - convert Kmeans* to the new junit test style, i.e., -args

--- SystemML/DML/test_integration/dml/test/integration/functions/external/kMeansTest.java
+++ SystemML/DML/test_integration/dml/test/integration/functions/external/kMeansTest.java
@@ -10,11 +10,13 @@ import dml.test.integration.TestConfiguration;
  * @author Amol Ghoting
  */
 public class kMeansTest extends AutomatedTestBase {
-
+	
+	private final static String TEST_KMEANS = "kMeans";
+	
 	@Override
 	public void setUp() {
 		baseDirectory = SCRIPT_DIR + "functions/external/";
-		availableTestConfigurations.put("kMeansTest", new TestConfiguration("kMeansTest", new String[] { "kCenters", "kCentersWithInit" }));
+		availableTestConfigurations.put(TEST_KMEANS, new TestConfiguration(TEST_KMEANS, new String[] { "kCenters", "kCentersWithInit" }));
 	}
 
 	@Test
@@ -24,22 +26,35 @@ public class kMeansTest extends AutomatedTestBase {
 		int cols = 10;
 		int centers = 5;
 
-		TestConfiguration config = availableTestConfigurations.get("kMeansTest");
+		TestConfiguration config = availableTestConfigurations.get(TEST_KMEANS);
 		config.addVariable("rows", rows);
 		config.addVariable("cols", cols);
-
+		
+		/* This is for running the junit test the new way, i.e., construct the arguments directly */
+		String KMEANS_HOME = baseDirectory;
+		dmlArgs = new String[]{"-f", KMEANS_HOME + TEST_KMEANS + ".dml",
+				               "-args", "\"" + KMEANS_HOME + INPUT_DIR + "M" + "\"", 
+				                        Integer.toString(rows), Integer.toString(cols), 
+				                        "\"" + KMEANS_HOME + INPUT_DIR + "initCenters" + "\"", 
+				                        "\"" + KMEANS_HOME + OUTPUT_DIR + "kcenters" + "\"",
+				                        "\"" + KMEANS_HOME + OUTPUT_DIR + "kcentersWithInit" + "\""};
+		dmlArgsDebug = new String[]{"-f", KMEANS_HOME + TEST_KMEANS + ".dml", "-d", 
+	                                "-args", "\"" + KMEANS_HOME + INPUT_DIR + "M" + "\"", 
+	                                         Integer.toString(rows), Integer.toString(cols), 
+	                                         "\"" + KMEANS_HOME + INPUT_DIR + "initCenters" + "\"", 
+	                                         "\"" + KMEANS_HOME + OUTPUT_DIR + "kcenters" + "\"",
+	                                         "\"" + KMEANS_HOME + OUTPUT_DIR + "kcentersWithInit" + "\""};
+		
 		double[][] M = getRandomMatrix(rows, cols, -1, 1, 0.05, -1);
 		double[][] initCenters = getRandomMatrix(centers, cols, -1, 1, 0.05, -1);
-		
 		
 		writeInputMatrix("M", M);
 		writeInputMatrix("initCenters",initCenters);
 		
 		loadTestConfiguration(config);
 
-		runTest();
+		runTest(true, false, null, -1);
 
-		
 		checkForResultExistence();
 	}
 }

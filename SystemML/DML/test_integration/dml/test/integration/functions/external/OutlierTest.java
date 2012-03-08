6553a213cc3b7d3bb9227ddeaa666a8ec0aa8be8
JIHONG MA <jihongma@us.ibm.com>
2012-03-08 15:49:07 -0800

    19808: DML TEST - update Outlier test case

--- SystemML/DML/test_integration/dml/test/integration/functions/external/OutlierTest.java
+++ SystemML/DML/test_integration/dml/test/integration/functions/external/OutlierTest.java
@@ -10,10 +10,12 @@ import dml.test.integration.TestConfiguration;
  */
 public class OutlierTest extends AutomatedTestBase {
 
+	private final static String TEST_OUTLIER = "Outlier"; 
+	
 	@Override
 	public void setUp() {
 		baseDirectory = SCRIPT_DIR + "functions/external/";
-		availableTestConfigurations.put("OutlierTest", new TestConfiguration("OutlierTest", new String[] { "o" }));
+		availableTestConfigurations.put("Outlier", new TestConfiguration(baseDirectory, "Outlier", new String[] { "o" }));
 	}
 
 	@Test
@@ -22,17 +24,29 @@ public class OutlierTest extends AutomatedTestBase {
 		int rows = 100;
 		int cols = 10;
 
-		TestConfiguration config = availableTestConfigurations.get("OutlierTest");
+		TestConfiguration config = availableTestConfigurations.get("Outlier");
 		config.addVariable("rows", rows);
 		config.addVariable("cols", cols);
 
+		/* This is for running the junit test by constructing the arguments directly */
+		String OUTLIER_HOME = baseDirectory;
+		dmlArgs = new String[]{"-f", OUTLIER_HOME + TEST_OUTLIER + ".dml",
+				               "-args", "\"" + OUTLIER_HOME + INPUT_DIR + "M" + "\"", 
+				                        Integer.toString(rows), Integer.toString(cols), 
+				                        "\"" + OUTLIER_HOME + OUTPUT_DIR + "o" + "\""};
+		dmlArgsDebug = new String[]{"-f", OUTLIER_HOME + TEST_OUTLIER + ".dml", "-d", 
+	                                "-args", "\"" + OUTLIER_HOME + INPUT_DIR + "M" + "\"", 
+	                                         Integer.toString(rows), Integer.toString(cols), 
+	                                         "\"" + OUTLIER_HOME + OUTPUT_DIR + "o" + "\""};
+		
 		double[][] M = getRandomMatrix(rows, cols, -1, 1, 0.05, -1);
 			
 		writeInputMatrix("M", M);
 		
 		loadTestConfiguration(config);
 
-		runTest();
+		// there is no expected number of M/R job calculated, set to default for now
+		runTest(true, false, null, -1);
 
 		
 		checkForResultExistence();

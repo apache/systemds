9308c4e2f082e59d0fd833c33d4e96ca9556969d
JIHONG MA <jihongma@us.ibm.com>
2012-03-08 16:29:26 -0800

    19808: DML TEST - update SeqMiner test case

--- SystemML/DML/test_integration/dml/test/integration/functions/external/SequenceMiner.java
+++ SystemML/DML/test_integration/dml/test/integration/functions/external/SequenceMiner.java
@@ -10,10 +10,12 @@ import dml.test.integration.TestConfiguration;
  */
 public class SequenceMiner extends AutomatedTestBase {
 
+	private final static String TEST_SEQMINER = "SeqMiner"; 
+	
 	@Override
 	public void setUp() {
 		baseDirectory = SCRIPT_DIR + "functions/external/";
-		availableTestConfigurations.put("SeqTest", new TestConfiguration("SeqTest", new String[] { "fseq", "sup" }));
+		availableTestConfigurations.put("SeqMiner", new TestConfiguration(baseDirectory, "SeqMiner", new String[] { "fseq", "sup" }));
 	}
 
 	@Test
@@ -22,10 +24,23 @@ public class SequenceMiner extends AutomatedTestBase {
 		int rows = 5;
 		int cols = 10;
 
-		TestConfiguration config = availableTestConfigurations.get("SeqTest");
+		TestConfiguration config = availableTestConfigurations.get("SeqMiner");
 		config.addVariable("rows", rows);
 		config.addVariable("cols", cols);
 
+		/* This is for running the junit test by constructing the arguments directly */
+		String SEQMINER_HOME = baseDirectory;
+		dmlArgs = new String[]{"-f", SEQMINER_HOME + TEST_SEQMINER + ".dml",
+				               "-args", "\"" + SEQMINER_HOME + INPUT_DIR + "M" + "\"", 
+				                        Integer.toString(rows), Integer.toString(cols), 
+				                        "\"" + SEQMINER_HOME + OUTPUT_DIR + "fseq" + "\"",
+				                        "\"" + SEQMINER_HOME + OUTPUT_DIR + "sup" + "\""};
+		dmlArgsDebug = new String[]{"-f", SEQMINER_HOME + TEST_SEQMINER + ".dml", "-d", 
+				                    "-args", "\"" + SEQMINER_HOME + INPUT_DIR + "M" + "\"", 
+                                             Integer.toString(rows), Integer.toString(cols), 
+                                             "\"" + SEQMINER_HOME + OUTPUT_DIR + "fseq" + "\"",
+                                             "\"" + SEQMINER_HOME + OUTPUT_DIR + "sup" + "\""};
+		
 		double[][] M = {{1, 2, 3, 4, -1, 2, 3, 4, 5, -1}, 
 				{1, 2, 3, 4, -1, 2, 3, 4, 5, -1}, 
 				{1, 2, 3, 4, -1, 2, 3, 4, 5, -1}, 
@@ -37,7 +52,8 @@ public class SequenceMiner extends AutomatedTestBase {
 		
 		loadTestConfiguration(config);
 
-		runTest();
+		// no expected number of M/R jobs are calculated, set to default for now
+		runTest(true, false, null, -1);
 
 		
 		checkForResultExistence();

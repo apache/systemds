834d9e25427d2ff0dfd1479c4472ca79f8db3c81
PRITHVIRAJ SEN <senp@us.ibm.com>
2012-04-11 13:16:30 -0700

    19888: Change DML scripts - L2SVM.dml

--- SystemML/DML/src/dml/lops/Append.java
+++ SystemML/DML/src/dml/lops/Append.java
@@ -12,7 +12,6 @@ public class Append extends Lops{
 	{
 		super(Lops.Type.Append, dt, vt);
 		init(input1, input2, input3, dt, vt, et);
-		
 	}
 	
 	public Append(Lops input1, Lops input2, Lops input3, DataType dt, ValueType vt) 
@@ -54,22 +53,37 @@ public class Append extends Lops{
 		return " Append: ";
 	}
 
+	//called when append executes in MR
 	public String getInstructions(int input_index1, int input_index2, int input_index3, int output_index) throws LopsException
 	{
+		return getInstructions(input_index1+"",
+							   input_index2+"",
+							   input_index3+"",
+							   output_index+"");	
+	}
+	
+	//....in CP
+	public String getInstructions(String input_index1, String input_index2, String input_index3, String output_index) throws LopsException
+	{
 		String offsetString;
-		
 		String offsetLabel = this.getInputs().get(2).getOutputParameters().getLabel();
-		
-		if (this.getInputs().get(2).getExecLocation() == ExecLocation.Data
-			&& ((Data) this.getInputs().get(2)).isLiteral())
+	
+		if (getExecType() == ExecType.MR ) {
+			if (this.getInputs().get(2).getExecLocation() == ExecLocation.Data
+				&& ((Data) this.getInputs().get(2)).isLiteral())
+				offsetString = "" + offsetLabel;
+			else
+				offsetString = "##" + offsetLabel + "##";
+		}
+		else {
 			offsetString = "" + offsetLabel;
-		else
-			offsetString = "##" + offsetLabel + "##";
+		}
 		
-		return "append" + OPERAND_DELIMITOR + 
-				input_index1 + VALUETYPE_PREFIX + this.getInputs().get(0).get_valueType() + OPERAND_DELIMITOR + 
-				input_index2 + VALUETYPE_PREFIX + this.getInputs().get(0).get_valueType() + OPERAND_DELIMITOR + 
-				output_index + VALUETYPE_PREFIX + this.get_valueType() + OPERAND_DELIMITOR +
-				offsetString;	
-	}	
+		return this.lps.execType 
+					 + OPERAND_DELIMITOR + "append" 
+					 + OPERAND_DELIMITOR + input_index1 + DATATYPE_PREFIX + this.getInputs().get(0).get_dataType() + VALUETYPE_PREFIX + this.getInputs().get(0).get_valueType() 
+					 + OPERAND_DELIMITOR + input_index2 + DATATYPE_PREFIX + this.getInputs().get(1).get_dataType() + VALUETYPE_PREFIX + this.getInputs().get(1).get_valueType() 
+					 + OPERAND_DELIMITOR + output_index + DATATYPE_PREFIX + this.get_dataType() + VALUETYPE_PREFIX + this.get_valueType() 
+					 + OPERAND_DELIMITOR + offsetString;	
+	}
 }

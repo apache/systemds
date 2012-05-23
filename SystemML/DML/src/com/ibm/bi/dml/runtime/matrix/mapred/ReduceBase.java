74d6d1ebd32be66642d7a38eb518f0d51501f53d
YUANYUAN TIAN <ytian@us.ibm.com>
2012-05-22 18:27:54 -0700

    22561: collect statistics for maponly jobs - remove unused imports

--- SystemML/DML/src/com/ibm/bi/dml/runtime/matrix/mapred/ReduceBase.java
+++ SystemML/DML/src/com/ibm/bi/dml/runtime/matrix/mapred/ReduceBase.java
@@ -2,7 +2,6 @@ package com.ibm.bi.dml.runtime.matrix.mapred;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Vector;

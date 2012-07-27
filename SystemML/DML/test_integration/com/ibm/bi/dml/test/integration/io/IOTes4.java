9bf13630253f2071ad76307bd35a53b6a1820037
HAOJUN WANG <haojunw@us.ibm.com>
2012-07-27 11:52:30 -0700

    21784: Input/Output statement filename parameter supports const string concatenation

---
+++ SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/io/IOTes4.java
@@ -0,0 +1,68 @@
+package com.ibm.bi.dml.test.integration.io;
+
+import org.junit.Test;
+
+import com.ibm.bi.dml.test.integration.AutomatedTestBase;
+import com.ibm.bi.dml.test.integration.TestConfiguration;
+import com.ibm.bi.dml.utils.LanguageException;
+
+
+/**
+ * <p>
+ * <b>Positive tests:</b>
+ * </p>
+ * <ul>
+ * <li>text format</li>
+ * <li>binary format</li>
+ * </ul>
+ * <p>
+ * <b>Negative tests:</b>
+ * </p>
+ * <ul>
+ * <li>wrong row dimension (format=text)</li>
+ * <li>wrong column dimension (format=text)</li>
+ * <li>wrong row and column dimensions (format=text)</li>
+ * <li>wrong format (format=text)</li>
+ * <li>wrong row dimension (format=binary)</li>
+ * <li>wrong column dimension (format=binary)</li>
+ * <li>wrong row and column dimensions (format=binary)</li>
+ * <li>wrong format (format=binary)</li>
+ * </ul>
+ * 
+ * 
+ */
+public class IOTes4 extends AutomatedTestBase {
+
+	@Override
+	public void setUp() {
+		baseDirectory = SCRIPT_DIR + "functions/io/";
+
+		// positive tests
+		
+		
+
+		// negative tests
+		availableTestConfigurations.put("SimpleTest", new TestConfiguration("IOTest4", new String[] { "a" }));
+	}
+
+	@Test
+	public void testSimple() {
+		int rows = 10;
+		int cols = 10;
+
+		TestConfiguration config = availableTestConfigurations.get("SimpleTest");
+		config.addVariable("rows", rows);
+		config.addVariable("cols", cols);
+		config.addVariable("format", "text");
+	
+		loadTestConfiguration("SimpleTest");
+
+		double[][] a = getRandomMatrix(rows, cols, -1, 1, 0.5, -1);
+		writeInputMatrix("a", a);
+		writeExpectedMatrix("a", a);
+
+		runTest(true, LanguageException.class);
+
+	}
+
+}

27a40ec66cc727d28f3ddaaa69906e86a52ed44d
MATTHIAS BOEHM <mboehm@us.ibm.com>
2012-09-13 12:03:23 -0700

    20296: SystemML external functions - External function for CP dynamic write

---
+++ SystemML/DML/src/com/ibm/bi/dml/packagesupport/DynamicWriteMatrixCP.java
@@ -0,0 +1,70 @@
+package com.ibm.bi.dml.packagesupport;
+
+import com.ibm.bi.dml.packagesupport.FIO;
+import com.ibm.bi.dml.packagesupport.Matrix;
+import com.ibm.bi.dml.packagesupport.PackageFunction;
+import com.ibm.bi.dml.packagesupport.PackageRuntimeException;
+import com.ibm.bi.dml.packagesupport.Scalar;
+import com.ibm.bi.dml.packagesupport.Scalar.ScalarType;
+import com.ibm.bi.dml.runtime.instructions.CPInstructions.MatrixObjectNew;
+import com.ibm.bi.dml.runtime.matrix.MatrixCharacteristics;
+import com.ibm.bi.dml.runtime.matrix.MatrixDimensionsMetaData;
+import com.ibm.bi.dml.runtime.matrix.io.MatrixBlock;
+import com.ibm.bi.dml.runtime.matrix.io.OutputInfo;
+import com.ibm.bi.dml.runtime.util.DataConverter;
+
+/**
+ * 
+ *
+ */
+public class DynamicWriteMatrixCP extends PackageFunction 
+{	
+	private static final long serialVersionUID = 1L;
+	private Scalar _success;
+	
+	@Override
+	public int getNumFunctionOutputs() 
+	{
+		return 1;
+	}
+
+	@Override
+	public FIO getFunctionOutput(int pos) 
+	{
+		return _success;
+	}
+
+	@Override
+	public void execute() 
+	{
+		boolean success = false;
+		
+		try
+		{
+			Matrix mat = (Matrix) this.getFunctionInput(0);
+			String fname = ((Scalar) this.getFunctionInput(1)).getValue();
+			String format = ((Scalar) this.getFunctionInput(2)).getValue();
+			
+			MatrixObjectNew mo = mat.getMatrixObject();
+			MatrixCharacteristics mc = ((MatrixDimensionsMetaData)mo.getMetaData()).getMatrixCharacteristics();
+			long rlen = mc.get_rows();
+			long clen = mc.get_cols();
+			int brlen = mc.get_rows_per_block();
+			int bclen = mc.get_cols_per_block();
+			OutputInfo oi = OutputInfo.stringToOutputInfo(format);
+			
+			MatrixBlock mb = mo.acquireRead();
+			DataConverter.writeMatrixToHDFS(mb, fname, oi, rlen, clen, brlen, bclen);			
+			mo.release();
+			
+			success = true;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			throw new PackageRuntimeException("Error executing dynamic write of matrix");
+		}
+		
+		_success = new Scalar(ScalarType.Boolean,String.valueOf(success));
+	}
+}

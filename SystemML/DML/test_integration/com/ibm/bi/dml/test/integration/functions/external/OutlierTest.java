c76da278f719fbb638ed9b2b31d394551b3573a8
MATTHIAS BOEHM <mboehm@us.ibm.com>
2012-10-02 12:27:01 -0700

    20296: SystemML external functions - Fix OutlierTest (wrong computation of expected results)

--- SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/functions/external/OutlierTest.java
+++ SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/functions/external/OutlierTest.java
@@ -1,5 +1,6 @@
 package com.ibm.bi.dml.test.integration.functions.external;
 
+import java.util.HashSet;
 import java.util.PriorityQueue;
 
 import org.junit.Test;
@@ -12,20 +13,44 @@ public class OutlierTest extends AutomatedTestBase {
 
 	private final static String TEST_OUTLIER = "Outlier"; 
 
+	private int _rows = 100;
+	private int _cols = 10;
+	private int _m = 5;
+	private int _K = 2;
+	private double _sparsity1 = 0.05;
+	private double _sparsity2 = 0.5;
+	
+	
 	@Override
+	@SuppressWarnings("deprecation")
 	public void setUp() {
 		baseDirectory = SCRIPT_DIR + "functions/external/";
 		availableTestConfigurations.put(TEST_OUTLIER, new TestConfiguration(TEST_OUTLIER, new String[] { "o"}));
 	}
 
 	@Test
-	public void testOutlierTest() {
-
-		int rows = 100;
-		int cols = 10;
-		int m = 5;
-		int K = 2;
-
+	public void testOutlierSparse() 
+	{
+		runOutlierTest(_rows, _cols, _m, _K, _sparsity1);
+	}
+	
+	@Test
+	public void testOutlierDense() 
+	{
+		runOutlierTest(_rows, _cols, _m, _K, _sparsity2);
+	}
+	
+	
+	/**
+	 * 
+	 * @param rows
+	 * @param cols
+	 * @param m 	num outliers
+	 * @param K		num neighbors
+	 */
+	@SuppressWarnings("unchecked")
+	public void runOutlierTest(int rows, int cols, int m, int K, double sparsity) 
+	{
 		TestConfiguration config = availableTestConfigurations.get("Outlier");
 		config.addVariable("rows", rows);
 		config.addVariable("cols", cols);
@@ -37,7 +62,7 @@ public class OutlierTest extends AutomatedTestBase {
 				Integer.toString(rows), Integer.toString(cols), 
 				 OUTLIER_HOME + OUTPUT_DIR + "o" };
 
-		double[][] M = getRandomMatrix(rows, cols, -1, 1, 0.05, -1);
+		double[][] M = getRandomMatrix(rows, cols, -1, 1, sparsity, 1471 );
 
 		writeInputMatrix("M", M);
 
@@ -45,26 +70,25 @@ public class OutlierTest extends AutomatedTestBase {
 
 		// there is no expected number of M/R job calculated, set to default for now
 		runTest(true, false, null, -1);
-
-
+		
 		//holds input
-		float[][] bin = new float[rows][rows];
+		float[][] bin = new float[rows][cols];
 		
 		//holds output
-		double [][] outMatrix = new double[rows][m];
+		double [][] outMatrix = new double[m][cols]; 
 
+		//copy input
 		for(int i =0; i < rows; i++)
 			for(int j=0; j < cols; j++)
 				bin[i][j] = (float) M[i][j];
-
+		
 		//determine knn for each point
-		PriorityQueue[] nearestNeighbors = new PriorityQueue [rows];
+		PriorityQueue<Float>[] nearestNeighbors = new PriorityQueue[rows];
 		double [][] dists = new double[rows][rows];
 		float [] knn_dist = new float [rows];
 		
 		//distance to knn to find top m
 		PriorityQueue<Float> outlier_knn = new PriorityQueue<Float>();
-
 		for (int i = 0; i < rows; i++) {
 			nearestNeighbors[i] = new PriorityQueue<Float>();
 			for (int j = 0; j < rows; j++) {
@@ -83,22 +107,35 @@ public class OutlierTest extends AutomatedTestBase {
 			knn_dist[i] = knn;
 		}
 		
-		while(outlier_knn.size() > m)
+		//remove outliers not in top m
+		while( outlier_knn.size()>m )
 			outlier_knn.poll();
 		
-		//find top outliers in reverse order 
-		while(outlier_knn.size() > 0)
+		//find top outliers rowIDs in reverse order
+		HashSet<Integer> outliers = new HashSet<Integer>(); 
+		while( outliers.size() < m )
 		{
 			float threshold = outlier_knn.poll();
 			for(int i=0; i < rows; i++)
 			{
+				//find outlier row
 				if(knn_dist[i] == threshold)
 				{
-					for(int j=0; i < cols; j++)
-						outMatrix[outlier_knn.size() -1][j] = bin[i][j];
+					//skip rows already processed (if multiple rows have equal threshold)
+					if( outliers.contains(i) )
+						continue;
+					
+					//copy row to output
+					for(int j=0; j < cols; j++)
+						outMatrix[outliers.size()][j] = bin[i][j];
+					outliers.add(i);
+					
+					//finish if we obtained m outliers
+					if( outliers.size() == m )
+						break;
 				}
-			}
-		}
+			}	
+		}	
 		
 		writeExpectedMatrix("o", outMatrix);
 

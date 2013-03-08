084e51b93bda56ab16f3bd45ff0559deb9563d6a
JIHONG MA <jihongma@us.ibm.com>
2013-03-08 11:47:02 -0800

    35916: Enable Write R MatrixMarket format support in DML  - update writeMMTest JUnit test case to cover both CP and MR mode.

--- SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/functions/data/WriteMMTest.java
+++ SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/functions/data/WriteMMTest.java
@@ -2,10 +2,15 @@ package com.ibm.bi.dml.test.integration.functions.data;
 
 
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
+import com.ibm.bi.dml.api.DMLScript.RUNTIME_PLATFORM;
+import com.ibm.bi.dml.lops.LopProperties.ExecType;
+import com.ibm.bi.dml.lops.MMTSJ.MMTSJType;
 import com.ibm.bi.dml.runtime.matrix.MatrixCharacteristics;
-import com.ibm.bi.dml.test.BinaryMatrixCharacteristics;
+import com.ibm.bi.dml.runtime.matrix.io.MatrixValue.CellIndex;
 import com.ibm.bi.dml.test.integration.AutomatedTestBase;
 import com.ibm.bi.dml.test.integration.TestConfiguration;
 import com.ibm.bi.dml.test.utils.TestUtils;
@@ -25,58 +30,95 @@ import com.ibm.bi.dml.test.utils.TestUtils;
  */
 public class WriteMMTest extends AutomatedTestBase {
 
+	private final static String TEST_NAME1 = "WriteMMTest";
+	private final static String TEST_NAME2 = "WriteMMComplexTest";
+	private final static String TEST_DIR = "functions/data/";
+	
+	//for CP
+	private final static int rows1 = 30;
+	private final static int cols1 = 10;
+	//for MR
+	private final static int rows2 = 700;  
+	private final static int cols2 = 100;
+	
+		
 	@Override
-	public void setUp() {
-		baseDirectory = SCRIPT_DIR + "functions/data/";
-		
-		// positive tests
-		availableTestConfigurations.put("SimpleTest", new TestConfiguration("WriteMMTest",
-				new String[] { "a" }));
-		availableTestConfigurations.put("ComplexTest", new TestConfiguration("WriteMMComplexTest",
-				new String[] { "a" }));
-		
-		
-		// negative tests
-	}
-	
-	@Test
-	public void testMM() {
-		int rows = 10;
-		int cols = 10;
-		
-		TestConfiguration config = availableTestConfigurations.get("SimpleTest");
-		loadTestConfiguration("SimpleTest");
-		
-		double[][] a = getRandomMatrix(rows, cols, -1, 1, 0.7, System.currentTimeMillis());
-		writeInputMatrixWithMTD("a", a, false, new MatrixCharacteristics(rows,cols,1000,1000));
-		writeExpectedMatrixMarket("a", a);
-		
-		runTest();
-		
-		compareResultsWithMM();
+	public void setUp() 
+	{
+		addTestConfiguration(
+				TEST_NAME1, 
+				new TestConfiguration(TEST_DIR, TEST_NAME1, 
+				new String[] { "B" })   ); 
+		addTestConfiguration(
+				TEST_NAME2, 
+				new TestConfiguration(TEST_DIR, TEST_NAME2, 
+				new String[] { "B" })   ); 
 	}
 
 	@Test
-	public void testComplex() {
-		
-		int rows = 100;
-		int cols = 100;
-		
-		TestConfiguration config = availableTestConfigurations.get("ComplexTest");
+	public void testWriteMMCP() 
+	{
+		runWriteMMTest(ExecType.CP, TEST_NAME1);
+	}
 	
+	@Test
+	public void testWriteMMMR() 
+	{
+		runWriteMMTest(ExecType.MR, TEST_NAME1);
+	}
+	
+	@Test
+	public void testWriteMMMRMerge()
+	{
+		runWriteMMTest(ExecType.MR, TEST_NAME2);
+	}
+	
+	private void runWriteMMTest( ExecType instType, String TEST_NAME )
+	{
+		//setup exec type, rows, cols
+		int rows = -1, cols = -1;
 		
-		loadTestConfiguration("ComplexTest");
 		
-		double[][] a = getRandomMatrix(rows, cols, -1, 1, 0.7, System.currentTimeMillis());
-		writeInputMatrixWithMTD("a", a, false, new MatrixCharacteristics(rows,cols,1000,1000));
-		writeExpectedMatrixMarket("a", a);
-		
-		
-		runTest();
-
+		if( instType == ExecType.CP ) {
+				rows = rows1;
+				cols = cols1;
+		}
+		else { //if type MR
+				rows = rows2;
+				cols = cols2;
+		}
 			
-		compareResultsWithMM();
 
-	} 
-
-} 
+		//rtplatform for MR
+		RUNTIME_PLATFORM platformOld = rtplatform;
+		rtplatform = (instType==ExecType.MR) ? RUNTIME_PLATFORM.HADOOP : RUNTIME_PLATFORM.HYBRID;
+	
+		try
+		{
+			TestConfiguration config = getTestConfiguration(TEST_NAME);
+			
+			/* This is for running the junit test the new way, i.e., construct the arguments directly */
+			String HOME = SCRIPT_DIR + TEST_DIR;
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[]{"-args", HOME + INPUT_DIR + "A" ,
+					                        Integer.toString(rows),
+					                        Integer.toString(cols),
+					                        HOME + OUTPUT_DIR + "B"  };
+			
+			loadTestConfiguration(config);
+	
+			//generate actual dataset
+			double[][] A = getRandomMatrix(rows, cols, -1, 1, 1, System.currentTimeMillis()); 
+			writeInputMatrixWithMTD("A", A, false, new MatrixCharacteristics(rows,cols, 1000, 1000));
+			writeExpectedMatrixMarket("B", A);
+	
+			runTest(true, false, null, -1);
+			compareResultsWithMM();
+		}
+		finally
+		{
+			rtplatform = platformOld;
+		}
+	}
+	
+}

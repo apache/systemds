aa94a15e1a8af308e19722b7458fb9a878ca7b05
HAOJUN WANG <haojunw@us.ibm.com>
2013-03-22 14:24:00 -0700

    37997: Logging: Adding prototype code for JMX based log level changing feature

---
+++ SystemML/Logging/run.sh
@@ -0,0 +1,1 @@
+java -cp $WORKSPACE/Logging/bin/Logging.jar:$BIGINSIGHTS_LIB/commons-lang-2.5.jar:$HADOOP_HOME/lib/commons-logging-1.1.1.jar com.ibm.biginsights.logging.jmx.Client
--- SystemML/Logging/src/com/ibm/biginsights/logging/jmx/Client.java
+++ SystemML/Logging/src/com/ibm/biginsights/logging/jmx/Client.java
@@ -15,44 +15,19 @@
  */
 package com.ibm.biginsights.logging.jmx;
 
-import javax.management.Attribute;
-import javax.management.AttributeChangeNotification;
-import javax.management.DynamicMBean;
 import javax.management.JMX;
 import javax.management.MBeanServerConnection;
-import javax.management.Notification;
-import javax.management.NotificationListener;
 import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.jmx.HierarchyDynamicMBean;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 
 public class Client {
 	private static final Log LOG = LogFactory.getLog(Client.class.getName());
-	/**
-     * Inner class that will handle the notifications.
-     */
-    public static class ClientListener implements NotificationListener {
-        public void handleNotification(Notification notification,
-                                       Object handback) {
-            LOG.info("Received notification: ClassName: " + notification.getClass().getName());
-            LOG.info("Source: " + notification.getSource());
-            LOG.info("Type: " + notification.getType());
-            LOG.info("Message: " + notification.getMessage());
-            if (notification instanceof AttributeChangeNotification) {
-                AttributeChangeNotification attrChangeNotif =
-                    (AttributeChangeNotification) notification;
-                LOG.info("Change Attribute: " + attrChangeNotif.getAttributeName());
-                LOG.info("AttributeType: " + attrChangeNotif.getAttributeType() + " NewValue: " + attrChangeNotif.getNewValue() + " OldValue: " + attrChangeNotif.getOldValue());
-            }
-        }
-    }
     
 	/**
 	 * @param args
@@ -67,53 +42,27 @@ public class Client {
     			new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:10011/jmxrmi");
     	JMXConnector jmxConnector = JMXConnectorFactory.connect(url, null);
 
-    	// Create listener
-    	// TODO: This part probably is not important
-    	ClientListener listener = new ClientListener();
-
     	// Get an MBeanServerConnection
     	MBeanServerConnection mbServerConn = jmxConnector.getMBeanServerConnection();
 
-    	// Get domains from MBeanServer
-    	// TODO: This part probably is not important
-/*    	String domains[] = mbServerConn.getDomains();
-    	Arrays.sort(domains);
-    	for (String domain : domains) {
-    		LOG.info("Domain = " + domain);
-    	}*/
 
     	// ----------------------
-    	// Manage the Hello MBean
+    	// Manage the Log4jConfig MXBean
     	// ----------------------
-    	// Construct the ObjectName for the Hello MBean
-    	//
-    	ObjectName mbeanName = new ObjectName("log4j:hiearchy=default");
+
+    	ObjectName mbeanName = new ObjectName("com.ibm.biginsights.logging.jmx:type=Log4jConfig");
 
     	// Create a dedicated proxy for the MBean instead of going 
     	// directly via the MBean server connection
-    	DynamicMBean mbeanProxy = JMX.newMBeanProxy(mbServerConn, mbeanName, DynamicMBean.class, true);
-
-    	// Add notification listener on Hello MBean
-    	// TODO: This probably is not important
-    	mbServerConn.addNotificationListener(mbeanName, listener, null, null);
+    	Log4jConfigMXBean mbeanProxy = JMX.newMBeanProxy(mbServerConn, mbeanName, Log4jConfigMXBean.class, true);
 
     	// Set attribute at the remote JMX server
-    	//Logger rootLogger = Logger.getLogger("com.ibm.biginsights.bigsql");
-    	//mbeanProxy.addLoggerMBean(rootLogger.getName());
-    	Attribute attr = new Attribute("priority", new String("DEBUG"));
-    	mbeanProxy.setAttribute(attr);
+    	mbeanProxy.setLogLevel("com.ibm.biginsights.bigsql", "INFO");
 
 
     	jmxConnector.close();
     }
 
-    private static void sleep(int millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
 
 }

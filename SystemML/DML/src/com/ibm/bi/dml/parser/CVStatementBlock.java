5e0e94a16885fd50a6bcadb24cb783752e5020a7
DOUGLAS R. BURDICK <drburdic@us.ibm.com>
2013-08-06 01:16:16 -0700

    20969: Various Engine Enhancements - fixes to validation for CVStatement

--- SystemML/DML/src/com/ibm/bi/dml/parser/CVStatementBlock.java
+++ SystemML/DML/src/com/ibm/bi/dml/parser/CVStatementBlock.java
@@ -1,5 +1,6 @@
 package com.ibm.bi.dml.parser;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -35,160 +36,199 @@ public class CVStatementBlock extends StatementBlock {
 		}	
 		
 		// build list of partition outputs
-		// NOTE: assume each partition output is DataIdentifier
+		// NOTE: assume each partition output is DataIdentifier with datatype matrix (and valuetype double)
 		VariableSet partitionOutputList = new VariableSet();
 		for (Expression output : cvs.get_partitionOutputs().getParamExpressions()){
+			try {
+				((DataIdentifier)output).setTypeInfo("double", "matrix");
+			} catch (ParseException e){
+				throw new LanguageException(e);
+			}
 			partitionOutputList.addVariable( ((DataIdentifier)output).getName(),(DataIdentifier)output);
+			
 		}
 		
 		// check train function exists
 		FunctionCallIdentifier trainFCI = cvs.get_trainFunctionCall();
-		if (!dmlProg.getFunctionStatementBlocks(trainFCI.getNamespace()).containsKey(trainFCI.getName())){
+		HashMap<String, FunctionStatementBlock> tempNS = dmlProg.getFunctionStatementBlocks(trainFCI.getNamespace());
+		if (tempNS == null){
+			LOG.error(this.printBlockErrorLocation() + "Namespace " + trainFCI.getNamespace() + " is undefined ");
+			throw new LanguageException(this.printBlockErrorLocation() + "Namespace " + trainFCI.getNamespace() + " is undefined ");
+		}
+		FunctionStatementBlock trainFSB = tempNS.get(trainFCI.getName());
+		if (trainFSB == null){
 			LOG.error(this.printBlockErrorLocation() + "CV training function " + trainFCI.getNamespace() + "::" + trainFCI.getName() + " is not available ");
 			throw new LanguageException(this.printBlockErrorLocation() + "CV training function " + trainFCI.getNamespace() + "::" + trainFCI.getName() + " is not available ");
 		}
 		
-		// check train function inputs are available from either:
-		//	1) already defined variables
-		//	2) partition function outputs
-		for (Expression input : trainFCI.getParamExpressions()){
-					
-			if (input instanceof DataIdentifier){
-				boolean found = false;
-				
-				if (ids.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
-				if (partitionOutputList.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
-			
-				if (found == false){
-					LOG.error(this.printBlockErrorLocation() + "In CV Statement, variable " + input.toString() + " is not available");
-					throw new LanguageException(this.printBlockErrorLocation() + "In CV Statement, variable " + input.toString() + " is not available");
-				}
-			}
-			else if (input instanceof ConstIdentifier) {
-				input.validateExpression(ids.getVariables(), constVars);
-			}
-			else {
-				LOG.error(this.printBlockErrorLocation() + "CV training function " + trainFCI.getNamespace() + "::" + trainFCI.getName() + " has unsupported formal parameter: " + input.toString());
-				throw new LanguageException(this.printBlockErrorLocation() + "CV training function " + trainFCI.getNamespace() + "::" + trainFCI.getName() + " has unsupported formal parameter: " + input.toString());
-			}
+		// check train function has correct number of parameters AND
+		// train function inputs are available from either:
+		//	 1) already defined variables
+		//	 2) partition function outputs
+		VariableSet tempIds = new VariableSet();
+		tempIds.addVariables(ids);
+		tempIds.addVariables(partitionOutputList);
+		try {
+			trainFCI.validateExpression(dmlProg,tempIds.getVariables(), constVars);
+		} catch(IOException e){
+			throw new LanguageException(e);
 		}
 		
 		// build list of training function outputs
-		// NOTE: assume each training function output is DataIdentifier
+		// check training function call has correct number of parameters		
+		FunctionStatement trainFS = (FunctionStatement)trainFSB.getStatement(0);
+		if (cvs.get_trainFunctionOutputs().getParamExpressions().size() != trainFS.getOutputParams().size()){
+			LOG.error(this.printBlockErrorLocation() + "CV training function " + trainFCI.getNamespace() + "::" + trainFCI.getName() + " has wrong number parameters ");
+			throw new LanguageException(this.printBlockErrorLocation() + "CV training function " + trainFCI.getNamespace() + "::" + trainFCI.getName() + " has wrong number parameters ");
+
+		}
+		
+		// for each train function parameter, set datatype and valuetype
 		VariableSet trainOutputList = new VariableSet();
-		for (Expression output : cvs.get_trainFunctionOutputs().getParamExpressions()){
+		for (int i = 0; i< cvs.get_trainFunctionOutputs().getParamExpressions().size(); i++){
+			DataIdentifier output = (DataIdentifier)cvs.get_trainFunctionOutputs().getParamExpressions().get(i);
+			try {
+				output.setTypeInfo(trainFS.getOutputParams().get(i).getValueType().toString(), trainFS.getOutputParams().get(i).getDataType().toString());
+			} catch (ParseException e) {
+				throw new LanguageException(e);
+			}
 			trainOutputList.addVariable( ((DataIdentifier)output).getName(),(DataIdentifier)output);
 		}
 		
+		////////////////////////////////////////////////////////////////////////
+		// validate test function
+		////////////////////////////////////////////////////////////////////////
+		
 		// check test function exists
 		FunctionCallIdentifier testFCI = cvs.get_testFunctionCall();
-		if (!dmlProg.getFunctionStatementBlocks(testFCI.getNamespace()).containsKey(testFCI.getName())){
+		tempNS = dmlProg.getFunctionStatementBlocks(testFCI.getNamespace());
+		if (tempNS == null){
+			LOG.error(this.printBlockErrorLocation() + "Namespace " + testFCI.getNamespace() + " is undefined ");
+			throw new LanguageException(this.printBlockErrorLocation() + "Namespace " + testFCI.getNamespace() + " is undefined ");
+		}
+		FunctionStatementBlock testFSB = tempNS.get(testFCI.getName());
+		if (testFSB == null){
 			LOG.error(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " is not available ");
 			throw new LanguageException(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " is not available ");
 		}
 		
-		// check test function inputs are available from either:
-		//	1) already defined variables
-		//	2) partition function outputs
-		//	3) training function outputs
-		for (Expression input : testFCI.getParamExpressions()){
-					
-			if (input instanceof DataIdentifier){
-				boolean found = false;
-				if (ids.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
-				if (partitionOutputList.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
-				if (trainOutputList.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
-				
-				if (found == false){
-					LOG.error(this.printBlockErrorLocation() + "In CV Statement, variable " + input.toString() + " is not available");
-					throw new LanguageException(this.printBlockErrorLocation() + "In CV Statement, variable " + input.toString() + " is not available");
-				}
-			}
-			else if (input instanceof ConstIdentifier){
-				// handles constants
-				input.validateExpression(ids.getVariables(), constVars);
-			}
-			else {
-				LOG.error(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " has unsupported formal parameter: " + input.toString());
-				throw new LanguageException(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " has unsupported formal parameter: " + input.toString());
-			}
-		}
-		
-		// check test function exists
-		Identifier aggFCI = cvs.get_aggFunctionCall();
-		if (aggFCI instanceof FunctionCallIdentifier){
-			if (!dmlProg.getFunctionStatementBlocks(testFCI.getNamespace()).containsKey(testFCI.getName())){
-				LOG.error(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " is not available ");
-				throw new LanguageException(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " is not available ");
-			}
-		}
-		
-		// check test function inputs are available from either:
-		//	1) already defined variables
-		//	2) partition function outputs
-		//	3) training function outputs
-		
-		ArrayList<Expression> aggFuncExprs = new ArrayList<Expression>();
-		if (aggFCI instanceof FunctionCallIdentifier){
-			aggFuncExprs = ((FunctionCallIdentifier)aggFCI).getParamExpressions();
-		}
-		else if (aggFCI instanceof BuiltinFunctionExpression){
-			if ( ((BuiltinFunctionExpression)aggFCI).getFirstExpr() != null )
-				aggFuncExprs.add(((BuiltinFunctionExpression)aggFCI).getFirstExpr());
-			if ( ((BuiltinFunctionExpression)aggFCI).getSecondExpr() != null )
-				aggFuncExprs.add(((BuiltinFunctionExpression)aggFCI).getSecondExpr());
-			if ( ((BuiltinFunctionExpression)aggFCI).getThirdExpr() != null )
-				aggFuncExprs.add(((BuiltinFunctionExpression)aggFCI).getThirdExpr());
-		}
-		else {
-			LOG.error(this.printBlockErrorLocation() + "CV aggregation function " + aggFCI.toString() + " is unsupported method type");
-			throw new LanguageException(this.printBlockErrorLocation() + "CV aggregation function " + aggFCI.toString() + " is unsupported method type");
+		// check test function has correct number of parameters AND
+		// test function inputs are available from either:
+		//	 1) already defined variables
+		//	 2) partition function outputs
+		//	 3) train function outputs
+		tempIds = new VariableSet();
+		tempIds.addVariables(ids);
+		tempIds.addVariables(partitionOutputList);
+		tempIds.addVariables(trainOutputList);
+		try {
+			testFCI.validateExpression(dmlProg,tempIds.getVariables(), constVars);
+		} catch(IOException e){
+			throw new LanguageException(e);
 		}
 		
 		// build list of test function outputs
-		// NOTE: assume each test function output is DataIdentifier
-		VariableSet testOutputList = new VariableSet();
-		for (Expression output : cvs.get_testFunctionOutputs().getParamExpressions()){
-			trainOutputList.addVariable( ((DataIdentifier)output).getName(),(DataIdentifier)output);
+		// check test function call has correct number of parameters		
+		FunctionStatement testFS = (FunctionStatement)testFSB.getStatement(0);
+		if (cvs.get_testFunctionOutputs().getParamExpressions().size() != testFS.getOutputParams().size()){
+			LOG.error(this.printBlockErrorLocation() + "CV test function " + testFCI.getNamespace() + "::" + testFCI.getName() + " has wrong number parameters ");
+			throw new LanguageException(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " has wrong number parameters ");
+
 		}
 		
-		
-		for (Expression input : aggFuncExprs) {
-					
-			if (input instanceof DataIdentifier){
-				boolean found = false;
-				if (ids.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
-				if (partitionOutputList.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
-				if (trainOutputList.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
-				if (testOutputList.containsVariable(((DataIdentifier) input).getName()))
-					found = true;
+		// for each test function parameter, set datatype and valuetype
+		VariableSet testOutputList = new VariableSet();
+		for (int i = 0; i< cvs.get_testFunctionOutputs().getParamExpressions().size(); i++){
+			DataIdentifier output = (DataIdentifier)cvs.get_testFunctionOutputs().getParamExpressions().get(i);
+			try {
+				output.setTypeInfo(testFS.getOutputParams().get(i).getValueType().toString(), testFS.getOutputParams().get(i).getDataType().toString());
+			} catch (ParseException e) {
+				throw new LanguageException(e);
+			}
+			testOutputList.addVariable( ((DataIdentifier)output).getName(),(DataIdentifier)output);
 				
-				if (found == false){
-					LOG.error(this.printBlockErrorLocation() + "In CV Statement test function, variable " + input.toString() + " is not available");
-					throw new LanguageException(this.printBlockErrorLocation() + "In CV Statement test function, variable " + input.toString() + " is not available");
+		}
+		
+		///////////////////////////////////////////////////////////////////////
+		// handle agg function
+		///////////////////////////////////////////////////////////////////////
+	
+		// check agg function exists
+		Identifier aggFCI = cvs.get_aggFunctionCall();
+		
+		
+		
+		if (aggFCI instanceof FunctionCallIdentifier){
+		
+			tempNS = dmlProg.getFunctionStatementBlocks(((FunctionCallIdentifier)aggFCI).getNamespace());
+			if (tempNS == null){
+				LOG.error(this.printBlockErrorLocation() + "Namespace " + ((FunctionCallIdentifier)aggFCI).getNamespace() + " is undefined ");
+				
+				throw new LanguageException(this.printBlockErrorLocation() + "Namespace " + testFCI.getNamespace() + " is undefined ");
+			}
+			FunctionStatementBlock aggFSB = tempNS.get(((FunctionCallIdentifier)aggFCI).getName());
+			if (aggFSB == null){
+				LOG.error(this.printBlockErrorLocation() + "CV aggregation function " + ((FunctionCallIdentifier)aggFCI).getNamespace() + "::" + ((FunctionCallIdentifier)aggFCI).getName() + " is not available ");
+				throw new LanguageException(this.printBlockErrorLocation() + "CV aggregation function " + ((FunctionCallIdentifier)aggFCI).getNamespace() + "::" + ((FunctionCallIdentifier)aggFCI).getName() + " is not available ");
+			}
+		}
+		
+		// check agg function has correct number of parameters AND
+		// agg function inputs are available from either:
+		//	 1) already defined variables
+		//	 2) partition function outputs
+		//	 3) train function outputs
+		//	 4) test function outputs
+		tempIds = new VariableSet();
+		tempIds.addVariables(ids);
+		tempIds.addVariables(partitionOutputList);
+		tempIds.addVariables(trainOutputList);
+		tempIds.addVariables(testOutputList);
+		
+		try {
+			if (aggFCI instanceof FunctionCallIdentifier){
+				FunctionCallIdentifier aggFCI_fci = ((FunctionCallIdentifier)aggFCI);
+				aggFCI_fci.validateExpression(dmlProg,tempIds.getVariables(), constVars);
+				FunctionStatementBlock aggFSB = dmlProg.getFunctionStatementBlock(aggFCI_fci.getNamespace(), aggFCI_fci.getName());
+				FunctionStatement aggFS = (FunctionStatement)aggFSB.getStatement(0);
+				if (cvs.get_aggFunctionOutputs().getParamExpressions().size() != aggFS.getOutputParams().size()){
+					LOG.error(this.printBlockErrorLocation() + "CV aggregate function " + ((FunctionCallIdentifier)aggFCI).getNamespace() + "::" + ((FunctionCallIdentifier)aggFCI).getName() + " has wrong number parameters ");
+					throw new LanguageException(this.printBlockErrorLocation() + "CV aggregate function " + ((FunctionCallIdentifier)aggFCI).getNamespace() + "::" + ((FunctionCallIdentifier)aggFCI).getNamespace()  + " has wrong number parameters ");
+			
+				}
+				
+				for (int i = 0; i < cvs.get_aggFunctionOutputs().getParamExpressions().size(); i++){
+					DataIdentifier output = (DataIdentifier)cvs.get_aggFunctionOutputs().getParamExpressions().get(i);
+					try {
+						output.setTypeInfo(aggFS.getOutputParams().get(i).getValueType().toString(), aggFS.getOutputParams().get(i).getDataType().toString());
+					} catch (ParseException e) {
+						throw new LanguageException(e);
+					}
+					ids.addVariable(output.getName(), new DataIdentifier(output));
 				}
 			}
-			else if (input instanceof ConstIdentifier){
-				// handles constants
-				input.validateExpression(ids.getVariables(), constVars);
-			}
 			else {
-				LOG.error(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " has unsupported formal parameter: " + input.toString());
-				throw new LanguageException(this.printBlockErrorLocation() + "CV training function " + testFCI.getNamespace() + "::" + testFCI.getName() + " has unsupported formal parameter: " + input.toString());
+				BuiltinFunctionExpression aggBFE = (BuiltinFunctionExpression)aggFCI;
+				aggBFE.validateExpression(tempIds.getVariables(), constVars);
+				DataIdentifier output = (DataIdentifier)cvs.get_aggFunctionOutputs().getParamExpressions().get(0);
+				try {
+					output.setTypeInfo(aggBFE.getOutput().getValueType().toString(), aggBFE.getOutput().getDataType().toString());
+				} catch (ParseException e) {
+					throw new LanguageException(e);
+				}
+				ids.addVariable(output.getName(), new DataIdentifier(output));
+				
 			}
+		} catch(IOException e){
+			throw new LanguageException(e);
 		}
+
+	
 		
 		// add error aggregate output to returned variables 
+		
 		for (Expression aggOutput : cvs.get_aggFunctionOutputs().getParamExpressions()){
-			ids.addVariable(((DataIdentifier)aggOutput).getName(), new DataIdentifier((DataIdentifier)aggOutput));
+			
 		}
 		
 		return ids;	

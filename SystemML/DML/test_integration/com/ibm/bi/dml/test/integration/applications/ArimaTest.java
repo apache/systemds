5b20b4910281002b1c176ae3db97570e614c268a
MATTHIAS BOEHM <mboehm@us.ibm.com>
2013-11-02 21:12:09 -0700

    20969: Various Engine Enhancements - Fix arima test (directory name)

--- SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/applications/ArimaTest.java
+++ SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/applications/ArimaTest.java
@@ -1,7 +1,7 @@
 /**
  * IBM Confidential
  * OCO Source Materials
- * (C) Copyright IBM Corp. 2010, 2013
+ * (C) Copyright IBM Corp. 2010, 2014
  * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
  */
 
@@ -29,7 +29,7 @@ public class ArimaTest extends AutomatedTestBase
 	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2014\n" +
                                              "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
 	
-	private final static String TEST_DIR = "applications/arima(box-jenkins)/";
+	private final static String TEST_DIR = "applications/arima_box-jenkins/";
 	private final static String TEST_ArimaTest = "arima";
 	
 	private int max_func_invoc, p, d, q, P, D, Q, s, include_mean, useJacobi;

d3ecb7918d0a4dd651c5ea32cb75202c6c33d322
MATTHIAS BOEHM <mboehm@us.ibm.com>
2013-12-05 14:27:21 -0800

    55555: SystemML Engine Enhancements - New right indexing compilation / runtime (map-only vrix, general case rix, incl tests) II

--- SystemML/DML/src/com/ibm/bi/dml/hops/IndexingOp.java
+++ SystemML/DML/src/com/ibm/bi/dml/hops/IndexingOp.java
@@ -1,7 +1,7 @@
 /**
  * IBM Confidential
  * OCO Source Materials
- * (C) Copyright IBM Corp. 2010, 2013
+ * (C) Copyright IBM Corp. 2010, 2014
  * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
  */
 
@@ -28,43 +28,25 @@ public class IndexingOp extends Hop
 	
 	public static String OPSTRING = "Indexing";
 	
-	private boolean _rowLowerEqualsUpper = false, _colLowerEqualsUpper = false;
+	private boolean _rowLowerEqualsUpper = false;
+	private boolean _colLowerEqualsUpper = false;
 	
-	public boolean getRowLowerEqualsUpper(){
-		return _rowLowerEqualsUpper;
-	}
+	private enum IndexingMethod { 
+		CP_RIX, //in-memory range index
+		MR_RIX, //general case range reindex
+		MR_VRIX, //vector (row/col) range index
+	};
 	
-	public boolean getColLowerEqualsUpper() {
-		return _colLowerEqualsUpper;
-	}
-	
-	public void setRowLowerEqualsUpper(boolean passed){
-		_rowLowerEqualsUpper  = passed;
-	}
-	
-	public void setColLowerEqualsUpper(boolean passed) {
-		_colLowerEqualsUpper = passed;
-	}
-	
-	//right indexing doesn't really need the dimensionality of the left matrix
-	//private static Lops dummy=new Data(null, Data.OperationTypes.READ, null, "-1", DataType.SCALAR, ValueType.INT, false);
 	
 	private IndexingOp() {
 		//default constructor for clone
 	}
 	
+	//right indexing doesn't really need the dimensionality of the left matrix
+	//private static Lops dummy=new Data(null, Data.OperationTypes.READ, null, "-1", DataType.SCALAR, ValueType.INT, false);
 	public IndexingOp(String l, DataType dt, ValueType vt, Hop inpMatrix, Hop inpRowL, Hop inpRowU, Hop inpColL, Hop inpColU, boolean passedRowsLEU, boolean passedColsLEU) {
 		super(Kind.Indexing, l, dt, vt);
-		/*
-		if(inpRowL==null)
-			inpRowL=new DataOp("1", DataType.SCALAR, ValueType.INT, DataOpTypes.PERSISTENTREAD, "1", -1, -1, -1, -1);
-		if(inpRowU==null)
-			inpRowU=new DataOp(Long.toString(get_dim1()), DataType.SCALAR, ValueType.INT, DataOpTypes.PERSISTENTREAD, Long.toString(get_dim1()), -1, -1, -1, -1);
-		if(inpColL==null)
-			inpColL=new DataOp("1", DataType.SCALAR, ValueType.INT, DataOpTypes.PERSISTENTREAD, "1", -1, -1, -1, -1);
-		if(inpColU==null)
-			inpColU=new DataOp(Long.toString(get_dim2()), DataType.SCALAR, ValueType.INT, DataOpTypes.PERSISTENTREAD, Long.toString(get_dim2()), -1, -1, -1, -1);
-	*/
+
 		getInput().add(0, inpMatrix);
 		getInput().add(1, inpRowL);
 		getInput().add(2, inpRowU);
@@ -82,6 +64,23 @@ public class IndexingOp extends Hop
 		setRowLowerEqualsUpper(passedRowsLEU);
 		setColLowerEqualsUpper(passedColsLEU);
 	}
+	
+	
+	public boolean getRowLowerEqualsUpper(){
+		return _rowLowerEqualsUpper;
+	}
+	
+	public boolean getColLowerEqualsUpper() {
+		return _colLowerEqualsUpper;
+	}
+	
+	public void setRowLowerEqualsUpper(boolean passed){
+		_rowLowerEqualsUpper  = passed;
+	}
+	
+	public void setColLowerEqualsUpper(boolean passed) {
+		_colLowerEqualsUpper = passed;
+	}
 
 	public Lop constructLops()
 			throws HopsException {
@@ -89,6 +88,10 @@ public class IndexingOp extends Hop
 			try {
 				ExecType et = optFindExecType();
 				if(et == ExecType.MR) {
+					Hop input = getInput().get(0);
+					IndexingMethod method = optFindIndexingMethod( _rowLowerEqualsUpper, _colLowerEqualsUpper,
+							                                       input._dim1, input._dim2, _dim1, _dim2);
+					
 					Lop dummy = Data.createLiteralLop(ValueType.INT, Integer.toString(-1));
 					RangeBasedReIndex reindex = new RangeBasedReIndex(
 							getInput().get(0).constructLops(), getInput().get(1).constructLops(), getInput().get(2).constructLops(),
@@ -100,23 +103,30 @@ public class IndexingOp extends Hop
 					
 					reindex.setAllPositions(this.getBeginLine(), this.getBeginColumn(), this.getEndLine(), this.getEndColumn());
 					
-					Group group1 = new Group(
-							reindex, Group.OperationTypes.Sort, DataType.MATRIX,
-							get_valueType());
-					group1.getOutputParameters().setDimensions(get_dim1(),
-							get_dim2(), get_rows_in_block(), get_cols_in_block(), getNnz());
-					
-					group1.setAllPositions(this.getBeginLine(), this.getBeginColumn(), this.getEndLine(), this.getEndColumn());
-	
-					Aggregate agg1 = new Aggregate(
-							group1, Aggregate.OperationTypes.Sum, DataType.MATRIX,
-							get_valueType(), et);
-					agg1.getOutputParameters().setDimensions(get_dim1(),
-							get_dim2(), get_rows_in_block(), get_cols_in_block(), getNnz());
-	
-					agg1.setAllPositions(this.getBeginLine(), this.getBeginColumn(), this.getEndLine(), this.getEndColumn());
-					
-					set_lops(agg1);
+					if( method == IndexingMethod.MR_RIX )
+					{
+						Group group1 = new Group(
+								reindex, Group.OperationTypes.Sort, DataType.MATRIX,
+								get_valueType());
+						group1.getOutputParameters().setDimensions(get_dim1(),
+								get_dim2(), get_rows_in_block(), get_cols_in_block(), getNnz());
+						
+						group1.setAllPositions(this.getBeginLine(), this.getBeginColumn(), this.getEndLine(), this.getEndColumn());
+		
+						Aggregate agg1 = new Aggregate(
+								group1, Aggregate.OperationTypes.Sum, DataType.MATRIX,
+								get_valueType(), et);
+						agg1.getOutputParameters().setDimensions(get_dim1(),
+								get_dim2(), get_rows_in_block(), get_cols_in_block(), getNnz());
+		
+						agg1.setAllPositions(this.getBeginLine(), this.getBeginColumn(), this.getEndLine(), this.getEndColumn());
+						
+						set_lops(agg1);
+					}
+					else //method == IndexingMethod.MR_VRIX
+					{
+						set_lops(reindex);
+					}
 				}
 				else {
 					Lop dummy = Data.createLiteralLop(ValueType.INT, Integer.toString(-1));
@@ -150,7 +160,6 @@ public class IndexingOp extends Hop
 			for (Hop h : getInput()) {
 				h.printMe();
 			}
-			;
 		}
 		set_visited(VISIT_STATUS.DONE);
 	}
@@ -218,6 +227,27 @@ public class IndexingOp extends Hop
 				setRequiresRecompile();
 		}
 		return _etype;
+	}
+	
+	/**
+	 * 
+	 * @param singleRow
+	 * @param singleCol
+	 * @param m1_dim1
+	 * @param m1_dim2
+	 * @param m2_dim1
+	 * @param m2_dim2
+	 * @return
+	 */
+	private static IndexingMethod optFindIndexingMethod( boolean singleRow, boolean singleCol, long m1_dim1, long m1_dim2, long m2_dim1, long m2_dim2 )
+	{
+		if(    singleRow && m1_dim2 == m2_dim2 && m2_dim2!=-1
+			|| singleCol && m1_dim1 == m2_dim1 && m2_dim1!=-1 )
+		{
+			return IndexingMethod.MR_VRIX;
+		}
+		
+		return IndexingMethod.MR_RIX; //general case
 	}
 	
 	@Override

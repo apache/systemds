b9a01910168eeb591ecb58c841e48dcfc98e8dc3
MATTHIAS BOEHM <mboehm@us.ibm.com>
2013-12-16 11:34:00 -0800

    55555: SystemML Engine Enhancements - Fix indexing tests (missing scripts, renamed scripts)

--- SystemML/DML/test/scripts/functions/indexing/RightIndexingVectorTest.R
+++ SystemML/DML/test/scripts/functions/indexing/RightIndexingVectorTest.R
@@ -12,11 +12,12 @@ options(digits=22)
 library("Matrix")
 
 A1=readMM(paste(args[1], "A.mtx", sep=""))
-A=as.matrix(A1);
+A = as.matrix(A1);
 
-A[13:13,1026:1026] = 7;
-A[14:14,1027:1027] = 7*7;
-A[1013,26] = 7;
-A[1014,27] = 7*7;
+B=A[1:(nrow(A)-10),7]
+C=A[ ,7]
+D=t(A[7, ]) #R outputs col vector
 
-writeMM(as(A,"CsparseMatrix"), paste(args[2], "A", sep=""), format="text")
+writeMM(as(B,"CsparseMatrix"), paste(args[2], "B", sep=""), format="text")
+writeMM(as(C,"CsparseMatrix"), paste(args[2], "C", sep=""), format="text")
+writeMM(as(D,"CsparseMatrix"), paste(args[2], "D", sep=""), format="text")

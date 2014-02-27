588d62871b7120f27b567e3b7969647dafebd3a4
Berthold Reinwald <reinwald@us.ibm.com>
2014-02-26 17:19:59 -0800

    61184: SystemML Engine Enhancements - Share

---
+++ SystemML/SystemML/src/main/java/com/ibm/bi/dml/api/test2.java
@@ -0,0 +1,10 @@
+package com.ibm.bi.dml.api;
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		
+	}
+
+}

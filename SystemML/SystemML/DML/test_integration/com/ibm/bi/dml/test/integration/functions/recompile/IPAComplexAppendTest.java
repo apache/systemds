a8e157aeadc3d3c4102adb7548342a04439f625f
MATTHIAS BOEHM <mboehm@us.ibm.com>
2014-05-13 16:45:50 -0700

    66141: SystemML Engine Enhancements - New testcase intra-procedural analysis (append with known/unknown pair)

---
+++ SystemML/SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/functions/recompile/IPAComplexAppendTest.java
@@ -0,0 +1,115 @@
+/**
+ * IBM Confidential
+ * OCO Source Materials
+ * (C) Copyright IBM Corp. 2010, 2014
+ * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
+ */
+
+package com.ibm.bi.dml.test.integration.functions.recompile;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.ibm.bi.dml.hops.OptimizerUtils;
+import com.ibm.bi.dml.parser.Expression.ValueType;
+import com.ibm.bi.dml.runtime.DMLRuntimeException;
+import com.ibm.bi.dml.runtime.matrix.MatrixCharacteristics;
+import com.ibm.bi.dml.runtime.matrix.io.MatrixBlock;
+import com.ibm.bi.dml.runtime.matrix.io.OutputInfo;
+import com.ibm.bi.dml.runtime.util.DataConverter;
+import com.ibm.bi.dml.runtime.util.MapReduceTool;
+import com.ibm.bi.dml.test.integration.AutomatedTestBase;
+import com.ibm.bi.dml.test.integration.TestConfiguration;
+import com.ibm.bi.dml.utils.Statistics;
+
+public class IPAComplexAppendTest extends AutomatedTestBase 
+{
+	@SuppressWarnings("unused")
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2014\n" +
+                                             "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
+	
+	private final static String TEST_NAME = "append_nnz";
+	private final static String TEST_DIR = "functions/recompile/";
+	
+	private final static int rows = 200000;
+	private final static int cols = 1000;
+	private final static int nnz  = 700; //ultra-sparse
+	
+	
+	@Override
+	public void setUp() 
+	{
+		addTestConfiguration( TEST_NAME, new TestConfiguration(TEST_DIR, TEST_NAME, new String[] { "Y" }) );
+	}
+
+	
+	
+	@Test
+	public void testComplexAppendNoIPA() 
+			throws DMLRuntimeException, IOException 
+	{
+		runIPAAppendTest(false);
+	}
+	
+	@Test
+	public void testComplexAppendIPA() 
+			throws DMLRuntimeException, IOException 
+	{
+		runIPAAppendTest(true);
+	}
+
+	/**
+	 * 
+	 * @param condition
+	 * @param branchRemoval
+	 * @param IPA
+	 * @throws DMLRuntimeException 
+	 * @throws IOException 
+	 */
+	private void runIPAAppendTest( boolean IPA ) 
+		throws DMLRuntimeException, IOException
+	{	
+		boolean oldFlagIPA = OptimizerUtils.ALLOW_INTER_PROCEDURAL_ANALYSIS;
+		
+		try
+		{
+			TestConfiguration config = getTestConfiguration(TEST_NAME);
+			
+			String HOME = SCRIPT_DIR + TEST_DIR;
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[]{"-args", 
+					                         HOME + OUTPUT_DIR + "X" };
+			
+			fullRScriptName = HOME + TEST_NAME + ".R";
+			rCmd = "Rscript" + " " + fullRScriptName + " " +
+					 HOME + EXPECTED_DIR + " " + HOME + EXPECTED_DIR;			
+			loadTestConfiguration(config);
+
+			OptimizerUtils.ALLOW_INTER_PROCEDURAL_ANALYSIS = IPA;
+			
+			//generate input data
+			MatrixBlock mb = MatrixBlock.randOperations(rows, cols, OptimizerUtils.getSparsity(rows, cols, nnz), -1, 1, "uniform", 7);
+			MatrixCharacteristics mc1 = new MatrixCharacteristics(rows,cols,1000,1000,nnz);
+			DataConverter.writeMatrixToHDFS(mb, HOME + OUTPUT_DIR + "X", OutputInfo.BinaryBlockOutputInfo, mc1);
+			MapReduceTool.writeMetaDataFile(HOME + OUTPUT_DIR + "X.mtd", ValueType.DOUBLE, mc1, OutputInfo.BinaryBlockOutputInfo);
+			
+			//run test
+			runTest(true, false, null, -1); 
+			
+			//check expected number of compiled and executed MR jobs
+			int expectedNumCompiled = 3; //GMR mm, GMR append, GMR sum
+			int expectedNumExecuted = 1;			
+			
+			Assert.assertEquals("Unexpected number of compiled MR jobs.", expectedNumCompiled, Statistics.getNoOfCompiledMRJobs()); 
+			Assert.assertEquals("Unexpected number of executed MR jobs.", expectedNumExecuted, Statistics.getNoOfExecutedMRJobs()); 
+		}
+		finally
+		{
+			OptimizerUtils.ALLOW_INTER_PROCEDURAL_ANALYSIS = oldFlagIPA;
+		}
+	}
+	
+}

8d88f3df602dc3923ebcbcf42869701b84a40816
SHIRISH TATIKONDA <statiko@us.ibm.com>
2014-06-25 12:01:15 -0700

    66141: SystemML Engine Enhancements - Additional test cases for MatrixMarket files

---
+++ SystemML/SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/functions/io/matrixmarket/ReadMMTest.java
@@ -0,0 +1,98 @@
+/**
+ * IBM Confidential
+ * OCO Source Materials
+ * (C) Copyright IBM Corp. 2010, 2013
+ * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
+ */
+
+package com.ibm.bi.dml.test.integration.functions.io.matrixmarket;
+
+import org.junit.Test;
+
+import com.ibm.bi.dml.api.DMLScript.RUNTIME_PLATFORM;
+import com.ibm.bi.dml.test.integration.AutomatedTestBase;
+import com.ibm.bi.dml.test.integration.TestConfiguration;
+import com.ibm.bi.dml.test.utils.TestUtils;
+
+public class ReadMMTest extends AutomatedTestBase 
+{
+	@SuppressWarnings("unused")
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2013\n" +
+                                             "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
+	
+	private final static String TEST_NAME = "ReadMMTest";
+	private final static String TEST_DIR = "functions/io/matrixmarket/";
+	
+	private final static double eps = 1e-9;
+
+	@Override
+	public void setUp() 
+	{
+		addTestConfiguration(
+				TEST_NAME, 
+				new TestConfiguration(TEST_DIR, TEST_NAME, 
+				new String[] { "Rout" })   );  
+	}
+	
+	@Test
+	public void testMatrixMarket1_CP() {
+		runMMTest(1, RUNTIME_PLATFORM.SINGLE_NODE);
+	}
+	
+	@Test
+	public void testMatrixMarket1_MR() {
+		runMMTest(1, RUNTIME_PLATFORM.HADOOP);
+	}
+	
+	@Test
+	public void testMatrixMarket2_CP() {
+		runMMTest(2, RUNTIME_PLATFORM.SINGLE_NODE);
+	}
+	
+	@Test
+	public void testMatrixMarket2_MR() {
+		runMMTest(2, RUNTIME_PLATFORM.HADOOP);
+	}
+	
+	@Test
+	public void testMatrixMarket3_CP() {
+		runMMTest(3, RUNTIME_PLATFORM.SINGLE_NODE);
+	}
+	
+	@Test
+	public void testMatrixMarket3_MR() {
+		runMMTest(3, RUNTIME_PLATFORM.HADOOP);
+	}
+	
+	private void runMMTest(int testNumber, RUNTIME_PLATFORM platform) {
+		
+		RUNTIME_PLATFORM oldPlatform = rtplatform;
+		rtplatform = platform;
+		
+		TestConfiguration config = getTestConfiguration(TEST_NAME);
+		
+		loadTestConfiguration(config);
+		
+		String HOME = SCRIPT_DIR + TEST_DIR;
+		String inputMatrixName = HOME + INPUT_DIR + "ReadMMTest.mtx";
+		String dmlOutput = HOME + OUTPUT_DIR + "dml.scalar";
+		String rOutput = HOME + OUTPUT_DIR + "R.scalar";
+		
+		fullDMLScriptName = HOME + TEST_NAME + "_1.dml";
+		programArgs = new String[]{"-args", inputMatrixName, dmlOutput};
+		
+		fullRScriptName = HOME + "mm_verify.R";
+		rCmd = "Rscript" + " " + fullRScriptName + " " + inputMatrixName + " " + rOutput;
+		
+		runTest(true, false, null, -1);
+		runRScript(true);
+		
+		double dmlScalar = TestUtils.readDMLScalar(dmlOutput); 
+		double rScalar = TestUtils.readRScalar(rOutput); 
+		
+		TestUtils.compareScalars(dmlScalar, rScalar, eps);
+
+		rtplatform = oldPlatform;
+	}
+	
+}

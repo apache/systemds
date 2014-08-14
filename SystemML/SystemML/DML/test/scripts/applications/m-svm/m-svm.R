92543060c7551704230055443def8e7488bee000
PRITHVIRAJ SEN <senp@us.ibm.com>
2014-08-13 22:06:03 -0700

    66230: SystemML Algorithms and Documentation - junit test for m-svm.dml

---
+++ SystemML/SystemML/DML/test/scripts/applications/m-svm/m-svm.R
@@ -0,0 +1,108 @@
+#-------------------------------------------------------------
+# IBM Confidential
+# OCO Source Materials
+# (C) Copyright IBM Corp. 2010, 2014
+# The source code for this program is not published or
+# otherwise divested of its trade secrets, irrespective of
+# what has been deposited with the U.S. Copyright Office.
+#-------------------------------------------------------------
+
+args <- commandArgs(TRUE)
+
+library("Matrix")
+
+X = as.matrix(readMM(paste(args[1], "X.mtx", sep="")))
+
+check_X = sum(X)
+if(check_X == 0){
+	print("X has no non-zeros")
+}else{
+	Y = as.matrix(readMM(paste(args[1], "Y.mtx", sep="")))
+	intercept = as.integer(args[6])
+	num_classes = as.integer(args[2])
+	epsilon = as.double(args[3])
+	lambda = as.double(args[4])
+	max_iterations = as.integer(args[5])
+ 
+	num_samples = nrow(X)
+	num_features = ncol(X)
+
+	if (intercept == 1) {
+ 		ones  = matrix(1, num_samples, 1);
+ 		X = cbind(X, ones);
+	}
+
+	num_rows_in_w = num_features
+	if(intercept == 1){
+		num_rows_in_w = num_rows_in_w + 1
+	}
+	w = matrix(0, num_rows_in_w, num_classes)
+
+	debug_mat = matrix(-1, max_iterations, num_classes)
+	for(iter_class in 1:num_classes){		  
+		Y_local = 2 * (Y == iter_class) - 1
+		w_class = matrix(0, num_features, 1)
+		if (intercept == 1) {
+			zero_matrix = matrix(0, 1, 1);
+ 			w_class = t(cbind(t(w_class), zero_matrix));
+ 		}
+ 
+		g_old = t(X) %*% Y_local
+ 		s = g_old
+
+		Xw = matrix(0, nrow(X), 1)
+		iter = 0
+ 		continue = 1
+ 		while(continue == 1)  {
+  			# minimizing primal obj along direction s
+  			step_sz = 0
+  			Xd = X %*% s
+  			wd = lambda * sum(w_class * s)
+  			dd = lambda * sum(s * s)
+  			continue1 = 1
+  			while(continue1 == 1){
+   				tmp_Xw = Xw + step_sz*Xd
+   				out = 1 - Y_local * (tmp_Xw)
+   				sv = (out > 0)
+   				out = out * sv
+   				g = wd + step_sz*dd - sum(out * Y_local * Xd)
+   				h = dd + sum(Xd * sv * Xd)
+   				step_sz = step_sz - g/h
+   				if (g*g/h < 0.0000000001){
+    				continue1 = 0
+   				}
+  			}
+ 
+  			#update weights
+  			w_class = w_class + step_sz*s
+ 			Xw = Xw + step_sz*Xd
+ 
+  			out = 1 - Y_local * Xw
+  			sv = (out > 0)
+  			out = sv * out
+  			obj = 0.5 * sum(out * out) + lambda/2 * sum(w_class * w_class)
+  			g_new = t(X) %*% (out * Y_local) - lambda * w_class
+
+  			tmp = sum(s * g_old)
+  
+  			train_acc = sum(Y_local*(X%*%w_class) >= 0)/num_samples*100
+  			print(paste("For class ", iter_class, " iteration ", iter, " training accuracy: ", train_acc, sep=""))
+  			debug_mat[iter+1,iter_class] = obj	   
+   
+  			if((step_sz*tmp < epsilon*obj) | (iter >= max_iterations-1)){
+   				continue = 0
+  			}
+ 
+  			#non-linear CG step
+  			be = sum(g_new * g_new)/sum(g_old * g_old)
+  			s = be * s + g_new
+  			g_old = g_new
+
+  			iter = iter + 1
+ 		}
+
+		w[,iter_class] = w_class
+	}
+
+	writeMM(as(w, "CsparseMatrix"), paste(args[7], "w", sep=""))
+}
---
+++ SystemML/SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/applications/MultiClassSVMTest.java
@@ -0,0 +1,130 @@
+/**
+ * IBM Confidential
+ * OCO Source Materials
+ * (C) Copyright IBM Corp. 2010, 2014
+ * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
+ */
+
+package com.ibm.bi.dml.test.integration.applications;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.ibm.bi.dml.runtime.matrix.io.MatrixValue.CellIndex;
+import com.ibm.bi.dml.test.integration.AutomatedTestBase;
+import com.ibm.bi.dml.test.integration.TestConfiguration;
+import com.ibm.bi.dml.test.utils.TestUtils;
+
+@RunWith(value = Parameterized.class)
+public class MultiClassSVMTest  extends AutomatedTestBase{
+	@SuppressWarnings("unused")
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2014\n" +
+                                             "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
+	
+	private final static String TEST_DIR = "applications/m-svm/";
+	private final static String TEST_MULTICLASSSVM = "m-svm";
+
+	private int numRecords, numFeatures, numClasses;
+    private double sparsity;
+    boolean intercept;
+    
+    public MultiClassSVMTest(int rows, int cols, int nc, boolean intercept, double sp) {
+		numRecords = rows;
+		numFeatures = cols;
+		numClasses = nc;
+		sparsity = sp;
+		this.intercept = intercept;
+	}
+    
+    @Parameters
+	 public static Collection<Object[]> data() {
+	   Object[][] data = new Object[][] { 
+			   //sparse tests (sparsity=0.01)
+			   {100, 50, 10, false, 0.01}, 
+			   {1000, 500, 10, false, 0.01}, 
+			   {1000, 500, 10, true, 0.01}, 
+			   {10000, 750, 10, false, 0.01}, 
+			   {100000, 1000, 10, false, 0.01},
+			   //dense tests (sparsity=0.7)
+			   {100, 50, 10, false, 0.7}, 
+			   {1000, 500, 10, false, 0.7}, 
+			   {1000, 500, 10, true, 0.7}, 
+			   {10000, 750, 10, false, 0.7} 
+			   };
+	   
+	   return Arrays.asList(data);
+	 }
+	 
+	 @Override
+	 public void setUp() {
+		 setUpBase();
+		 addTestConfiguration(TEST_MULTICLASSSVM, new TestConfiguration(TEST_DIR, "m-svm",
+	                new String[] { "w" }));
+	 }
+	 
+	 @Test
+	 public void testMULTICLASSSVM()
+	 {
+		 int rows = numRecords;
+		 int cols = numFeatures;
+		 int classes = numClasses;
+		 double sparsity = this.sparsity;
+		 double tol = 0.001;
+		 double reg = 1;
+		 int maxiter = 100;
+	        
+		 TestConfiguration config = getTestConfiguration(TEST_MULTICLASSSVM);
+	        
+		 String MULTICLASSSVM_HOME = SCRIPT_DIR + TEST_DIR;
+		 fullDMLScriptName = MULTICLASSSVM_HOME + TEST_MULTICLASSSVM + ".dml";
+		 programArgs = new String[]{"-stats", "-nvargs", 
+				 "X=" + MULTICLASSSVM_HOME + INPUT_DIR + "X", 
+				 "Y=" + MULTICLASSSVM_HOME + INPUT_DIR + "Y",
+				 "classes=" + Integer.toString(classes),
+				 "tol=" + Double.toString(tol),
+				 "reg=" + Double.toString(reg),
+				 "maxiter=" + Integer.toString(maxiter),
+				 "icpt=" + ((intercept) ? "1" : "0"),
+				 "model=" + MULTICLASSSVM_HOME + OUTPUT_DIR + "w",
+				 "Log=" + MULTICLASSSVM_HOME + OUTPUT_DIR + "Log"};
+			
+		 fullRScriptName = MULTICLASSSVM_HOME + TEST_MULTICLASSSVM + ".R";
+		 rCmd = "Rscript" + " " + 
+				 fullRScriptName + " " + 
+				 MULTICLASSSVM_HOME + INPUT_DIR + " " + 
+				 Integer.toString(classes) + " " + 
+				 Double.toString(tol) + " " +
+				 Double.toString(reg) + " " +
+				 Integer.toString(maxiter) + " " +
+				 ((intercept) ? "1" : "0") + " " +
+				 MULTICLASSSVM_HOME + EXPECTED_DIR;
+				
+		 loadTestConfiguration(config);
+
+		 double[][] X = getRandomMatrix(rows, cols, 0, 1, sparsity, -1);
+		 double[][] Y = getRandomMatrix(rows, 1, 0, 1, 1, -1);
+		 for(int i=0; i<rows; i++){
+			 Y[i][0] = (int)(Y[i][0]*classes) + 1;
+			 Y[i][0] = (Y[i][0] > classes) ? classes : Y[i][0];
+	     }	
+	        
+		 writeInputMatrixWithMTD("X", X, true);
+		 writeInputMatrixWithMTD("Y", Y, true);
+	        
+		 runTest(true, false, null, -1);
+	        
+		 runRScript(true);
+		 disableOutAndExpectedDeletion();
+	        
+		 HashMap<CellIndex, Double> wR = readRMatrixFromFS("w");
+		 HashMap<CellIndex, Double> wDML= readDMLMatrixFromHDFS("w");
+		 boolean success = TestUtils.compareMatrices(wR, wDML, Math.pow(10, -12), "w", "w");
+		 System.out.println(success+"");
+	 }
+}

e4498d8a23129f7ab47e00152e2dedd75ebcb62d
MATTHIAS BOEHM <mboehm@us.ibm.com>
2015-01-02 18:09:09 -0800

    55555: SystemML Engine Enhancements - Fix univariate statistics test (missing reset of rtplatform state)

--- SystemML/SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/applications/descriptivestats/UnivariateStatsTest.java
+++ SystemML/SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/applications/descriptivestats/UnivariateStatsTest.java
@@ -1,7 +1,7 @@
 /**
  * IBM Confidential
  * OCO Source Materials
- * (C) Copyright IBM Corp. 2010, 2013
+ * (C) Copyright IBM Corp. 2010, 2015
  * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
  */
 
@@ -21,7 +21,7 @@ import com.ibm.bi.dml.test.utils.TestUtils;
 public class UnivariateStatsTest extends AutomatedTestBase
 {
 	@SuppressWarnings("unused")
-	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2013\n" +
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2015\n" +
                                              "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
 	
 	private final static String TEST_DIR = "applications/descriptivestats/";
@@ -714,6 +714,9 @@ public class UnivariateStatsTest extends AutomatedTestBase
 			}
 			TestUtils.compareMatrices(dmlfile, rfile, epsilon, file+"-DML", file+"-R");
 		}
+		
+		//reset runtime platform
+		rtplatform = oldrt;
 	}
 	
 	@Test

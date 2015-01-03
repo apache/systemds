ef24c5f3df94ebd6a77ee0e4b9ff033e07c6e6f2
MATTHIAS BOEHM <mboehm@us.ibm.com>
2015-01-02 18:09:09 -0800

    55555: SystemML Engine Enhancements - Fix gmr reducer block outputs (support lists of output blocks per index)

--- SystemML/SystemML/DML/src/main/java/com/ibm/bi/dml/runtime/matrix/mapred/GMRReducer.java
+++ SystemML/SystemML/DML/src/main/java/com/ibm/bi/dml/runtime/matrix/mapred/GMRReducer.java
@@ -1,7 +1,7 @@
 /**
  * IBM Confidential
  * OCO Source Materials
- * (C) Copyright IBM Corp. 2010, 2014
+ * (C) Copyright IBM Corp. 2010, 2015
  * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
  */
 
@@ -9,6 +9,7 @@
 package com.ibm.bi.dml.runtime.matrix.mapred;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.hadoop.mapred.JobConf;
@@ -30,7 +31,7 @@ public class GMRReducer extends ReduceBase
 implements Reducer<MatrixIndexes, TaggedMatrixValue, MatrixIndexes, MatrixValue>
 {
 	@SuppressWarnings("unused")
-	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2014\n" +
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2015\n" +
                                              "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
 		
 	private MatrixValue realOutValue;
@@ -97,18 +98,20 @@ implements Reducer<MatrixIndexes, TaggedMatrixValue, MatrixIndexes, MatrixValue>
 		for(int i=0; i<resultIndexes.length; i++)
 		{
 			byte output=resultIndexes[i];
-			IndexedMatrixValue outValue=cachedValues.getFirst(output);
-			if(outValue==null)
+			
+			ArrayList<IndexedMatrixValue> outValueList = cachedValues.get(output);
+			if( outValueList == null ) 
 				continue;
-			if(valueClass.equals(MatrixPackedCell.class))
+		
+			for(IndexedMatrixValue outValue : outValueList) //for all blocks of given index
 			{
-				realOutValue.copy(outValue.getValue());
-				collectOutput_N_Increase_Counter(outValue.getIndexes(), 
-						realOutValue, i, reporter);
+				if(valueClass.equals(MatrixPackedCell.class)) {
+					realOutValue.copy(outValue.getValue());
+					collectOutput_N_Increase_Counter(outValue.getIndexes(), realOutValue, i, reporter);
+				}
+				else
+					collectOutput_N_Increase_Counter(outValue.getIndexes(), outValue.getValue(), i, reporter);		
 			}
-			else
-				collectOutput_N_Increase_Counter(outValue.getIndexes(), 
-					outValue.getValue(), i, reporter);
 		}
 	}
 	

5657383d83ffaa89770a87e3f6f33b28e9ef2cec
MATTHIAS BOEHM <mboehm@us.ibm.com>
2015-01-11 17:08:20 -0800

    55555: SystemML Engine Enhancements - Improved decision empty blocks for mapmult/pmmj (no empty blocks if textcell write)

--- SystemML/SystemML/DML/src/main/java/com/ibm/bi/dml/hops/OptimizerUtils.java
+++ SystemML/SystemML/DML/src/main/java/com/ibm/bi/dml/hops/OptimizerUtils.java
@@ -1,7 +1,7 @@
 /**
  * IBM Confidential
  * OCO Source Materials
- * (C) Copyright IBM Corp. 2010, 2014
+ * (C) Copyright IBM Corp. 2010, 2015
  * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
  */
 
@@ -12,8 +12,11 @@ import java.util.HashMap;
 import com.ibm.bi.dml.api.DMLScript;
 import com.ibm.bi.dml.conf.ConfigurationManager;
 import com.ibm.bi.dml.conf.DMLConfig;
+import com.ibm.bi.dml.hops.Hop.DataOpTypes;
+import com.ibm.bi.dml.hops.Hop.FileFormatTypes;
 import com.ibm.bi.dml.hops.Hop.OpOp2;
 import com.ibm.bi.dml.hops.rewrite.HopRewriteUtils;
+import com.ibm.bi.dml.lops.LopProperties.ExecType;
 import com.ibm.bi.dml.runtime.DMLRuntimeException;
 import com.ibm.bi.dml.runtime.controlprogram.LocalVariableMap;
 import com.ibm.bi.dml.runtime.controlprogram.parfor.stat.InfrastructureAnalyzer;
@@ -26,7 +29,7 @@ import com.ibm.bi.dml.runtime.util.UtilFunctions;
 public class OptimizerUtils 
 {
 	@SuppressWarnings("unused")
-	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2014\n" +
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2015\n" +
                                              "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
 	
 	////////////////////////////////////////////////////////
@@ -430,6 +433,26 @@ public class OptimizerUtils
 		return rowSize;
 	}
 	
+
+	/**
+	 * 
+	 * @return
+	 * @throws HopsException 
+	 */
+	public static boolean allowsToFilterEmptyBlockOutputs( Hop hop ) 
+		throws HopsException
+	{
+		boolean ret = true;
+		for( Hop p : hop.getParent() ) {
+			p.optFindExecType(); //ensure exec type evaluated
+			ret &=   (  p.getExecType()==ExecType.CP 
+					 ||(p instanceof AggBinaryOp && allowsToFilterEmptyBlockOutputs(p) )
+					 ||(p instanceof DataOp && ((DataOp)p).get_dataop()==DataOpTypes.PERSISTENTWRITE && ((DataOp)p).getFormatType()==FileFormatTypes.TEXT))
+				  && !(p instanceof FunctionOp || (p instanceof DataOp && ((DataOp)p).getFormatType()!=FileFormatTypes.TEXT) ); //no function call or transient write
+		}
+			
+		return ret;	
+	}
 	
 	////////////////////////
 	// Sparsity Estimates //
--- SystemML/SystemML/DML/src/main/java/com/ibm/bi/dml/runtime/matrix/mapred/MapperBase.java
+++ SystemML/SystemML/DML/src/main/java/com/ibm/bi/dml/runtime/matrix/mapred/MapperBase.java
@@ -1,7 +1,7 @@
 /**
  * IBM Confidential
  * OCO Source Materials
- * (C) Copyright IBM Corp. 2010, 2014
+ * (C) Copyright IBM Corp. 2010, 2015
  * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
  */
 
@@ -31,6 +31,7 @@ import com.ibm.bi.dml.runtime.instructions.MRInstructions.AggregateBinaryInstruc
 import com.ibm.bi.dml.runtime.instructions.MRInstructions.CSVReblockInstruction;
 import com.ibm.bi.dml.runtime.instructions.MRInstructions.DataGenMRInstruction;
 import com.ibm.bi.dml.runtime.instructions.MRInstructions.MRInstruction;
+import com.ibm.bi.dml.runtime.instructions.MRInstructions.PMMJMRInstruction;
 import com.ibm.bi.dml.runtime.instructions.MRInstructions.ReblockInstruction;
 import com.ibm.bi.dml.runtime.matrix.data.Converter;
 import com.ibm.bi.dml.runtime.matrix.data.MatrixBlock;
@@ -43,7 +44,7 @@ import com.ibm.bi.dml.runtime.matrix.data.TaggedMatrixValue;
 public abstract class MapperBase extends MRBaseForCommonInstructions
 {
 	@SuppressWarnings("unused")
-	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2014\n" +
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2015\n" +
                                              "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
 	
 	protected static final Log LOG = LogFactory.getLog(MapperBase.class);
@@ -208,8 +209,8 @@ public abstract class MapperBase extends MRBaseForCommonInstructions
 		if( ret && mapper_instructions!=null )
 			for( Vector<MRInstruction> vinst : mapper_instructions )
 				for( MRInstruction inst : vinst ){
-					ret &= (inst instanceof AggregateBinaryInstruction 
-						   && !((AggregateBinaryInstruction)inst).getOutputEmptyBlocks() );
+					ret &= (inst instanceof AggregateBinaryInstruction && !((AggregateBinaryInstruction)inst).getOutputEmptyBlocks() )
+						  ||(inst instanceof PMMJMRInstruction && !((PMMJMRInstruction)inst).getOutputEmptyBlocks() );
 					count++; //ensure that mapper instructions exists
 				}
 		

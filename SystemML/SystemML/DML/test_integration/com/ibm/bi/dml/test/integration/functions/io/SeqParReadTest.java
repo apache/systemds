64febdeed5167c384a3ddd1a8167e887df042ab8
Deepak Kumar <dkumar@us.ibm.com>
2015-02-12 11:22:32 -0800

    84471: Parallel Read/Write for Text Inputs - JUnit Tests for Parallel reads merged in HDP

---
+++ SystemML/SystemML/DML/test_integration/com/ibm/bi/dml/test/integration/functions/io/SeqParReadTest.java
@@ -0,0 +1,255 @@
+package com.ibm.bi.dml.test.integration.functions.io;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.ibm.bi.dml.hops.OptimizerUtils;
+import com.ibm.bi.dml.parser.Expression.ValueType;
+import com.ibm.bi.dml.runtime.DMLRuntimeException;
+import com.ibm.bi.dml.runtime.matrix.MatrixCharacteristics;
+import com.ibm.bi.dml.runtime.matrix.data.MatrixBlock;
+import com.ibm.bi.dml.runtime.matrix.data.OutputInfo;
+import com.ibm.bi.dml.runtime.util.DataConverter;
+import com.ibm.bi.dml.runtime.util.MapReduceTool;
+import com.ibm.bi.dml.test.integration.AutomatedTestBase;
+import com.ibm.bi.dml.test.integration.TestConfiguration;
+import com.ibm.bi.dml.test.utils.TestUtils;
+
+public class SeqParReadTest extends AutomatedTestBase {
+
+	@SuppressWarnings("unused")
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2015\n" +
+                                             "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
+	
+	private final static String TEST_NAME = "SeqParReadTest";
+	private final static String TEST_DIR = "functions/io/";
+	
+	private final static int rowsA = 2000;
+	private final static int colsA = 1000;
+	private final static int colsB = 7;
+	
+	private final static double sparsity1 = 0.7;
+	private final static double sparsity2 = 0.01;
+	
+	private final static double eps = 1e-9;
+
+	
+	@Override
+	public void setUp() 
+	{
+		TestUtils.clearAssertionInformation();
+		addTestConfiguration(
+				TEST_NAME, 
+				new TestConfiguration(TEST_DIR, TEST_NAME, 
+				new String[] { "Rout" })   ); 
+	}
+	
+	@Test
+	public void testSeqReadCSVSparseSmall() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.CSVOutputInfo, false, false);
+	}
+
+	@Test
+	public void testSeqReadTextcellSparseSmall() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.TextCellOutputInfo, false, false);
+	}
+	
+	@Test
+	public void testSeqReadMMSparseSmall() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.MatrixMarketOutputInfo, false, false);
+	}
+
+	@Test
+	public void testParReadCSVSparseSmall() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.CSVOutputInfo, false, false);
+	}
+
+	@Test
+	public void testParReadTextcellSparseSmall() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.TextCellOutputInfo, false, false);
+	}
+	
+	@Test
+	public void testParReadMMSparseSmall() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.MatrixMarketOutputInfo, false, false);
+	}
+	
+	@Test
+	public void testSeqReadCSVDenseSmall() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.CSVOutputInfo, true, false);
+	}
+
+	@Test
+	public void testSeqReadTextcellDenseSmall() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.TextCellOutputInfo, true, false);
+	}
+	
+	@Test
+	public void testSeqReadMMDenseSmall() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.MatrixMarketOutputInfo, true, false);
+	}
+
+	@Test
+	public void testParReadCSVDenseSmall() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.CSVOutputInfo, true, false);
+	}
+
+	@Test
+	public void testParReadTextcellDenseSmall() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.TextCellOutputInfo, true, false);
+	}
+	
+	@Test
+	public void testParReadMMDenseSmall() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.MatrixMarketOutputInfo, false, false);
+	}
+	
+	@Test
+	public void testSeqReadCSVSparseBig() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.CSVOutputInfo, false, true);
+	}
+
+	@Test
+	public void testSeqReadTextcellSparseBig() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.TextCellOutputInfo, false, true);
+	}
+	
+	@Test
+	public void testSeqReadMMSparseBig() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.MatrixMarketOutputInfo, false, true);
+	}
+
+	@Test
+	public void testParReadCSVSparseBig() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.CSVOutputInfo, false, true);
+	}
+
+	@Test
+	public void testParReadTextcellSparseBig() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.TextCellOutputInfo, false, true);
+	}
+	
+	@Test
+	public void testParReadMMSparseBig() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.MatrixMarketOutputInfo, false, true);
+	}
+	
+	@Test
+	public void testSeqReadCSVDenseBig() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.CSVOutputInfo, true, true);
+	}
+
+	@Test
+	public void testSeqReadTextcellDenseBig() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.TextCellOutputInfo, true, true);
+	}
+	
+	@Test
+	public void testSeqReadMMDenseBig() {
+		runReadTypeFormatSparsitySizeTest(false, OutputInfo.MatrixMarketOutputInfo, true, true);
+	}
+
+	@Test
+	public void testParReadCSVDenseBig() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.CSVOutputInfo, true, true);
+	}
+
+	@Test
+	public void testParReadTextcellDenseBig() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.TextCellOutputInfo, true, true);
+	}
+	
+	@Test
+	public void testParReadMMDenseBig() {
+		runReadTypeFormatSparsitySizeTest(true, OutputInfo.MatrixMarketOutputInfo, true, true);
+	}
+
+	/*
+	 * Generate a matrix (big/small dense/sparse) and write it as AX(text/csv/mm/bin) & BX(mm)
+	 * Read AX and sum-up using DML script scripts/functions/io/SeqParReadTest.dml
+	 * Read BX and sum-up using RScript scripts/functions/io/matrixmarket/mm_verify.R in MM format
+	 * Compare the two results
+	 * 
+	 * @param parallel : Is the read parallel or not(sequential)
+	 * @param fmt : text/csv/mm
+	 * @param dense : Is the matrix dense or not(sparse)
+	 * @param big : Is the matrix big or not(small)
+	 * 
+	 */
+	
+	private void runReadTypeFormatSparsitySizeTest(boolean parallel, OutputInfo fmt, boolean dense, boolean big ) {
+		
+		boolean oldpar = OptimizerUtils.PARALLEL_READ_TEXTFORMATS;
+
+		try
+		{
+			OptimizerUtils.PARALLEL_READ_TEXTFORMATS = parallel;
+			
+			TestConfiguration config = getTestConfiguration(TEST_NAME);
+			
+			loadTestConfiguration(config);
+			
+			String HOME = SCRIPT_DIR + TEST_DIR;
+			
+			//generate actual dataset
+			double[][] A = getRandomMatrix(rowsA, big?colsA:colsB, 0, 1, dense?sparsity2:sparsity1, 7); 
+			writeMatrix(A, HOME + INPUT_DIR + "AX", fmt, rowsA, big?colsA:colsB, 1000, 1000, rowsA*(big?colsA:colsB));
+			
+			//always write in MM format for R
+			writeMatrix(A, HOME + INPUT_DIR + "BX", OutputInfo.MatrixMarketOutputInfo, rowsA, big?colsA:colsB, 1000, 1000, rowsA*(big?colsA:colsB));
+			
+			String dmlOutput = HOME + OUTPUT_DIR + "dml.scalar";
+			String rOutput = HOME + OUTPUT_DIR + "R.scalar";
+			
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[]{"-args", HOME + INPUT_DIR + "AX", dmlOutput};
+			
+			fullRScriptName = HOME + "matrixmarket/mm_verify.R";
+			rCmd = "Rscript" + " " + fullRScriptName + " " + HOME + INPUT_DIR + "BX " + rOutput;
+			
+			runTest(true, false, null, -1);
+			runRScript(true);
+			
+			double dmlScalar = TestUtils.readDMLScalar(dmlOutput); 
+			double rScalar = TestUtils.readRScalar(rOutput); 
+			
+			TestUtils.compareScalars(dmlScalar, rScalar, eps);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+		finally
+		{
+			OptimizerUtils.PARALLEL_READ_TEXTFORMATS = oldpar;		
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param A
+	 * @param dir
+	 * @param oi
+	 * @param rows
+	 * @param cols
+	 * @param brows
+	 * @param bcols
+	 * @throws DMLRuntimeException
+	 * @throws IOException
+	 */
+	private void writeMatrix( double[][] A, String fname, OutputInfo oi, long rows, long cols, int brows, int bcols, long nnz ) 
+		throws DMLRuntimeException, IOException
+	{
+		MapReduceTool.deleteFileIfExistOnHDFS(fname);
+		MapReduceTool.deleteFileIfExistOnHDFS(fname+".mtd");
+		
+		MatrixCharacteristics mc = new MatrixCharacteristics(rows, cols, brows, bcols, nnz);
+		MatrixBlock mb = DataConverter.convertToMatrixBlock(A);
+		DataConverter.writeMatrixToHDFS(mb, fname, oi, mc);
+		if( oi != OutputInfo.MatrixMarketOutputInfo )
+			MapReduceTool.writeMetaDataFile(fname+".mtd", ValueType.DOUBLE, mc, oi);
+	}
+}

176fddb3ad324b0c3aadbe9b96ca37e95063f7da
MATTHIAS BOEHM <mboehm@us.ibm.com>
2015-04-07 16:15:14 -0700

    89826: SystemML Engine - Reverting previous change reblock / csvreblock instruction (missing refresh)

--- SystemML/SystemML/src/main/java/com/ibm/bi/dml/runtime/instructions/SPInstructionParser.java
+++ SystemML/SystemML/src/main/java/com/ibm/bi/dml/runtime/instructions/SPInstructionParser.java
@@ -14,9 +14,11 @@ import com.ibm.bi.dml.runtime.DMLRuntimeException;
 import com.ibm.bi.dml.runtime.DMLUnsupportedOperationException;
 import com.ibm.bi.dml.runtime.instructions.spark.AggregateUnarySPInstruction;
 import com.ibm.bi.dml.runtime.instructions.spark.ArithmeticBinarySPInstruction;
+import com.ibm.bi.dml.runtime.instructions.spark.CSVReblockSPInstruction;
 import com.ibm.bi.dml.runtime.instructions.spark.MMCJSPInstruction;
 import com.ibm.bi.dml.runtime.instructions.spark.MapMultSPInstruction;
 import com.ibm.bi.dml.runtime.instructions.spark.MatrixIndexingSPInstruction;
+import com.ibm.bi.dml.runtime.instructions.spark.ReblockSPInstruction;
 import com.ibm.bi.dml.runtime.instructions.spark.RelationalBinarySPInstruction;
 import com.ibm.bi.dml.runtime.instructions.spark.ReorgSPInstruction;
 import com.ibm.bi.dml.runtime.instructions.spark.SPInstruction;
@@ -119,6 +121,11 @@ public class SPInstructionParser extends InstructionParser {
 			case RelationalBinary:
 				return RelationalBinarySPInstruction.parseInstruction(str);
 				
+			// Reblock instructions	
+			case Reblock:
+				return ReblockSPInstruction.parseInstruction(str);
+			case CSVReblock:
+				return CSVReblockSPInstruction.parseInstruction(str);
 			
 			case INVALID:
 			default:

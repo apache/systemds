203a5555a1f7fc85b18a65ac55e22d742de462e4
MATTHIAS BOEHM <mboehm@us.ibm.com>
2015-05-16 15:36:34 -0700

    89826: SystemML Engine - Fix sort mr job, write file of partition splits (potential for wrong key class with numred to 1)

--- SystemML/SystemML/src/main/java/com/ibm/bi/dml/runtime/matrix/sort/SamplingSortMRInputFormat.java
+++ SystemML/SystemML/src/main/java/com/ibm/bi/dml/runtime/matrix/sort/SamplingSortMRInputFormat.java
@@ -9,7 +9,6 @@ package com.ibm.bi.dml.runtime.matrix.sort;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -28,7 +27,6 @@ import org.apache.hadoop.mapred.SequenceFileRecordReader;
 import org.apache.hadoop.util.IndexedSortable;
 import org.apache.hadoop.util.QuickSort;
 
-import com.ibm.bi.dml.runtime.matrix.SortMR;
 import com.ibm.bi.dml.runtime.matrix.data.Converter;
 import com.ibm.bi.dml.runtime.matrix.data.MatrixCell;
 import com.ibm.bi.dml.runtime.matrix.data.Pair;
@@ -57,79 +55,15 @@ extends SequenceFileInputFormat<K,V>
 		job.setClass(TARGET_KEY_CLASS, keyClass, WritableComparable.class);
 		job.setClass(TARGET_VALUE_CLASS, valueClass, Writable.class);
 	}
-  
-	static class Sampler implements IndexedSortable {
-		private ArrayList<WritableComparable> records = new ArrayList<WritableComparable>();
-		
-		public int compare(int i, int j) {
-			WritableComparable left =  records.get(i);
-			WritableComparable right = records.get(j);
-			return left.compareTo(right);
-		}
-
-		public void swap(int i, int j) {
-			WritableComparable left = records.get(i);
-			WritableComparable right = records.get(j);
-			records.set(j, left);
-			records.set(i, right);
-		}
-    
-		public void addValues(Collection<WritableComparable> col)
-		{
-			records.addAll(col);
-		}
-		
-		public void addValue(WritableComparable r)
-		{
-			records.add(r);
-		}
-		
-		public String toString()
-		{
-			return records.toString();
-		}
-
-	/**
-	 * Find the split points for a given sample. The sample keys are sorted
-	 * and down sampled to find even split points for the partitions. The
-	 * returned keys should be the start of their respective partitions.
-	 * @param numPartitions the desired number of partitions
-	 * @return an array of size numPartitions - 1 that holds the split points
-	 */
-		 ArrayList<WritableComparable> createPartitions(int numPartitions) {
-			int numRecords = records.size();
-		//	System.out.println("Making " + numPartitions + " from " + numRecords + 
-          //           " records");
-			if (numPartitions > numRecords) {
-				throw new IllegalArgumentException
-				("Requested more partitions than input keys (" + numPartitions +
-						" > " + numRecords + ")");
-			}
-			new QuickSort().sort(this, 0, records.size());
-			//System.out.println("after sort: "+ toString());
-			float stepSize = numRecords / (float) numPartitions;
-			//System.out.println("Step size is " + stepSize);
-			ArrayList<WritableComparable> result = new ArrayList<WritableComparable>(numPartitions-1);
-			for(int i=1; i < numPartitions; ++i) {
-				result.add(records.get(Math.round(stepSize * i)));
-			}
-		//	for(WritableComparable ret: result)
-		//		System.out.println("pivot: "+ret);
-			return result;
-		}
-		
-		 
-	}
-	
-	
+  	
 	@Override
-	public RecordReader<K,V> getRecordReader(InputSplit split,
-            JobConf job, Reporter reporter)
-		throws IOException {
+	public RecordReader<K,V> getRecordReader(InputSplit split,JobConf job, Reporter reporter)
+        throws IOException 
+    {
 		if(reporter!=null)
 			reporter.setStatus(split.toString());
 		return new SequenceFileRecordReader<K,V>(job, (FileSplit) split);
-	}
+    }
 	
 	  /**
 	   * Use the input splits to take samples of the input and generate sample
@@ -141,19 +75,18 @@ extends SequenceFileInputFormat<K,V>
 	 * @throws IllegalAccessException 
 	 * @throws InstantiationException 
 	   */
+	@SuppressWarnings({ "unchecked", "unused", "deprecation" })
 	public static int writePartitionFile(JobConf conf, Path partFile) 
-	  throws IOException, InstantiationException, IllegalAccessException {
-	    
+	  throws IOException, InstantiationException, IllegalAccessException
+	{    
 		SamplingSortMRInputFormat inFormat = new SamplingSortMRInputFormat();
 	    Sampler sampler = new Sampler();
-	    
-	    
+	       
 	    Class<? extends WritableComparable> targetKeyClass;
 		targetKeyClass=(Class<? extends WritableComparable>) conf.getClass(TARGET_KEY_CLASS, WritableComparable.class);
 	    //get input converter information
 		int brlen = MRJobConfiguration.getNumRowsPerBlock(conf, (byte) 0);
 		int bclen = MRJobConfiguration.getNumColumnsPerBlock(conf, (byte) 0);
-		String instruction=conf.get(SortMR.COMBINE_INSTRUCTION);
 		
 	    //indicate whether the matrix value in this mapper is a matrix cell or a matrix block
 	    int partitions = conf.getNumReduceTasks();
@@ -203,17 +136,17 @@ extends SequenceFileInputFormat<K,V>
 	    if (outFs.exists(partFile)) {
 	    	outFs.delete(partFile, false);
 	    }
-	    SequenceFile.Writer writer = 
-	    	SequenceFile.createWriter(outFs, conf, partFile, targetKeyClass, NullWritable.class);
-	    NullWritable nullValue = NullWritable.get();
-	    int index0=-1;
-	    int i=0;
-	    boolean lessthan0=true;
 	    
-	    for(WritableComparable splitValue: sampler.createPartitions(partitions)) {
+	    //note: key value always double/null as expected by partitioner
+	    SequenceFile.Writer writer = 
+	    	SequenceFile.createWriter(outFs, conf, partFile, DoubleWritable.class, NullWritable.class);
+	    NullWritable nullValue = NullWritable.get();
+	    int index0=-1, i=0;
+	    boolean lessthan0=true;
+	    for(WritableComparable splitValue : sampler.createPartitions(partitions)) 
+	    {
 	    	writer.append(splitValue, nullValue);
-	    	if(lessthan0 && ((DoubleWritable)splitValue).get()>=0)
-	    	{
+	    	if(lessthan0 && ((DoubleWritable)splitValue).get()>=0) {
 	    		index0=i;
 	    		lessthan0=false;
 	    	}
@@ -222,6 +155,7 @@ extends SequenceFileInputFormat<K,V>
 	    if(lessthan0)
 	    	index0=partitions-1;
 	    writer.close();
+	    
 	    return index0;
 	  }
 
@@ -235,4 +169,62 @@ extends SequenceFileInputFormat<K,V>
 		return lastResult;
 	}
 	
+
+	private static class Sampler implements IndexedSortable 
+	{
+		private ArrayList<WritableComparable> records = new ArrayList<WritableComparable>();
+		
+		@SuppressWarnings("unchecked")
+		public int compare(int i, int j) {
+			WritableComparable left =  records.get(i);
+			WritableComparable right = records.get(j);
+			return left.compareTo(right);
+		}
+
+		public void swap(int i, int j) {
+			WritableComparable left = records.get(i);
+			WritableComparable right = records.get(j);
+			records.set(j, left);
+			records.set(i, right);
+		}
+		
+		public void addValue(WritableComparable r)
+		{
+			records.add(r);
+		}
+		
+		public String toString()
+		{
+			return records.toString();
+		}
+
+	/**
+	 * Find the split points for a given sample. The sample keys are sorted
+	 * and down sampled to find even split points for the partitions. The
+	 * returned keys should be the start of their respective partitions.
+	 * @param numPartitions the desired number of partitions
+	 * @return an array of size numPartitions - 1 that holds the split points
+	 */
+		public ArrayList<WritableComparable> createPartitions(int numPartitions) 
+		{
+			int numRecords = records.size();
+			if (numPartitions > numRecords) {
+				throw new IllegalArgumentException
+				("Requested more partitions than input keys (" + numPartitions +
+						" > " + numRecords + ")");
+			}
+			new QuickSort().sort(this, 0, records.size());
+			//System.out.println("after sort: "+ toString());
+			float stepSize = numRecords / (float) numPartitions;
+			//System.out.println("Step size is " + stepSize);
+			ArrayList<WritableComparable> result = new ArrayList<WritableComparable>(numPartitions-1);
+			for(int i=1; i < numPartitions; ++i) {
+				result.add(records.get(Math.round(stepSize * i)));
+			}
+			
+			return result;
+		}
+		
+		 
+	}
 }

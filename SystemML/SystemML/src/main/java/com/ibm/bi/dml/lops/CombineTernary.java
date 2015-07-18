8f7d0815295295b0e4c23c40c566816b8fa22482
MATTHIAS BOEHM <mboehm@us.ibm.com>
2015-07-18 16:11:49 -0700

    89826: SystemML Engine - Fix mr covariance w/ weights (wrong instruction code combineternary)

--- SystemML/SystemML/src/main/java/com/ibm/bi/dml/lops/CombineTernary.java
+++ SystemML/SystemML/src/main/java/com/ibm/bi/dml/lops/CombineTernary.java
@@ -1,7 +1,7 @@
 /**
  * IBM Confidential
  * OCO Source Materials
- * (C) Copyright IBM Corp. 2010, 2014
+ * (C) Copyright IBM Corp. 2010, 2015
  * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
  */
 
@@ -23,7 +23,7 @@ import com.ibm.bi.dml.parser.Expression.ValueType;
 public class CombineTernary extends Lop 
 {
 	@SuppressWarnings("unused")
-	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2013\n" +
+	private static final String _COPYRIGHT = "Licensed Materials - Property of IBM\n(C) Copyright IBM Corp. 2010, 2015\n" +
                                              "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
 	
 	public enum OperationTypes {
@@ -65,7 +65,7 @@ public class CombineTernary extends Lop
 		StringBuilder sb = new StringBuilder();
 		sb.append( getExecType() );
 		sb.append( Lop.OPERAND_DELIMITOR );
-		sb.append( "combinetertiary" );
+		sb.append( "combineternary" );
 		sb.append( OPERAND_DELIMITOR );
 		
 		sb.append( getInputs().get(0).prepInputOperand(input_index1));

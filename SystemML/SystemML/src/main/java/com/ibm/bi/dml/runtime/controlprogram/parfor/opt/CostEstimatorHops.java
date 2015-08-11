e78c307eafa2af213359e155198f44ba3479929c
MATTHIAS BOEHM <mboehm@us.ibm.com>
2015-08-11 12:45:58 -0700

    94919: Spark Backend - Fix parfor optimizer wrt handling memory estimates of spark hops (preliminary version), incl updated docs

--- SystemML/SystemML/src/main/java/com/ibm/bi/dml/runtime/controlprogram/parfor/opt/CostEstimatorHops.java
+++ SystemML/SystemML/src/main/java/com/ibm/bi/dml/runtime/controlprogram/parfor/opt/CostEstimatorHops.java
@@ -28,6 +28,7 @@ public class CostEstimatorHops extends CostEstimator
                                              "US Government Users Restricted Rights - Use, duplication  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.";
 	
 	public static long DEFAULT_MEM_MR = -1;
+	public static long DEFAULT_MEM_SP = 20*1024*1024;
 	
 	private OptTreePlanMappingAbstract _map = null;
 	
@@ -59,27 +60,36 @@ public class CostEstimatorHops extends CostEstimator
 		double value = h.getMemEstimate();
 		
 		//handle specific cases 
-		if( value >= DEFAULT_MEM_MR )   	  
+		long DEFAULT_MEM_REMOTE = OptimizerUtils.isSparkExecutionMode() ? 
+								DEFAULT_MEM_SP : DEFAULT_MEM_MR;
+		
+		if( value >= DEFAULT_MEM_REMOTE )   	  
 		{
-			if( h.getExecType()==ExecType.MR ) //CP estimate but MR type
-				value = DEFAULT_MEM_MR;
+			//check for CP estimate but MR type
+			if( h.getExecType()==ExecType.MR || h.getExecType()==ExecType.SPARK  ) 
+			{
+				value = DEFAULT_MEM_REMOTE;
+			}
+			//check for invalid cp memory estimate
 			else if ( h.getExecType()==ExecType.CP && value >= OptimizerUtils.getLocalMemBudget() )
 			{
 				if( DMLScript.rtplatform != DMLScript.RUNTIME_PLATFORM.SINGLE_NODE && h.getForcedExecType()==null )
 					LOG.warn("Memory estimate larger than budget but CP exec type (op="+h.getOpString()+", name="+h.getName()+", memest="+h.getMemEstimate()+").");
-				value = DEFAULT_MEM_MR;
+				value = DEFAULT_MEM_REMOTE;
 			}
+			//check for non-existing exec type
 			else if ( h.getExecType()==null)
 			{
 				//note: if exec type is 'null' lops have never been created (e.g., r(T) for tsmm),
 				//in that case, we do not need to raise a warning 
-				value = DEFAULT_MEM_MR;
+				value = DEFAULT_MEM_REMOTE;
 			}
 		}
 		
-		if( h.getForcedExecType()==ExecType.MR ) //forced runtime platform
+		//check for forced runtime platform
+		if( h.getForcedExecType()==ExecType.MR  || h.getExecType()==ExecType.SPARK) 
 		{
-			value = DEFAULT_MEM_MR;
+			value = DEFAULT_MEM_REMOTE;
 		}
 		
 		if( value <= 0 ) //no mem estimate
@@ -87,7 +97,6 @@ public class CostEstimatorHops extends CostEstimator
 			LOG.warn("Cannot get memory estimate for hop (op="+h.getOpString()+", name="+h.getName()+", memest="+h.getMemEstimate()+").");
 			value = CostEstimator.DEFAULT_MEM_ESTIMATE_CP;
 		}
-		
 		
 		LOG.trace("Memory estimate "+h.getName()+", "+h.getOpString()+"("+node.getExecType()+")"+"="+OptimizerRuleBased.toMB(value));
 		

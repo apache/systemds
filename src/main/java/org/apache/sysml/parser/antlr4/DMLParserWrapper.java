f5f033777abe0e26c193ebef278b9030b4f149db
Niketan Pansare <npansar@us.ibm.com>
2015-12-08 13:33:57 -0800

    Minor cleanup in the parser.

--- src/main/java/org/apache/sysml/parser/antlr4/DMLParserWrapper.java
+++ src/main/java/org/apache/sysml/parser/antlr4/DMLParserWrapper.java
@@ -26,8 +26,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
--- src/main/java/org/apache/sysml/parser/python/PyDMLParserWrapper.java
+++ src/main/java/org/apache/sysml/parser/python/PyDMLParserWrapper.java
@@ -23,8 +23,6 @@ import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 

4a4426bf311eed273db6a52dbf44f2c3d4d6c7c5
Tatsuya.Nishiyama <nishiyama.tatsuya0@gmail.com>
2015-12-21 10:56:46 +0900

    Change building spark configuration parameters and setting default parameters.

--- scripts/sparkDML.sh
+++ scripts/sparkDML.sh
@@ -39,12 +39,13 @@ fi
 
 # Default Values
 
-master=yarn-client
-driver_memory=20G
-num_executors=5
-executor_memory=60G
-executor_cores=24
+master="--master yarn-client"
+driver_memory="--driver-memory 20G"
+num_executors="--num-executors 5"
+executor_memory="--executor-memory 60G"
+executor_cores="--executor-cores 24"
 conf="--conf spark.driver.maxResultSize=0 --conf spark.akka.frameSize=128"
+
 
 # error help print
 
@@ -83,17 +84,16 @@ EOF
   exit 1
 }
 
-
 # command line parameter processing
 
 while true ; do
   case "$1" in
     -h)                printUsageExit ; exit 1 ;;
-    --master)          master=$2 ; shift 2 ;;
-    --driver-memory)   driver_memory=$2 ; shift 2 ;;
-    --num-executors)   num_executors=$2 ; shift 2 ;;
-    --executor-memory) executor_memory=$2 ; shift 2 ;;
-    --executor-cores)  executor_cores=$2 ; shift 2 ;;
+    --master)          master="--master "$2 ; shift 2 ;;
+    --driver-memory)   driver_memory="--driver-memory "$2 ; shift 2 ;;
+    --num-executors)   num_executors="--num-executors "$2 ; shift 2 ;;
+    --executor-memory) executor_memory="--executor-memory "$2 ; shift 2 ;;
+    --executor-cores)  executor_cores="--executor-cores "$2 ; shift 2 ;;
     --conf)            conf=${conf}' --conf '$2 ; shift 2 ;;
      -f)               f=$2 ; shift 2 ;;
     --stats)           stats="-stats" ; shift 1 ;;
@@ -114,11 +114,11 @@ if [ ${SPARK_CONF_DIR} ]; then
 fi
 
 $SPARK_HOME/bin/spark-submit \
-     --master ${master} \
-     --driver-memory ${driver_memory} \
-     --num-executors ${num_executors} \
-     --executor-memory ${executor_memory} \
-     --executor-cores ${executor_cores} \
+     ${master} \
+     ${driver_memory} \
+     ${num_executors} \
+     ${executor_memory} \
+     ${executor_cores} \
      --properties-file ${spark_conf}/spark-defaults.conf \
      ${conf} \
      ${SYSTEMML_HOME}/SystemML.jar \

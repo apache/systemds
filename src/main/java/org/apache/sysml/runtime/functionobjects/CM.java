eee798bd849909106c53ba0d256937c76c5c8650
Mike Dusenberry <mwdusenb@us.ibm.com>
2016-01-11 10:45:20 -0800

    More cleanup up of CM.java.

--- src/main/java/org/apache/sysml/runtime/functionobjects/CM.java
+++ src/main/java/org/apache/sysml/runtime/functionobjects/CM.java
@@ -295,27 +295,10 @@ public class CM extends ValueFunction
 		
 		return cm1;
 	}
-	
-	/*
-	 //following the SPSS definition.
-	public Data execute(Data in1, double in2, double w2) throws DMLRuntimeException {
-		CMObject cm=(CMObject) in1;
-		double oldweight=cm._weight;
-		cm._weight+=w2;
-		double v=w2/cm._weight*(in2-cm._mean);
-		cm._mean+=v;
-		double oldm2=cm._m2;
-		double oldm3=cm._m3;
-		double oldm4=cm._m4;
-		double weightProduct=cm._weight*oldweight;
-		double vsquare=Math.pow(v, 2);
-		cm._m2=oldm2+weightProduct/w2*vsquare;
-		cm._m3=oldm3-3*v*oldm2+weightProduct/Math.pow(w2,2)*(cm._weight-2*w2)*Math.pow(v, 3);
-		cm._m4=oldm4-4*v*oldm3+6*vsquare*oldm2
-		+((Math.pow(cm._weight, 2)-3*w2*oldweight)/Math.pow(w2,3))*Math.pow(v, 4)*weightProduct;
-		return cm;
-	}*/
 
+	/**
+	 * Combining stats from two partitions of the data.
+	 */
 	@Override
 	public Data execute(Data in1, Data in2) throws DMLRuntimeException 
 	{
@@ -430,38 +413,4 @@ public class CM extends ValueFunction
 		
 		return cm1;
 	}
-	/*
-	 private double Q(CMObject cm1, CMObject cm2, int power)
-	{
-		return cm1._weight*Math.pow(cm1._mean,power)+cm2._weight*Math.pow(cm2._mean,power);
-	}
-	
-	//following the SPSS definition, it is wrong
-	public Data execute(Data in1, Data in2) throws DMLRuntimeException 
-	{
-		CMObject cm1=(CMObject) in1;
-		CMObject cm2=(CMObject) in2;
-		double w=cm1._weight+cm2._weight;
-		double q1=cm1._mean*cm1._weight+cm2._mean*cm2._weight;
-		double mean=q1/w;
-		double p1=mean-cm1._mean;
-		double p2=mean-cm2._mean;
-		double q2=Q(cm1, cm2, 2);
-		double q3=Q(cm1, cm2, 3);
-		double q4=Q(cm1, cm2, 4);
-		double mean2=Math.pow(mean, 2);
-		double mean3=Math.pow(mean, 3);
-		double mean4=Math.pow(mean, 4);
-		double m2 = cm1._m2+cm2._m2 + q2 - 2*mean*q1 + w*mean2;
-		double m3 = cm1._m3+cm2._m3 - 3*(p1*cm1._m2+p2*cm2._m2) 
-		- 3*mean*(Math.pow(cm1._mean, 2)+Math.pow(cm2._mean, 2)) + 4*q3 - w*mean3;
-		double m4 = cm1._m4+cm2._m4 - 4*(p1*cm1._m3+p2*cm2._m3) + 6*(Math.pow(p1, 2)*cm1._m2+Math.pow(p2, 2)*cm2._m2)-4*q4-4*mean*q3+6*mean2*q2-4*mean3*q1+2*w*mean4;
-		cm1._m2=m2;
-		cm1._m3=m3;
-		cm1._m4=m4;
-		cm1._mean=mean;
-		cm1._weight=w;
-		return cm1;
-	}*/
-
 }

dbd9e1b1db38d55830a971ded4f71e90d768021c
Tommy YU <tummyyu@163.com>
2016-02-25 16:30:12 +0800

    add test, readme for project import, and clean hard code for script folder path

--- src/main/scala/org/apache/sysml/api/ml/scala/LogisticRegression.scala
+++ src/main/scala/org/apache/sysml/api/ml/scala/LogisticRegression.scala
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.sysml.api.ml.scala
 
 import org.apache.sysml.api.{MLContext, MLOutput}
@@ -113,7 +132,7 @@ class LogisticRegressionModel(
   }
 }
 
-object LogisticRegressionTest {
+object LogisticRegressionExample {
   import org.apache.spark.{ SparkConf, SparkContext }
   import org.apache.spark.sql.types._
 import org.apache.spark.mllib.linalg.Vectors
@@ -125,7 +144,6 @@ import org.apache.spark.mllib.regression.LabeledPoint
     val sqlContext = new org.apache.spark.sql.SQLContext(sc);
 
 import sqlContext.implicits._
-    ScriptsUtils.setSystemmlHome("C:\\aWorkFolder\\git\\systemml\\incubator-systemml\\scripts")
     val training = sc.parallelize(Seq(
       LabeledPoint(1.0, Vectors.dense(1.0, 0.0, 3.0)),
       LabeledPoint(1.0, Vectors.dense(1.0, 0.4, 2.1)),

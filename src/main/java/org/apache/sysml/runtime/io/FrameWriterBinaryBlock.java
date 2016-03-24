216a501fd0e389d2d8f5b82796133dac4fc61c8a
Arvind Surve <acs_s@yahoo.com>
2016-03-24 11:57:01 -0700

    [SYSTEMML-573] Frame write

--- src/main/java/org/apache/sysml/runtime/io/FrameWriterBinaryBlock.java
+++ src/main/java/org/apache/sysml/runtime/io/FrameWriterBinaryBlock.java
@@ -19,26 +19,251 @@
 
 package org.apache.sysml.runtime.io;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.sysml.conf.ConfigurationManager;
 import org.apache.sysml.runtime.DMLRuntimeException;
 import org.apache.sysml.runtime.DMLUnsupportedOperationException;
+import org.apache.sysml.runtime.controlprogram.ParForProgramBlock.PDataPartitionFormat;
 import org.apache.sysml.runtime.matrix.data.FrameBlock;
+import org.apache.sysml.runtime.matrix.data.MatrixBlock;
+import org.apache.sysml.runtime.matrix.data.MatrixIndexes;
+import org.apache.sysml.runtime.matrix.mapred.DistributedCacheInput;
+import org.apache.sysml.runtime.util.MapReduceTool;
 
 public class FrameWriterBinaryBlock extends FrameWriter
 {
 	/**
 	 * @param src
 	 * @param fname
+	 * @param rlen
+	 * @param clen
 	 * @return
 	 * @throws IOException 
 	 * @throws DMLRuntimeException 
 	 * @throws DMLUnsupportedOperationException 
 	 */
 	@Override
-	public void writeFrameToHDFS( FrameBlock src, String fname )
+	public void writeFrameToHDFS( FrameBlock src, String fname, long rlen, long clen )
 		throws IOException, DMLRuntimeException, DMLUnsupportedOperationException 
 	{
+		//prepare file access
+		JobConf job = new JobConf(ConfigurationManager.getCachedJobConf());
+		Path path = new Path( fname );
+
+		//if the file already exists on HDFS, remove it.
+		MapReduceTool.deleteFileIfExistOnHDFS( fname );
+			
+		//core write
+		writeBinaryBlockFrameToHDFS(path, job, src, rlen, clen);
+	}
+
+	/**
+	 * 
+	 * @param path
+	 * @param job
+	 * @param src
+	 * @param rlen
+	 * @param clen
+	 * @return 
+	 * @throws IOException
+	 * @throws DMLUnsupportedOperationException 
+	 * @throws DMLRuntimeException 
+	 */
+	@SuppressWarnings("deprecation")
+	protected void writeBinaryBlockFrameToHDFS( Path path, JobConf job, FrameBlock src, long rlen, long clen )
+		throws IOException, DMLRuntimeException, DMLUnsupportedOperationException
+	{
+		FileSystem fs = FileSystem.get(job);
+		int brlen = ConfigurationManager.getBlocksize();
+		int bclen = ConfigurationManager.getBlocksize();
+
+		// 1) create sequence file writer 
+		SequenceFile.Writer writer = null;
+		writer = new SequenceFile.Writer(fs, job, path, MatrixIndexes.class, MatrixBlock.class);
+		
+		try
+		{
+			// 2) bound check for src block
+			if( src.getNumRows() > rlen || src.getNumColumns() > clen )
+			{
+				throw new IOException("Frame block [1:"+src.getNumRows()+",1:"+src.getNumColumns()+"] " +
+						              "out of overall frame range [1:"+rlen+",1:"+clen+"].");
+			}
+		
+			//3) reblock and write
+			MatrixIndexes indexes = new MatrixIndexes();
+
+			if( rlen <= brlen && clen <= bclen ) //opt for single block
+			{
+				//directly write single block
+				indexes.setIndexes(1, 1);
+				writer.append(indexes, src);
+			}
+			else //general case
+			{
+				//initialize blocks for reuse (at most 4 different blocks required)
+				FrameBlock[] blocks = createFrameBlocksForReuse(src.getSchema(), src.getColumnNames(), rlen);  
+				
+				//create and write subblocks of frame
+				for(int blockRow = 0; blockRow < (int)Math.ceil(src.getNumRows()/(double)brlen); blockRow++)
+					for(int blockCol = 0; blockCol < (int)Math.ceil(src.getNumColumns()/(double)bclen); blockCol++)
+					{
+						int maxRow = (blockRow*brlen + brlen < src.getNumRows()) ? brlen : src.getNumRows() - blockRow*brlen;
+						int maxCol = (blockCol*bclen + bclen < src.getNumColumns()) ? bclen : src.getNumColumns() - blockCol*bclen;
+				
+						int row_offset = blockRow*brlen;
+						int col_offset = blockCol*bclen;
+						
+						//get reuse frame block
+						FrameBlock block = getFrameBlockForReuse(blocks);
+	
+						//copy subpart to block
+						src.sliceOperations( row_offset, row_offset+maxRow-1, 
+								             col_offset, col_offset+maxCol-1, block );
+						
+						//append block to sequence file
+						indexes.setIndexes(blockRow+1, blockCol+1);
+						writer.append(indexes, block);
+					}
+			}
+		
+		}
+		finally
+		{
+			IOUtilFunctions.closeSilently(writer);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param path
+	 * @param job
+	 * @param src
+	 * @param rlen
+	 * @param clen
+	 * @param pformat
+	 * @return 
+	 * @throws IOException
+	 * @throws DMLRuntimeException
+	 * @throws DMLUnsupportedOperationException
+	 */
+	@SuppressWarnings("deprecation")
+	public void writePartitionedBinaryBlockFrameToHDFS( Path path, JobConf job, FrameBlock src, long rlen, long clen, PDataPartitionFormat pformat )
+			throws IOException, DMLRuntimeException, DMLUnsupportedOperationException
+	{
+		FileSystem fs = FileSystem.get(job);
+		int brlen = ConfigurationManager.getBlocksize();
+		int bclen = ConfigurationManager.getBlocksize();
+		
+		//initialize blocks for reuse 
+		FrameBlock[] blocks = createFrameBlocksForReuse(src.getSchema(), src.getColumnNames(), rlen);  
+		
+		switch( pformat )
+		{
+			case ROW_BLOCK_WISE_N:
+			{
+				long numBlocks = ((rlen-1)/brlen)+1;
+				long numPartBlocks = (long)Math.ceil(((double)DistributedCacheInput.PARTITION_SIZE)/clen/brlen);
+						
+				int count = 0;		
+				for( int k = 0; k<numBlocks; k+=numPartBlocks )
+				{
+					// 1) create sequence file writer, with right replication factor 
+					// (config via MRConfigurationNames.DFS_REPLICATION not possible since sequence file internally calls fs.getDefaultReplication())
+					Path path2 = new Path(path.toString()+File.separator+(++count));
+					SequenceFile.Writer writer = new SequenceFile.Writer(fs, job, path2, MatrixIndexes.class, FrameBlock.class);
+					
+					//3) reblock and write
+					try
+					{
+						MatrixIndexes indexes = new MatrixIndexes();
+			
+						//create and write subblocks of frame
+						for(int blockRow = k; blockRow < Math.min((int)Math.ceil(src.getNumRows()/(double)brlen),k+numPartBlocks); blockRow++)
+							for(int blockCol = 0; blockCol < (int)Math.ceil(src.getNumColumns()/(double)bclen); blockCol++)
+							{
+								int maxRow = (blockRow*brlen + brlen < src.getNumRows()) ? brlen : src.getNumRows() - blockRow*brlen;
+								int maxCol = (blockCol*bclen + bclen < src.getNumColumns()) ? bclen : src.getNumColumns() - blockCol*bclen;
+						
+								int row_offset = blockRow*brlen;
+								int col_offset = blockCol*bclen;
+								
+								//get reuse frame block
+								FrameBlock block = getFrameBlockForReuse(blocks);
+			
+								//copy subpart to block
+								src.sliceOperations( row_offset, row_offset+maxRow-1, 
+										             col_offset, col_offset+maxCol-1, block );
+								
+								//append block to sequence file
+								indexes.setIndexes(blockRow+1, blockCol+1);
+								writer.append(indexes, block);
+							}
+					}
+					finally
+					{
+						IOUtilFunctions.closeSilently(writer);
+					}
+				}
+				break;
+			}
+			case COLUMN_BLOCK_WISE_N:
+			{
+				long numBlocks = ((clen-1)/bclen)+1;
+				long numPartBlocks = (long)Math.ceil(((double)DistributedCacheInput.PARTITION_SIZE)/rlen/bclen);
+				
+				int count = 0;		
+				for( int k = 0; k<numBlocks; k+=numPartBlocks )
+				{
+					// 1) create sequence file writer, with right replication factor 
+					// (config via MRConfigurationNames.DFS_REPLICATION not possible since sequence file internally calls fs.getDefaultReplication())
+					Path path2 = new Path(path.toString()+File.separator+(++count));
+					SequenceFile.Writer writer = new SequenceFile.Writer(fs, job, path2, MatrixIndexes.class, FrameBlock.class);
+					
+					//3) reblock and write
+					try
+					{
+						MatrixIndexes indexes = new MatrixIndexes();
+			
+						//create and write subblocks of frame
+						for(int blockRow = 0; blockRow < (int)Math.ceil(src.getNumRows()/(double)brlen); blockRow++)
+							for(int blockCol = k; blockCol < Math.min((int)Math.ceil(src.getNumColumns()/(double)bclen),k+numPartBlocks); blockCol++)
+							{
+								int maxRow = (blockRow*brlen + brlen < src.getNumRows()) ? brlen : src.getNumRows() - blockRow*brlen;
+								int maxCol = (blockCol*bclen + bclen < src.getNumColumns()) ? bclen : src.getNumColumns() - blockCol*bclen;
+						
+								int row_offset = blockRow*brlen;
+								int col_offset = blockCol*bclen;
+								
+								//get reuse frame block
+								FrameBlock block = getFrameBlockForReuse(blocks);
+			
+								//copy subpart to block
+								src.sliceOperations( row_offset, row_offset+maxRow-1, 
+										             col_offset, col_offset+maxCol-1, block );
+								
+								//append block to sequence file
+								indexes.setIndexes(blockRow+1, blockCol+1);
+								writer.append(indexes, block);
+							}
+					}
+					finally
+					{
+						IOUtilFunctions.closeSilently(writer);
+					}
+				}
+				break;
+			}
+				
+			default:
+				throw new DMLRuntimeException("Unsupported partition format for distributed cache input: "+pformat);
+		}
 	}
 
 }
--- src/main/java/org/apache/sysml/runtime/io/FrameWriterTextCSV.java
+++ src/main/java/org/apache/sysml/runtime/io/FrameWriterTextCSV.java
@@ -19,12 +19,28 @@
 
 package org.apache.sysml.runtime.io;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.sysml.conf.ConfigurationManager;
 import org.apache.sysml.runtime.DMLRuntimeException;
 import org.apache.sysml.runtime.DMLUnsupportedOperationException;
+import org.apache.sysml.runtime.matrix.CSVReblockMR;
 import org.apache.sysml.runtime.matrix.data.CSVFileFormatProperties;
 import org.apache.sysml.runtime.matrix.data.FrameBlock;
+import org.apache.sysml.runtime.util.MapReduceTool;
 
 /**
  * 
@@ -44,15 +60,308 @@ public class FrameWriterTextCSV extends FrameWriter
 	/**
 	 * @param src
 	 * @param fname
+	 * @param rlen
+	 * @param clen
 	 * @return
 	 * @throws IOException 
 	 * @throws DMLRuntimeException 
 	 * @throws DMLUnsupportedOperationException 
 	 */
 	@Override
-	public void writeFrameToHDFS( FrameBlock src, String fname )
+	public void writeFrameToHDFS(FrameBlock src, String fname, long rlen, long clen) 
 		throws IOException, DMLRuntimeException, DMLUnsupportedOperationException 
 	{
+		//validity check frame dimensions
+		if( src.getNumRows() != rlen || src.getNumColumns() != clen ) {
+			throw new IOException("Frame dimensions mismatch with metadata: "+src.getNumRows()+"x"+src.getNumColumns()+" vs "+rlen+"x"+clen+".");
+		}
+		
+		//prepare file access
+		JobConf job = new JobConf(ConfigurationManager.getCachedJobConf());
+		Path path = new Path( fname );
+
+		//if the file already exists on HDFS, remove it.
+		MapReduceTool.deleteFileIfExistOnHDFS( fname );
+			
+		//core write
+		writeCSVFrameToHDFS(path, job, src, rlen, clen, _props);
+	}
+
+	/**
+	 * 
+	 * @param path
+	 * @param job
+	 * @param src
+	 * @param rlen
+	 * @param clen
+	 * @param props
+	 * @return
+	 * @throws IOException
+	 */
+	protected void writeCSVFrameToHDFS( Path path, JobConf job, FrameBlock src, long rlen, long clen, CSVFileFormatProperties props )
+		throws IOException
+	{
+		FileSystem fs = FileSystem.get(job);
+        BufferedWriter br=new BufferedWriter(new OutputStreamWriter(fs.create(path,true)));		
+		
+		try
+		{
+			//for obj reuse and preventing repeated buffer re-allocations
+			StringBuilder sb = new StringBuilder();
+			
+			props = (props==null)? new CSVFileFormatProperties() : props;
+			String delim = props.getDelim();
+			
+			// Write header line, if needed
+			if( props.hasHeader() ) 
+			{
+				//write row chunk-wise to prevent OOM on large number of columns
+				for( int bj=0; bj<clen; bj+=BLOCKSIZE_J )
+				{
+					for( int j=bj; j < Math.min(clen,bj+BLOCKSIZE_J); j++) 
+					{
+						sb.append("C"+ (j+1));
+						if ( j < clen-1 )
+							sb.append(delim);
+					}
+					br.write( sb.toString() );
+		            sb.setLength(0);	
+				}
+				sb.append('\n');
+				br.write( sb.toString() );
+	            sb.setLength(0);
+			}
+			
+			// Write data lines
+			for( int i=0; i<rlen; i++ ) 
+			{
+				//write row chunk-wise to prevent OOM on large number of columns
+				for( int bj=0; bj<clen; bj+=BLOCKSIZE_J )
+				{
+					for( int j=bj; j<Math.min(clen,bj+BLOCKSIZE_J); j++ )
+					{
+						String lvalue = (String)src.get(i, j);
+						sb.append(lvalue);
+						
+						if( j != clen-1 )
+							sb.append(delim);
+					}
+					br.write( sb.toString() );
+		            sb.setLength(0);
+				}
+				
+				sb.append('\n');
+				br.write( sb.toString() ); //same as append
+				sb.setLength(0); 
+			}
+		}
+		finally
+		{
+			IOUtilFunctions.closeSilently(br);
+		}
+	}
+
+
+	
+	/**
+	 * Method to merge multiple CSV part files on HDFS into a single CSV file on HDFS. 
+	 * The part files are created by CSV_WRITE MR job. 
+	 * 
+	 * This method is invoked from CP-write instruction.
+	 * 
+	 * @param srcFileName
+	 * @param destFileName
+	 * @param csvprop
+	 * @param rlen
+	 * @param clen
+	 * @throws IOException
+	 */
+	public void mergeCSVPartFiles(String srcFileName, String destFileName, CSVFileFormatProperties csvprop, long rlen, long clen) 
+		throws IOException 
+	{	
+		Configuration conf = new Configuration(ConfigurationManager.getCachedJobConf());
+
+		Path srcFilePath = new Path(srcFileName);
+		Path mergedFilePath = new Path(destFileName);
+		FileSystem hdfs = FileSystem.get(conf);
+
+		if (hdfs.exists(mergedFilePath)) {
+			hdfs.delete(mergedFilePath, true);
+		}
+		OutputStream out = hdfs.create(mergedFilePath, true);
+
+		// write out the header, if needed
+		if (csvprop.hasHeader()) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < clen; i++) {
+				sb.append("C" + (i + 1));
+				if (i < clen - 1)
+					sb.append(csvprop.getDelim());
+			}
+			sb.append('\n');
+			out.write(sb.toString().getBytes());
+			sb.setLength(0);
+		}
+
+		// if the source is a directory
+		if (hdfs.isDirectory(srcFilePath)) {
+			try {
+				FileStatus[] contents = hdfs.listStatus(srcFilePath);
+				Path[] partPaths = new Path[contents.length];
+				int numPartFiles = 0;
+				for (int i = 0; i < contents.length; i++) {
+					if (!contents[i].isDirectory()) {
+						partPaths[i] = contents[i].getPath();
+						numPartFiles++;
+					}
+				}
+				Arrays.sort(partPaths);
+
+				for (int i = 0; i < numPartFiles; i++) {
+					InputStream in = hdfs.open(partPaths[i]);
+					try {
+						IOUtils.copyBytes(in, out, conf, false);
+						if(i<numPartFiles-1)
+							out.write('\n');
+					} 
+					finally {
+						IOUtilFunctions.closeSilently(in);
+					}
+				}
+			} finally {
+				IOUtilFunctions.closeSilently(out);
+			}
+		} else if (hdfs.isFile(srcFilePath)) {
+			InputStream in = null;
+			try {
+				in = hdfs.open(srcFilePath);
+				IOUtils.copyBytes(in, out, conf, true);
+			} finally {
+				IOUtilFunctions.closeSilently(in);
+				IOUtilFunctions.closeSilently(out);
+			}
+		} else {
+			throw new IOException(srcFilePath.toString()
+					+ ": No such file or directory");
+		}
+	}
+		
+	/**
+	 * 
+	 * @param srcFileName
+	 * @param destFileName
+	 * @param rlen
+	 * @param clen
+	 * @throws IOException
+	 */
+	@SuppressWarnings("unchecked")
+	public void addHeaderToCSV(String srcFileName, String destFileName, long rlen, long clen) 
+		throws IOException 
+	{
+		Configuration conf = new Configuration(ConfigurationManager.getCachedJobConf());
+
+		Path srcFilePath = new Path(srcFileName);
+		Path destFilePath = new Path(destFileName);
+		FileSystem hdfs = FileSystem.get(conf);
+		
+		if ( !_props.hasHeader() ) {
+			// simply move srcFile to destFile
+			
+			/*
+			 * TODO: Remove this roundabout way! 
+			 * For example: destFilePath = /user/biadmin/csv/temp/out/file.csv 
+			 *              & the only path that exists already on HDFS is /user/biadmin/csv/.
+			 * In this case: the directory structure /user/biadmin/csv/temp/out must be created. 
+			 * Simple hdfs.rename() does not seem to create this directory structure.
+			 */
+			
+			// delete the destination file, if exists already
+			//boolean ret1 = 
+			hdfs.delete(destFilePath, true);
+			
+			// Create /user/biadmin/csv/temp/out/file.csv so that ..../temp/out/ is created.
+			//boolean ret2 = 
+			hdfs.createNewFile(destFilePath);
+			
+			// delete the file "file.csv" but preserve the directory structure /user/biadmin/csv/temp/out/
+			//boolean ret3 = 
+			hdfs.delete(destFilePath, true);
+			
+			// finally, move the data to destFilePath = /user/biadmin/csv/temp/out/file.csv
+			//boolean ret4 = 
+			hdfs.rename(srcFilePath, destFilePath);
+
+			//System.out.println("Return values = del:" + ret1 + ", createNew:" + ret2 + ", del:" + ret3 + ", rename:" + ret4);
+			return;
+		}
+	
+		// construct the header line
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < clen; i++) {
+			sb.append("C" + (i + 1));
+			if (i < clen - 1)
+				sb.append(_props.getDelim());
+		}
+		sb.append('\n');
+
+		if (hdfs.isDirectory(srcFilePath)) {
+
+			// compute sorted order among part files
+			ArrayList<Path> files=new ArrayList<Path>();
+			for(FileStatus stat: hdfs.listStatus(srcFilePath, CSVReblockMR.hiddenFileFilter))
+				files.add(stat.getPath());
+			Collections.sort(files);
+		
+			// first part file path
+			Path firstpart = files.get(0);
+			
+			// create a temp file, and add header and contents of first part
+			Path tmp = new Path(firstpart.toString() + ".tmp");
+			OutputStream out = hdfs.create(tmp, true);
+			out.write(sb.toString().getBytes());
+			sb.setLength(0);
+			
+			// copy rest of the data from firstpart
+			InputStream in = null;
+			try {
+				in = hdfs.open(firstpart);
+				IOUtils.copyBytes(in, out, conf, true);
+			} finally {
+				IOUtilFunctions.closeSilently(in);
+				IOUtilFunctions.closeSilently(out);
+			}
+			
+			// rename tmp to firstpart
+			hdfs.delete(firstpart, true);
+			hdfs.rename(tmp, firstpart);
+			
+			// rename srcfile to destFile
+			hdfs.delete(destFilePath, true);
+			hdfs.createNewFile(destFilePath); // force the creation of directory structure
+			hdfs.delete(destFilePath, true);  // delete the file, but preserve the directory structure
+			hdfs.rename(srcFilePath, destFilePath); // move the data 
+		
+		} else if (hdfs.isFile(srcFilePath)) {
+			// create destination file
+			OutputStream out = hdfs.create(destFilePath, true);
+			
+			// write header
+			out.write(sb.toString().getBytes());
+			sb.setLength(0);
+			
+			// copy the data from srcFile
+			InputStream in = null;
+			try {
+				in = hdfs.open(srcFilePath);
+				IOUtils.copyBytes(in, out, conf, true);
+			} 
+			finally {
+				IOUtilFunctions.closeSilently(in);
+				IOUtilFunctions.closeSilently(out);
+			}
+		} else {
+			throw new IOException(srcFilePath.toString() + ": No such file or directory");
+		}
 	}
 
 }

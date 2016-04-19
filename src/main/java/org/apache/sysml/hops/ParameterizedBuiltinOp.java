apache/systemds

commit 6bbcca3984465612d58d5d0613987accdb27d780

Improved mr remove empty compilation (broadcast-based pmm if possible)
This patch introduces advanced operator selection for MR remove empty
row operations. Similar to SYSTEMML-215, we employ a broadcast-based
operation if the selection vector fits into the broadcast. However,
instead of introducing a new physical operator, we directly compile a
PMM operation for simplification and consistency (across alternative
formulations of row selection) because this operator has precisely the
semantics of the actual selection step in remove empty. On a scenario of
250GB, this change improved performance from 1,587s (4 jobs) to 573s (3
jobs) by preventing an expensive shuffle-heavy job altogether.

src/main/java/org/apache/sysml/hops/ParameterizedBuiltinOp.java
@@ -36,6 +36,7 @@ import org.apache.sysml.lops.Lop;
 import org.apache.sysml.lops.LopProperties.ExecType;
 import org.apache.sysml.lops.LopsException;
 import org.apache.sysml.lops.OutputParameters.Format;
+import org.apache.sysml.lops.PMMJ;
 import org.apache.sysml.lops.PartialAggregate.CorrectionLocationType;
 import org.apache.sysml.lops.ParameterizedBuiltin;
 import org.apache.sysml.lops.RepMat;
@@ -617,35 +618,56 @@ public class ParameterizedBuiltinOp extends Hop implements MultiThreadedHop
 				Lop loffset = offsets.constructLops();
 				Lop lmaxdim = maxDim.constructLops();
 				
-				boolean requiresRep =   ((clen>bclen || clen<=0) &&  rmRows) 
-						             || ((rlen>brlen || rlen<=0) && !rmRows);
+				double mestPM = OptimizerUtils.estimatePartitionedSizeExactSparsity(rlen, 1, brlen, bclen, 1.0);
+				Lop rmEmpty = null;
 				
-				if( requiresRep ) {
-					Lop pos = createOffsetLop(input, rmRows); //ncol of left input (determines num replicates)
-					loffset = new RepMat(loffset, pos, rmRows, DataType.MATRIX, ValueType.DOUBLE);
-					loffset.getOutputParameters().setDimensions(rlen, clen, brlen, bclen, nnz);
-					setLineNumbers(loffset);
+				//a) broadcast-based PMM (permutation matrix mult)
+				if( rmRows && mestPM < OptimizerUtils.getRemoteMemBudgetMap() )
+				{
+					boolean needPart = !offsets.dimsKnown() || offsets.getDim1() > DistributedCacheInput.PARTITION_SIZE;
+					if( needPart ){ //requires partitioning
+						loffset = new DataPartition(loffset, DataType.MATRIX, ValueType.DOUBLE, (mestPM>OptimizerUtils.getLocalMemBudget())?ExecType.MR:ExecType.CP, PDataPartitionFormat.ROW_BLOCK_WISE_N);
+						loffset.getOutputParameters().setDimensions(offsets.getDim1(), 1, rlen, clen, rlen);
+						setLineNumbers(loffset);	
+					}
+					
+					rmEmpty = new PMMJ(loffset, linput, lmaxdim, getDataType(), getValueType(), needPart, true, ExecType.MR);
+					setOutputDimensions(rmEmpty);
+					setLineNumbers(rmEmpty);
+				}
+				//b) general case: repartition-based rmempty
+				else
+				{
+					boolean requiresRep =   ((clen>bclen || clen<=0) &&  rmRows) 
+							             || ((rlen>brlen || rlen<=0) && !rmRows);
+					
+					if( requiresRep ) {
+						Lop pos = createOffsetLop(input, rmRows); //ncol of left input (determines num replicates)
+						loffset = new RepMat(loffset, pos, rmRows, DataType.MATRIX, ValueType.DOUBLE);
+						loffset.getOutputParameters().setDimensions(rlen, clen, brlen, bclen, nnz);
+						setLineNumbers(loffset);
+					}
+					
+					Group group1 = new Group(linput, Group.OperationTypes.Sort, getDataType(), getValueType());
+					setLineNumbers(group1);
+					group1.getOutputParameters().setDimensions(rlen, clen, brlen, bclen, nnz);
+				
+					Group group2 = new Group( loffset, Group.OperationTypes.Sort, getDataType(), getValueType());
+					setLineNumbers(group2);
+					group2.getOutputParameters().setDimensions(rlen, clen, brlen, bclen, nnz);
+				
+					HashMap<String, Lop> inMap = new HashMap<String, Lop>();
+					inMap.put("target", group1);
+					inMap.put("offset", group2);
+					inMap.put("maxdim", lmaxdim);
+					inMap.put("margin", inputlops.get("margin"));
+					
+					rmEmpty = new ParameterizedBuiltin(inMap, HopsParameterizedBuiltinLops.get(_op), getDataType(), getValueType(), et);			
+					setOutputDimensions(rmEmpty);
+					setLineNumbers(rmEmpty);
 				}
 				
-				Group group1 = new Group(linput, Group.OperationTypes.Sort, getDataType(), getValueType());
-				setLineNumbers(group1);
-				group1.getOutputParameters().setDimensions(rlen, clen, brlen, bclen, nnz);
-			
-				Group group2 = new Group( loffset, Group.OperationTypes.Sort, getDataType(), getValueType());
-				setLineNumbers(group2);
-				group2.getOutputParameters().setDimensions(rlen, clen, brlen, bclen, nnz);
-			
-				HashMap<String, Lop> inMap = new HashMap<String, Lop>();
-				inMap.put("target", group1);
-				inMap.put("offset", group2);
-				inMap.put("maxdim", lmaxdim);
-				inMap.put("margin", inputlops.get("margin"));
-				
-				ParameterizedBuiltin pbilop = new ParameterizedBuiltin(inMap, HopsParameterizedBuiltinLops.get(_op), getDataType(), getValueType(), et);			
-				setOutputDimensions(pbilop);
-				setLineNumbers(pbilop);
-			
-				Group group3 = new Group( pbilop, Group.OperationTypes.Sort, getDataType(), getValueType());
+				Group group3 = new Group( rmEmpty, Group.OperationTypes.Sort, getDataType(), getValueType());
 				setLineNumbers(group3);
 				group3.getOutputParameters().setDimensions(-1, -1, brlen, bclen, -1);
 				

src/main/java/org/apache/sysml/api/DMLScript.java
@@ -81,9 +81,7 @@ import org.apache.sysml.utils.Explain.ExplainCounts;
 import org.apache.sysml.utils.Explain.ExplainType;
 import org.apache.sysml.utils.Statistics;
 import org.apache.sysml.yarn.DMLAppMasterUtils;
-// import org.apache.sysml.utils.visualize.DotGraph;
 import org.apache.sysml.yarn.DMLYarnClientProxy;
-import org.xml.sax.SAXException;
 
 
 public class DMLScript 

src/main/java/org/apache/sysml/runtime/instructions/mr/PMMJMRInstruction.java
@@ -80,8 +80,8 @@ public class PMMJMRInstruction extends BinaryMRInstructionBase implements IDistr
 		String opcode = parts[0];
 		byte in1 = Byte.parseByte(parts[1]);
 		byte in2 = Byte.parseByte(parts[2]);
+		long nrow = UtilFunctions.toLong(Double.parseDouble(parts[3]));
 		byte out = Byte.parseByte(parts[4]);
-		long nrow = Long.parseLong(parts[3]);
 		CacheType ctype = CacheType.valueOf(parts[5]);
 		boolean outputEmpty = Boolean.parseBoolean(parts[6]);
 		

commit ef58207dbfcc0a43a57672b3537f439b87b4a4e1

[SYSTEMML-638] Fix mr remove empty compilation (robustness unknowns)
While running random-forest over small input data, we have seen
compilation issues to unknown sizes. This issue was introduced with
commit 6bbcca3984465612d58d5d0613987accdb27d780 (improved compilation
for mr remove empty).

src/main/java/org/apache/sysml/hops/ParameterizedBuiltinOp.java
@@ -622,12 +622,12 @@ public class ParameterizedBuiltinOp extends Hop implements MultiThreadedHop
 				Lop rmEmpty = null;
 				
 				//a) broadcast-based PMM (permutation matrix mult)
-				if( rmRows && mestPM < OptimizerUtils.getRemoteMemBudgetMap() )
+				if( rmRows && rlen > 0 && mestPM < OptimizerUtils.getRemoteMemBudgetMap() )
 				{
 					boolean needPart = !offsets.dimsKnown() || offsets.getDim1() > DistributedCacheInput.PARTITION_SIZE;
 					if( needPart ){ //requires partitioning
 						loffset = new DataPartition(loffset, DataType.MATRIX, ValueType.DOUBLE, (mestPM>OptimizerUtils.getLocalMemBudget())?ExecType.MR:ExecType.CP, PDataPartitionFormat.ROW_BLOCK_WISE_N);
-						loffset.getOutputParameters().setDimensions(offsets.getDim1(), 1, rlen, clen, rlen);
+						loffset.getOutputParameters().setDimensions(rlen, 1, brlen, bclen, rlen);
 						setLineNumbers(loffset);	
 					}
 					
8a4c9d940accdbf55a01836a01f3b6e7e72dc1ef
Mike Dusenberry <mwdusenb@us.ibm.com>
2016-05-04 16:35:30 -0700

    [SYSTEMML-540] Fixing the ported R test script.
    The issue with the R tests is indeed due to the port from DML -> R involving different R semantics.
    
    By default, R works with matrices in a column-wise manner, i.e., shaping a vector into a matrix will do so in a column wise manner:
    ```r
    > x = matrix(1:8, 2, 4)
    > x
         [,1] [,2] [,3] [,4]
    [1,]    1    3    5    7
    [2,]    2    4    6    8
    ```
    There is a `byrow` boolean flag to the `matrix(...)` that can be set to `TRUE` to enable a row-wise treatment:
    ```r
    > x = matrix(1:8, 2, 4, byrow=TRUE)
    > x
         [,1] [,2] [,3] [,4]
    [1,]    1    2    3    4
    [2,]    5    6    7    8
    ```
    
    **However**, this row-wise treatment is *not* applied when unrolling a matrix back into a vector, regardless of the flag:
    ```r
    > matrix(x, 1, 8)
         [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8]
    [1,]    1    5    2    6    3    7    4    8
    > matrix(x, 1, 8, byrow=TRUE)
         [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8]
    [1,]    1    5    2    6    3    7    4    8
    ```
    
    Therefore, in R, we have to transpose the matrix first, and then unroll into a vector:
    ```r
    > matrix(t(x), 1, 8)
         [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8]
    ```
    
    Note that this isn't necessary in DML.

--- src/test/java/org/apache/sysml/test/integration/functions/tensor/Conv2DTest.java
+++ src/test/java/org/apache/sysml/test/integration/functions/tensor/Conv2DTest.java
@@ -119,11 +119,7 @@ public class Conv2DTest extends AutomatedTestBase
 			// Run comparison R script
 			runRScript(true);
 			HashMap<CellIndex, Double> bHM = readRMatrixFromFS("B");
-			
-			// Uncomment this if people in dev mailing list agree to this approach
-			// and R script itself is debugged
-//			runRScript(true);
-//			HashMap<CellIndex, Double> bHM = readRMatrixFromFS("B");
+
 			
 			HashMap<CellIndex, Double> dmlfile = readDMLMatrixFromHDFS("B");
 			TestUtils.compareMatrices(dmlfile, bHM, epsilon, "B-DML", "B-R");

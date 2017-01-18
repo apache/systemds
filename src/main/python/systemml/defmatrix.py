f574aa7cb21e9ef63faccfbdbef6c26b7b917c21
Imran Younus <imranyounus@gmail.com>
2017-01-17 17:38:21 -0800

    adding python3 style div of scalar by matrix

--- src/main/python/systemml/defmatrix.py
+++ src/main/python/systemml/defmatrix.py
@@ -894,6 +894,12 @@ class matrix(object):
         """
         return binary_op(self, other, ' / ')
 
+    def __rtruediv__(self, other):
+        """
+        Performs division (Python 3 way).
+        """
+        return binary_op(other, self, ' / ')
+
     def __mod__(self, other):
         return binary_op(self, other, ' % ')
 

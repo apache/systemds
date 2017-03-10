b945543d933c37ad92adc04ccbfa51410434e777
Matthias Boehm <mboehm7@gmail.com>
2017-03-09 17:58:44 -0800

    [SYSTEMML-1386] Fix execution context creation w/ forced singlenode

--- src/main/java/org/apache/sysml/runtime/controlprogram/context/ExecutionContextFactory.java
+++ src/main/java/org/apache/sysml/runtime/controlprogram/context/ExecutionContextFactory.java
@@ -20,6 +20,8 @@
 package org.apache.sysml.runtime.controlprogram.context;
 
 import org.apache.sysml.api.DMLScript;
+import org.apache.sysml.api.DMLScript.RUNTIME_PLATFORM;
+import org.apache.sysml.hops.OptimizerUtils;
 import org.apache.sysml.runtime.controlprogram.Program;
 
 public class ExecutionContextFactory 
@@ -41,6 +43,14 @@ public class ExecutionContextFactory
 		switch( DMLScript.rtplatform )
 		{
 			case SINGLE_NODE:
+				//NOTE: even in case of forced singlenode operations, users might still 
+				//want to run remote parfor which requires the correct execution context
+				if( OptimizerUtils.getDefaultExecutionMode()==RUNTIME_PLATFORM.HYBRID)
+					ec = new ExecutionContext(allocateVars, prog);
+				else
+					ec = new SparkExecutionContext(allocateVars, prog);
+				break;
+				
 			case HADOOP:
 			case HYBRID:
 				ec = new ExecutionContext(allocateVars, prog);

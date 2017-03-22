97da0004f1423d40d63372e59c0424d28793ef92
Nakul Jindal <nakul02@gmail.com>
2017-03-22 12:08:21 -0700

    [HOTFIX] Changed unit test LRUCacheMapTest to run only with mvn verify
    Closes #436

--- src/test/java/org/apache/sysml/test/unit/LRUCacheMapTest.java
+++ src/test/java/org/apache/sysml/test/unit/LRUCacheMapTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sysml.test.utils;
+package org.apache.sysml.test.unit;
 
 import org.apache.sysml.utils.LRUCacheMap;
 import org.junit.Assert;

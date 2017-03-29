59c23e05a221b4810b996d617ec15f33d8d9c54c
Niketan Pansare <npansar@us.ibm.com>
2017-03-28 23:14:32 -0700

    comment mlcontextutil

--- src/main/java/org/apache/sysml/api/mlcontext/MLContextUtil.java
+++ src/main/java/org/apache/sysml/api/mlcontext/MLContextUtil.java
@@ -889,11 +889,12 @@ public final class MLContextUtil {
 				}
 				sb.append(") ");
 
-				sb.append(key);
-				sb.append(": ");
-				String str = object.toString();
-				str = StringUtils.abbreviate(str, 100);
-				sb.append(str);
+				// Can cause java.lang.OutOfMemoryError: Java heap space at org.apache.sysml.runtime.matrix.data.MatrixBlock.toString(MatrixBlock.java:5804)
+				// sb.append(key);
+				// sb.append(": ");
+				// String str = object.toString();
+				// str = StringUtils.abbreviate(str, 100);
+				// sb.append(str);
 				sb.append("\n");
 			}
 		}

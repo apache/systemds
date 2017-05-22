4d40e620d5b4c77ad56a9bb3a2b2d420718c8450
Nakul Jindal <nakul02@gmail.com>
2017-05-22 11:46:29 -0700

    Refactored into functions for reuse

--- src/test/java/org/apache/sysml/test/gpu/LibMatrixCUDATest.java
+++ src/test/java/org/apache/sysml/test/gpu/LibMatrixCUDATest.java
@@ -1,87 +1,137 @@
 package org.apache.sysml.test.gpu;
 
-import org.apache.commons.math3.util.FastMath;
 import org.apache.spark.sql.SparkSession;
-import org.apache.sysml.api.DMLScript;
 import org.apache.sysml.api.mlcontext.MLContext;
 import org.apache.sysml.api.mlcontext.Matrix;
-import org.apache.sysml.api.mlcontext.MatrixMetadata;
 import org.apache.sysml.api.mlcontext.Script;
 import org.apache.sysml.api.mlcontext.ScriptFactory;
-import org.apache.sysml.parser.Expression;
-import org.apache.sysml.runtime.controlprogram.caching.MatrixObject;
-import org.apache.sysml.runtime.controlprogram.context.ExecutionContext;
-import org.apache.sysml.runtime.controlprogram.context.ExecutionContextFactory;
-import org.apache.sysml.runtime.instructions.cp.BooleanObject;
-import org.apache.sysml.runtime.instructions.gpu.context.GPUContext;
-import org.apache.sysml.runtime.instructions.gpu.context.GPUContextPool;
-import org.apache.sysml.runtime.matrix.MatrixCharacteristics;
-import org.apache.sysml.runtime.matrix.MatrixFormatMetaData;
-import org.apache.sysml.runtime.matrix.MetaData;
-import org.apache.sysml.runtime.matrix.data.LibMatrixCUDA;
-import org.apache.sysml.runtime.matrix.data.MatrixBlock;
-import org.apache.sysml.runtime.matrix.data.OutputInfo;
-import org.apache.sysml.runtime.matrix.data.SparseBlock;
 import org.apache.sysml.test.integration.AutomatedTestBase;
 import org.apache.sysml.test.utils.TestUtils;
+import org.apache.sysml.utils.Statistics;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.Random;
-
-import static org.junit.Assert.*;
+import java.util.Set;
 
 /**
  * Unit tests for GPU methods
  */
 public class LibMatrixCUDATest extends AutomatedTestBase {
 
-    private final static String TEST_DIR = "org/apache/sysml/api/mlcontext";
-    private final static String TEST_NAME = "LibMatrixCUDATest";
+	private final static String TEST_DIR = "org/apache/sysml/api/mlcontext";
+	private final static String TEST_NAME = "LibMatrixCUDATest";
 
-    @Override
-    public void setUp() {
-        TestUtils.clearAssertionInformation();
-        addTestConfiguration(TEST_DIR, TEST_NAME);
-        getAndLoadTestConfiguration(TEST_NAME);
-    }
+	private final double THRESHOLD = 1e-9;
 
-	@Test
-	public void test1() throws Exception {
-        SparkSession spark = createSystemMLSparkSession("LibMatrixCUDATest", "local");
+	@Override public void setUp() {
+		TestUtils.clearAssertionInformation();
+		addTestConfiguration(TEST_DIR, TEST_NAME);
+		getAndLoadTestConfiguration(TEST_NAME);
+	}
 
-        MLContext cpuMLC = new MLContext(spark);
-        Script generateScript = ScriptFactory.dmlFromString("A = rand(rows=100, cols=100)").out("A");
-        Matrix A = cpuMLC.execute(generateScript).getMatrix("A");
+	@Test public void testSin() throws Exception {
+		SparkSession spark = createSystemMLSparkSession("LibMatrixCUDATest", "local");
 
-        Script sinScript = ScriptFactory.dmlFromString("B = sin(A)").in("A", A).out("B");
-        Matrix B = cpuMLC.execute(sinScript).getMatrix("B");
-        cpuMLC.close();
-        spark.stop();
+		String function = "sin";
+		String heavyHitterOpCode = "gpu_sin";
 
-        MLContext gpuMLC = new MLContext(spark);
-        gpuMLC.setGPU(true);
-        gpuMLC.setForceGPU(true);
-        gpuMLC.setStatistics(true);
-        sinScript = ScriptFactory.dmlFromString("B = sin(A)").in("A", A).out("B");
-        Matrix BGpu = gpuMLC.execute(sinScript).getMatrix("B");
+		int m = 100;
+		int n = 100;
+		double sparsity = 1.0;
 
-        double[][] b = B.to2DDoubleArray();
-        double[][] bgpu = BGpu.to2DDoubleArray();
+		Matrix in1 = generateInputMatrix(spark, m, n, sparsity);
+		Matrix outCPU = runUnaryOpOnCPU(spark, function, in1);
+		assertHeavyHitterPresent(heavyHitterOpCode);
+		Matrix outGPU = runUnaryOpOnGPU(spark, function, in1);
+		assertEqualMatrices(outCPU, outGPU);
 
-        for (int i = 0; i < b.length; i++) {
-            Assert.assertArrayEquals(b[i], bgpu[i], 1e-9);
-        }
-        gpuMLC.close();
-        spark.stop();
+		spark.stop();
 
 	}
 
-    @After
-    public void tearDown() {
-        super.tearDown();
-    }
+	/**
+	 * asserts that the expected op was executed
+	 *
+	 * @param heavyHitterOpCode opcode of the heavy hitter for the unary op
+	 */
+	private void assertHeavyHitterPresent(String heavyHitterOpCode) {
+		Set<String> heavyHitterOpCodes = Statistics.getCPHeavyHitterOpCodes();
+		Assert.assertTrue(heavyHitterOpCodes.contains(heavyHitterOpCode));
+	}
+
+	/**
+	 * Asserts that the values in two matrices are in {@link LibMatrixCUDATest#THRESHOLD} of each other
+	 *
+	 * @param expected expected matrix
+	 * @param actual   actual matrix
+	 */
+	private void assertEqualMatrices(Matrix expected, Matrix actual) {
+		double[][] expected2D = expected.to2DDoubleArray();
+		double[][] actual2D = actual.to2DDoubleArray();
+		for (int i = 0; i < expected2D.length; i++) {
+			Assert.assertArrayEquals(expected2D[i], actual2D[i], THRESHOLD);
+		}
+	}
+
+	/**
+	 * Runs the program for a unary op on the GPU
+	 *
+	 * @param spark    valid instance of {@link SparkSession}
+	 * @param function dml builtin function string of the unary op
+	 * @param in1      input matrix
+	 * @return output matrix from running the unary op on the input matrix (on GPU)
+	 */
+	private Matrix runUnaryOpOnGPU(SparkSession spark, String function, Matrix in1) {
+		String scriptStr2 = "out = " + function + "(in1)";
+		MLContext gpuMLC = new MLContext(spark);
+		gpuMLC.setGPU(true);
+		gpuMLC.setForceGPU(true);
+		gpuMLC.setStatistics(true);
+		Script sinScript2 = ScriptFactory.dmlFromString(scriptStr2).in("in1", in1).out("out");
+		Matrix outGPU = gpuMLC.execute(sinScript2).getMatrix("out");
+		gpuMLC.close();
+		return outGPU;
+	}
+
+	/**
+	 * Runs the program for a unary op on the cpu
+	 *
+	 * @param spark    valid instance of {@link SparkSession}
+	 * @param function dml builtin function string of the unary op
+	 * @param in1      input matrix
+	 * @return output matrix from running the unary op on the input matrix
+	 */
+	private Matrix runUnaryOpOnCPU(SparkSession spark, String function, Matrix in1) {
+		String scriptStr1 = "out = " + function + "(in1)";
+		MLContext cpuMLC = new MLContext(spark);
+		Script sinScript = ScriptFactory.dmlFromString(scriptStr1).in("in1", in1).out("out");
+		Matrix outCPU = cpuMLC.execute(sinScript).getMatrix("out");
+		cpuMLC.close();
+		return outCPU;
+	}
+
+	/**
+	 * Generates a random input matrix with a given size and sparsity
+	 *
+	 * @param spark    valid instance of {@link SparkSession}
+	 * @param m        number of rows
+	 * @param n        number of columns
+	 * @param sparsity sparsity (1 = completely dense, 0 = completely sparse)
+	 * @return a random matrix with given size and sparsity
+	 */
+	private Matrix generateInputMatrix(SparkSession spark, int m, int n, double sparsity) {
+		// Generate a random matrix of size m * n
+		MLContext genMLC = new MLContext(spark);
+		Script generateScript = ScriptFactory
+				.dmlFromString("in1 = rand(rows=" + m + ", cols=" + n + ", sparsity = " + sparsity + ")").out("in1");
+		Matrix in1 = genMLC.execute(generateScript).getMatrix("in1");
+		genMLC.close();
+		return in1;
+	}
+
+	@After public void tearDown() {
+		super.tearDown();
+	}
 
 }

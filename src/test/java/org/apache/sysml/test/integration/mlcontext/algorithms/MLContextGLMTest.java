b0ee1922e596b7a0772031b36404ecc06cf8ba85
Janardhan <pulivarthi_1140396@nitkkr.ac.in>
2017-07-22 13:48:56 +0530

    sample script at first to test my understanding

---
+++ src/test/java/org/apache/sysml/test/integration/mlcontext/algorithms/MLContextGLMTest.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysml.test.integration.mlcontext.algorithms;
+
+import static org.apache.sysml.api.mlcontext.ScriptFactory.dmlFromFile;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.apache.log4j.Logger;
+import org.apache.sysml.api.mlcontext.Script;
+import org.apache.sysml.test.integration.mlcontext.MLContextTestBase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MLContextGLMTest extends MLContextTestBase {
+        protected static Logger log = logger.getLogger(MLContextGLMTest.class);
+        
+        protected final static String TEST_SCRIPT = "scripts/algorithms/GLM.dml";
+        
+        @Test 
+        public void testGLMPoissonwithRandomMatrixSparse() {
+                runGLMTestMLC(GLMType.POISSON_LOG, MatrixType.RandomMatrix, true);
+                                
+        }
+        
+        @Test
+        public void testGLMPoissonwithRandomMatrixDense() {
+                runGLMTestMLC(GLMType.POISSON_LOG, MatrixType.RandomMatrix, false);
+        }
+        
+        @Test 
+        public void testGLMGammawithRandomMatrixSparse() {
+                runGLMTestMLC(GLMType.GAMMA_LOG, MatrixType.RandomMatrix, true);
+        }
+        
+        @Test 
+        public void testGLMGammawithRandomMatrixDense() {
+                runGLMTestMLC(GLMType.GAMMA_LOG, MatrixType.RandomMatrix, false);
+        }
+               
+        @Test
+        public void testGLMBinomialwithRandomMatrixSparse() {
+                runGLMTestMLC(GLMType.BINOMIAL_PROBIT, MatrixType.RandomMatrix, true);
+        }
+        
+        @Test
+        public void testGLMBinomialwithRandomMatrixDense() {
+                runGLMTestMLC(GLMType.BINOMIAL_PROBIT, MatrixType.RandomMatrix, false);
+        }
+        
+        private void runGLMTestMLC( GLMType type, MatrixType MType, boolean sparse) {
+                String[] addArgs = new String[4];
+		String param4Name = "lpow=";
+                
+                if (sparse) { 
+                       sparsity = 0.1;
+                } else {
+                       sparsity = 0.7;
+                }
+                                        
+                switch(MType) { 
+                        case RandomMatrix:
+                                double[][] X = getRandomMatrix(10, 3, 0, 1, sparsity, -1);
+                                double[][] Y = TestUtils.round(getRandomMatrix(10, 1, 0, 1, sparsity, -1);
+                                break;
+                }
+                
+		switch(type) {
+			case POISSON_LOG: //dfam, vpow, link, lpow
+				addArgs[0] = "1"; addArgs[1] = "1.0"; addArgs[2] = "1"; addArgs[3] = "0.0";
+				break;
+			case GAMMA_LOG:   //dfam, vpow, link, lpow
+				addArgs[0] = "1"; addArgs[1] = "2.0"; addArgs[2] = "1"; addArgs[3] = "0.0";
+				break;
+			case BINOMIAL_PROBIT: //dfam, vpow, link, yneg 
+				addArgs[0] = "2"; addArgs[1] = "0.0"; addArgs[2] = "3"; addArgs[3] = "2";
+				param4Name = "yneg=";
+				break;
+		}
+                
+                Script glm = dmlFromFile(TEST_SCRIPT);
+                glm.in("X", X).in("Y", Y).in("dfam", addArgs[0]).in("vpow", addArgs[1]).in("link", addArgs[2]).in(param4Name, addArgs[3]).in("icpt", "0").in("tol", "0.000000001").in("moi", "5").out("B");
+                ml.execute(glm);
+        }
+                       
+}

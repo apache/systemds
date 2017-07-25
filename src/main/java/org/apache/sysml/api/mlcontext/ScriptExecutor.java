037f903d2daca7c499b6ef044ce5356c40737c65
Deron Eriksson <deron@apache.org>
2017-07-25 16:43:00 -0700

    [SYSTEMML-1804] Compile time for statistics via MLContext
    If statistics is on via the MLContext API, compute and display the
    program compile-time.

--- src/main/java/org/apache/sysml/api/mlcontext/ScriptExecutor.java
+++ src/main/java/org/apache/sysml/api/mlcontext/ScriptExecutor.java
@@ -182,10 +182,8 @@ public class ScriptExecutor {
 			return;
 
 		try {
-			ExplainType explainType = (explainLevel != null) ? 
-				explainLevel.getExplainType() : ExplainType.RUNTIME;
-			System.out.println(Explain.display(
-				dmlProgram, runtimeProgram, explainType, null));
+			ExplainType explainType = (explainLevel != null) ? explainLevel.getExplainType() : ExplainType.RUNTIME;
+			System.out.println(Explain.display(dmlProgram, runtimeProgram, explainType, null));
 		} catch (Exception e) {
 			throw new MLContextException("Exception occurred while explaining dml program", e);
 		}
@@ -294,6 +292,9 @@ public class ScriptExecutor {
 
 		// main steps in script execution
 		setup(script);
+		if (statistics) {
+			Statistics.startCompileTimer();
+		}
 		parseScript();
 		liveVariableAnalysis();
 		validateScript();
@@ -307,6 +308,9 @@ public class ScriptExecutor {
 		countCompiledMRJobsAndSparkInstructions();
 		initializeCachingAndScratchSpace();
 		cleanupRuntimeProgram();
+		if (statistics) {
+			Statistics.stopCompileTimer();
+		}
 
 		try {
 			createAndInitializeExecutionContext();
@@ -338,9 +342,9 @@ public class ScriptExecutor {
 		// Set global variable indicating the script type
 		DMLScript.SCRIPT_TYPE = script.getScriptType();
 		setGlobalFlags();
-		//reset all relevant summary statistics 
+		// reset all relevant summary statistics
 		Statistics.resetNoOfExecutedJobs();
-		if( statistics ) {
+		if (statistics) {
 			CacheStatistics.reset();
 			Statistics.reset();
 		}

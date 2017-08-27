bf515a1ddf7f4143b5e23051c2324ed9b446ab57
Niketan Pansare <npansar@us.ibm.com>
2017-08-27 07:56:45 -0700

    improved performance of sparse im2col

--- src/main/java/org/apache/sysml/runtime/matrix/data/LibMatrixDNNIm2ColHelper.java
+++ src/main/java/org/apache/sysml/runtime/matrix/data/LibMatrixDNNIm2ColHelper.java
@@ -22,7 +22,6 @@ import java.util.Arrays;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.sysml.runtime.DMLRuntimeException;
 
 /**
  * This class contains the different implementation of im2col operation
@@ -48,7 +47,8 @@ public class LibMatrixDNNIm2ColHelper {
 				}
 				else {
 					if(LOG.isTraceEnabled()) LOG.trace("Using SparseIm2colWorkerAllChannels operator to perform im2col.");
-					im2ColOutBlock.reset(im2ColOutBlock.getNumRows(), im2ColOutBlock.getNumColumns(), true);
+					long worstCaseNNZ = params.R*params.S*input.getNonZeros();
+					im2ColOutBlock.reset(im2ColOutBlock.getNumRows(), im2ColOutBlock.getNumColumns(), true, worstCaseNNZ);
 					im2ColOutBlock.allocateSparseRowsBlock();
 					return new SparseIm2colWorkerAllChannels(input, im2ColOutBlock, params);
 				}
@@ -68,7 +68,8 @@ public class LibMatrixDNNIm2ColHelper {
 				}
 				else {
 					if(LOG.isTraceEnabled()) LOG.trace("Using SparseIm2colWorker operator to perform im2col.");
-					im2ColOutBlock.reset(im2ColOutBlock.getNumRows(), im2ColOutBlock.getNumColumns(), true);
+					long worstCaseNNZ = params.R*params.S*input.getNonZeros();
+					im2ColOutBlock.reset(im2ColOutBlock.getNumRows(), im2ColOutBlock.getNumColumns(), true, worstCaseNNZ);
 					im2ColOutBlock.allocateSparseRowsBlock();
 					return new SparseIm2colWorker(input, im2ColOutBlock, params);
 				}
@@ -263,7 +264,7 @@ public class LibMatrixDNNIm2ColHelper {
 	 * Performing sparse im2col for all channels for a given row n of the input matrix.
 	 */
 	static class SparseIm2colWorkerAllChannels implements Im2colWorker {
-		MatrixBlock input;  MatrixBlock output;
+		MatrixBlock input;  MatrixBlock output; Im2colSparseMetadata meta;
 		int CRS; int S; int R; int P; int Q; int H; int W; int RS; int HW;
 		int stride_h; int stride_w; int pad_h; int pad_w;
 		public SparseIm2colWorkerAllChannels(MatrixBlock input, MatrixBlock im2ColOutBlock, ConvolutionParameters params) {
@@ -275,6 +276,7 @@ public class LibMatrixDNNIm2ColHelper {
 			this.H = params.H; this.W = params.W; this.R = params.R; this.S = params.S; this.P = params.P; this.Q = params.Q;
 			this.stride_h = params.stride_h; this.stride_w = params.stride_w;
 			this.pad_h = params.pad_h; this.pad_w = params.pad_w;
+			meta = new Im2colSparseMetadata(CRS);
 			if(!input.isInSparseFormat()) 
 				throw new RuntimeException("Incorrect operator selection. Expected dense input for SparseIm2colWorkerAllChannels");
 		}
@@ -287,6 +289,7 @@ public class LibMatrixDNNIm2ColHelper {
 		@Override
 		public void execute(int n) {
 			if( !input.sparseBlock.isEmpty(n) ) {
+				meta.reset();
 				output.reset(output.getNumRows(), output.getNumColumns(), true, 0);
 				int apos = input.sparseBlock.pos(n);
 				int alen = input.sparseBlock.size(n);
@@ -304,19 +307,33 @@ public class LibMatrixDNNIm2ColHelper {
 					int wInput = chw % W; 
 					
 					appendInputValueToIm2colOutput(output, cInput, hInput, wInput, avals[j], 
-							R, S, P, Q, stride_h, stride_w, pad_h, pad_w);
+							R, S, P, Q, stride_h, stride_w, pad_h, pad_w, meta);
 				}
-				output.sortSparseRows();
+				if(meta.sortRows)
+					output.sortSparseRows();
+				output.setNonZeros(meta.nnz);
 			}
-			else
+			else {
 				output.reset(output.getNumRows(), output.getNumColumns(), true, 0);
-			
-			try {
-				output.recomputeNonZeros();
-				output.examSparsity();
-			} catch (DMLRuntimeException e) {
-				throw new RuntimeException(e);
+				output.setNonZeros(0);
 			}
+		}
+	}
+	
+	/**
+	 * This class stores metadata for maintaining sparse output of im2col.
+	 */
+	private static class Im2colSparseMetadata {
+		public int [] lastIndexPerRow;
+		public int nnz;
+		public boolean sortRows;
+		public Im2colSparseMetadata(int numRows) {
+			lastIndexPerRow = new int[numRows];
+		}
+		public void reset() {
+			Arrays.fill(lastIndexPerRow, 0);
+			sortRows = false;
+			nnz = 0;
 		}
 	}
 	
@@ -336,9 +353,12 @@ public class LibMatrixDNNIm2ColHelper {
 	 * @param stride_w stride width
 	 * @param pad_h pad height
 	 * @param pad_w pad width
+	 * @param meta computes meta information for the given sparse block
 	 */
 	private static void appendInputValueToIm2colOutput(MatrixBlock output, int cInput, int hInput, int wInput, double value, 
-			int R, int S, int P, int Q, int stride_h, int stride_w, int pad_h, int pad_w) {
+			int R, int S, int P, int Q, int stride_h, int stride_w, int pad_h, int pad_w, Im2colSparseMetadata meta) {
+		if(value == 0) 
+			return;
 		int RS = R*S;
 		// For the given h,w index, insert avals[j] into respective r,s,p,q locations
 		for(int r = 0; r < R; r++) {
@@ -352,6 +372,10 @@ public class LibMatrixDNNIm2ColHelper {
 					if(0 <= q && q < Q && (wInput - s + pad_w) % stride_w == 0) {
 						// chw -> [crs, pq]
 						output.appendValue(outRowIndex, p*Q + q, value);
+						meta.nnz++;
+						if(meta.lastIndexPerRow[outRowIndex] > p*Q + q)
+							meta.sortRows = true;
+						meta.lastIndexPerRow[outRowIndex] = p*Q + q;
 					}
 				}
 			}
@@ -362,7 +386,7 @@ public class LibMatrixDNNIm2ColHelper {
 	 * Performing sparse im2col for a given channel c and for a given row n of the input matrix.
 	 */
 	static class SparseIm2colWorker implements Im2colWorker {
-		MatrixBlock input; MatrixBlock output;
+		MatrixBlock input; MatrixBlock output; Im2colSparseMetadata meta;
 		int CRS; int S; int R; int P; int Q; int H; int W; int HW; int RS;
 		int stride_h; int stride_w; int pad_h; int pad_w; double [] temp;
 		public SparseIm2colWorker(MatrixBlock input, MatrixBlock im2ColOutBlock, ConvolutionParameters params) {
@@ -375,6 +399,7 @@ public class LibMatrixDNNIm2ColHelper {
 			this.stride_h = params.stride_h; this.stride_w = params.stride_w;
 			this.pad_h = params.pad_h; this.pad_w = params.pad_w;
 			temp = new double[input.getNumColumns()];
+			meta = new Im2colSparseMetadata(CRS);
 		}
 		
 		@Override
@@ -385,6 +410,7 @@ public class LibMatrixDNNIm2ColHelper {
 		@Override
 		public void execute(int n, int cInput) {
 			if( !input.sparseBlock.isEmpty(n) ) {
+				meta.reset();
 				output.reset(output.getNumRows(), output.getNumColumns(), true, 0);
 				int apos = input.sparseBlock.pos(n);
 				int alen = input.sparseBlock.size(n);
@@ -402,19 +428,16 @@ public class LibMatrixDNNIm2ColHelper {
 						int wInput = chw % W; 
 						
 						appendInputValueToIm2colOutput(output, cInput, hInput, wInput, avals[j], 
-								R, S, P, Q, stride_h, stride_w, pad_h, pad_w);
+								R, S, P, Q, stride_h, stride_w, pad_h, pad_w, meta);
 					}
 				}
-				output.sortSparseRows();
+				if(meta.sortRows)
+					output.sortSparseRows();
+				output.setNonZeros(meta.nnz);
 			}
-			else
+			else {
 				output.reset(output.getNumRows(), output.getNumColumns(), true, 0);
-			
-			try {
-				output.recomputeNonZeros();
-				output.examSparsity();
-			} catch (DMLRuntimeException e) {
-				throw new RuntimeException(e);
+				output.setNonZeros(0);
 			}
 		}
 		

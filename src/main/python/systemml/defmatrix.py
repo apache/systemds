e93c6562efada84ac2f8c9d3b47bd06c7c36f7a4
Janardhan <pulivarthi_1140396@nitkkr.ac.in>
2017-09-07 12:17:21 -0400

    change `^` to `xor(a, b)`

--- src/main/python/systemml/defmatrix.py
+++ src/main/python/systemml/defmatrix.py
@@ -350,7 +350,7 @@ class matrix(object):
     
     1. Arithmetic operators: +, -, *, /, //, %, ** as well as dot (i.e. matrix multiplication)
     2. Indexing in the matrix
-    3. Relational/Boolean operators: <, <=, >, >=, ==, !=, &, |, ^
+    3. Relational/Boolean operators: <, <=, >, >=, ==, !=, &, |, xor
     
     In addition, following functions are supported for matrix:
     
@@ -968,7 +968,7 @@ class matrix(object):
         return binary_op(other, self, ' | ')
     
      def __xor__(self, other):
-        return binary_op(other, self, ' ^ ')
+        return binary_op(other, self, ' xor ')
 
     def logical_not(self):
         inputs = []

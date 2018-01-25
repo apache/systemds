52aae222c688859de18d84fd92ae7171dec5dae0
Matthias Boehm <mboehm7@gmail.com>
2018-01-24 18:47:04 -0800

    [SYSTEMML-2075] Fix invalid constant folding of matrix-scalar and/or
    This patch fixes an issue of invalid constant folding of matrix-scalar
    logical operations, which occured due to the recently added support for
    elementwise logical operations.

--- src/main/java/org/apache/sysml/hops/rewrite/RewriteConstantFolding.java
+++ src/main/java/org/apache/sysml/hops/rewrite/RewriteConstantFolding.java
@@ -268,14 +268,14 @@ public class RewriteConstantFolding extends HopRewriteRule
 	
 	private static boolean isApplicableFalseConjunctivePredicate( Hop hop ) throws HopsException {
 		ArrayList<Hop> in = hop.getInput();
-		return (   HopRewriteUtils.isBinary(hop, OpOp2.AND) 
+		return (   HopRewriteUtils.isBinary(hop, OpOp2.AND) && hop.getDataType().isScalar()
 				&& ( (in.get(0) instanceof LiteralOp && !((LiteralOp)in.get(0)).getBooleanValue())
 				   ||(in.get(1) instanceof LiteralOp && !((LiteralOp)in.get(1)).getBooleanValue())) );
 	}
 	
 	private static boolean isApplicableTrueDisjunctivePredicate( Hop hop ) throws HopsException {
 		ArrayList<Hop> in = hop.getInput();
-		return (   HopRewriteUtils.isBinary(hop, OpOp2.OR)
+		return (   HopRewriteUtils.isBinary(hop, OpOp2.OR) && hop.getDataType().isScalar()
 				&& ( (in.get(0) instanceof LiteralOp && ((LiteralOp)in.get(0)).getBooleanValue())
 				   ||(in.get(1) instanceof LiteralOp && ((LiteralOp)in.get(1)).getBooleanValue())) );
 	}

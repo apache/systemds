17eab07bf9a4980989d951fd8ee6b5467910009e
EdgarLGB <guobao.li@atos.net>
2018-05-25 18:24:43 +0200

    [MINOR] Clean up some code

--- src/main/java/org/apache/sysml/runtime/instructions/cp/ParamservBuiltinCPInstruction.java
+++ src/main/java/org/apache/sysml/runtime/instructions/cp/ParamservBuiltinCPInstruction.java
@@ -19,6 +19,24 @@
 
 package org.apache.sysml.runtime.instructions.cp;
 
+import static org.apache.sysml.parser.Statement.PSFrequency;
+import static org.apache.sysml.parser.Statement.PSModeType;
+import static org.apache.sysml.parser.Statement.PSScheme;
+import static org.apache.sysml.parser.Statement.PS_AGGREGATION_FUN;
+import static org.apache.sysml.parser.Statement.PS_BATCH_SIZE;
+import static org.apache.sysml.parser.Statement.PS_EPOCHS;
+import static org.apache.sysml.parser.Statement.PS_FEATURES;
+import static org.apache.sysml.parser.Statement.PS_FREQUENCY;
+import static org.apache.sysml.parser.Statement.PS_HYPER_PARAMS;
+import static org.apache.sysml.parser.Statement.PS_LABELS;
+import static org.apache.sysml.parser.Statement.PS_MODE;
+import static org.apache.sysml.parser.Statement.PS_MODEL;
+import static org.apache.sysml.parser.Statement.PS_PARALLELISM;
+import static org.apache.sysml.parser.Statement.PS_SCHEME;
+import static org.apache.sysml.parser.Statement.PS_UPDATE_FUN;
+import static org.apache.sysml.parser.Statement.PS_VAL_FEATURES;
+import static org.apache.sysml.parser.Statement.PS_VAL_LABELS;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -26,7 +44,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.apache.sysml.parser.Statement;
 import org.apache.sysml.runtime.DMLRuntimeException;
 import org.apache.sysml.runtime.controlprogram.caching.MatrixObject;
 import org.apache.sysml.runtime.controlprogram.context.ExecutionContext;
@@ -43,9 +60,17 @@ import org.apache.sysml.utils.NativeHelper;
 public class ParamservBuiltinCPInstruction extends ParameterizedBuiltinCPInstruction {
 
 	private static final int DEFAULT_BATCH_SIZE = 64;
-	private static final Statement.PSFrequency DEFAULT_UPDATE_FREQUENCY = Statement.PSFrequency.BATCH;
+	private static final PSFrequency DEFAULT_UPDATE_FREQUENCY = PSFrequency.BATCH;
 	private static final int DEFAULT_LEVEL_PARALLELISM = InfrastructureAnalyzer.getLocalParallelism();
-	private static final Statement.PSScheme DEFAULT_SCHEME = Statement.PSScheme.DISJOINT_CONTIGUOUS;
+	private static final PSScheme DEFAULT_SCHEME = PSScheme.DISJOINT_CONTIGUOUS;
+
+	private int _workerNum;
+	private PSModeType _mode;
+	private String _updFunc;
+	private String _aggFunc;
+	private PSFrequency _freq;
+	private int _epochs;
+	private long _batchSize;
 
 	protected ParamservBuiltinCPInstruction(Operator op, LinkedHashMap<String, String> paramsMap, CPOperand out,
 			String opcode, String istr) {
@@ -54,40 +79,29 @@ public class ParamservBuiltinCPInstruction extends ParameterizedBuiltinCPInstruc
 
 	@Override
 	public void processInstruction(ExecutionContext ec) {
-		// Determine the number of workers
-		int workerNum = getWorkerNum();
+
+		init();
 
 		// Start the parameter server
 		ParamServer ps = createPS();
 
-		String aggFunc = getParam(Statement.PS_AGGREGATION_FUN);
-		ListObject globalParams = ec.getListObject(getParam(Statement.PS_MODEL));
-		ListObject hyperParams = getHyperParams(ec);
-
 		// Create aggregation service
-		LocalAggregationService aggService = new LocalAggregationService(aggFunc,
+		ListObject globalParams = ec.getListObject(getParam(PS_MODEL));
+		ListObject hyperParams = getHyperParams(ec);
+		LocalAggregationService aggService = new LocalAggregationService(_aggFunc,
 				ExecutionContextFactory.createContext(ec.getProgram()), ParamservUtils.copyList(globalParams),
-				ParamservUtils.copyList(hyperParams), ps, workerNum);
+				ParamservUtils.copyList(hyperParams), ps, _workerNum);
 
 		Thread aggThread = new Thread(aggService);
 
-		String updFunc = getParam(Statement.PS_UPDATE_FUN);
-		Statement.PSFrequency freq = getFrequency();
-		int epochs = Integer.valueOf(getParam(Statement.PS_EPOCHS));
-		if (epochs <= 0) {
-			throw new DMLRuntimeException(
-					String.format("Paramserv function: The argument '%s' could not be less than or equal to 0.",
-							Statement.PS_EPOCHS));
-		}
-		long batchSize = getBatchSize();
-
 		// Create the local workers
-		List<LocalPSWorker> workers = IntStream.range(0, workerNum).mapToObj(
-				i -> new LocalPSWorker((long) i, updFunc, freq, epochs, batchSize, ParamservUtils.copyList(hyperParams),
-						ExecutionContextFactory.createContext(ec.getProgram()), ps)).collect(Collectors.toList());
+		List<LocalPSWorker> workers = IntStream.range(0, _workerNum).mapToObj(
+				i -> new LocalPSWorker((long) i, _updFunc, _freq, _epochs, _batchSize,
+						ParamservUtils.copyList(hyperParams), ExecutionContextFactory.createContext(ec.getProgram()),
+						ps)).collect(Collectors.toList());
 
 		// Do data partition
-		doDataPartition(workerNum, ec, workers);
+		doDataPartition(ec, workers);
 
 		// Create the worker threads
 		List<Thread> threads = workers.stream().map(Thread::new).collect(Collectors.toList());
@@ -125,11 +139,26 @@ public class ParamservBuiltinCPInstruction extends ParameterizedBuiltinCPInstruc
 		ps.getParams().clear();
 	}
 
-	private Statement.PSFrequency getFrequency() {
-		if (!getParameterMap().containsKey(Statement.PS_FREQUENCY)) {
+	private void init() {
+		_mode = PSModeType.valueOf(getParam(PS_MODE));
+		_workerNum = getWorkerNum();
+		_updFunc = getParam(PS_UPDATE_FUN);
+		_aggFunc = getParam(PS_AGGREGATION_FUN);
+		_freq = getFrequency();
+		_epochs = Integer.valueOf(getParam(PS_EPOCHS));
+		if (_epochs <= 0) {
+			throw new DMLRuntimeException(
+					String.format("Paramserv function: The argument '%s' could not be less than or equal to 0.",
+							PS_EPOCHS));
+		}
+		_batchSize = getBatchSize();
+	}
+
+	private PSFrequency getFrequency() {
+		if (!getParameterMap().containsKey(PS_FREQUENCY)) {
 			return DEFAULT_UPDATE_FREQUENCY; // default updating frequency
 		}
-		return Statement.PSFrequency.valueOf(getParam(Statement.PS_FREQUENCY));
+		return PSFrequency.valueOf(getParam(PS_FREQUENCY));
 	}
 
 	/**
@@ -139,11 +168,10 @@ public class ParamservBuiltinCPInstruction extends ParameterizedBuiltinCPInstruc
 	 */
 	private int getWorkerNum() {
 		int workerNum = DEFAULT_LEVEL_PARALLELISM;
-		if (getParameterMap().containsKey(Statement.PS_PARALLELISM)) {
-			workerNum = Integer.valueOf(getParam(Statement.PS_PARALLELISM));
+		if (getParameterMap().containsKey(PS_PARALLELISM)) {
+			workerNum = Integer.valueOf(getParam(PS_PARALLELISM));
 		}
-		Statement.PSModeType mode = Statement.PSModeType.valueOf(getParam(Statement.PS_MODE));
-		switch (mode) {
+		switch (_mode) {
 		case LOCAL:
 			int vcores = InfrastructureAnalyzer.getLocalParallelism();
 			if ("openblas".equals(NativeHelper.getCurrentBLAS())) {
@@ -165,8 +193,7 @@ public class ParamservBuiltinCPInstruction extends ParameterizedBuiltinCPInstruc
 	 */
 	private ParamServer createPS() {
 		ParamServer ps = null;
-		Statement.PSModeType mode = Statement.PSModeType.valueOf(getParam(Statement.PS_MODE));
-		switch (mode) {
+		switch (_mode) {
 		case LOCAL:
 			ps = new LocalParamServer();
 			break;
@@ -177,38 +204,38 @@ public class ParamservBuiltinCPInstruction extends ParameterizedBuiltinCPInstruc
 	}
 
 	private long getBatchSize() {
-		if (!getParameterMap().containsKey(Statement.PS_BATCH_SIZE)) {
+		if (!getParameterMap().containsKey(PS_BATCH_SIZE)) {
 			return DEFAULT_BATCH_SIZE;
 		}
-		long batchSize = Integer.valueOf(getParam(Statement.PS_BATCH_SIZE));
+		long batchSize = Integer.valueOf(getParam(PS_BATCH_SIZE));
 		if (batchSize <= 0) {
 			throw new DMLRuntimeException(String.format(
 					"Paramserv function: the number of argument '%s' could not be less than or equal to 0.",
-					Statement.PS_BATCH_SIZE));
+					PS_BATCH_SIZE));
 		}
 		return batchSize;
 	}
 
 	private ListObject getHyperParams(ExecutionContext ec) {
 		ListObject hyperparams = new ListObject(Collections.emptyList());
-		if (getParameterMap().containsKey(Statement.PS_HYPER_PARAMS)) {
-			hyperparams = ec.getListObject(getParam(Statement.PS_HYPER_PARAMS));
+		if (getParameterMap().containsKey(PS_HYPER_PARAMS)) {
+			hyperparams = ec.getListObject(getParam(PS_HYPER_PARAMS));
 		}
 		return hyperparams;
 	}
 
-	private void doDataPartition(int workerNum, ExecutionContext ec, List<LocalPSWorker> workers) {
-		MatrixObject features = ec.getMatrixObject(getParam(Statement.PS_FEATURES));
-		MatrixObject labels = ec.getMatrixObject(getParam(Statement.PS_LABELS));
-		MatrixObject valFeatures = ec.getMatrixObject(getParam(Statement.PS_VAL_FEATURES));
-		MatrixObject valLabels = ec.getMatrixObject(getParam(Statement.PS_VAL_LABELS));
-		Statement.PSScheme scheme = DEFAULT_SCHEME;
-		if (getParameterMap().containsKey(Statement.PS_SCHEME)) {
-			scheme = Statement.PSScheme.valueOf(getParam(Statement.PS_SCHEME));
+	private void doDataPartition(ExecutionContext ec, List<LocalPSWorker> workers) {
+		MatrixObject features = ec.getMatrixObject(getParam(PS_FEATURES));
+		MatrixObject labels = ec.getMatrixObject(getParam(PS_LABELS));
+		MatrixObject valFeatures = ec.getMatrixObject(getParam(PS_VAL_FEATURES));
+		MatrixObject valLabels = ec.getMatrixObject(getParam(PS_VAL_LABELS));
+		PSScheme scheme = DEFAULT_SCHEME;
+		if (getParameterMap().containsKey(PS_SCHEME)) {
+			scheme = PSScheme.valueOf(getParam(PS_SCHEME));
 		}
 		switch (scheme) {
 		case DISJOINT_CONTIGUOUS:
-			disjointContiguous(workerNum, features, labels, valFeatures, valLabels, workers);
+			disjointContiguous(features, labels, valFeatures, valLabels, workers);
 			break;
 		case DISJOINT_RANDOM:
 		case OVERLAP_RESHUFFLE:
@@ -218,28 +245,28 @@ public class ParamservBuiltinCPInstruction extends ParameterizedBuiltinCPInstruc
 		}
 	}
 
-	private void disjointContiguous(int workerNum, MatrixObject features, MatrixObject labels, MatrixObject valFeatures,
+	private void disjointContiguous(MatrixObject features, MatrixObject labels, MatrixObject valFeatures,
 			MatrixObject valLabels, List<LocalPSWorker> workers) {
 		// training data
-		List<MatrixObject> pfs = disjointContiguous(workerNum, features);
-		List<MatrixObject> pls = disjointContiguous(workerNum, labels);
+		List<MatrixObject> pfs = disjointContiguous(features);
+		List<MatrixObject> pls = disjointContiguous(labels);
 		for (int i = 0; i < workers.size(); i++) {
 			workers.get(i).setFeatures(pfs.get(i));
 			workers.get(i).setLabels(pls.get(i));
 		}
 
 		// validation data
-		List<MatrixObject> pvfs = disjointContiguous(workerNum, valFeatures);
-		List<MatrixObject> pvls = disjointContiguous(workerNum, valLabels);
+		List<MatrixObject> pvfs = disjointContiguous(valFeatures);
+		List<MatrixObject> pvls = disjointContiguous(valLabels);
 		for (int i = 0; i < workers.size(); i++) {
 			workers.get(i).setValFeatures(pvfs.get(i));
 			workers.get(i).setValLabels(pvls.get(i));
 		}
 	}
 
-	private List<MatrixObject> disjointContiguous(int workerNum, MatrixObject mo) {
+	private List<MatrixObject> disjointContiguous(MatrixObject mo) {
 		List<MatrixObject> list = new ArrayList<>();
-		long stepSize = mo.getNumRows() / workerNum;
+		long stepSize = mo.getNumRows() / _workerNum;
 		long begin = 1;
 		while (begin < mo.getNumRows()) {
 			long end = begin + stepSize;

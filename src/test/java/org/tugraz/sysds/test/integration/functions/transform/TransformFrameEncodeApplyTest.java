cf45faf8a421b0a35e7939a8fa27d0cad40dbea2
Matthias Boehm <mboehm7@gmail.com>
2018-10-28 18:23:57 +0100

    Refactored test package names from org.apache.sysml to org.tugraz.sysds

---
+++ src/test/java/org/tugraz/sysds/test/integration/functions/transform/TransformFrameEncodeApplyTest.java
@@ -0,0 +1,314 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.tugraz.sysds.test.integration.functions.transform;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.tugraz.sysds.api.DMLScript;
+import org.tugraz.sysds.api.DMLScript.RUNTIME_PLATFORM;
+import org.tugraz.sysds.runtime.io.MatrixReaderFactory;
+import org.tugraz.sysds.runtime.matrix.data.InputInfo;
+import org.tugraz.sysds.runtime.util.DataConverter;
+import org.tugraz.sysds.test.integration.AutomatedTestBase;
+import org.tugraz.sysds.test.integration.TestConfiguration;
+import org.tugraz.sysds.test.utils.TestUtils;
+import org.tugraz.sysds.utils.Statistics;
+
+public class TransformFrameEncodeApplyTest extends AutomatedTestBase 
+{
+	private final static String TEST_NAME1 = "TransformFrameEncodeApply";
+	private final static String TEST_DIR = "functions/transform/";
+	private final static String TEST_CLASS_DIR = TEST_DIR + TransformFrameEncodeApplyTest.class.getSimpleName() + "/";
+	
+	//dataset and transform tasks without missing values
+	private final static String DATASET1 = "homes3/homes.csv";
+	private final static String SPEC1    = "homes3/homes.tfspec_recode.json"; 
+	private final static String SPEC1b   = "homes3/homes.tfspec_recode2.json"; 
+	private final static String SPEC2    = "homes3/homes.tfspec_dummy.json";
+	private final static String SPEC2b   = "homes3/homes.tfspec_dummy2.json";
+	private final static String SPEC3    = "homes3/homes.tfspec_bin.json"; //incl recode
+	private final static String SPEC3b   = "homes3/homes.tfspec_bin2.json"; //incl recode
+	private final static String SPEC6    = "homes3/homes.tfspec_recode_dummy.json"; 
+	private final static String SPEC6b   = "homes3/homes.tfspec_recode_dummy2.json"; 
+	
+	//dataset and transform tasks with missing values
+	private final static String DATASET2 = "homes/homes.csv";
+	private final static String SPEC4    = "homes3/homes.tfspec_impute.json";
+	private final static String SPEC4b   = "homes3/homes.tfspec_impute2.json";
+	private final static String SPEC5    = "homes3/homes.tfspec_omit.json";
+	private final static String SPEC5b   = "homes3/homes.tfspec_omit2.json";
+	
+	public enum TransformType {
+		RECODE,
+		DUMMY,
+		RECODE_DUMMY,
+		BIN,
+		IMPUTE,
+		OMIT,
+	}
+	
+	@Override
+	public void setUp()  {
+		TestUtils.clearAssertionInformation();
+		addTestConfiguration(TEST_NAME1, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME1, new String[] { "y" }) );
+	}
+	
+	@Test
+	public void testHomesRecodeIDsSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.RECODE, false);
+	}
+	
+	@Test
+	public void testHomesRecodeIDsSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.RECODE, false);
+	}
+	
+	@Test
+	public void testHomesRecodeIDsHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.RECODE, false);
+	}
+	
+	@Test
+	public void testHomesDummycodeIDsSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.DUMMY, false);
+	}
+	
+	@Test
+	public void testHomesDummycodeIDsSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.DUMMY, false);
+	}
+	
+	@Test
+	public void testHomesDummycodeIDsHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.DUMMY, false);
+	}
+	
+	@Test
+	public void testHomesRecodeDummycodeIDsSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.RECODE_DUMMY, false);
+	}
+	
+	@Test
+	public void testHomesRecodeDummycodeIDsSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.RECODE_DUMMY, false);
+	}
+	
+	@Test
+	public void testHomesRecodeDummycodeIDsHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.RECODE_DUMMY, false);
+	}
+	
+	@Test
+	public void testHomesBinningIDsSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.BIN, false);
+	}
+	
+	@Test
+	public void testHomesBinningIDsSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.BIN, false);
+	}
+	
+	@Test
+	public void testHomesBinningIDsHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.BIN, false);
+	}
+	
+	@Test
+	public void testHomesOmitIDsSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.OMIT, false);
+	}
+	
+	@Test
+	public void testHomesOmitIDsSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.OMIT, false);
+	}
+	
+	@Test
+	public void testHomesOmitIDsHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.OMIT, false);
+	}
+	
+	@Test
+	public void testHomesImputeIDsSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.IMPUTE, false);
+	}
+	
+	@Test
+	public void testHomesImputeIDsSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.IMPUTE, false);
+	}
+	
+	@Test
+	public void testHomesImputeIDsHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.IMPUTE, false);
+	}
+
+	@Test
+	public void testHomesRecodeColnamesSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.RECODE, true);
+	}
+	
+	@Test
+	public void testHomesRecodeColnamesSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.RECODE, true);
+	}
+	
+	@Test
+	public void testHomesRecodeColnamesHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.RECODE, true);
+	}
+	
+	@Test
+	public void testHomesDummycodeColnamesSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.DUMMY, true);
+	}
+	
+	@Test
+	public void testHomesDummycodeColnamesSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.DUMMY, true);
+	}
+	
+	@Test
+	public void testHomesDummycodeColnamesHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.DUMMY, true);
+	}
+	
+	@Test
+	public void testHomesRecodeDummycodeColnamesSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.RECODE_DUMMY, true);
+	}
+	
+	@Test
+	public void testHomesRecodeDummycodeColnamesSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.RECODE_DUMMY, true);
+	}
+	
+	@Test
+	public void testHomesRecodeDummycodeColnamesHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.RECODE_DUMMY, true);
+	}
+	
+	@Test
+	public void testHomesBinningColnamesSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.BIN, true);
+	}
+	
+	@Test
+	public void testHomesBinningColnamesSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.BIN, true);
+	}
+	
+	@Test
+	public void testHomesBinningColnamesHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.BIN, true);
+	}
+	
+	@Test
+	public void testHomesOmitColnamesSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.OMIT, true);
+	}
+	
+	@Test
+	public void testHomesOmitvColnamesSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.OMIT, true);
+	}
+	
+	@Test
+	public void testHomesOmitvColnamesHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.OMIT, true);
+	}
+	
+	@Test
+	public void testHomesImputeColnamesSingleNodeCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SINGLE_NODE, "csv", TransformType.IMPUTE, true);
+	}
+	
+	@Test
+	public void testHomesImputeColnamesSparkCSV() {
+		runTransformTest(RUNTIME_PLATFORM.SPARK, "csv", TransformType.IMPUTE, true);
+	}
+	
+	@Test
+	public void testHomesImputeColnamesHybridCSV() {
+		runTransformTest(RUNTIME_PLATFORM.HYBRID, "csv", TransformType.IMPUTE, true);
+	}
+	
+	private void runTransformTest( RUNTIME_PLATFORM rt, String ofmt, TransformType type, boolean colnames )
+	{
+		//set runtime platform
+		RUNTIME_PLATFORM rtold = rtplatform;
+		rtplatform = rt;
+
+		boolean sparkConfigOld = DMLScript.USE_LOCAL_SPARK_CONFIG;
+		if( rtplatform == RUNTIME_PLATFORM.SPARK || rtplatform == RUNTIME_PLATFORM.HYBRID)
+			DMLScript.USE_LOCAL_SPARK_CONFIG = true;
+
+		//set transform specification
+		String SPEC = null; String DATASET = null;
+		switch( type ) {
+			case RECODE: SPEC = colnames?SPEC1b:SPEC1; DATASET = DATASET1; break;
+			case DUMMY:  SPEC = colnames?SPEC2b:SPEC2; DATASET = DATASET1; break;
+			case BIN:    SPEC = colnames?SPEC3b:SPEC3; DATASET = DATASET1; break;
+			case IMPUTE: SPEC = colnames?SPEC4b:SPEC4; DATASET = DATASET2; break;
+			case OMIT:   SPEC = colnames?SPEC5b:SPEC5; DATASET = DATASET2; break;
+			case RECODE_DUMMY: SPEC = colnames?SPEC6b:SPEC6; DATASET = DATASET1; break;
+		}
+
+		if( !ofmt.equals("csv") )
+			throw new RuntimeException("Unsupported test output format");
+		
+		try
+		{
+			getAndLoadTestConfiguration(TEST_NAME1);
+			
+			String HOME = SCRIPT_DIR + TEST_DIR;
+			fullDMLScriptName = HOME + TEST_NAME1 + ".dml";
+			programArgs = new String[]{"-explain", "recompile_hops", "-nvargs", 
+				"DATA=" + HOME + "input/" + DATASET,
+				"TFSPEC=" + HOME + "input/" + SPEC,
+				"TFDATA1=" + output("tfout1"),
+				"TFDATA2=" + output("tfout2"),
+				"OFMT=" + ofmt };
+	
+			runTest(true, false, null, -1); 
+			
+			//read input/output and compare
+			double[][] R1 = DataConverter.convertToDoubleMatrix(MatrixReaderFactory
+				.createMatrixReader(InputInfo.CSVInputInfo)
+				.readMatrixFromHDFS(output("tfout1"), -1L, -1L, 1000, 1000, -1));
+			double[][] R2 = DataConverter.convertToDoubleMatrix(MatrixReaderFactory
+				.createMatrixReader(InputInfo.CSVInputInfo)
+				.readMatrixFromHDFS(output("tfout2"), -1L, -1L, 1000, 1000, -1));
+			TestUtils.compareMatrices(R1, R2, R1.length, R1[0].length, 0);		
+			
+			if( rt == RUNTIME_PLATFORM.HYBRID ) {
+				Assert.assertEquals("Wrong number of executed Spark instructions: " + 
+					Statistics.getNoOfExecutedSPInst(), new Long(2), new Long(Statistics.getNoOfExecutedSPInst()));
+			}
+		}
+		catch(Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		finally {
+			rtplatform = rtold;
+			DMLScript.USE_LOCAL_SPARK_CONFIG = sparkConfigOld;
+		}
+	}
+}

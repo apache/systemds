91d6c3394aab696f03f3c14b7734c939e45a8b87
Iulian Antonov <antonov.iulian@gmail.com>
2019-08-29 20:21:44 +0200

    [SYSTEMDS-115] Various cleanups of slicefinder builtin function
    fixed some bugs and added comments TODO
    
    Closes #33.

--- src/test/java/org/tugraz/sysds/test/functions/builtin/BuiltinSliceFinderTest.java
+++ src/test/java/org/tugraz/sysds/test/functions/builtin/BuiltinSliceFinderTest.java
@@ -17,15 +17,17 @@
 package org.tugraz.sysds.test.functions.builtin;
 
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Random;
+
 import org.junit.Test;
 import org.tugraz.sysds.common.Types.ExecMode;
 import org.tugraz.sysds.lops.LopProperties.ExecType;
+import org.tugraz.sysds.runtime.matrix.data.MatrixValue;
 import org.tugraz.sysds.test.AutomatedTestBase;
 import org.tugraz.sysds.test.TestConfiguration;
 import org.tugraz.sysds.test.TestUtils;
-
-//package io;
-import java.util.*;
 
 public class BuiltinSliceFinderTest extends AutomatedTestBase {
 
@@ -33,7 +35,7 @@ public class BuiltinSliceFinderTest extends AutomatedTestBase {
 	private final static String TEST_DIR = "functions/builtin/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinSliceFinderTest.class.getSimpleName() + "/";
 
-	private final static int rows = 32000;
+	private final static int rows = 2000;
 	private final static int cols = 10;
 
 	@Override
@@ -61,10 +63,11 @@ public class BuiltinSliceFinderTest extends AutomatedTestBase {
 		String dml_test_name = TEST_NAME;
 		loadTestConfiguration(getTestConfiguration(TEST_NAME));
 		String HOME = SCRIPT_DIR + TEST_DIR;
+
 		try {
 			loadTestConfiguration(getTestConfiguration(TEST_NAME));
 			fullDMLScriptName = HOME + dml_test_name + ".dml";
-			programArgs = new String[]{"-explain", "-args", input("AA"), input("B")};
+			programArgs = new String[]{"-explain", "-args", input("A"), input("B"), input("Y0"), output("C")};
 			double[][] A = TestUtils.ceil(getRandomMatrix(rows, cols, 0, 10, 1, 7));
 			double[][] B = TestUtils.ceil(getRandomMatrix(10, 1, 0, 10, 1.0, 3));
 			double[][] As = new double[rows][cols];
@@ -77,13 +80,18 @@ public class BuiltinSliceFinderTest extends AutomatedTestBase {
 					Y[i] += A[i][k] * B[k][0];
 
 			double AA[][] = new double[rows][cols+1];
+
+			int value0 = 7;
+			int value1 = 2;
+			int coll0 = 5;
+			int coll1 = 3;
 			
 			switch (test) {
 				case 1:
-					AA = modifyValue(A, Y,7,5);
+					AA = modifyValue(A, Y,value0,coll0);
 					break;
 				case 2:
-					AA = modifyValue(A, Y, 6, 3);
+					AA = modifyValue(A, Y, value0, coll0);
 					for(int i = 0;i<rows;i++){
 						for(int j = 0; j < cols+1;j++){
 							if(j == cols )
@@ -92,10 +100,10 @@ public class BuiltinSliceFinderTest extends AutomatedTestBase {
 								As[i][j] = AA[i][j];
 						}
 					}
-					AA = modifyValue(As,Ys,3,3);
+					AA = modifyValue(As,Ys,value1,coll0);
 					break;
 				case 3:
-					AA = modifyValue(A, Y, 6, 3);
+					AA = modifyValue(A, Y, value0, coll0);
 					for(int i = 0;i<rows;i++){
 						for(int j = 0; j < cols+1;j++){
 							if(j == cols ){
@@ -104,16 +112,38 @@ public class BuiltinSliceFinderTest extends AutomatedTestBase {
 								As[i][j] = AA[i][j];
 							}
 						}
+
 					}
-					AA = modifyValue(As,Ys,3,7);
+					AA = modifyValue(As,Ys,value1,coll1);
 					break;
 			}
-
-			writeInputMatrixWithMTD("AA", AA, true);
+			double[][] Y0 = new double[rows][1];
+			for(int i = 0; i< rows;i++){
+				Y0[i][0]= AA[i][10];
+			}
+			writeInputMatrixWithMTD("A", A, true);
 			writeInputMatrixWithMTD("B", B, true);
-
+			writeInputMatrixWithMTD("Y0", Y0, true);
 			runTest(true, false, null, -1);
 
+			HashMap<MatrixValue.CellIndex, Double> dmlfile = readDMLMatrixFromHDFS("C");
+			double[][] ress = new double [5][20];
+			for (Entry<MatrixValue.CellIndex, Double> a : dmlfile.entrySet()) {
+				MatrixValue.CellIndex ci = a.getKey();
+				ress[ci.row-1][ci.column-1] = a.getValue();
+			}
+			for(int i = 0; i < 5; i++){
+				if(test == 1 ){
+					if(ress[i][3] == value0 && ress[i][4] ==  coll0+1 && ress[i][12] == 0 && ress[i][13] == 0){
+						System.out.print("Test passed!");
+					}
+				}else{
+					if(((ress[i][3] == value0 | ress[i][3] == value1)  && (ress[i][4] ==  coll0+1 | ress[i][4] == coll1 +1)) && ((ress[i][12] == value0 | ress[i][12] == value1)
+							&& (ress[i][13] == coll0+1 | ress[i][13] == coll1+1))){
+						System.out.print("Test passed!");
+					}
+				}
+			}
 		}
 		finally {
 			rtplatform = platformOld;

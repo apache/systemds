dd23da2627fc4774740351b35206c24d1a5b54ff
Mark Dokter <mark@dokter.cc>
2019-08-30 17:20:21 +0200

    [SYSTEMDS-154] fixed license header of all touched files

--- src/main/java/org/tugraz/sysds/runtime/instructions/gpu/context/GPUContextPool.java
+++ src/main/java/org/tugraz/sysds/runtime/instructions/gpu/context/GPUContextPool.java
@@ -1,4 +1,6 @@
 /*
+ * Modifications Copyright 2019 Graz University of Technology
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,7 +18,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.tugraz.sysds.runtime.instructions.gpu.context;
+ package org.tugraz.sysds.runtime.instructions.gpu.context;
 
 import static jcuda.driver.JCudaDriver.cuDeviceGetCount;
 import static jcuda.driver.JCudaDriver.cuInit;
--- src/test/java/org/tugraz/sysds/test/applications/NNTest.java
+++ src/test/java/org/tugraz/sysds/test/applications/NNTest.java
@@ -1,4 +1,6 @@
 /*
+ * Modifications Copyright 2019 Graz University of Technology
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,7 +18,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
+ 
 package org.tugraz.sysds.test.applications;
 
 import static org.tugraz.sysds.api.mlcontext.ScriptFactory.dmlFromFile;

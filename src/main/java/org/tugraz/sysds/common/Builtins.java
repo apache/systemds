b8efb39395e6fa33c1de1d94b7d63a01a68e3e50
Shafaq Siddiqi <shafaq.siddiqi@tugraz.at>
2020-02-08 21:31:14 +0100

    [SYSTEMDS-193] New builtin function for outlier detection via std dev
    Outliers detection using standard deviation and repair using row
    deletion, mean and median imputation
    
    Closes #89.

--- src/main/java/org/tugraz/sysds/common/Builtins.java
+++ src/main/java/org/tugraz/sysds/common/Builtins.java
@@ -122,6 +122,7 @@ public enum Builtins {
 	NAIVEBAYES("naivebayes", true, false),
 	OUTER("outer", false),
 	OUTLIER("outlier", true, false), //TODO parameterize opposite
+	OUTLIERBYSD("outlierBySd", true),
 	PNMF("pnmf", true),
 	PPRED("ppred", false),
 	PROD("prod", false),

1df57841bf3cc5931ea244ee83b687ababc466eb
Shafaq Siddiqi <shafaq.siddiqi@tugraz.at>
2020-02-15 22:00:10 +0100

    [SYSTEMDS-197] Improved functional dependency discovery (discoverFD)
    1. Included Mask and threshold as input parameters
    2. Output Matrix FD contains the scores for FDs
    
    Closes #95.

--- src/test/java/org/tugraz/sysds/test/functions/builtin/BuiltinFDTest.java
+++ src/test/java/org/tugraz/sysds/test/functions/builtin/BuiltinFDTest.java
@@ -30,13 +30,6 @@ public class BuiltinFDTest extends AutomatedTestBase {
 	private final static String TEST_DIR = "functions/builtin/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinFDTest.class.getSimpleName() + "/";
 
-//	private final static double eps = 0.001;
-//	private final static int rows = 1000;
-//	private final static int colsX = 500;
-//	private final static double spSparse = 0.01;
-//	private final static double spDense = 0.7;
-//	private final static int max_iter = 10;
-
 	@Override
 	public void setUp() {
 		TestUtils.clearAssertionInformation();
@@ -46,35 +39,48 @@ public class BuiltinFDTest extends AutomatedTestBase {
 	@Test
 	public void testFD1() {
 		double[][] X =  {{7,1,1,2,2,1},{7,2,2,3,2,1},{7,3,1,4,1,1},{7,4,2,5,3,1},{7,5,3,6,5,1}, {7,6,5,1,4,1}};
-		runFDTests(X, LopProperties.ExecType.CP);
+		double[][] M = {{0, 1, 1, 0, 1, 1}};
+		runFDTests(X, M, 1, LopProperties.ExecType.CP );
 	}
 
 	@Test
 	public void testFD2() {
 		double[][] X =  {{1,1,1,1,1},{2,1,2,2,1},{3,2,1,1,1},{4,2,2,2,1},{5,3,3,1,1}};
-		runFDTests(X, LopProperties.ExecType.CP);
+		double[][] M = {{1, 1, 1, 1, 1}};
+		runFDTests(X, M, 0.8, LopProperties.ExecType.CP );
 	}
 
 	@Test
 	public void testFD3() {
 		double[][] X =  {{1,1},{2,1},{3,2},{2,2},{4,2}, {5,3}};
-		runFDTests(X, LopProperties.ExecType.CP);
+		double[][] M = {{0, 1}};
+		runFDTests(X, M, 0.8, LopProperties.ExecType.CP );
 	}
+	
 	@Test
 	public void testFD4() {
 		double[][] X =  {{1,1,1,1,1,1,2},{2,1,2,2,1,2,2},{3,2,1,1,1,3,2},{4,2,2,2,1,4,2},{5,3,3,1,1,5,1}};
-		runFDTests(X, LopProperties.ExecType.CP);
+		double[][] M = {{0, 1, 1, 1, 0, 1, 1}};
+		runFDTests(X, M, 0.8, LopProperties.ExecType.CP );
 	}
-
-	private void runFDTests(double [][] X , LopProperties.ExecType instType) {
+	
+	@Test
+	public void testFD5() {
+		double[][] X =  {{7,1,1,2,2,1},{7,2,2,3,2,1},{7,3,1,4,1,1},{7,4,2,5,3,1},{7,5,3,6,5,1}, {7,6,5,1,4,1}};
+		double[][] M = {{0, 1, 1, 0, 1, 1}};
+		runFDTests(X, M, 0.8, LopProperties.ExecType.CP );
+	}
+	
+	private void runFDTests(double [][] X , double[][] M, double threshold, LopProperties.ExecType instType) {
 		Types.ExecMode platformOld = setExecMode(instType);
 		try
 		{
 			loadTestConfiguration(getTestConfiguration(TEST_NAME));
 			String HOME = SCRIPT_DIR + TEST_DIR;
 			fullDMLScriptName = HOME + TEST_NAME + ".dml";
-			programArgs = new String[]{"-stats","-args", input("X")};
+			programArgs = new String[]{"-stats","-args", input("X"), input("M"), String.valueOf(threshold), output("B")};
 			writeInputMatrixWithMTD("X", X, true);
+			writeInputMatrixWithMTD("M", M, true);
 			runTest(true, false, null, -1);
 		}
 		finally {

3ab9e0d2a4ea1bd7a64d24f3f378a5ca9dc06cad
Matthias Boehm <mboehm7@gmail.com>
2020-03-19 00:35:46 +0100

    [SYSTEMDS-271,281] Fix l2svm app tests and ternary ctable tests
    1) The modified l2svm application test (as part of the reintroduced
    compression framework) was enumerating underdetermined systems
    (nrow<<ncol) which resulted in flaky R tests. For now, I revert this to
    the original test.
    
    2) The new ctable-sum rewrite triggered on other tests that specifically
    test other seq-table rewrites. We now ensure proper separation via
    dedicated program cuts.

--- src/test/java/org/tugraz/sysds/test/applications/L2SVMTest.java
+++ src/test/java/org/tugraz/sysds/test/applications/L2SVMTest.java
@@ -18,126 +18,99 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
+ 
 package org.tugraz.sysds.test.applications;
 
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.tugraz.sysds.api.DMLScript;
-import org.tugraz.sysds.common.Types.ExecMode;
+import org.junit.runners.Parameterized.Parameters;
 import org.tugraz.sysds.runtime.matrix.data.MatrixValue.CellIndex;
+import org.tugraz.sysds.test.AutomatedTestBase;
 import org.tugraz.sysds.test.TestUtils;
-import org.tugraz.sysds.test.TestConstants.MatrixType;
-import org.tugraz.sysds.test.TestConstants.SparsityType;
 
 @RunWith(value = Parameterized.class)
-@net.jcip.annotations.NotThreadSafe
-public class L2SVMTest extends ApplicationTestBase {
+public class L2SVMTest extends AutomatedTestBase 
+{
 	protected final static String TEST_DIR = "applications/l2svm/";
-	private final static String TEST_CONF = "SystemDS-config-L2SVM.xml";
-	private final static File TEST_CONF_FILE = new File(SCRIPT_DIR + TEST_DIR, TEST_CONF);
 	protected final static String TEST_NAME = "L2SVM";
-	protected String TEST_CLASS_DIR = TEST_DIR + L2SVMTest.class.getSimpleName() + "/" + Integer.toString(id) + "/";
+	protected String TEST_CLASS_DIR = TEST_DIR + L2SVMTest.class.getSimpleName() + "/";
 
+	protected int numRecords, numFeatures;
+	protected double sparsity;
 	protected boolean intercept;
 
-	protected double epsilon = 1e-6;
-	protected double lambda = 1.0;
-	protected int maxiterations = 3;
-	protected int maxNumberOfMRJobs = 49;
-
-	protected double[][] X;
-	protected double[][] Y;
-
-	List<String> proArgs;
-	List<String> rArgs;
-
-	public L2SVMTest(int id, SparsityType sparType, MatrixType matrixType, ExecMode newPlatform) {
-		super( id, sparType, matrixType, newPlatform);
+	public L2SVMTest(int rows, int cols, double sp, boolean intercept) {
+		numRecords = rows;
+		numFeatures = cols;
+		sparsity = sp;
+		intercept = this.intercept;
 	}
-
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+			//sparse tests (sparsity=0.01)
+			{100, 50, 0.01, false}, {1000, 500, 0.01, false}, {10000, 750, 0.01, false}, {10000, 750, 0.01, true}, {100000, 1000, 0.01, false},
+			//dense tests (sparsity=0.7)
+			{100, 50, 0.7, false}, {1000, 500, 0.7, false}, {1000, 500, 0.7, true}, {10000, 750, 0.7, false} });
+	}
+	
 	@Override
 	public void setUp() {
-
 		addTestConfiguration(TEST_CLASS_DIR, TEST_NAME);
-		// addTestConfiguration(TEST_CLASS_DIR, TEST_NAME);
+	}
+	
+	@Test
+	public void testL2SVM()
+	{
+		System.out.println("------------ BEGIN " + TEST_NAME 
+			+ " TEST WITH {" + numRecords + ", " + numFeatures
+			+ ", " + sparsity + ", " + intercept + "} ------------");
+		int rows = numRecords;
+		int cols = numFeatures;
+		double epsilon = 1e-10;
+		double lambda = 1.0;
+		int maxiterations = 3;
+		int maxNumberOfMRJobs = 21;
+
 		getAndLoadTestConfiguration(TEST_NAME);
 
-		proArgs = Arrays.asList("-stats",
-			"100",
-			"-nvargs",
-			"X=" + input("X"),
-			"Y=" + input("Y"),
-			"icpt=" + (intercept ? 1 : 0),
-			"tol=" + epsilon,
-			"reg=" + lambda,
-			"maxiter=" + maxiterations,
-			"model=" + output("w"),
-			"Log=" + output("Log"));
+		List<String> proArgs = new ArrayList<>();
+		proArgs.add("-stats");
+		proArgs.add("-nvargs");
+		proArgs.add("X=" + input("X"));
+		proArgs.add("Y=" + input("Y"));
+		proArgs.add("icpt=" + (intercept ? 1 : 0));
+		proArgs.add("tol=" + epsilon);
+		proArgs.add("reg=" + lambda);
+		proArgs.add("maxiter=" + maxiterations);
+		proArgs.add("model=" + output("w"));
+		proArgs.add("Log=" + output("Log"));
+		programArgs = proArgs.toArray(new String[proArgs.size()]);
+		fullDMLScriptName = getScript();
+		rCmd = getRCmd(inputDir(), (intercept ? Integer.toString(1) : Integer.toString(0)), Double.toString(epsilon), 
+				Double.toString(lambda), Integer.toString(maxiterations), expectedDir());
 
-		rArgs = Arrays.asList(inputDir(),
-			(intercept ? Integer.toString(1) : Integer.toString(0)),
-			Double.toString(epsilon),
-			Double.toString(lambda),
-			Integer.toString(maxiterations),
-			expectedDir());
-
-		X = getRandomMatrix(rows, cols, 0, 1, sparsity, -1);
-		// 0.999 because sometimes the value is rounded up from 1.0000001 to 2.
-		Y = TestUtils.round(getRandomMatrix(rows, 1, 0, 1, 1, 14));
+		double[][] X = getRandomMatrix(rows, cols, 0, 1, sparsity, -1);
+		double[][] Y = getRandomMatrix(rows, 1, -1, 1, 1, -1);
+		for(int i=0; i<rows; i++)
+			Y[i][0] = (Y[i][0] > 0) ? 1 : -1;
 
 		writeInputMatrixWithMTD("X", X, true);
 		writeInputMatrixWithMTD("Y", Y, true);
-
-		platformOld = rtplatform;
-
-		fullDMLScriptName = getScript();
-		programArgs = proArgs.toArray(new String[proArgs.size()]);
-		rCmd = getRCmd(rArgs.toArray(new String[rArgs.size()]));
-
 		runTest(true, EXCEPTION_NOT_EXPECTED, null, maxNumberOfMRJobs);
 
-	}
-
-	@After
-	@Override
-	public void teardown() {
-		rtplatform = platformOld;
-		DMLScript.USE_LOCAL_SPARK_CONFIG = sparkConfigOld;
-	}
-
-	@Test
-	public void testL2SVM() {
 		runRScript(true);
+
 		HashMap<CellIndex, Double> wR = readRMatrixFromFS("w");
-		HashMap<CellIndex, Double> wSYSTEMDS = readDMLMatrixFromHDFS("w");
+		HashMap<CellIndex, Double> wSYSTEMDS= readDMLMatrixFromHDFS("w");
 		TestUtils.compareMatrices(wR, wSYSTEMDS, epsilon, "wR", "wSYSTEMDS");
-	}
-
-	@Test
-	public void testCompressedL2SVM() {
-		boolean compressedHH = heavyHittersContainsSubString("compress");
-		boolean sPCompressedHH =  heavyHittersContainsSubString("sp_compress");
-		assertTrue("Compression was not a heavy hitter in execution " + this.toString() ,compressedHH || sPCompressedHH);
-	}
-
-	/**
-	 * Override default configuration with custom test configuration to ensure scratch space and local temporary
-	 * directory locations are also updated.
-	 */
-	@Override
-	protected File getConfigTemplateFile() {
-		// Instrumentation in this test's output log to show custom configuration file used for template.
-		System.out.println("This test case overrides default configuration with " + TEST_CONF_FILE.getPath());
-		return TEST_CONF_FILE;
 	}
 }

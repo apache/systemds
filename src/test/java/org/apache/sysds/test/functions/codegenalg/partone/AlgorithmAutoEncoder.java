e5b43faa8e144e433cf294469af9f6ca118a474f
Janardhan <j143@protonmail.com>
2020-04-28 00:15:58 +0200

    [SYSTEMML-2121] AutoEncoder test for codegenalg suite
    This patch adds a test case for AutoEncoder with codegen
    enabled against a corresponding R script.
    
    Closes #890.

--- src/test/java/org/apache/sysds/test/functions/codegenalg/partone/AlgorithmAutoEncoder.java
+++ src/test/java/org/apache/sysds/test/functions/codegenalg/partone/AlgorithmAutoEncoder.java
@@ -20,7 +20,9 @@
 package org.apache.sysds.test.functions.codegenalg.partone;
 
 import java.io.File;
+import java.util.HashMap;
 
+import org.apache.sysds.runtime.matrix.data.MatrixValue;
 import org.junit.Assert;
 import org.junit.Test;
 import org.apache.sysds.api.DMLScript;
@@ -37,11 +39,12 @@ public class AlgorithmAutoEncoder extends AutomatedTestBase
 	private final static String TEST_DIR = "functions/codegenalg/";
 	private final static String TEST_CLASS_DIR = TEST_DIR + AlgorithmAutoEncoder.class.getSimpleName() + "/";
 	
-	private final static int rows = 2468;
+	private final static int rows = 1068;
 	private final static int cols = 784;
 	
 	private final static double sparsity1 = 0.7; //dense
 	private final static double sparsity2 = 0.1; //sparse
+	private final static double eps       = 1e-5;
 	
 	private final static int H1 = 500;
 	private final static int H2 = 2;
@@ -179,22 +182,66 @@ public class AlgorithmAutoEncoder extends AutomatedTestBase
 			TestConfiguration config = getTestConfiguration(TEST_NAME);
 			loadTestConfiguration(config);
 			
-			fullDMLScriptName = "scripts/staging/autoencoder-2layer.dml";
+			fullDMLScriptName = SCRIPT_DIR + TEST_DIR + "/Algorithm_AutoEncoder.dml";
+			//"scripts/staging/autoencoder-2layer.dml";
 			programArgs = new String[]{ "-stats", "-nvargs", "X="+input("X"),
-				"H1="+H1, "H2="+H2, "EPOCH="+epochs, "BATCH="+batchsize, 
+				"H1="+H1, "H2="+H2, "EPOCH="+epochs, "BATCH="+batchsize,
+				"W1_rand="+input("W1_rand"),"W2_rand="+input("W2_rand"),
+				"W3_rand="+input("W3_rand"), "W4_rand="+input("W4_rand"),
+				"order_rand="+input("order_rand"),
 				"W1_out="+output("W1"), "b1_out="+output("b1"),
 				"W2_out="+output("W2"), "b2_out="+output("b2"),
 				"W3_out="+output("W3"), "b3_out="+output("b3"),
 				"W4_out="+output("W4"), "b4_out="+output("b4")};
+
+			rCmd = getRCmd(inputDir(), String.valueOf(H1), String.valueOf(H2),
+					String.valueOf(epochs), String.valueOf(batchsize), expectedDir());
 			OptimizerUtils.ALLOW_ALGEBRAIC_SIMPLIFICATION = rewrites;
 			
 			//generate actual datasets
 			double[][] X = getRandomMatrix(rows, cols, 0, 1, sparse?sparsity2:sparsity1, 714);
 			writeInputMatrixWithMTD("X", X, true);
-			
+
+			//generate rand matrices for W1, W2, W3, W4 here itself for passing onto both DML and R scripts
+			double[][] W1_rand = getRandomMatrix(H1, cols, 0, 1, sparse?sparsity2:sparsity1, 800);
+			writeInputMatrixWithMTD("W1_rand", W1_rand, true);
+			double[][] W2_rand = getRandomMatrix(H2, H1, 0, 1, sparse?sparsity2:sparsity1, 900);
+			writeInputMatrixWithMTD("W2_rand", W2_rand, true);
+			double[][] W3_rand = getRandomMatrix(H1, H2, 0, 1, sparse?sparsity2:sparsity1, 589);
+			writeInputMatrixWithMTD("W3_rand", W3_rand, true);
+			double[][] W4_rand = getRandomMatrix(cols, H1, 0, 1, sparse?sparsity2:sparsity1, 150);
+			writeInputMatrixWithMTD("W4_rand", W4_rand, true);
+			double[][] order_rand = getRandomMatrix(rows, 1, 0, 1, sparse?sparsity2:sparsity1, 143);
+			writeInputMatrixWithMTD("order_rand", order_rand, true); //for the permut operation on input X
+
 			//run script
 			runTest(true, false, null, -1); 
-			//TODO R script
+			runRScript(true);
+
+			HashMap<MatrixValue.CellIndex, Double> dmlW1 = readDMLMatrixFromHDFS("W1");
+			HashMap<MatrixValue.CellIndex, Double> dmlW2 = readDMLMatrixFromHDFS("W2");
+			HashMap<MatrixValue.CellIndex, Double> dmlW3 = readDMLMatrixFromHDFS("W3");
+			HashMap<MatrixValue.CellIndex, Double> dmlW4 = readDMLMatrixFromHDFS("W4");
+			HashMap<MatrixValue.CellIndex, Double> dmlb1 = readDMLMatrixFromHDFS("b1");
+			HashMap<MatrixValue.CellIndex, Double> dmlb2 = readDMLMatrixFromHDFS("b2");
+			HashMap<MatrixValue.CellIndex, Double> dmlb3 = readDMLMatrixFromHDFS("b3");
+			HashMap<MatrixValue.CellIndex, Double> dmlb4 = readDMLMatrixFromHDFS("b4");
+			HashMap<MatrixValue.CellIndex, Double> rW1  = readRMatrixFromFS("W1");
+			HashMap<MatrixValue.CellIndex, Double> rW2  = readRMatrixFromFS("W2");
+			HashMap<MatrixValue.CellIndex, Double> rW3  = readRMatrixFromFS("W3");
+			HashMap<MatrixValue.CellIndex, Double> rW4  = readRMatrixFromFS("W4");
+			HashMap<MatrixValue.CellIndex, Double> rb1  = readRMatrixFromFS("b1");
+			HashMap<MatrixValue.CellIndex, Double> rb2  = readRMatrixFromFS("b2");
+			HashMap<MatrixValue.CellIndex, Double> rb3  = readRMatrixFromFS("b3");
+			HashMap<MatrixValue.CellIndex, Double> rb4  = readRMatrixFromFS("b4");
+			TestUtils.compareMatrices(dmlW1, rW1, eps, "Stat-DML", "Stat-R");
+			TestUtils.compareMatrices(dmlW2, rW2, eps, "Stat-DML", "Stat-R");
+			TestUtils.compareMatrices(dmlW3, rW3, eps, "Stat-DML", "Stat-R");
+			TestUtils.compareMatrices(dmlW4, rW4, eps, "Stat-DML", "Stat-R");
+			TestUtils.compareMatrices(dmlb1, rb1, eps, "Stat-DML", "Stat-R");
+			TestUtils.compareMatrices(dmlb2, rb2, eps, "Stat-DML", "Stat-R");
+			TestUtils.compareMatrices(dmlb3, rb3, eps, "Stat-DML", "Stat-R");
+			TestUtils.compareMatrices(dmlb4, rb4, eps, "Stat-DML", "Stat-R");
 			
 			Assert.assertTrue(heavyHittersContainsSubString("spoof") 
 				|| heavyHittersContainsSubString("sp_spoof"));

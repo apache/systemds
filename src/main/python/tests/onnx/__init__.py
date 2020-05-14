905f6e69098cadd67cb019552fe35ba18357fe12
Lukas Timpl <lukas.timpl@student.tugraz.at>
2020-05-14 23:39:04 +0200

    [SYSTEMDS-263] ONNX graph importer (Python API, docs, tests)
    This PR implements a first poc-implementation for an ONNX importer.
    
    It adds support for the following operators: Add, Sub, MatMul, Neg, Xor,
    Or, And, Relu, Tanh, Sigmoid, Softmax, Dropout, MaxPool, Conv, If; as
    well as the logic for nested sub-graphs.
    
    AMLS project SS 2020
    Closes #904.

--- src/main/python/tests/onnx/__init__.py
+++ src/main/python/tests/onnx/__init__.py
@@ -1,4 +1,3 @@
-#-------------------------------------------------------------
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -17,6 +16,3 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-#-------------------------------------------------------------
-
-__all__ = ['context', 'matrix', 'onnx_systemds']

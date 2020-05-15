e6fd31ea611665ae5abe84018a76d1f23183e1a5
Shafaq Siddiqi <shafaq.siddiqi@tugraz.at>
2020-05-15 13:32:24 +0200

    fix headers

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinGMMTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinGMMTest.java
@@ -1,8 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.sysds.test.functions.builtin;
 
 import org.apache.sysds.common.Types;
 import org.apache.sysds.lops.LopProperties;
-import org.apache.sysds.runtime.matrix.data.MatrixBlock;
 import org.apache.sysds.runtime.matrix.data.MatrixValue;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
@@ -29,7 +47,6 @@ public class BuiltinGMMTest extends AutomatedTestBase {
 	public void testGMMCP() {
 		runGMMTest(3,3,"VVI", LopProperties.ExecType.CP);
 	}
-
 
 
 	private void runGMMTest(int G_mixtures, int iter, String model,  LopProperties.ExecType instType)
@@ -62,6 +79,4 @@ public class BuiltinGMMTest extends AutomatedTestBase {
 			rtplatform = platformOld;
 		}
 	}
-
-
 }

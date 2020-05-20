0e30cde7d18d36941c644a2eeedb4b1303d3ed2b
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2020-05-20 11:46:21 +0200

    Adapt to PR comments: - change assert in function to stop with error message - add new line at the end of DML file - add license in test file - add test case that's expected to fail

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinToOneHotTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinToOneHotTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.sysds.test.functions.builtin;
 
 import org.apache.sysds.common.Types;
@@ -9,7 +28,10 @@ import org.apache.sysds.test.TestUtils;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+
+import static org.junit.Assert.fail;
 
 public class BuiltinToOneHotTest extends AutomatedTestBase {
     private final static String TEST_NAME = "to_one_hot";
@@ -28,20 +50,21 @@ public class BuiltinToOneHotTest extends AutomatedTestBase {
 
     @Test
     public void runSimpleTest() {
-        runToOneHotTest(false, false, LopProperties.ExecType.CP);
+        runToOneHotTest(false, false, LopProperties.ExecType.CP, false);
     }
 
-    private void runToOneHotTest(boolean scalar, boolean sparse, LopProperties.ExecType instType) {
+    @Test
+    public void runFailingSimpleTest() {
+        runToOneHotTest(false, false, LopProperties.ExecType.CP, true);
+    }
+
+    private void runToOneHotTest(boolean scalar, boolean sparse,
+                                 LopProperties.ExecType instType, boolean shouldFail) {
         Types.ExecMode platformOld = setExecMode(instType);
 
         try
         {
             loadTestConfiguration(getTestConfiguration(TEST_NAME));
-
-            String HOME = SCRIPT_DIR + TEST_DIR;
-            fullDMLScriptName = HOME + TEST_NAME + ".dml";
-            programArgs = new String[]{"-explain", "-args", input("A"), String.format("%d", numClasses),
-                    output("B") };
 
             //generate actual dataset
             double[][] doubles = getRandomMatrix(rows, cols, 1, numClasses, 1, 7);
@@ -54,21 +77,47 @@ public class BuiltinToOneHotTest extends AutomatedTestBase {
                 }
             }
 
+            int max = -1;
+
+            for(int i = 0; i < rows; i++) {
+                if(A[i][0] > max) {
+                    max = (int) A[i][0];
+                }
+            }
+
+            // script fails if numClasses provided is smaller than maximum value in A
+            int numClassesPassed = shouldFail ? max - 1 : max;
+
+            String HOME = SCRIPT_DIR + TEST_DIR;
+            fullDMLScriptName = HOME + TEST_NAME + ".dml";
+            programArgs = new String[]{"-explain", "-args", input("A"), String.format("%d", numClassesPassed),
+                    output("B") };
+
+
             writeInputMatrixWithMTD("A", A, false);
 
             runTest(true, false, null, -1);
 
-            HashMap<MatrixValue.CellIndex, Double> expected = new HashMap<MatrixValue.CellIndex, Double>();
-            for(int i = 0; i < A.length; i++) {
-                for(int j = 0; j < A[i].length; j++) {
-                    // indices start with 1 here
-                    expected.put(new MatrixValue.CellIndex(i + 1, (int) A[i][j]), 1.0);
+            if(!shouldFail) {
+                HashMap<MatrixValue.CellIndex, Double> expected = new HashMap<MatrixValue.CellIndex, Double>();
+                for(int i = 0; i < A.length; i++) {
+                    for(int j = 0; j < A[i].length; j++) {
+                        // indices start with 1 here
+                        expected.put(new MatrixValue.CellIndex(i + 1, (int) A[i][j]), 1.0);
+                    }
+                }
+
+                //compare matrices
+                HashMap<MatrixValue.CellIndex, Double> result = readDMLMatrixFromHDFS("B");
+                TestUtils.compareMatrices(result, expected, eps, "Stat-DML", "Stat-Java");
+            } else {
+                try {
+                    readDMLMatrixFromHDFS("B");
+                    fail("File should not have been written");
+                } catch(AssertionError e) {
+                    // exception expected
                 }
             }
-
-            //compare matrices
-            HashMap<MatrixValue.CellIndex, Double> result = readDMLMatrixFromHDFS("B");
-            TestUtils.compareMatrices(result, expected, eps, "Stat-DML", "Stat-Java");
         }
         finally {
             rtplatform = platformOld;

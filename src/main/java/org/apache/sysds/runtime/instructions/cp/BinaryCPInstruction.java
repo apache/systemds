d3c4219db972169fd4b2047d55f8ecfe82a31e87
Shafaq Siddiqi <shafaq.siddiqi@tugraz.at>
2020-05-27 15:29:57 +0200

    Missing condition in BinaryCPInstruction

--- src/main/java/org/apache/sysds/runtime/instructions/cp/BinaryCPInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/cp/BinaryCPInstruction.java
@@ -90,7 +90,7 @@ public abstract class BinaryCPInstruction extends ComputationCPInstruction {
 	
 	protected static void checkOutputDataType(CPOperand in1, CPOperand in2, CPOperand out) {
 		// check for valid data type of output
-		if((in1.getDataType() == DataType.MATRIX || in2.getDataType() == DataType.MATRIX) )
+		if((in1.getDataType() == DataType.MATRIX || in2.getDataType() == DataType.MATRIX) && out.getDataType() != DataType.MATRIX)
 			throw new DMLRuntimeException("Element-wise matrix operations between variables " + in1.getName() + 
 					" and " + in2.getName() + " must produce a matrix, which " + out.getName() + " is not");
 	}

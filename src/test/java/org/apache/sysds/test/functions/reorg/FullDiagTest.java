e9a5d4d8577eb341473a406cf2aa74150cac599d
Matthias Boehm <mboehm7@gmail.com>
2020-05-29 20:17:58 +0200

    [SYSTEMDS-401] Fix spark diagM2V operations (wrong block indexes, tests)
    This patch fixes the spark rorg (rdiag) instruction for creating correct
    output block indexes in the case of matrix-to-vector ops, which obtain
    the diagonal of a matrix into a vector representation. So far, for an
    input (r, c), we returned (r, c), which is now fixed to (r, 1).

--- src/test/java/org/apache/sysds/test/functions/reorg/FullDiagTest.java
+++ src/test/java/org/apache/sysds/test/functions/reorg/FullDiagTest.java
@@ -23,17 +23,18 @@ import java.util.HashMap;
 import java.util.Random;
 
 import org.junit.Test;
-import org.apache.sysds.api.DMLScript;
 import org.apache.sysds.common.Types.ExecMode;
 import org.apache.sysds.runtime.matrix.data.MatrixValue.CellIndex;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.apache.sysds.test.TestUtils;
 
-public class DiagV2MTest extends AutomatedTestBase
+public class FullDiagTest extends AutomatedTestBase
 {
 	private final static String TEST_DIR = "functions/reorg/";
-	private static final String TEST_CLASS_DIR = TEST_DIR + DiagV2MTest.class.getSimpleName() + "/";
+	private final static String TEST_NAME1 = "DiagV2MTest";
+	private final static String TEST_NAME2 = "DiagM2VTest";
+	private static final String TEST_CLASS_DIR = TEST_DIR + FullDiagTest.class.getSimpleName() + "/";
 
 	private final static double epsilon=0.0000000001;
 	private final static int rows = 1059;
@@ -42,68 +43,63 @@ public class DiagV2MTest extends AutomatedTestBase
 	
 	@Override
 	public void setUp() {
-		addTestConfiguration("DiagV2MTest", 
-			new TestConfiguration(TEST_CLASS_DIR, "DiagV2MTest", new String[] {"C"}));
-	}
-	
-	public void commonReorgTest(ExecMode platform)
-	{
-		TestConfiguration config = getTestConfiguration("DiagV2MTest");
-	    
-		ExecMode prevPlfm=rtplatform;
-		
-	    rtplatform = platform;
-		boolean sparkConfigOld = DMLScript.USE_LOCAL_SPARK_CONFIG;
-		if( rtplatform == ExecMode.SPARK )
-			DMLScript.USE_LOCAL_SPARK_CONFIG = true;
-
-		try {
-	        config.addVariable("rows", rows);
-			loadTestConfiguration(config);
-	          
-			/* This is for running the junit test the new way, i.e., construct the arguments directly */
-			String RI_HOME = SCRIPT_DIR + TEST_DIR;
-			fullDMLScriptName = RI_HOME + "DiagV2MTest" + ".dml";
-			programArgs = new String[]{"-explain", "-args",  input("A"), Long.toString(rows), output("C") };
-			
-			fullRScriptName = RI_HOME + "DiagV2MTest" + ".R";
-			rCmd = "Rscript" + " " + fullRScriptName + " " + inputDir() + " " + expectedDir();
-	
-			Random rand=new Random(System.currentTimeMillis());
-			double sparsity=0.599200924665577;//rand.nextDouble();
-			double[][] A = getRandomMatrix(rows, 1, min, max, sparsity, 1397289950533L); // System.currentTimeMillis()
-	        writeInputMatrix("A", A, true);
-	        sparsity=rand.nextDouble();   
-			
-	        boolean exceptionExpected = false;
-			int expectedNumberOfJobs = -1;
-			runTest(true, exceptionExpected, null, expectedNumberOfJobs);
-			
-			runRScript(true);
-		
-			for(String file: config.getOutputFiles())
-			{
-				HashMap<CellIndex, Double> dmlfile = readDMLMatrixFromHDFS(file);
-				HashMap<CellIndex, Double> rfile = readRMatrixFromFS(file);
-			//	System.out.println(file+"-DML: "+dmlfile);
-			//	System.out.println(file+"-R: "+rfile);
-				TestUtils.compareMatrices(dmlfile, rfile, epsilon, file+"-DML", file+"-R");
-			}
-		}
-		finally {
-			rtplatform = prevPlfm;
-			DMLScript.USE_LOCAL_SPARK_CONFIG = sparkConfigOld;
-		}
+		addTestConfiguration(TEST_NAME1, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME1, new String[] {"C"}));
+		addTestConfiguration(TEST_NAME2, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME2, new String[] {"C"}));
 	}
 	
 	@Test
 	public void testDiagV2MCP() {
-		commonReorgTest(ExecMode.SINGLE_NODE);
+		commonReorgTest(TEST_NAME1, ExecMode.SINGLE_NODE);
 	}
 	
 	@Test
 	public void testDiagV2MSP() {
-		commonReorgTest(ExecMode.SPARK);
+		commonReorgTest(TEST_NAME1, ExecMode.SPARK);
+	}
+	
+	@Test
+	public void testDiagM2VCP() {
+		commonReorgTest(TEST_NAME2, ExecMode.SINGLE_NODE);
+	}
+	
+	@Test
+	public void testDiagM2VSP() {
+		commonReorgTest(TEST_NAME2, ExecMode.SPARK);
+	}
+	
+	public void commonReorgTest(String testname, ExecMode platform)
+	{
+		TestConfiguration config = getTestConfiguration(testname);
+		ExecMode prevPlfm = setExecMode(platform);
+		
+		try {
+			config.addVariable("rows", rows);
+			loadTestConfiguration(config);
+	
+			String RI_HOME = SCRIPT_DIR + TEST_DIR;
+			fullDMLScriptName = RI_HOME + testname + ".dml";
+			programArgs = new String[]{"-explain", "-args",  input("A"), output("C") };
+			fullRScriptName = RI_HOME + testname + ".R";
+			rCmd = "Rscript" + " " + fullRScriptName + " " + inputDir() + " " + expectedDir();
+	
+			Random rand=new Random(System.currentTimeMillis());
+			int cols = testname.equals(TEST_NAME1) ? 1 : rows;
+			double sparsity=0.599200924665577;
+			double[][] A = getRandomMatrix(rows, cols, min, max, sparsity, 1397289950533L);
+			writeInputMatrixWithMTD("A", A, true);
+			sparsity=rand.nextDouble();
+			
+			runTest(true, EXCEPTION_NOT_EXPECTED, null, -1);
+			runRScript(true);
+		
+			for(String file: config.getOutputFiles()) {
+				HashMap<CellIndex, Double> dmlfile = readDMLMatrixFromHDFS(file);
+				HashMap<CellIndex, Double> rfile = readRMatrixFromFS(file);
+				TestUtils.compareMatrices(dmlfile, rfile, epsilon, file+"-DML", file+"-R");
+			}
+		}
+		finally {
+			resetExecMode(prevPlfm);
+		}
 	}
 }
-

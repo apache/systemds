870b77c364424dc661bf7972e5e4fb5aa576de52
Herbert Mühlburger <mail@muehlburger.at>
2020-06-09 20:08:14 +0200

    Refactor and remove unused parameters

--- src/main/java/org/apache/sysds/runtime/io/FrameWriterProto.java
+++ src/main/java/org/apache/sysds/runtime/io/FrameWriterProto.java
@@ -19,64 +19,62 @@
 
 package org.apache.sysds.runtime.io;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Iterator;
+
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.JobConf;
-import org.apache.sysds.common.Types;
 import org.apache.sysds.conf.ConfigurationManager;
 import org.apache.sysds.protobuf.SysdsProtos;
 import org.apache.sysds.runtime.DMLRuntimeException;
 import org.apache.sysds.runtime.matrix.data.FrameBlock;
 import org.apache.sysds.runtime.util.HDFSTool;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.Iterator;
-
-public class FrameWriterProto extends FrameWriter
-{
+public class FrameWriterProto extends FrameWriter {
 	@Override
-	public void writeFrameToHDFS(FrameBlock src, String fname, long rlen, long clen) throws IOException, DMLRuntimeException {
-		//prepare file access
+	public void writeFrameToHDFS(FrameBlock src, String fname, long rlen, long clen)
+		throws IOException, DMLRuntimeException {
+		// prepare file access
 		JobConf job = new JobConf(ConfigurationManager.getCachedJobConf());
 		Path path = new Path(fname);
 
-		//if the file already exists on HDFS, remove it.
+		// if the file already exists on HDFS, remove it.
 		HDFSTool.deleteFileIfExistOnHDFS(fname);
 
-		//validity check frame dimensions
-		if( src.getNumRows() != rlen || src.getNumColumns() != clen ) {
-			throw new IOException("Frame dimensions mismatch with metadata: " +
-					src.getNumRows()+"x"+src.getNumColumns()+" vs "+rlen+"x"+clen+".");
+		// validity check frame dimensions
+		if(src.getNumRows() != rlen || src.getNumColumns() != clen) {
+			throw new IOException("Frame dimensions mismatch with metadata: " + src.getNumRows() + "x"
+				+ src.getNumColumns() + " vs " + rlen + "x" + clen + ".");
 		}
 
 		writeProtoFrameToHDFS(path, job, src, rlen, clen);
 	}
 
-	protected void writeProtoFrameToHDFS(Path path, JobConf jobConf, FrameBlock src,
-										 long rlen, long clen) throws IOException
-	{
+	protected void writeProtoFrameToHDFS(Path path, JobConf jobConf, FrameBlock src, long rlen, long clen)
+		throws IOException {
 		FileSystem fileSystem = IOUtilFunctions.getFileSystem(path, jobConf);
-		writeProtoFrameToFile(path, fileSystem, src, 0, (int)rlen);
+		writeProtoFrameToFile(path, fileSystem, src, 0, (int) rlen);
 		IOUtilFunctions.deleteCrcFilesFromLocalFileSystem(fileSystem, path);
 	}
 
 	protected void writeProtoFrameToFile(Path path, FileSystem fileSystem, FrameBlock src, int lowerRowBound,
-										 int upperRowBound) throws IOException
-	{
+		int upperRowBound) throws IOException {
 		// what about > 2GB Protobuf Messages?
 		OutputStream outputStream = fileSystem.create(path, true);
 		SysdsProtos.Frame.Builder frameBuilder = SysdsProtos.Frame.newBuilder();
 		try {
 			Iterator<String[]> stringRowIterator = src.getStringRowIterator(lowerRowBound, upperRowBound);
-			while (stringRowIterator.hasNext()) {
+			while(stringRowIterator.hasNext()) {
 				String[] row = stringRowIterator.next();
 				frameBuilder.addRowsBuilder().addAllColumnData(Arrays.asList(row));
 			}
 			frameBuilder.build().writeTo(outputStream);
-		} finally {
-				IOUtilFunctions.closeSilently(outputStream);
+		}
+		finally {
+			IOUtilFunctions.closeSilently(outputStream);
 		}
 	}
 }

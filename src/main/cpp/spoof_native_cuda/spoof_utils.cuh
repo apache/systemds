2bdb4d4eb34a370b0932577fd1ea0032cd4aafb6
Mark Dokter <mark@dokter.cc>
2020-06-14 14:42:07 +0200

    more cellwise template cleanup

--- src/main/cpp/spoof_native_cuda/spoof_utils.cuh
+++ src/main/cpp/spoof_native_cuda/spoof_utils.cuh
@@ -1,3 +1,4 @@
+
 template<typename T>
 __device__ T getValue(T* data, int rowIndex) {
     return data[rowIndex];
@@ -6,4 +7,14 @@ __device__ T getValue(T* data, int rowIndex) {
 template<typename T>
 __device__ T getValue(T* data, int n, int rowIndex, int colIndex) {
     return data[rowIndex * n + colIndex];
+}
+
+template<typename T>
+__device__ T modulus(T a, T b) {
+   return (*reinterpret_cast<int*>(&a)) & (*reinterpret_cast<int*>(&b));
+}
+
+template<typename T>
+__device__ T intDiv(T a, T b) {
+    return (*reinterpret_cast<int*>(&a)) & (*reinterpret_cast<int*>(&b));
 }
--- src/main/java/org/apache/sysds/hops/codegen/cplan/cpp/CellWise.java
+++ src/main/java/org/apache/sysds/hops/codegen/cplan/cpp/CellWise.java
@@ -15,48 +15,28 @@ public class CellWise implements CodeTemplate {
             "// CellType: %TYPE%\n" +
             "// AggOp: %AGG_OP%\n" +
             "// SparseSafe: $SPARSE_SAFE%\n" +
-            "// SEQ: %SEQ%\n"
+            "// SEQ: %SEQ%\n" +
+            "#include \"spoof_utils.cuh\"\n" +
+            "\n"
                 + "template<typename T>\n"
-                + "__device__ T getValue(T* data, int rowIndex) {\n"
-                + "    return data[rowIndex];\n"
+                + "__device__ T genexec(T a, T** b, T* scalars, int m, int n, int grix, int rix, int cix) {\n"
+                + "%BODY_dense%"
+                + "    return %OUT%;\n"
                 + "}\n"
                 + "\n"
-                + "template<typename T>\n"
-                + "__device__ T getValue(T* data, int n, int rowIndex, int colIndex) {\n"
-                + "     return data[rowIndex * n + colIndex];\n"
-                + "}\n"
-                    + "template<typename T>\n"
-                    + "__device__ T modulus(T a, T b) {\n" +
-                    "   return a - a / b * b;\n"
-                    + "//     return *reinterpret_cast<int*>(&a) & *reinterpret_cast<int*>(&b);\n"
-                    + "}\n"
-                    + "template<typename T>\n"
-                    + "__device__ T intDiv(T a, T b) {\n" +
-                    "   T tmp = a / b;\n" +
-                    "   return *reinterpret_cast<int*>(&tmp);"
-                    + "//     return *reinterpret_cast<int*>(&a) & *reinterpret_cast<int*>(&b);\n"
-                    + "}\n"
-                + "\n"
-                    + "template<typename T>\n"
-                    + "__device__ T genexec(T a, T** b, T* scalars, int m, int n, int grix, int rix, int cix) {\n"
-                    + "%BODY_dense%"
-                    + "    return %OUT%;\n"
-                    + "}\n"
-                    + "\n"
-                    + "template<typename T, int VT>\n"
-                    + "__global__\n"
-                    + "void %TMP% (T *a, T** b, T* c, T* scalars, int m, int n, int grix, int rix, int cix) {\n"
-                    + "     int tid = blockIdx.x * blockDim.x + threadIdx.x;\n"
-                    + "     int first_idx = tid * VT;\n"
-                    + "     int last_idx = min(first_idx + VT, m * n);\n"
-                    + "     #pragma unroll\n"
-                    + "     for(int i = first_idx; i < last_idx; i++) {\n"
-                    + "         int row = i / m;\n"
-                    + "         int col = i % n;\n"
-                    + "         c[i] = genexec(a[i], b, scalars, m, n, grix, row, col);\n"
-                    + "     }\n"
-                    + "}\n";
-
+                + "template<typename T, int VT>\n"
+                + "__global__\n"
+                + "void %TMP% (T *a, T** b, T* c, T* scalars, int m, int n, int grix, int rix, int cix) {\n"
+                + "     int tid = blockIdx.x * blockDim.x + threadIdx.x;\n"
+                + "     int first_idx = tid * VT;\n"
+                + "     int last_idx = min(first_idx + VT, m * n);\n"
+                + "     #pragma unroll\n"
+                + "     for(int i = first_idx; i < last_idx; i++) {\n"
+                + "         int row = i / m;\n"
+                + "         int col = i % n;\n"
+                + "         c[i] = genexec(a[i], b, scalars, m, n, grix, row, col);\n"
+                + "     }\n"
+                + "}\n";
 
     public static final String TEMPLATE_FULL_AGG = "%TMP%\n" +
             "// CellType: %TYPE%\n" +

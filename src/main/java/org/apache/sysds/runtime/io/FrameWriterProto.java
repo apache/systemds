fdb83e55e8ddea0428841f7141d4fd269421ea1d
Herbert Mühlburger <mail@muehlburger.at>
2020-06-25 12:43:50 +0200

    [SYSTEMDS-511] Add protobuf support
    Adds support for Protobuf file format, for both reads and write.
    
    AMLS project SS2020, part 1
    
    Closes #971

---
+++ src/main/java/org/apache/sysds/runtime/io/FrameWriterProto.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.runtime.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.sysds.conf.ConfigurationManager;
+import org.apache.sysds.protobuf.SysdsProtos;
+import org.apache.sysds.runtime.matrix.data.FrameBlock;
+import org.apache.sysds.runtime.util.HDFSTool;
+
+public class FrameWriterProto extends FrameWriter {
+	@Override
+	public void writeFrameToHDFS(FrameBlock src, String fname, long rlen, long clen) throws IOException {
+		// prepare file access
+		JobConf job = new JobConf(ConfigurationManager.getCachedJobConf());
+		Path path = new Path(fname);
+
+		// if the file already exists on HDFS, remove it.
+		HDFSTool.deleteFileIfExistOnHDFS(fname);
+
+		// validity check frame dimensions
+		if(src.getNumRows() != rlen || src.getNumColumns() != clen) {
+			throw new IOException("Frame dimensions mismatch with metadata: " + src.getNumRows() + "x"
+				+ src.getNumColumns() + " vs " + rlen + "x" + clen + ".");
+		}
+
+		writeProtoFrameToHDFS(path, job, src, rlen, clen);
+	}
+
+	protected void writeProtoFrameToHDFS(Path path, JobConf jobConf, FrameBlock src, long rlen, long clen)
+		throws IOException {
+		FileSystem fileSystem = IOUtilFunctions.getFileSystem(path, jobConf);
+		writeProtoFrameToFile(path, fileSystem, src, 0, (int) rlen);
+		IOUtilFunctions.deleteCrcFilesFromLocalFileSystem(fileSystem, path);
+	}
+
+	protected void writeProtoFrameToFile(Path path, FileSystem fileSystem, FrameBlock src, int lowerRowBound,
+		int upperRowBound) throws IOException {
+		// Current Protobuf protocol is based on 32-bit signed arithmetic, meaning potential problems in files of > 2GB.
+		// see:
+		// https://stackoverflow.com/questions/34128872/google-protobuf-maximum-size#:~:text=Protobuf%20has%20a%20hard%20limit,manually%20if%20you%20need%20to.
+		OutputStream outputStream = fileSystem.create(path, true);
+		SysdsProtos.Frame.Builder frameBuilder = SysdsProtos.Frame.newBuilder();
+		try {
+			Iterator<String[]> stringRowIterator = src.getStringRowIterator(lowerRowBound, upperRowBound);
+			while(stringRowIterator.hasNext()) {
+				String[] row = stringRowIterator.next();
+				frameBuilder.addRowsBuilder().addAllColumnData(Arrays.asList(row));
+			}
+			frameBuilder.build().writeTo(outputStream);
+		}
+		finally {
+			IOUtilFunctions.closeSilently(outputStream);
+		}
+	}
+}
---
+++ src/test/java/org/apache/sysds/test/functions/io/proto/FrameReaderWriterProtoTest.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.test.functions.io.proto;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.apache.sysds.common.Types;
+import org.apache.sysds.runtime.io.FrameReader;
+import org.apache.sysds.runtime.io.FrameReaderFactory;
+import org.apache.sysds.runtime.io.FrameWriter;
+import org.apache.sysds.runtime.io.FrameWriterFactory;
+import org.apache.sysds.runtime.matrix.data.FrameBlock;
+import org.apache.sysds.runtime.util.DataConverter;
+import org.apache.sysds.test.TestUtils;
+import org.junit.Test;
+
+public class FrameReaderWriterProtoTest {
+
+	private static final String FILENAME_SINGLE = "target/testTemp/functions/data/FrameReaderWriterProtoTest/testFrameBlock.proto";
+	private static final long SEED = 4669201;
+
+	private FrameWriter frameWriterProto = FrameWriterFactory.createFrameWriter(Types.FileFormat.PROTO);
+	private FrameReader frameReaderProto = FrameReaderFactory.createFrameReader(Types.FileFormat.PROTO);
+
+	@Test
+	public void testWriteReadFrameBlockWithSinleRowAndSingleColumnFromHDFS() throws IOException {
+		testWriteReadFrameBlockWith(1, 1);
+	}
+
+	@Test
+	public void testWriteReadFrameBlockWithSingleRowAndMultipleColumnsFromHDFS() throws IOException {
+		testWriteReadFrameBlockWith(1, 23);
+	}
+
+	@Test
+	public void testWriteReadFrameBlockWithMultipleRowsAndSingleColumnFromHDFS() throws IOException {
+		testWriteReadFrameBlockWith(21, 1);
+	}
+
+	@Test
+	public void testWriteReadFrameBlockWithSmallMultipleRowsAndMultipleColumnsFromHDFS() throws IOException {
+		testWriteReadFrameBlockWith(42, 35);
+	}
+
+	@Test
+	public void testWriteReadFrameBlockWithMediumMultipleRowsAndMultipleColumnsFromHDFS() throws IOException {
+		testWriteReadFrameBlockWith(694, 164);
+	}
+
+	public void testWriteReadFrameBlockWith(int rows, int cols) throws IOException {
+		final Random random = new Random(SEED);
+		Types.ValueType[] schema = TestUtils.generateRandomSchema(cols, random);
+		FrameBlock expectedFrame = TestUtils.generateRandomFrameBlock(rows, cols, schema, random);
+
+		frameWriterProto.writeFrameToHDFS(expectedFrame, FILENAME_SINGLE, rows, cols);
+		FrameBlock actualFrame = frameReaderProto.readFrameFromHDFS(FILENAME_SINGLE, schema, rows, cols);
+
+		String[][] expected = DataConverter.convertToStringFrame(expectedFrame);
+		String[][] actual = DataConverter.convertToStringFrame(actualFrame);
+
+		TestUtils.compareFrames(expected, actual, rows, cols);
+	}
+}

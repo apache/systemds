1b3e86292631c62a532030144be26fbe6eb46f2d
arnabp <arnab.phani@tugraz.at>
2020-07-18 09:33:37 +0200

    [SYSTEMDS-335] Fix bug in exposed eviction policy names

--- src/main/java/org/apache/sysds/api/DMLOptions.java
+++ src/main/java/org/apache/sysds/api/DMLOptions.java
@@ -131,7 +131,7 @@ public class DMLOptions {
 							dmlOptions.linReuseType = ReuseCacheType.NONE;
 						else if (lineageType.equalsIgnoreCase("policy_lru"))
 							dmlOptions.linCachePolicy = LineageCachePolicy.LRU;
-						else if (lineageType.equalsIgnoreCase("policy_weighted"))
+						else if (lineageType.equalsIgnoreCase("policy_costnsize"))
 							dmlOptions.linCachePolicy = LineageCachePolicy.COSTNSIZE;
 						else if (lineageType.equalsIgnoreCase("policy_hybrid"))
 							dmlOptions.linCachePolicy = LineageCachePolicy.HYBRID;

cd854d7e202a6af831ba0b3818ecc8b65e70692e
Olga Ovcharenko <olga.ovcharenko@student.tugraz.at>
2020-08-15 14:56:44 +0200

    [SYSTEMDS-2621] DBScan and dist builtin
    - Implemented dist for euclidean distance matrix.
    - Implemented DBSCAN
    - Added R dbscan package
    - Fixed error for similar points and all noise points
    - Fixed DBSCAN
    - Updated DBSCAN and added documentation
    - Added tests for dist and DBScan.
    
    Closes #1003.

---
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinDistTest.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.test.functions.builtin;
+
+import org.apache.sysds.common.Types.ExecMode;
+import org.apache.sysds.lops.LopProperties.ExecType;
+import org.apache.sysds.runtime.matrix.data.MatrixValue.CellIndex;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+import org.apache.sysds.test.TestUtils;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class BuiltinDistTest extends AutomatedTestBase
+{
+	private final static String TEST_NAME = "dist";
+	private final static String TEST_DIR = "functions/builtin/";
+	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinDistTest.class.getSimpleName() + "/";
+	
+	private final static double eps = 1e-3;
+	private final static int rows = 1765;
+	private final static double spDense = 0.99;
+	
+	@Override
+	public void setUp() {
+		addTestConfiguration(TEST_NAME,new TestConfiguration(TEST_CLASS_DIR, TEST_NAME,new String[]{"B"})); 
+	}
+
+	@Test
+	public void testDistDefaultCP() { runDist(true, ExecType.CP); }
+	
+	@Test
+	public void testDistSP() {
+		runDist(true, ExecType.SPARK);
+	}
+
+	private void runDist(boolean defaultProb, ExecType instType)
+	{
+		ExecMode platformOld = setExecMode(instType);
+		
+		try
+		{
+			loadTestConfiguration(getTestConfiguration(TEST_NAME));
+			
+			String HOME = SCRIPT_DIR + TEST_DIR;
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[]{"-args", input("A"), output("B") };
+			fullRScriptName = HOME + TEST_NAME + ".R";
+			rCmd = "Rscript" + " " + fullRScriptName + " " + inputDir() + " " + expectedDir();
+			
+			//generate actual dataset 
+			double[][] A = getRandomMatrix(rows, 10, -1, 1, spDense, 7);
+			writeInputMatrixWithMTD("A", A, true);
+			
+			runTest(true, false, null, -1);
+			runRScript(true);
+			
+			//compare matrices
+			HashMap<CellIndex, Double> dmlfile = readDMLMatrixFromHDFS("B");
+			HashMap<CellIndex, Double> rfile  = readRMatrixFromFS("B");
+			TestUtils.compareMatrices(dmlfile, rfile, eps, "Stat-DML", "Stat-R");
+		}
+		finally {
+			rtplatform = platformOld;
+		}
+	}
+}

4d4c3f905caf7689ee761459eb6cd7f8f71d0c96
Janardhan Pulivarthi <j143@protonmail.com>
2020-08-17 08:04:35 +0530

    Now able to call SystemDS via pyspark
    ```
    Using Python version 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020 23:03:10)
    SparkSession available as 'spark'.
    >>> from systemds.mlcontext import MLContext
    >>> ml = MLContext(sc)
    2020-08-17 08:02:49 WARN  MLContext:251 - Apache Spark null or above is recommended for SystemDS null
    
    Welcome to Apache SystemDS!
    
    >>>
    ```

---
+++ src/main/python/systemds/mlcontext/classloader.py
@@ -0,0 +1,64 @@
+
+__all__ = ['createJavaObject']
+
+import os
+import pandas as pd
+
+import py4j.java_gateway
+from pyspark.sql import SparkSession
+
+_initializedSparkSession = False
+def _createJavaObject(sc, obj_type):
+
+    if obj_type == 'mlcontext':
+        return sc._jvm.org.apache.sysds.api.mlcontext.MLContext(sc._jsc)
+    else:
+        return sc._jvm.org.apache.sysds.api.mlcontext.MLContext(sc._jsc)
+        # raise ValueError('Incorrect usage: supported values: mlcontext')
+
+def _getJarFileName(sc, suffix):
+    import imp, fnmatch
+    jar_file_name = '_ignore.jar'
+    java_dir = os.path.join(imp.find_module("systemds")[1], "systemds-java")
+    java_dir = os.path.join(java_dir, "lib")
+    for file in os.listdir(java_dir):
+        if fnmatch.fnmatch(file, 'systemds-*-SNAPSHOT' + suffix + '.jar'):
+            jar_file_name = os.path.join(java_dir, file)
+    return jar_file_name
+
+def _getLoaderInstance(sc, jar_file_name, className, hint):
+    if os.path.isfile(jar_file_name):
+        sc._jsc.addJar(jar_file_name)
+        jar_file_url = sc._jvm.java.io.File(jar_file_name).toURI().toURL()
+        url_class = sc._jvm.java.net.URL
+        jar_file_url_arr = sc._gateway.new_array(url_class, 1)
+        jar_file_url_arr[0] = jar_file_url
+        url_class_loader = sc._jvm.java.net.URLClassLoader(jar_file_url_arr, sc._jsc.getClass().getClassLoader())
+        c1 = sc._jvm.java.lang.Class.forName(className, True, url_class_loader)
+        return c1.newInstance()
+    else:
+        raise ImportError('Hint: Download the jar from http://systemds.apache.org/download')
+
+def createJavaObject(sc, obj_type):
+    """
+    Performs appropriate check if SystemDS.jar is available and
+    returns the handle to MLContext object on JVM
+
+    :param sc: SparkContext
+    :param obj_type: Type of object to create ('mlcontext')
+    :return:
+    """
+    try:
+        return _createJavaObject(sc, obj_type)
+    except (TypeError):
+        ret = None
+        hint = 'Provide the following argument to pyspark: --driver-class-path '
+        # First Load SystemDS
+        jar_file_name = _getJarFileName(sc, '')
+        x = _getLoaderInstance(sc, jar_file_name, 'org.apache.sysds.utils.SystemDSLoaderUtils', hint + 'SystemDS.jar')
+        x.loadSystemDS(jar_file_name)
+        try:
+            ret = _createJavaObject(sc, obj_type)
+        except (py4j.protocol.Py4JError, TypeError):
+            raise ImportError('Hint: ' + hint + jar_file_name)
+        return ret
--- src/main/python/systemds/mlcontext/mlcontext.py
+++ src/main/python/systemds/mlcontext/mlcontext.py
@@ -12,77 +12,16 @@ from typing import Dict, Iterable, Sequence, Tuple, Union
 
 import numpy as np
 from py4j.java_gateway import GatewayParameters, JavaGateway
+from py4j.java_gateway import JavaObject
 from py4j.protocol import Py4JNetworkError
+
+import pyspark.mllib.common
 
 from systemds.utils.helpers import get_module_dir
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
-# class MLContext(object):
-#     """
-#     Create an MLContext object with spark session or Spark Context
-#     """
-#
-#     def __init__(self):
-#         """
-#         Starts a new instance of MLContext
-#         """
-#
-#         systemds_java_path = os.path.join(get_module_dir(), "systemds-java")
-#         # nt means its Windows
-#         cp_separator = ";" if os.name == "nt" else ":"
-#         lib_cp = os.path.join(systemds_java_path, "lib", "*")
-#         systemds_cp = os.path.join(systemds_java_path, "*")
-#         classpath = cp_separator.join([lib_cp, systemds_cp])
-#
-#         command = ["java", "-cp", classpath]
-#
-#         sys_root = os.environ.get("SYSTEMDS_ROOT")
-#         if sys_root != None:
-#             files = glob(os.path.join(sys_root, "conf", "log4j*.properties"))
-#             if len(files) > 1:
-#                 print("WARNING: Multiple logging files")
-#             if len(files) == 0:
-#                 print("WARNING: No log4j file found at: "
-#                       + os.path.join(sys_root, "conf")
-#                       + " therefore using default settings")
-#             else:
-#                 # print("Using Log4J file at " + files[0])
-#                 command.append("-Dlog4j.configuration=file:" + files[0])
-#         else:
-#             print("Default Log4J used, since environment $SYSTEMDS_ROOT not set")
-#
-#         command.append("org.apache.sysds.api.MLContext")
-#
-#         port = self.__get_open_port()
-#         command.append(str(port))
-#
-#         process = Popen(command, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-#
-#         # Py4j connect to the started process.
-#         gateway_parameters = GatewayParameters(
-#             port=port, eager_load=True, read_timeout=5)
-#         self.java_gateway = JavaGateway(
-#             gateway_parameters=gateway_parameters, java_process=process)
-#
-#     def close(self):
-#         """Close the connection to the java process"""
-#         process : Popen = self.java_gateway.java_process
-#         self.java_gateway.shutdown()
-#         os.kill(process.pid, 14)
-#
-#     def __get_open_port(self):
-#         """Get a random available port."""
-#         # TODO Verify that it is not taking some critical ports change to select a good port range.
-#         # TODO If it tries to select a port already in use, find anotherone. (recursion)
-#         # https://stackoverflow.com/questions/2838244/get-open-tcp-port-in-python
-#         import socket
-#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         s.bind(("", 0))
-#         s.listen(1)
-#         port = s.getsockname()[1]
-#         s.close()
-#         return port
-
+from .classloader import *
 
 class MLContext(object):
     """
@@ -133,19 +72,6 @@ class MLContext(object):
                     raise ValueError("path: %s does not exist" % scriptString)
             else:
                 script_java = self._sc._jvm.org.apache.sysml.api.mlcontext.ScriptFactory.dml(scriptString)
-        elif script.scriptType == "pydml":
-            if scriptString.endswith(".pydml"):
-                if scriptString.startswith("http"):
-                    script_java = self._sc._jvm.org.apache.sysml.api.mlcontext.ScriptFactory.pydmlFromUrl(scriptString)
-                elif os.path.exists(scriptString):
-                    script_java = self._sc._jvm.org.apache.sysml.api.mlcontext.ScriptFactory.pydmlFromFile(scriptString)
-                elif script.isResource == True:
-                    script_java = self._sc._jvm.org.apache.sysml.api.mlcontext.ScriptFactory.pydmlFromResource(
-                        scriptString)
-                else:
-                    raise ValueError("path: %s does not exist" % scriptString)
-            else:
-                script_java = self._sc._jvm.org.apache.sysml.api.mlcontext.ScriptFactory.pydml(scriptString)
 
         for key, val in script._input.items():
             # `in` is a reserved word ("keyword") in Python, so `script_java.in(...)` is not

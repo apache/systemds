ea69f55341d9f57d67be7191b07796767a769ee9
Matthias Boehm <mboehm7@gmail.com>
2020-10-31 15:48:39 +0100

    [MINOR] Cleanup warnings (imports, serial IDs, static, formatting)

--- src/main/java/org/apache/sysds/runtime/privacy/propagation/PrivacyPropagator.java
+++ src/main/java/org/apache/sysds/runtime/privacy/propagation/PrivacyPropagator.java
@@ -22,7 +22,6 @@ package org.apache.sysds.runtime.privacy.propagation;
 import java.util.*;
 
 import org.apache.sysds.parser.DataExpression;
-import org.apache.sysds.runtime.DMLRuntimeException;
 import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
 import org.apache.sysds.runtime.instructions.Instruction;
 import org.apache.sysds.runtime.instructions.cp.*;

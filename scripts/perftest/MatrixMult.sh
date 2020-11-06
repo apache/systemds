5399d2a0f1c9077cf5a0bbf5d1e151906e5b53ae
baunsgaard <baunsgaard@tugraz.at>
2020-11-06 15:59:32 +0100

    Add licenses

--- scripts/perftest/MatrixMult.sh
+++ scripts/perftest/MatrixMult.sh
@@ -1,3 +1,25 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#-------------------------------------------------------------
+
 # Import MKL
 if [ -f ~/intel/bin/compilervars.sh ]; then
     . ~/intel/bin/compilervars.sh intel64
@@ -5,13 +27,16 @@ else
     . /opt/intel/bin/compilervars.sh intel64
 fi
 
+# Set properties
 export LOG4JPROP='scripts/perftest/conf/log4j-off.properties'
 export SYSDS_QUIET=1
 
+# Logging output
 LogName='scripts/perftest/results/MM.log'
 mkdir -p 'scripts/perftest/results'
 rm -f $LogName
 
+# Baseline
 perf stat -d -d -d -r 5 \
     systemds scripts/perftest/scripts/MM.dml \
     -config scripts/perftest/conf/std.xml \
@@ -19,6 +44,7 @@ perf stat -d -d -d -r 5 \
     -args 5000 5000 5000 1.0 1.0 3 \
     >>$LogName 2>&1
 
+# MKL
 perf stat -d -d -d -r 5 \
     systemds scripts/perftest/scripts/MM.dml \
     -config scripts/perftest/conf/mkl.xml \
--- scripts/perftest/README.md
+++ scripts/perftest/README.md
@@ -1,13 +1,34 @@
+<!--
+{% comment %}
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to you under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+{% end comment %}
+-->
+
 # Perf tests SystemDS
 
-to run all performance tests for SystemDS, simply download systemds,
-and execute:
+to run all performance tests for SystemDS, simply download systemds, install the prerequisites and execute.
 
 There are a few prerequisites:
 
 - First follow the install guide: <http://apache.github.io/systemds/site/install>
 - Setup Intel MKL: <http://apache.github.io/systemds/site/run>
+- Install Perf stat: <https://linoxide.com/linux-how-to/install-perf-tool-centos-ubuntu/>
 
 ```bash
 ./scripts/perftest/runAll.sh
 ```
+
+look inside the runAll script to see how to run individual tests.

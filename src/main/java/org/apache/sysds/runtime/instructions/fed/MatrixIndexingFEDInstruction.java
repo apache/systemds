873a719d54e9bf7b4fbe2e82e52fb35d2abdf610
Olga <ovcharenko.folga@gmail.com>
2020-11-09 19:39:32 +0100

    Simplified indexing

--- src/main/java/org/apache/sysds/runtime/instructions/fed/MatrixIndexingFEDInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/MatrixIndexingFEDInstruction.java
@@ -19,7 +19,9 @@
 
 package org.apache.sysds.runtime.instructions.fed;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;

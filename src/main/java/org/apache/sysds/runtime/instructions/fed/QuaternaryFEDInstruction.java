7158129297b6ec1292c8365f8c3eb41e95a1eee6
ywcb00 <ywcb00@ywcb.org>
2020-12-05 13:20:01 +0100

    feat(QuaternaryFEDInstruction): change class to be abstract (so that the different quaternary instructions can have it as a base) 	implement static method parseInstruction() 		NOTE: not fully implemented yet 	add protected method checkDataTypes() to check if the inputs are matrices and throw an exceptoin if not 	add TODOs for unclear things of not implemented things
    feat(QuaternaryWCeMMFEDInstruction): add class implementation inherited by QuaternaryFEDInstruction
    	start implementing method processInstruction()
    		NOTE: not fully implemented yet!

--- src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryFEDInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryFEDInstruction.java
@@ -1,30 +1,82 @@
 
-
 package org.apache.sysds.runtime.instructions.fed;
 
-import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
+import org.apache.sysds.lops.WeightedCrossEntropy.WCeMMType;
+import org.apache.sysds.common.Types.DataType;
+import org.apache.sysds.runtime.DMLRuntimeException;
+import org.apache.sysds.runtime.instructions.InstructionUtils;
 import org.apache.sysds.runtime.instructions.cp.CPOperand;
+import org.apache.sysds.runtime.instructions.fed.QuaternaryWCeMMFEDInstruction;
 import org.apache.sysds.runtime.matrix.operators.Operator;
+import org.apache.sysds.runtime.matrix.operators.QuaternaryOperator;
 
-public class QuaternaryFEDInstruction extends ComputationFEDInstruction
+public abstract class QuaternaryFEDInstruction extends ComputationFEDInstruction
 {
-  private CPOperand _input4 = null;
-  private boolean _cacheU = false;
-  private boolean _cacheV = false;
+  protected CPOperand _input4 = null;
+  // TODO: use this variables when adding cache
+  // protected boolean _cacheU = false;
+  // protected boolean _cacheV = false;
 
-  public QuaternaryFEDInstruction(Operator op, CPOperand in1, CPOperand in2, CPOperand in3, CPOperand in4,
-    CPOperand out, boolean cacheU, boolean cacheV, String opcode, String str)
+  protected QuaternaryFEDInstruction(FEDInstruction.FEDType type, Operator operator,
+    CPOperand in1, CPOperand in2, CPOperand in3, CPOperand in4, CPOperand out, String opcode, String instruction_str)
   {
-    super(FEDType.Quaternary, op, in1, in2, in3, out, opcode, str);
+    // TODO: there are only 3 inputs in ComputationFEDInstruction. Add one?
+    super(type, operator, in1, in2, in3, out, opcode, instruction_str);
     _input4 = in4;
-    _cacheU = cacheU;
-    _cacheV = cacheV;
+    // TODO: assign the following variables when adding cache
+    // _cacheU = cacheU;
+    // _cacheV = cacheV;
   }
 
-  @Override
-  public void processInstruction(ExecutionContext ec)
+  public static QuaternaryFEDInstruction parseInstruction(String str)
   {
-    assert false: "Not implemented yet!";
+    String[] parts = InstructionUtils.getInstructionPartsWithValueType(str);
+    String opcode = parts[0];
+
+    // TODO: can we assume the same opcodes as for quaternarySP?
+    if(!InstructionUtils.isDistQuaternaryOpcode(opcode))
+    {
+      throw new DMLRuntimeException("QuaternaryFED.parseInstruction(): Unknown opcode " + opcode);
+    }
+
+    int add_input_4 = (opcode.endsWith("wcemm")) ? 1 : 0;
+
+    CPOperand in1 = new CPOperand(parts[1]);
+    CPOperand in2 = new CPOperand(parts[2]);
+    CPOperand in3 = new CPOperand(parts[3]);
+    CPOperand out = new CPOperand(parts[4] + add_input_4);
+
+    InstructionUtils.checkNumFields(parts, 5 + add_input_4);
+
+    // TODO: can the output data type be checked here?
+
+    // TODO: parse Operator here
+    // Operator operator =
+
+    // TODO: add cacheU and cacheV and other paramters in instruction?
+
+    if(opcode.endsWith("wcemm"))
+    {
+      CPOperand in4 = new CPOperand(parts[4]);
+      checkDataTypes(in1, in2, in3, in4);
+
+      final WCeMMType wcemm_type = WCeMMType.valueOf(parts[6]);
+      QuaternaryOperator quaternary_operator = (wcemm_type.hasFourInputs() ? new QuaternaryOperator(wcemm_type, Double.parseDouble(in4.getName())) : new QuaternaryOperator(wcemm_type));
+
+      return new QuaternaryWCeMMFEDInstruction(quaternary_operator, in1, in2, in3, in4, out, opcode, str);
+    }
+
+    assert false: "Not implemented yet!\n";
+
+    return null;
+  }
+
+  protected static void checkDataTypes(CPOperand in1, CPOperand in2, CPOperand in3, CPOperand in4)
+  {
+    if(in1.getDataType() != DataType.MATRIX || in2.getDataType() != DataType.MATRIX || in3.getDataType() != DataType.MATRIX || in4.getDataType() != DataType.MATRIX)
+    {
+      throw new DMLRuntimeException("Federated quaternary operations supported with matrices only yet");
+    }
   }
 
 }
---
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryWCeMMFEDInstruction.java
@@ -0,0 +1,48 @@
+
+package org.apache.sysds.runtime.instructions.fed;
+
+import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
+import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
+import org.apache.sysds.runtime.controlprogram.federated.FederatedRequest;
+import org.apache.sysds.runtime.controlprogram.federated.FederationUtils;
+import org.apache.sysds.runtime.instructions.cp.CPOperand;
+import org.apache.sysds.runtime.matrix.operators.Operator;
+
+public class QuaternaryWCeMMFEDInstruction extends QuaternaryFEDInstruction
+{
+  // input1 ... federated X
+  // input2 ... U
+  // input3 ... V
+  // _input4 ... W
+  protected QuaternaryWCeMMFEDInstruction(Operator operator,
+    CPOperand in1, CPOperand in2, CPOperand in3, CPOperand in4, CPOperand out, String opcode, String instruction_str)
+  {
+    super(FEDType.Quaternary, operator, in1, in2, in3, in4, out, opcode, instruction_str);
+  }
+
+  @Override
+  public void processInstruction(ExecutionContext execution_context)
+  {
+    MatrixObject matrix_object_1 = execution_context.getMatrixObject(input1);
+    MatrixObject matrix_object_2 = execution_context.getMatrixObject(input2);
+    MatrixObject matrix_object_3 = execution_context.getMatrixObject(input3);
+    // TODO: include _input4
+
+    if(matrix_object_1.isFederated() && !matrix_object_2.isFederated() && !matrix_object_3.isFederated())
+    {
+      FederatedRequest fed_req_1 = matrix_object_1.getFedMapping().broadcast(matrix_object_2);
+      FederatedRequest fed_req_2 = matrix_object_1.getFedMapping().broadcast(matrix_object_3);
+      FederatedRequest fed_req_3 = FederationUtils.callInstruction(instString, output,
+        new CPOperand[]{input1, input2, input3},
+        new long[]{matrix_object_1.getFedMapping().getID(), fed_req_1.getID(), fed_req_2.getID()});
+      FederatedRequest fed_req_4 = matrix_object_1.getFedMapping().cleanup(getTID(), fed_req_1.getID());
+      FederatedRequest fed_req_5 = matrix_object_1.getFedMapping().cleanup(getTID(), fed_req_2.getID());
+      matrix_object_1.getFedMapping().execute(getTID(), true, fed_req_1, fed_req_2, fed_req_3, fed_req_4, fed_req_5);
+
+      // TODO: continue here
+    }
+
+    assert false: "Not implemented yet!\n";
+  }
+
+}

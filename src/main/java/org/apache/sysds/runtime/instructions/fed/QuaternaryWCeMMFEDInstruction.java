74febc0b7d314d239b20b3ef03870547378111b7
ywcb00 <ywcb00@ywcb.org>
2020-12-15 14:55:00 +0100

    feat(FEDInstructionUtils.java): add if block for rewriting the cp wcemm instruction to federated
    feat(QuaternaryFEDInstruction.java): parse the inputs and outputs according to cp wcemm instruction
    
    feat(QuaternaryWCeMMFEDInstruction.java): include input 4 and remove Future<>

--- src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryWCeMMFEDInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryWCeMMFEDInstruction.java
@@ -22,11 +22,10 @@ package org.apache.sysds.runtime.instructions.fed;
 import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
 import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
 import org.apache.sysds.runtime.controlprogram.federated.FederatedRequest;
-import org.apache.sysds.runtime.controlprogram.federated.FederatedResponse;
 import org.apache.sysds.runtime.controlprogram.federated.FederationUtils;
 import org.apache.sysds.runtime.instructions.cp.CPOperand;
+import org.apache.sysds.runtime.instructions.cp.ScalarObject;
 import org.apache.sysds.runtime.matrix.operators.Operator;
-import java.util.concurrent.Future;
 
 public class QuaternaryWCeMMFEDInstruction extends QuaternaryFEDInstruction
 {
@@ -46,26 +45,28 @@ public class QuaternaryWCeMMFEDInstruction extends QuaternaryFEDInstruction
     MatrixObject matrix_object_1 = execution_context.getMatrixObject(input1);
     MatrixObject matrix_object_2 = execution_context.getMatrixObject(input2);
     MatrixObject matrix_object_3 = execution_context.getMatrixObject(input3);
-    // TODO: include _input4
+    // MatrixObject matrix_object_4 = execution_context.getMatrixObject(_input4);
+    ScalarObject scalar_object_4 = execution_context.getScalarInput(_input4);
 
     if(matrix_object_1.isFederated() && !matrix_object_2.isFederated() && !matrix_object_3.isFederated())
     {
-      FederatedRequest fed_req_1 = matrix_object_1.getFedMapping().broadcast(matrix_object_2);
-      FederatedRequest fed_req_2 = matrix_object_1.getFedMapping().broadcast(matrix_object_3);
-      FederatedRequest fed_req_3 = FederationUtils.callInstruction(instString, output,
-        new CPOperand[]{input1, input2, input3},
-        new long[]{matrix_object_1.getFedMapping().getID(), fed_req_1.getID(), fed_req_2.getID()});
-      FederatedRequest fed_req_4 = matrix_object_1.getFedMapping().cleanup(getTID(), fed_req_1.getID());
-      FederatedRequest fed_req_5 = matrix_object_1.getFedMapping().cleanup(getTID(), fed_req_2.getID());
+      FederatedRequest fed_req_init_1 = matrix_object_1.getFedMapping().broadcast(matrix_object_2);
+      FederatedRequest fed_req_init_2 = matrix_object_1.getFedMapping().broadcast(matrix_object_3);
+      // FederatedRequest fed_req_init_3 = matrix_object_1.getFedMapping().broadcast(matrix_object_4);
+      FederatedRequest fed_req_init_3 = matrix_object_1.getFedMapping().broadcast(scalar_object_4);
+      FederatedRequest fed_req_compute_1 = FederationUtils.callInstruction(instString, output,
+        new CPOperand[]{input1, input2, input3, _input4},
+        new long[]{matrix_object_1.getFedMapping().getID(), fed_req_init_1.getID(), fed_req_init_2.getID(), fed_req_init_3.getID()});
+      FederatedRequest fed_req_cleanup_1 = matrix_object_1.getFedMapping().cleanup(getTID(), fed_req_init_1.getID());
+      FederatedRequest fed_req_cleanup_2 = matrix_object_1.getFedMapping().cleanup(getTID(), fed_req_init_2.getID());
+      FederatedRequest fed_req_cleanup_3 = matrix_object_1.getFedMapping().cleanup(getTID(), fed_req_init_3.getID());
 
       // execute federated instructions
-      Future<FederatedResponse>[] tmp = matrix_object_1.getFedMapping().execute(getTID(), true, fed_req_1, fed_req_2, fed_req_3, fed_req_4, fed_req_5);
+      matrix_object_1.getFedMapping().execute(getTID(), true, fed_req_init_1, fed_req_init_2, fed_req_init_3, fed_req_compute_1, fed_req_cleanup_1, fed_req_cleanup_2, fed_req_cleanup_3);
 
       // TODO: do something with the output
 
     }
-
-    assert false: "Not implemented yet!\n";
   }
 
 }

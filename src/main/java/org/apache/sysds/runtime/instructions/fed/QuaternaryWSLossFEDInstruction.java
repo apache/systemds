4b8dba2aef6704174334997a4ba13083e0350291
ywcb00 <ywcb00@ywcb.org>
2020-12-29 18:01:44 +0100

    refactor(QuaternaryWSLossFEDInstruction.java): refactor code like suggested in the last PR review

--- src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryWSLossFEDInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryWSLossFEDInstruction.java
@@ -19,16 +19,17 @@
 
 package org.apache.sysds.runtime.instructions.fed;
 
-import org.apache.sysds.runtime.controlprogram.federated.FederatedRequest.RequestType;
-import org.apache.sysds.runtime.matrix.operators.AggregateUnaryOperator;
-import org.apache.sysds.runtime.instructions.InstructionUtils;
 import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
 import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
 import org.apache.sysds.runtime.controlprogram.federated.FederatedRequest;
+import org.apache.sysds.runtime.controlprogram.federated.FederatedRequest.RequestType;
 import org.apache.sysds.runtime.controlprogram.federated.FederatedResponse;
 import org.apache.sysds.runtime.controlprogram.federated.FederationMap;
 import org.apache.sysds.runtime.controlprogram.federated.FederationUtils;
+import org.apache.sysds.runtime.DMLRuntimeException;
 import org.apache.sysds.runtime.instructions.cp.CPOperand;
+import org.apache.sysds.runtime.instructions.InstructionUtils;
+import org.apache.sysds.runtime.matrix.operators.AggregateUnaryOperator;
 import org.apache.sysds.runtime.matrix.operators.Operator;
 import org.apache.sysds.runtime.matrix.operators.QuaternaryOperator;
 
@@ -47,67 +48,66 @@ public class QuaternaryWSLossFEDInstruction extends QuaternaryFEDInstruction
 	}
 
 	@Override
-	public void processInstruction(ExecutionContext execution_context)
+	public void processInstruction(ExecutionContext ec)
 	{
 		QuaternaryOperator qop = (QuaternaryOperator) _optr;
 
-		MatrixObject matrix_object_1 = execution_context.getMatrixObject(input1);
-		MatrixObject matrix_object_2 = execution_context.getMatrixObject(input2);
-		MatrixObject matrix_object_3 = execution_context.getMatrixObject(input3);
-		MatrixObject matrix_object_4 = null;
-		if(qop.hasFourInputs())
-		{
-			matrix_object_4 = execution_context.getMatrixObject(_input4);
+		MatrixObject X = ec.getMatrixObject(input1);
+		MatrixObject U = ec.getMatrixObject(input2);
+		MatrixObject V = ec.getMatrixObject(input3);
+		MatrixObject W = null;
+		if(qop.hasFourInputs()) {
+			W = ec.getMatrixObject(_input4);
 		}
 
-		if(matrix_object_1.isFederated() && !matrix_object_2.isFederated() && !matrix_object_3.isFederated() && (matrix_object_4 == null || !matrix_object_4.isFederated()))
+		if(!(X.isFederated() && !U.isFederated() && !V.isFederated() && (W == null || !W.isFederated())))
+			throw new DMLRuntimeException("Unsupported federated inputs (X, U, V, W) = ("
+				+X.isFederated()+", "+U.isFederated()+", "+V.isFederated() + (W != null ? W.isFederated() : "none") +")");
+
+		FederationMap fedMap = X.getFedMapping();
+		FederatedRequest[] frInit1 = fedMap.broadcastSliced(U, false);
+		FederatedRequest frInit2 = fedMap.broadcast(V);
+		FederatedRequest[] frInit3 = null;
+		FederatedRequest frCompute1 = null;
+
+		if(W != null)
 		{
-			FederationMap federation_mapping = matrix_object_1.getFedMapping();
-			FederatedRequest[] fed_req_init_1 = federation_mapping.broadcastSliced(matrix_object_2, false);
-			FederatedRequest fed_req_init_2 = federation_mapping.broadcast(matrix_object_3);
-			FederatedRequest[] fed_req_init_3 = null;
-			FederatedRequest fed_req_compute_1 = null;
-
-			if(matrix_object_4 != null)
-			{
-				fed_req_init_3 = federation_mapping.broadcastSliced(matrix_object_4, false);
-				fed_req_compute_1 = FederationUtils.callInstruction(instString, output,
-					new CPOperand[]{input1, input2, input3, _input4},
-					new long[]{federation_mapping.getID(), fed_req_init_1[0].getID(), fed_req_init_2.getID(), fed_req_init_3[0].getID()});
-			}
-			else
-			{
-				fed_req_compute_1 = FederationUtils.callInstruction(instString, output,
-					new CPOperand[]{input1, input2, input3},
-					new long[]{federation_mapping.getID(), fed_req_init_1[0].getID(), fed_req_init_2.getID()});
-			}
-			FederatedRequest fed_req_get_1 = new FederatedRequest(RequestType.GET_VAR, fed_req_compute_1.getID());
-			FederatedRequest fed_req_cleanup_1 = federation_mapping.cleanup(getTID(), fed_req_compute_1.getID());
-			FederatedRequest fed_req_cleanup_2 = federation_mapping.cleanup(getTID(), fed_req_init_1[0].getID());
-			FederatedRequest fed_req_cleanup_3 = federation_mapping.cleanup(getTID(), fed_req_init_2.getID());
-
-			Future<FederatedResponse>[] response;
-			if(fed_req_init_3 != null)
-			{
-				FederatedRequest fed_req_cleanup_4 = federation_mapping.cleanup(getTID(), fed_req_init_3[0].getID());
-				// execute federated instructions
-				federation_mapping.execute(getTID(), true, fed_req_init_1, fed_req_init_2);
-				response = federation_mapping.execute(getTID(), true, fed_req_init_3,
-					fed_req_compute_1, fed_req_get_1,
-					fed_req_cleanup_1, fed_req_cleanup_2, fed_req_cleanup_3, fed_req_cleanup_4);
-			}
-			else
-			{
-				// execute federated instructions
-				response = federation_mapping.execute(getTID(), true, fed_req_init_1, fed_req_init_2,
-					fed_req_compute_1, fed_req_get_1,
-					fed_req_cleanup_1, fed_req_cleanup_2, fed_req_cleanup_3);
-			}
-
-			AggregateUnaryOperator aop = InstructionUtils.parseBasicAggregateUnaryOperator("uak+");
-
-			execution_context.setVariable(output.getName(), FederationUtils.aggScalar(aop, response));
+			frInit3 = fedMap.broadcastSliced(W, false);
+			frCompute1 = FederationUtils.callInstruction(instString, output,
+				new CPOperand[]{input1, input2, input3, _input4},
+				new long[]{fedMap.getID(), frInit1[0].getID(), frInit2.getID(), frInit3[0].getID()});
 		}
+		else
+		{
+			frCompute1 = FederationUtils.callInstruction(instString, output,
+				new CPOperand[]{input1, input2, input3},
+				new long[]{fedMap.getID(), frInit1[0].getID(), frInit2.getID()});
+		}
+		FederatedRequest frGet1 = new FederatedRequest(RequestType.GET_VAR, frCompute1.getID());
+		FederatedRequest frCleanup1 = fedMap.cleanup(getTID(), frCompute1.getID());
+		FederatedRequest frCleanup2 = fedMap.cleanup(getTID(), frInit1[0].getID());
+		FederatedRequest frCleanup3 = fedMap.cleanup(getTID(), frInit2.getID());
+
+		Future<FederatedResponse>[] response;
+		if(frInit3 != null)
+		{
+			FederatedRequest frCleanup4 = fedMap.cleanup(getTID(), frInit3[0].getID());
+			// execute federated instructions
+			fedMap.execute(getTID(), true, frInit1, frInit2);
+			response = fedMap.execute(getTID(), true, frInit3,
+				frCompute1, frGet1,
+				frCleanup1, frCleanup2, frCleanup3, frCleanup4);
+		}
+		else
+		{
+			// execute federated instructions
+			response = fedMap.execute(getTID(), true, frInit1, frInit2,
+				frCompute1, frGet1,
+				frCleanup1, frCleanup2, frCleanup3);
+		}
+
+		AggregateUnaryOperator aop = InstructionUtils.parseBasicAggregateUnaryOperator("uak+");
+
+		ec.setVariable(output.getName(), FederationUtils.aggScalar(aop, response));
 	}
-
 }

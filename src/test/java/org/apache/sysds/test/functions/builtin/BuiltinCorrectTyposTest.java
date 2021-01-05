4e65ec2f6efbbeb7ae686b3dd16e99c039435ab2
Tim Sagaster <tim.sagaster@student.tugraz.at>
2021-01-05 22:35:44 +0100

    added methods to corrupt data

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinCorrectTyposTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinCorrectTyposTest.java
@@ -34,6 +34,11 @@ import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.apache.sysds.test.TestUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 import java.io.IOException;
 
 public class BuiltinCorrectTyposTest extends AutomatedTestBase 
@@ -43,6 +48,16 @@ public class BuiltinCorrectTyposTest extends AutomatedTestBase
 	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinCorrectTyposTest.class.getSimpleName() + "/";
 	
 	private final static Types.ValueType[] schema = {Types.ValueType.STRING};
+
+	private final static int numberDataPoints = 1000;
+	private final static int maxFrequencyErrors = 40;
+	// for every error number below between (1 and maxFrequencyErrors) identical errors are made
+	// errors can still overlap though
+	private final static int numberCharSwaps = 10;
+	private final static int numberCharChanges = 10;
+	private final static int numberCharAdds = 10;
+	private final static int numberCharDeletions = 10;
+	private final static int numberWrongCapitalizations = 10;
 
 	// private final static double eps = 1e-10;
 	// private final static int rows = 70;
@@ -109,10 +124,151 @@ public class BuiltinCorrectTyposTest extends AutomatedTestBase
     }
 	}
 
-  private static int initFrameData(FrameBlock frame) {
-    String[] strings = new String[] {"AuStRiA", "Austria", "AUSTRIA", "India", "IIT", "INDIA", "India", "Pakistan", "PakistaN", "Austria", "Austria"};
-    frame.appendColumn(strings);
-
-    return strings.length;
+	private static int initFrameData(FrameBlock frame) {
+		List<Integer> bins = new ArrayList<Integer>();
+		String[] correctStrings = getCorrectData(numberDataPoints, bins);
+		String[] corruptedStrings = getCorruptedData(correctStrings, bins, maxFrequencyErrors, numberCharSwaps, numberCharChanges, numberCharAdds, numberCharDeletions, numberWrongCapitalizations);
+		frame.appendColumn(corruptedStrings);
+		return corruptedStrings.length;
   }
+
+
+	private static String[] getCorrectData(int numberDataPoints, List<Integer> bins) {
+		//String[] allCountries = new String[] {"Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "Estonia", "Finland", "France", "Georgia", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Monaco", "Montenegro", "Netherlands", "North Macedonia (formerly Macedonia)", "Norway", "Poland", "Portugal", "Romania", "Russia", "San Marino", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine", "United Kingdom (UK)", "Vatican City (Holy See)"};
+
+		String[] allCountries = new String[] {"Austria", "Belarus", "Denmark", "Germany", "Italy", "Liechtenstein"};
+
+		//String[] allCountries = new String[] {"A", "1"};
+
+		List<String> chosenCountries = new ArrayList<String>();
+		int remainingDataPoints = numberDataPoints;
+		bins.add(0);
+		for (int i = 0; i < allCountries.length-1; i++) {
+			int rand = ThreadLocalRandom.current().nextInt(0, (int)(remainingDataPoints/Math.sqrt(allCountries.length)));
+			for (int j = 0; j < rand; j++) {
+				chosenCountries.add(allCountries[i]);
+			}
+			remainingDataPoints -= rand;
+			bins.add(numberDataPoints - remainingDataPoints);
+		}
+		for (int i = 0; i < remainingDataPoints; i++) {
+			chosenCountries.add(allCountries[allCountries.length - 1]);
+		}
+		bins.add(numberDataPoints);
+		String[] string_array = new String[chosenCountries.size()];
+		chosenCountries.toArray(string_array);
+		return string_array;
+	}
+
+	private static String[] getCorruptedData(String[] data, List<Integer> bins, int maxFrequencyErrors, int numberSwaps, int numberChanges, int numberAdds, int numberDeletions, int numberWrongCapitalizations) {
+		for (int i = 0; i < numberSwaps; i++) {
+			makeSwapErrors(data, bins, maxFrequencyErrors);
+		}
+		for (int i = 0; i < numberChanges; i++) {
+			makeChangeErrors(data, bins, maxFrequencyErrors);
+		}
+		for (int i = 0; i < numberAdds; i++) {
+			makeAddErrors(data, bins, maxFrequencyErrors);
+		}
+		for (int i = 0; i < numberDeletions; i++) {
+			makeDeletionErrors(data, bins, maxFrequencyErrors);
+		}
+		for (int i = 0; i < numberWrongCapitalizations; i++) {
+			makeCapitalizationErrors(data, bins, maxFrequencyErrors);
+		}
+		return data;
+	}
+
+
+
+	private static void makeSwapErrors(String[] data, List<Integer> bins, int maxFrequencyErrors){
+		int randIndex = ThreadLocalRandom.current().nextInt(0, data.length);
+		int leftBinIndex = Integer.max(0, (-Collections.binarySearch(bins, randIndex) - 2));
+		int rightBinIndex = Integer.max(1, (-Collections.binarySearch(bins, randIndex) - 1));
+		int leftIndex = bins.get(leftBinIndex);
+		int rightIndex = bins.get(rightBinIndex) - 1;
+
+		int charPos = ThreadLocalRandom.current().nextInt(1, data[randIndex].length());
+		for (int j = 0; j < ThreadLocalRandom.current().nextInt(1, maxFrequencyErrors + 1); j++) {
+			int randErrorIndex = ThreadLocalRandom.current().nextInt(leftIndex, rightIndex + 1);
+			String str = data[randErrorIndex];
+			if (str.length() > 1 && charPos < str.length()) {
+				data[randErrorIndex] = str.substring(0, charPos - 1) + str.charAt(charPos) + str.charAt(charPos - 1) + str.substring(charPos + 1);
+			}
+		}
+	}
+
+	private static void makeChangeErrors(String[] data, List<Integer> bins, int maxFrequencyErrors){
+		int randIndex = ThreadLocalRandom.current().nextInt(0, data.length);
+		int leftBinIndex = Integer.max(0, (-Collections.binarySearch(bins, randIndex) - 2));
+		int rightBinIndex = Integer.max(1, (-Collections.binarySearch(bins, randIndex) - 1));
+		int leftIndex = bins.get(leftBinIndex);
+		int rightIndex = bins.get(rightBinIndex) - 1;
+
+		int charPos = ThreadLocalRandom.current().nextInt(0, data[randIndex].length());
+		String newChar = Character.toString((char)ThreadLocalRandom.current().nextInt('a', 'z' + 1));
+		for (int j = 0; j < ThreadLocalRandom.current().nextInt(1, maxFrequencyErrors + 1); j++) {
+			int randErrorIndex = ThreadLocalRandom.current().nextInt(leftIndex, rightIndex + 1);
+			String str = data[randErrorIndex];
+			if (str.length() > 0 && charPos < str.length()) {
+				data[randErrorIndex] =  str.substring(0, charPos) + newChar + str.substring(charPos + 1);
+			}
+		}
+	}
+
+	private static void makeAddErrors(String[] data, List<Integer> bins, int maxFrequencyErrors){
+		int randIndex = ThreadLocalRandom.current().nextInt(0, data.length);
+		int leftBinIndex = Integer.max(0, (-Collections.binarySearch(bins, randIndex) - 2));
+		int rightBinIndex = Integer.max(1, (-Collections.binarySearch(bins, randIndex) - 1));
+		int leftIndex = bins.get(leftBinIndex);
+		int rightIndex = bins.get(rightBinIndex) - 1;
+
+		int charPos = ThreadLocalRandom.current().nextInt(0, data[randIndex].length() + 1);
+		String newChar = Character.toString((char)ThreadLocalRandom.current().nextInt('a', 'z' + 1));
+		for (int j = 0; j < ThreadLocalRandom.current().nextInt(1, maxFrequencyErrors + 1); j++) {
+			int randErrorIndex = ThreadLocalRandom.current().nextInt(leftIndex, rightIndex + 1);
+			String str = data[randErrorIndex];
+			if (str.length() > 0 && charPos < str.length() + 1) {
+				data[randErrorIndex] =  str.substring(0, charPos) + newChar + str.substring(charPos);
+			}
+		}
+	}
+
+	private static void makeDeletionErrors(String[] data, List<Integer> bins, int maxFrequencyErrors){
+		int randIndex = ThreadLocalRandom.current().nextInt(0, data.length);
+		int leftBinIndex = Integer.max(0, (-Collections.binarySearch(bins, randIndex) - 2));
+		int rightBinIndex = Integer.max(1, (-Collections.binarySearch(bins, randIndex) - 1));
+		int leftIndex = bins.get(leftBinIndex);
+		int rightIndex = bins.get(rightBinIndex) - 1;
+
+		int charPos = ThreadLocalRandom.current().nextInt(0, data[randIndex].length());
+		for (int j = 0; j < ThreadLocalRandom.current().nextInt(1, maxFrequencyErrors + 1); j++) {
+			int randErrorIndex = ThreadLocalRandom.current().nextInt(leftIndex, rightIndex + 1);
+			String str = data[randErrorIndex];
+			if (str.length() > 1 && charPos < str.length()) {
+				data[randErrorIndex] =  str.substring(0, charPos) + str.substring(charPos + 1);
+			}
+		}
+	}
+
+	private static void makeCapitalizationErrors(String[] data, List<Integer> bins, int maxFrequencyErrors){
+		int randIndex = ThreadLocalRandom.current().nextInt(0, data.length);
+		int leftBinIndex = Integer.max(0, (-Collections.binarySearch(bins, randIndex) - 2));
+		int rightBinIndex = Integer.max(1, (-Collections.binarySearch(bins, randIndex) - 1));
+		int leftIndex = bins.get(leftBinIndex);
+		int rightIndex = bins.get(rightBinIndex) - 1;
+
+		for (int j = 0; j < ThreadLocalRandom.current().nextInt(1, maxFrequencyErrors + 1); j++) {
+			int randErrorIndex = ThreadLocalRandom.current().nextInt(leftIndex, rightIndex + 1);
+			String str = data[randErrorIndex];
+			if (str.length() > 0) {
+				if (Character.isUpperCase(str.charAt(0))) {
+					data[randErrorIndex] = str.substring(0, 1).toLowerCase() + str.substring(1);
+				}
+				else {
+					data[randErrorIndex] = str.substring(0, 1).toUpperCase() + str.substring(1);
+				}
+			}
+		}
+	}
 }

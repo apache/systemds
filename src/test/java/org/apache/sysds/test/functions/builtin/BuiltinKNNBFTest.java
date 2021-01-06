8c23e91aaf8bcd9878e332c69d96fa29d2d0793c
ywcb00 <ywcb00@ywcb.org>
2021-01-06 16:28:05 +0100

    feat(knnbf): add test files and script

---
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinKNNBFTest.java
@@ -0,0 +1,125 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.test.functions.builtin;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.sysds.common.Types.ExecMode;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.runtime.meta.MatrixCharacteristics;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+import org.apache.sysds.test.TestUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class BuiltinKNNBFTest extends AutomatedTestBase
+{
+  private final static Log LOG = LogFactory.getLog(BuiltinKNNBFTest.class.getName());
+
+  private final static String TEST_NAME = "knnbf";
+  private final static String TEST_DIR = "functions/builtin/";
+  private final static String TEST_CLASS_DIR = TEST_DIR + BuiltinKNNBFTest.class.getSimpleName() + "/";
+
+  private final static String OUTPUT_NAME = "B";
+
+  private final static Double TEST_TOLERANCE = 1e-9;
+
+  @Parameterized.Parameter()
+  public int rows;
+  @Parameterized.Parameter(1)
+  public int cols;
+  @Parameterized.Parameter(2)
+  public int query_rows;
+  @Parameterized.Parameter(3)
+  public int query_cols;
+  @Parameterized.Parameter(4)
+  public boolean continuous;
+  @Parameterized.Parameter(5)
+  public int k_value;
+  @Parameterized.Parameter(6)
+  public double sparsity;
+
+  @Override
+  public void setUp()
+  {
+    addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {OUTPUT_NAME}));
+  }
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data()
+  {
+    return Arrays.asList(new Object[][] {
+      // {rows, cols, query_rows, query_cols, continuous, k_value, sparsity}
+      // {1000, 500, 35, 450, true, 7, 0.1},
+      // {1000, 500, 35, 450, true, 7, 0.9}
+      {10, 10, 10, 10, true, 7, 1}
+    });
+  }
+
+  @Test
+  public void testKNN()
+  {
+    runKNNTest(ExecMode.SINGLE_NODE);
+  }
+
+  private void runKNNTest(ExecMode exec_mode)
+  {
+    ExecMode platform_old = setExecMode(exec_mode);
+
+    getAndLoadTestConfiguration(TEST_NAME);
+
+    String HOME = SCRIPT_DIR + TEST_DIR;
+
+    double[][] X = getRandomMatrix(rows, cols, 0, 1, sparsity, 431);
+    double[][] T = getRandomMatrix(query_rows, query_cols, 0, 1, 1, 65);
+    double[][] CL = getRandomMatrix(rows, 1, 0, 1, 1, 7);
+
+    writeInputMatrixWithMTD("X", X, true);
+    writeInputMatrixWithMTD("T", T, true);
+    writeInputMatrixWithMTD("CL", CL, true);
+
+    fullDMLScriptName = HOME + TEST_NAME + ".dml";
+    programArgs = new String[] {"-nvargs",
+      "in_X=" + input("X"), "in_T=" + input("T"), "in_continuous=" + (continuous ? "1" : "0"), "in_k=" + Integer.toString(k_value),
+      "out_B=" + output(OUTPUT_NAME)};
+
+    fullRScriptName = HOME + TEST_NAME + ".R";
+    rCmd = getRCmd(inputDir(), (continuous ? "1" : "0"), Integer.toString(k_value),
+			expectedDir());
+
+
+    System.out.println(programArgs);
+    // TODO: add this line when both test scripts are implemented
+    runTest(true, false, null, -1);
+    runRScript(true);
+
+    // TODO: add this line when both test scripts are implemented
+    compareResultsWithR(TEST_TOLERANCE);
+
+    // restore execution mode
+    setExecMode(platform_old);
+  }
+}
---
+++ src/test/scripts/functions/builtin/knnbf.R
@@ -0,0 +1,75 @@
+#-------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#-------------------------------------------------------------
+# TODO arguments and order
+args <- commandArgs(TRUE)
+library("Matrix")
+
+# read test data
+data_train            <- as.matrix(readMM(paste(args[1], "/X.mtx", sep="")))
+data_test             <- as.matrix(readMM(paste(args[1], "/T.mtx", sep="")))
+CL                    <- as.matrix(readMM(paste(args[1], "/CL.mtx", sep="")))
+
+# str(data_train)
+# str(data_test)
+# str(CL)
+## depends how i get the labels/catagories, maybe they are in the training/testing set
+# data_train_labels     <- as.matrix(read.csv(args[3], stringsAsFactors = FALSE))
+# data_test_labels      <- as.matrix(read.csv(args[4], stringsAsFactors = FALSE))
+
+is_continuous <- as.integer(args[2])
+K <- as.integer(args[3])
+
+# ---- normalize -----
+normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x))) }
+
+if(is_continuous == 1)
+{
+  # normalize all but last col (last is our target col)
+  data_train_n <- as.data.frame(lapply(data_train[1:NCOL(data_train)], normalize))
+  data_test_n  <- as.data.frame(lapply(data_test[1:NCOL(data_test)], normalize))
+}
+
+# get the labels, last col
+data_train_labels <- CL
+# data_train_labels     <- CL[1:nrow(data_train),1] #data_train[1:NROW(data_train), NCOL(data_train)]
+# data_test_labels      <- CL[nrow(data_train)+1:nrow(data_test),1]
+
+table(CL)
+#print(K)
+#print(dim(data_train))
+#print(dim(data_test))
+#print(dim(CL))
+
+# ------ training -------
+#install.packages("class")
+library(class)
+test_pred <- knn(train= data_train, test= data_test, cl= data_train_labels, k=K)
+print("-----------")
+print(test_pred)
+print("-----------")
+# NNR is native NNR, do we realy need to test that?
+writeMM(as(test_pred, "CsparseMatrix"), paste(args[4], "B", sep=""));
+
+## feature importance with random forest
+#install.packages("randomForest")
+#library(randomForest)
+#rf <- randomForest(as.factor(CL)~., data=dat)
+#rf$importance

75525f24d6e455196d8693174f0a7cc676960ca8
Dominic "Dodo" Schablas <dodothedeveloper@gmail.com>
2021-01-12 18:27:13 +0100

    Removing BayesianOptimisation from builting function and moving the files into the staging directory.
    * Moving new implementation to the correct directory.
    * Removing the uneeded bayesianOptimisationLM.dml file, which would call
      the bayesianOptimisation.dml script.

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinBayesianOptimisationTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinBayesianOptimisationTest.java
@@ -11,7 +11,8 @@ import org.apache.sysds.lops.LopProperties.ExecType;
 
 public class BuiltinBayesianOptimisationTest extends AutomatedTestBase {
 
-	private final static String TEST_NAME = "bayesianOptimisationLM";
+	private final static String TEST_NAME = "bayesianOptimisation";
+	//private final static String TEST_DIR = "./scripts/staging/functions/bayesian_optimisation/";
 	private final static String TEST_DIR = "functions/builtin/";
     private final static String TEST_CLASS_DIR = TEST_DIR + BuiltinBayesianOptimisationTest.class.getSimpleName() + "/";
 
@@ -21,7 +22,8 @@ public class BuiltinBayesianOptimisationTest extends AutomatedTestBase {
 	@Override
 	public void setUp()
 	{
-		addTestConfiguration( TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] { "R" })   );
+		//addTestConfiguration( TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] { "R" })   );
+        addTestConfiguration(TEST_DIR, TEST_NAME);
 	}
 
     @Test
@@ -33,10 +35,10 @@ public class BuiltinBayesianOptimisationTest extends AutomatedTestBase {
 		{
 			TestConfiguration config = getTestConfiguration(TEST_NAME);
 			loadTestConfiguration(config);
-			String HOME = SCRIPT_DIR + TEST_DIR;
-
-			fullDMLScriptName = HOME + TEST_NAME + ".dml";
-			programArgs = new String[] {"-args", "testFunc", input("X"), input("y"), "10" ,"2", output("R")};
+            String HOME = SCRIPT_DIR + TEST_DIR;
+		//fullDMLScriptName = TEST_DIR + TEST_NAME;
+			fullDMLScriptName = "./scripts/staging/bayesian_optimisation/bayesianOptimisation.dml"; //HOME + TEST_NAME + ".dml";
+			programArgs = new String[] {"-args", "testFunc", input("X"), input("y"), "iterations=10" , output("R")};
 			double[][] X = getRandomMatrix(rows, cols, 0, 1, 0.8, -1);
 			double[][] y = getRandomMatrix(rows, 1, 0, 1, 0.8, -1);
 			writeInputMatrixWithMTD("X", X, true);

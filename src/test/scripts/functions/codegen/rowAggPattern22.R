69fc5e6eafcc3264bad8ea6302de3f3dd0da178e
Mark Dokter <mark@dokter.cc>
2021-01-14 16:30:57 +0100

    raggt22 debug/test dml code for sparse

--- src/test/scripts/functions/codegen/rowAggPattern22.R
+++ src/test/scripts/functions/codegen/rowAggPattern22.R
@@ -24,9 +24,11 @@ options(digits=22)
 library("Matrix")
 library("matrixStats")
 
-X = matrix(seq(1,300), 60, 5, byrow=TRUE);
-# X[,5:45] = matrix(0, 60, 41);
-Xmax = ((rowMaxs(X)%*%matrix(1,1,5))+0.5)
+X = matrix(seq(1,3000), 60, 50, byrow=TRUE);
+X[,5:45] = matrix(0, 60, 41);
+# X[,2:4] = matrix(0, 60, 3);
+
+Xmax = ((rowMaxs(X)%*%matrix(1,1,50))+0.5)
 R = ((7+X)+(X-7)+(X^2<=7))/Xmax
 
 print.table(R[1,], digits=3, zero.print = ".")

67d3574be63094d3dc519d0b6cc1276ec7816ca6
ywcb00 <ywcb00@ywcb.org>
2021-01-16 16:26:59 +0100

    refactor(QuaternaryFEDInstruction.java): replace hardcoded opcode strings with constants from lops

--- src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryFEDInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/QuaternaryFEDInstruction.java
@@ -22,10 +22,17 @@ package org.apache.sysds.runtime.instructions.fed;
 import org.apache.sysds.common.Types.DataType;
 import org.apache.sysds.common.Types.ExecType;
 import org.apache.sysds.lops.Lop;
+import org.apache.sysds.lops.WeightedCrossEntropy;
 import org.apache.sysds.lops.WeightedCrossEntropy.WCeMMType;
+import org.apache.sysds.lops.WeightedDivMM;
+import org.apache.sysds.lops.WeightedDivMMR;
 import org.apache.sysds.lops.WeightedDivMM.WDivMMType;
+import org.apache.sysds.lops.WeightedSigmoid;
 import org.apache.sysds.lops.WeightedSigmoid.WSigmoidType;
+import org.apache.sysds.lops.WeightedSquaredLoss;
+import org.apache.sysds.lops.WeightedSquaredLossR;
 import org.apache.sysds.lops.WeightedSquaredLoss.WeightsType;
+import org.apache.sysds.lops.WeightedUnaryMM;
 import org.apache.sysds.lops.WeightedUnaryMM.WUMMType;
 import org.apache.sysds.runtime.DMLRuntimeException;
 import org.apache.sysds.runtime.instructions.InstructionUtils;
@@ -50,27 +57,14 @@ public abstract class QuaternaryFEDInstruction extends ComputationFEDInstruction
 	public static QuaternaryFEDInstruction parseInstruction(String str) {
 		if(str.startsWith(ExecType.SPARK.name())) {
 			// rewrite the spark instruction to a cp instruction
-			str = str.replace(ExecType.SPARK.name(), ExecType.CP.name());
-			str = str.replace("mapwcemm", "wcemm");
-			str = str.replace("mapwdivmm", "wdivmm");
-			str = str.replace("mapwsigmoid", "wsigmoid");
-			str = str.replace("mapwsloss", "wsloss");
-			str = str.replace("mapwumm", "wumm");
-			if(str.contains("redwsloss") || str.contains("redwdivmm")) {
-				str = str.replace("redwsloss", "wsloss");
-				str = str.replace("redwdivmm", "wdivmm");
-				// remove booleans which indicate cacheU and cacheV for redwsloss
-				str = str.replace(Lop.OPERAND_DELIMITOR + "true", "");
-				str = str.replace(Lop.OPERAND_DELIMITOR + "false", "");
-			}
-			str += Lop.OPERAND_DELIMITOR + "1"; // num threads
+			str = rewriteSparkInstructionToCP(str);
 		}
 
 		String[] parts = InstructionUtils.getInstructionPartsWithValueType(str);
 		String opcode = parts[0];
 
-		int addInput4 = (opcode.equals("wcemm") || opcode.equals("wsloss") || opcode.equals("wdivmm")) ? 1 : 0;
-		int addUOpcode = (opcode.equals("wumm") ? 1 : 0);
+		int addInput4 = (opcode.equals(WeightedCrossEntropy.OPCODE_CP) || opcode.equals(WeightedSquaredLoss.OPCODE_CP) || opcode.equals(WeightedDivMM.OPCODE_CP)) ? 1 : 0;
+		int addUOpcode = (opcode.equals(WeightedUnaryMM.OPCODE_CP) ? 1 : 0);
 
 		InstructionUtils.checkNumFields(parts, 6 + addInput4 + addUOpcode);
 
@@ -86,7 +80,7 @@ public abstract class QuaternaryFEDInstruction extends ComputationFEDInstruction
 		{
 			CPOperand in4 = new CPOperand(parts[4]);
 
-			if(opcode.equals("wcemm")) {
+			if(opcode.equals(WeightedCrossEntropy.OPCODE_CP)) {
 				final WCeMMType wcemm_type = WCeMMType.valueOf(parts[6]);
 				if(wcemm_type.hasFourInputs())
 					checkDataTypes(new DataType[] {DataType.SCALAR, DataType.MATRIX}, in4);
@@ -94,7 +88,7 @@ public abstract class QuaternaryFEDInstruction extends ComputationFEDInstruction
 					Double.parseDouble(in4.getName())) : new QuaternaryOperator(wcemm_type));
 				return new QuaternaryWCeMMFEDInstruction(qop, in1, in2, in3, in4, out, opcode, str);
 			}
-			else if(opcode.equals("wdivmm"))
+			else if(opcode.equals(WeightedDivMM.OPCODE_CP))
 			{
 				final WDivMMType wdivmm_type = WDivMMType.valueOf(parts[6]);
 				if(wdivmm_type.hasFourInputs())
@@ -102,7 +96,7 @@ public abstract class QuaternaryFEDInstruction extends ComputationFEDInstruction
 				qop = new QuaternaryOperator(wdivmm_type);
 				return new QuaternaryWDivMMFEDInstruction(qop, in1, in2, in3, in4, out, opcode, str);
 			}
-			else if(opcode.equals("wsloss")) {
+			else if(opcode.equals(WeightedSquaredLoss.OPCODE_CP)) {
 				final WeightsType weights_type = WeightsType.valueOf(parts[6]);
 				if(weights_type.hasFourInputs())
 					checkDataTypes(DataType.MATRIX, in4);
@@ -110,12 +104,12 @@ public abstract class QuaternaryFEDInstruction extends ComputationFEDInstruction
 				return new QuaternaryWSLossFEDInstruction(qop, in1, in2, in3, in4, out, opcode, str);
 			}
 		}
-		else if(opcode.equals("wsigmoid")) {
+		else if(opcode.equals(WeightedSigmoid.OPCODE_CP)) {
 			final WSigmoidType wsigmoid_type = WSigmoidType.valueOf(parts[5]);
 			qop = new QuaternaryOperator(wsigmoid_type);
 			return new QuaternaryWSigmoidFEDInstruction(qop, in1, in2, in3, out, opcode, str);
 		}
-		else if(opcode.equals("wumm"))
+		else if(opcode.equals(WeightedUnaryMM.OPCODE_CP))
 		{
 			final WUMMType wumm_type = WUMMType.valueOf(parts[6]);
 			String uopcode = parts[1];
@@ -145,5 +139,41 @@ public abstract class QuaternaryFEDInstruction extends ComputationFEDInstruction
 				return true;
 		}
 		return false;
+	}
+
+	private static String rewriteSparkInstructionToCP(String inst_str)
+	{
+		// rewrite the spark instruction to a cp instruction
+		inst_str = inst_str.replace(ExecType.SPARK.name(), ExecType.CP.name());
+		if(inst_str.contains(WeightedCrossEntropy.OPCODE))
+		{
+			inst_str = inst_str.replace(WeightedCrossEntropy.OPCODE, WeightedCrossEntropy.OPCODE_CP);
+		}
+		else if(inst_str.contains(WeightedDivMM.OPCODE))
+		{
+			inst_str = inst_str.replace(WeightedDivMM.OPCODE, WeightedDivMM.OPCODE_CP);
+		}
+		else if(inst_str.contains(WeightedSigmoid.OPCODE))
+		{
+			inst_str = inst_str.replace(WeightedSigmoid.OPCODE, WeightedSigmoid.OPCODE_CP);
+		}
+		else if(inst_str.contains(WeightedSquaredLoss.OPCODE))
+		{
+			inst_str = inst_str.replace(WeightedSquaredLoss.OPCODE, WeightedSquaredLoss.OPCODE_CP);
+		}
+		else if(inst_str.contains(WeightedUnaryMM.OPCODE))
+		{
+			inst_str = inst_str.replace(WeightedUnaryMM.OPCODE, WeightedUnaryMM.OPCODE_CP);
+		}
+		else if(inst_str.contains(WeightedDivMMR.OPCODE) || inst_str.contains(WeightedSquaredLossR.OPCODE)) {
+			inst_str = inst_str.replace(WeightedDivMMR.OPCODE, WeightedDivMM.OPCODE_CP);
+			inst_str = inst_str.replace(WeightedSquaredLossR.OPCODE, WeightedSquaredLoss.OPCODE_CP);
+			// remove booleans which indicate cacheU and cacheV for redwsloss
+			inst_str = inst_str.replace(Lop.OPERAND_DELIMITOR + "true", "");
+			inst_str = inst_str.replace(Lop.OPERAND_DELIMITOR + "false", "");
+		}
+		inst_str += Lop.OPERAND_DELIMITOR + "1"; // num threads
+
+		return inst_str;
 	}
 }

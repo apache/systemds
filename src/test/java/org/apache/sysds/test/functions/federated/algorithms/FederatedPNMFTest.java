125ffb815e6880b52fbcfb2d70d3e2d05bbadefc
ywcb00 <ywcb00@ywcb.org>
2021-02-09 16:06:06 +0100

    refactor(**): change blocksize constant to uppercase name in FedAlsCG test, FedPNMF test, and all quaternary primitive tests

--- src/test/java/org/apache/sysds/test/functions/federated/algorithms/FederatedPNMFTest.java
+++ src/test/java/org/apache/sysds/test/functions/federated/algorithms/FederatedPNMFTest.java
@@ -46,7 +46,7 @@ public class FederatedPNMFTest extends AutomatedTestBase
 
 	private final static String OUTPUT_NAME = "Z";
 	private final static double TOLERANCE = 0.2;
-	private final static int blocksize = 1024;
+	private final static int BLOCKSIZE = 1024;
 
 	@Parameterized.Parameter()
 	public int rows;
@@ -106,8 +106,8 @@ public class FederatedPNMFTest extends AutomatedTestBase
 		double[][] X1 = getRandomMatrix(fed_rows, fed_cols, 1, 2, sparsity, 13);
 		double[][] X2 = getRandomMatrix(fed_rows, fed_cols, 1, 2, sparsity, 2);
 
-		writeInputMatrixWithMTD("X1", X1, false, new MatrixCharacteristics(fed_rows, fed_cols, blocksize, fed_rows * fed_cols));
-		writeInputMatrixWithMTD("X2", X2, false, new MatrixCharacteristics(fed_rows, fed_cols, blocksize, fed_rows * fed_cols));
+		writeInputMatrixWithMTD("X1", X1, false, new MatrixCharacteristics(fed_rows, fed_cols, BLOCKSIZE, fed_rows * fed_cols));
+		writeInputMatrixWithMTD("X2", X2, false, new MatrixCharacteristics(fed_rows, fed_cols, BLOCKSIZE, fed_rows * fed_cols));
 
 		// empty script name because we don't execute any script, just start the worker
 		fullDMLScriptName = "";

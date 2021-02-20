945fe91e53d8b7be01a540fee120d5d11aa8a8a8
Dominic "Dodo" Schablas <dodothedeveloper@gmail.com>
2021-02-20 21:12:19 +0100

    First working implementation.
    * The comparison test with a lm function using the default parameters,
      accidently used the TEST and not the TRAINING dataset. Making the lm
    using the searched parameters losing always.
    * Fixing license headers.

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinBayesianOptimisationTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinBayesianOptimisationTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.sysds.test.functions.builtin;
 
 import org.junit.Test;
@@ -17,7 +36,7 @@ public class BuiltinBayesianOptimisationTest extends AutomatedTestBase {
     private final static String TEST_CLASS_DIR = TEST_DIR + BuiltinBayesianOptimisationTest.class.getSimpleName() + "/";
 
 	private final static int rows = 300;
-	private final static int cols = 1;//20;
+	private final static int cols = 20;
 
 	@Override
 	public void setUp()
@@ -37,18 +56,15 @@ public class BuiltinBayesianOptimisationTest extends AutomatedTestBase {
 			loadTestConfiguration(config);
             String HOME = SCRIPT_DIR + TEST_DIR;
 		//fullDMLScriptName = TEST_DIR + TEST_NAME;
-			fullDMLScriptName = "./scripts/staging/bayesian_optimisation/test/bayesianOptimisationSinTest.dml"; //HOME + TEST_NAME + ".dml";
-			programArgs = new String[] {"-args", output("R")};
+			fullDMLScriptName = "./scripts/staging/bayesian_optimisation/test/bayesianOptimisationMLTest.dml"; //HOME + TEST_NAME + ".dml";
+			programArgs = new String[] {"-args", input("X"), input("y"), output("R")};
 			double[][] X = getRandomMatrix(rows, cols, 0, 1, 0.8, -1);
 			double[][] y = getRandomMatrix(rows, 1, 0, 1, 0.8, -1);
 			writeInputMatrixWithMTD("X", X, true);
 			writeInputMatrixWithMTD("y", y, true);
 
-
-            String[] bounds = new String[] {"1", "1", "1"};
-            String functionNameToOptimize = "name";
-
 			runTest(true, EXCEPTION_NOT_EXPECTED, null, -1);
+			Assert.assertTrue(TestUtils.readDMLBoolean(output("R")));
 		}
 		finally {
 			resetExecMode(modeOld);

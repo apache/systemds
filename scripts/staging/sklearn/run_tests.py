deef45ad13c3508c5ae07e3522e0c630a64f916a
Alexander Thien <alexander.thien@student.tugraz.at>
2021-02-21 19:46:20 +0100

    testfiles + functions

--- scripts/staging/sklearn/run_tests.py
+++ scripts/staging/sklearn/run_tests.py
@@ -22,6 +22,105 @@ def test001():
         return test_script(path)
     return False
 
+def test002():
+    pipeline = make_pipeline(normalize(), KMeans())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test002_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test002.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
+def test003():
+    pipeline = make_pipeline(imputeByMean(), KMeans())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test003_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test003.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
+def test004():
+    pipeline = make_pipeline(imputByMedian(), KMeans())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test004_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test004.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
+def test005():
+    pipeline = make_pipeline(pca(), KMeans())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test005_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test005.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
+def test006():
+    pipeline = make_pipeline(StandardScaler(), dbscan())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test006_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test006.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
+def test007():
+    pipeline = make_pipeline(normalize(), dbscan())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test007_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test007.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
+def test008():
+    pipeline = make_pipeline(imputeByMean(), dbscan())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test008_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test008.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
+def test009():
+    pipeline = make_pipeline(imputeByMedian(), dbscan())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test009_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test009.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
+def test010():
+    pipeline = make_pipeline(pca(), dbscan())
+    mapper = SklearnToDMLMapper(pipeline)
+    mapper.transform()
+    path = 'test010_gen.dml'
+    mapper.save(path)
+    result = compare_script(path, 'test010.dml')
+    if result == True:
+        return test_script(path)
+    return False
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
@@ -30,6 +129,16 @@ if __name__ == '__main__':
     options = parser.parse_args()
     tests = [
         test001
+        test002
+        test003
+        test004
+        test005
+        test006
+        test007
+        test008
+        test009
+        test010
+
     ]
 
     results = []

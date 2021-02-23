202aae0162130fc43c602edcd23b3de0827f898a
Olga <ovcharenko.folga@gmail.com>
2021-02-23 22:13:00 +0200

    Added aligned weights and cleaned fed cov

--- src/main/java/org/apache/sysds/runtime/instructions/fed/CovarianceFEDInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/CovarianceFEDInstruction.java
@@ -23,11 +23,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.sysds.common.Types;
 import org.apache.sysds.runtime.DMLRuntimeException;
 import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
 import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
+import org.apache.sysds.runtime.controlprogram.federated.FederatedData;
 import org.apache.sysds.runtime.controlprogram.federated.FederatedRange;
 import org.apache.sysds.runtime.controlprogram.federated.FederatedRequest;
 import org.apache.sysds.runtime.controlprogram.federated.FederatedResponse;
@@ -88,82 +91,176 @@ public class CovarianceFEDInstruction extends BinaryFEDInstruction {
 	public void processInstruction(ExecutionContext ec) {
 		MatrixObject mo1 = ec.getMatrixObject(input1);
 		MatrixObject mo2 = ec.getMatrixObject(input2);
-		COVOperator cop = ((COVOperator)_optr);
+		MatrixObject weights = input3 != null ? ec.getMatrixObject(input3) : null;
 
 		if(mo1.isFederated() && mo2.isFederated() && !mo1.getFedMapping().isAligned(mo2.getFedMapping(), false))
 			throw new DMLRuntimeException("Not supported matrix-matrix binary operation: covariance.");
 
-		FederatedRequest fr2 = null;
-		if(mo1.isFederated() && mo2.isFederated() && mo1.getFedMapping().isAligned(mo2.getFedMapping(), false)) {
-			fr2 = FederationUtils.callInstruction(instString, output, new CPOperand[]{input1, input2},
-				new long[]{mo1.getFedMapping().getID(), mo2.getFedMapping().getID()});
-			Future<FederatedResponse>[] tmp = mo1.getFedMapping().execute(getTID(), true, fr2);
+		boolean moAligned = mo1.isFederated() && mo2.isFederated() && mo1.getFedMapping().isAligned(mo2.getFedMapping(), false);
+		boolean weightsAligned = weights == null || (weights.isFederated() && mo2.isFederated() && weights.getFedMapping()
+			.isAligned(mo2.getFedMapping(), false));
 
-			//means
-			Future<FederatedResponse>[] meanTmp1 = processMean(mo1, 0);
-			Future<FederatedResponse>[] meanTmp2 = processMean(mo2, 1);
+		// all aligned
+		if(moAligned && weightsAligned)
+			processAlignedFedCov(ec, mo1, mo2, weights);
+		// weights are not aligned, broadcast
+		else if(moAligned)
+			processFedCovWeights(ec, mo1, mo2, weights);
+		else
+			processCov(ec, mo1, mo2);
+	}
 
-			double res = aggCov(tmp, meanTmp1, meanTmp2, mo1.getFedMapping());
-			ec.setVariable(output.getName(), new DoubleObject(res));
+	private void processAlignedFedCov(ExecutionContext ec, MatrixObject mo1, MatrixObject mo2, MatrixObject mo3) {
+		FederatedRequest fr1;
+		if(mo3 == null)
+			fr1 = FederationUtils.callInstruction(instString, output,
+				new CPOperand[]{input1, input2}, new long[]{mo1.getFedMapping().getID(), mo2.getFedMapping().getID()});
+		else
+			fr1 = FederationUtils.callInstruction(instString, output,
+				new CPOperand[]{input1, input2, input3}, new long[]{mo1.getFedMapping().getID(), mo2.getFedMapping().getID(), mo3.getFedMapping().getID()});
+
+		FederatedRequest fr2 = new FederatedRequest(FederatedRequest.RequestType.GET_VAR, fr1.getID());
+		FederatedRequest fr3 = mo1.getFedMapping().cleanup(getTID(), fr1.getID());
+		Future<FederatedResponse>[] covTmp = mo1.getFedMapping().execute(getTID(), fr1, fr2, fr3);
+
+		//means
+		Future<FederatedResponse>[] meanTmp1 = processMean(mo1, 0);
+		Future<FederatedResponse>[] meanTmp2 = processMean(mo2, 1);
+
+		ImmutableTriple<Double[], Double[], Double[]> res = getResponses(covTmp, meanTmp1, meanTmp2);
+
+		double result = aggCov(res.left, res.middle, res.right, mo1.getFedMapping().getFederatedRanges());
+		ec.setVariable(output.getName(), new DoubleObject(result));
+	}
+
+	private void processFedCovWeights(ExecutionContext ec, MatrixObject mo1, MatrixObject mo2, MatrixObject mo3) {
+
+		FederatedRequest[] fr2 = mo1.getFedMapping().broadcastSliced(mo3, false);
+			FederatedRequest fr1 = FederationUtils.callInstruction(instString, output,
+				new CPOperand[]{input1, input2}, new long[]{mo1.getFedMapping().getID(), mo2.getFedMapping().getID()});
+		FederatedRequest fr3 = new FederatedRequest(FederatedRequest.RequestType.GET_VAR, fr1.getID());
+		FederatedRequest fr4 = mo1.getFedMapping().cleanup(getTID(), fr1.getID());
+		FederatedRequest fr5 = mo1.getFedMapping().cleanup(getTID(), fr2[0].getID());
+		Future<FederatedResponse>[] covTmp = mo1.getFedMapping().execute(getTID(), fr1, fr2[0], fr3, fr4, fr5);
+
+		//means
+		Future<FederatedResponse>[] meanTmp1 = processMean(mo1, 0);
+		Future<FederatedResponse>[] meanTmp2 = processMean(mo2, 1);
+
+		ImmutableTriple<Double[], Double[], Double[]> res = getResponses(covTmp, meanTmp1, meanTmp2);
+
+		double result = aggCov(res.left, res.middle, res.right, mo1.getFedMapping().getFederatedRanges());
+		ec.setVariable(output.getName(), new DoubleObject(result));
+	}
+
+	private void processCov(ExecutionContext ec, MatrixObject mo1, MatrixObject mo2) {
+		MatrixBlock mb;
+		MatrixObject mo;
+		COVOperator cop = ((COVOperator)_optr);
+
+		if(!mo1.isFederated() && mo2.isFederated()) {
+			mo = mo2;
+			mb = ec.getMatrixInput(input1.getName());
 		}
 		else {
-			MatrixBlock mb;
-			MatrixObject mo;
-			if(!mo1.isFederated() && mo2.isFederated()) {
-				mo = mo2;
-				mb = ec.getMatrixInput(input1.getName());
-			}
-			else {
-				mo = mo1;
-				mb = ec.getMatrixInput(input2.getName());
-			}
+			mo = mo1;
+			mb = ec.getMatrixInput(input2.getName());
+		}
 
-			FederationMap fedMapping = mo.getFedMapping();
-			List<CM_COV_Object> globalCmobj = new ArrayList<>();
-			long varID = FederationUtils.getNextFedDataID();
+		FederationMap fedMapping = mo.getFedMapping();
+		List<CM_COV_Object> globalCmobj = new ArrayList<>();
+		long varID = FederationUtils.getNextFedDataID();
+		fedMapping.mapParallel(varID, (range, data) -> {
 
-			fedMapping.mapParallel(varID, (range, data) -> {
-
-				FederatedResponse response;
-				try {
-					if(input3 == null) {
-						response = data.executeFederatedOperation(new FederatedRequest(FederatedRequest.RequestType.EXEC_UDF,
-							-1,
-							new CovarianceFEDInstruction.COVFunction(data.getVarID(),
-								mb.slice(range.getBeginDimsInt()[0], range.getEndDimsInt()[0] - 1),
-								cop))).get();
-					}
-					else {
-						MatrixBlock wtBlock = ec.getMatrixInput(input2.getName());
-
-						response = data.executeFederatedOperation(new FederatedRequest(FederatedRequest.RequestType.EXEC_UDF,
-							-1,
-							new CovarianceFEDInstruction.COVWeightsFunction(data.getVarID(),
-								mb.slice(range.getBeginDimsInt()[0], range.getEndDimsInt()[0] - 1),
-								cop,
-								wtBlock))).get();
-					}
-
-					if(!response.isSuccessful())
-						response.throwExceptionFromResponse();
-					synchronized(globalCmobj) {
-						globalCmobj.add((CM_COV_Object) response.getData()[0]);
-					}
-				}
-				catch(Exception e) {
-					throw new DMLRuntimeException(e);
-				}
-				return null;
-			});
-
-			Optional<CM_COV_Object> res = globalCmobj.stream().reduce((arg0, arg1) -> (CM_COV_Object) cop.fn.execute(arg0, arg1));
+			FederatedResponse response;
 			try {
-				ec.setScalarOutput(output.getName(), new DoubleObject(res.get().getRequiredResult(cop)));
+				if(input3 == null) {
+					response = data.executeFederatedOperation(new FederatedRequest(FederatedRequest.RequestType.EXEC_UDF, -1,
+						new CovarianceFEDInstruction.COVFunction(data.getVarID(),
+							mb.slice(range.getBeginDimsInt()[0], range.getEndDimsInt()[0] - 1),
+							cop))).get();
+				}
+				// with weights
+				else {
+					MatrixBlock wtBlock = ec.getMatrixInput(input2.getName());
+					response = data.executeFederatedOperation(new FederatedRequest(FederatedRequest.RequestType.EXEC_UDF, -1,
+						new CovarianceFEDInstruction.COVWeightsFunction(data.getVarID(),
+							mb.slice(range.getBeginDimsInt()[0], range.getEndDimsInt()[0] - 1),
+							cop, wtBlock))).get();
+				}
+
+				if(!response.isSuccessful())
+					response.throwExceptionFromResponse();
+				synchronized(globalCmobj) {
+					globalCmobj.add((CM_COV_Object) response.getData()[0]);
+				}
 			}
 			catch(Exception e) {
 				throw new DMLRuntimeException(e);
 			}
+			return null;
+		});
+
+		Optional<CM_COV_Object> res = globalCmobj.stream().reduce((arg0, arg1) -> (CM_COV_Object) cop.fn.execute(arg0, arg1));
+		try {
+			ec.setScalarOutput(output.getName(), new DoubleObject(res.get().getRequiredResult(cop)));
 		}
+		catch(Exception e) {
+			throw new DMLRuntimeException(e);
+		}
+	}
+
+	private static ImmutableTriple<Double[], Double[], Double[]> getResponses(Future<FederatedResponse>[] covFfr, Future<FederatedResponse>[] mean1Ffr, Future<FederatedResponse>[] mean2Ffr) {
+		Double[] cov = new Double[covFfr.length];
+		Double[] mean1 = new Double[mean1Ffr.length];
+		Double[] mean2 = new Double[mean2Ffr.length];
+		IntStream.range(0, covFfr.length).forEach(i -> {
+			try {
+				cov[i] = ((ScalarObject) covFfr[i].get().getData()[0]).getDoubleValue();
+				mean1[i] = ((ScalarObject) mean1Ffr[1].get().getData()[0]).getDoubleValue();
+				mean2[i] = ((ScalarObject) mean2Ffr[2].get().getData()[0]).getDoubleValue();
+			}
+			catch(Exception e) {
+				throw new DMLRuntimeException("CovarianceFEDInstruction: incorrect means or cov.");
+			}
+		});
+
+		return new ImmutableTriple<>(cov, mean1, mean2);
+	}
+
+	private static double aggCov(Double[] covValues, Double[] mean1, Double[] mean2, FederatedRange[] ranges) {
+		double cov = covValues[0];
+		long size1 = ranges[0].getSize();
+		double mean = (mean1[0] + mean2[0]) / 2;
+
+		for(int i = 0; i < covValues.length - 1; i++) {
+			long size2 = ranges[i+1].getSize();
+			double nextMean = (mean1[i+1] + mean2[i+1]) / 2;
+			double newMean = (size1 * mean + size2 * nextMean) / (size1 + size2);
+
+			cov = (size1 * cov + size2 * covValues[i+1] + size1 * (mean - newMean) * (mean - newMean)
+				+ size2 * (nextMean - newMean) * (nextMean - newMean)) / (size1 + size2);
+
+			mean = newMean;
+			size1 = size1 + size2;
+		}
+		return cov;
+	}
+
+	private Future<FederatedResponse>[] processMean(MatrixObject mo1, int var){
+		String[] parts = instString.split("°");
+		String meanInstr = instString.replace(getOpcode(), getOpcode().replace("cov", "uamean"));
+		meanInstr = meanInstr.replace((var == 0 ? parts[2] : parts[3]) + "°", "");
+		meanInstr = meanInstr.replace(parts[4], parts[4].replace("FP64", "STRING°16"));
+		Future<FederatedResponse>[] meanTmp = null;
+
+		//create federated commands for aggregation
+		FederatedRequest meanFr1 = FederationUtils.callInstruction(meanInstr, output,
+			new CPOperand[]{var == 0 ? input2 : input1}, new long[]{mo1.getFedMapping().getID()});
+		FederatedRequest meanFr2 = new FederatedRequest(FederatedRequest.RequestType.GET_VAR, meanFr1.getID());
+		FederatedRequest meanFr3 = mo1.getFedMapping().cleanup(getTID(), meanFr1.getID());
+		meanTmp = mo1.getFedMapping().execute(getTID(), meanFr1, meanFr2, meanFr3);
+		return meanTmp;
 	}
 
 	private static class COVFunction extends FederatedUDF {
@@ -203,51 +300,5 @@ public class CovarianceFEDInstruction extends BinaryFEDInstruction {
 			MatrixBlock mb = ((MatrixObject) data[0]).acquireReadAndRelease();
 			return new FederatedResponse(FederatedResponse.ResponseType.SUCCESS, mb.covOperations(_op, _mo2, _weights));
 		}
-	}
-
-	private static double aggCov(Future<FederatedResponse>[] ffr, Future<FederatedResponse>[] mean1Ffr, Future<FederatedResponse>[] mean2Ffr, FederationMap map) {
-		try {
-			FederatedRange[] ranges = map.getFederatedRanges();
-			double cov = ((ScalarObject)ffr[0].get().getData()[0]).getDoubleValue();
-			double mean1 = ((ScalarObject)mean1Ffr[0].get().getData()[0]).getDoubleValue();
-			double mean2 = ((ScalarObject)mean2Ffr[0].get().getData()[0]).getDoubleValue();
-			double mean = (mean1 + mean2) / 2;
-			long size1 = ranges[0].getSize();
-			for(int i=0; i < ffr.length - 1; i++) {
-				double nextCov = ((ScalarObject)ffr[i+1].get().getData()[0]).getDoubleValue();
-				double nextMean1 = ((ScalarObject)mean1Ffr[i+1].get().getData()[0]).getDoubleValue();
-				double nextMean2 = ((ScalarObject)mean2Ffr[i+1].get().getData()[0]).getDoubleValue();
-				long size2 = ranges[i+1].getSize();
-
-				double nextMean = (nextMean1 + nextMean2) / 2;
-				double newMean = (size1 * mean + size2 * nextMean) / (size1 + size2);
-				long newSize = size1 + size2;
-
-				cov = (size1*cov + size2*nextCov + size1*(mean-newMean)*(mean-newMean) + size2*(nextMean-newMean)*(nextMean-newMean)) / newSize;
-
-				mean = newMean;
-				size1 = newSize;
-			}
-			return cov;
-		}
-		catch (Exception ex) {
-			throw new DMLRuntimeException(ex);
-		}
-	}
-
-	private Future<FederatedResponse>[] processMean(MatrixObject mo1, int var){
-		String[] parts = instString.split("°");
-		String meanInstr = instString.replace(getOpcode(), getOpcode().replace("cov", "uamean"));
-		meanInstr = meanInstr.replace((var == 0 ? parts[2] : parts[3]) + "°", "");
-		meanInstr = meanInstr.replace(parts[4], parts[4].replace("FP64", "STRING°16"));
-		Future<FederatedResponse>[] meanTmp = null;
-
-		//create federated commands for aggregation
-		FederatedRequest meanFr1 = FederationUtils.callInstruction(meanInstr, output,
-			new CPOperand[]{var == 0 ? input2 : input1}, new long[]{mo1.getFedMapping().getID()});
-		FederatedRequest meanFr2 = new FederatedRequest(FederatedRequest.RequestType.GET_VAR, meanFr1.getID());
-		FederatedRequest meanFr3 = mo1.getFedMapping().cleanup(getTID(), meanFr1.getID());
-		meanTmp = mo1.getFedMapping().execute(getTID(), meanFr1, meanFr2, meanFr3);
-		return meanTmp;
 	}
 }
--- src/test/java/org/apache/sysds/test/functions/federated/primitives/FederatedCovarianceTest.java
+++ src/test/java/org/apache/sysds/test/functions/federated/primitives/FederatedCovarianceTest.java
@@ -38,82 +38,67 @@ import org.junit.runners.Parameterized;
 @net.jcip.annotations.NotThreadSafe
 public class FederatedCovarianceTest extends AutomatedTestBase {
 
-	private final static String TEST_NAME = "FederatedCovarianceTest";
-
+	private final static String TEST_NAME1 = "FederatedCovarianceTest";
+	private final static String TEST_NAME2 = "FederatedCovarianceAlignedTest";
 	private final static String TEST_DIR = "functions/federated/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + FederatedCovarianceTest.class.getSimpleName() + "/";
 
 	private final static int blocksize = 1024;
-	@Parameterized.Parameter()
+	@Parameterized.Parameter
 	public int rows;
 	@Parameterized.Parameter(1)
 	public int cols;
 
-	@Parameterized.Parameter(2)
-	public boolean rowPartitioned;
-
 	@Parameterized.Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-			{20, 1, true},
+			{20, 1},
+//			{100, 1}, {1000, 1}
 		});
 	}
 
 	@Override
 	public void setUp() {
 		TestUtils.clearAssertionInformation();
-		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {"S.scalar"}));
+		addTestConfiguration(TEST_NAME1, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME1, new String[] {"S.scalar"}));
+		addTestConfiguration(TEST_NAME2, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME2, new String[] {"S.scalar"}));
 	}
 
 	@Test
-	public void testCovCP() { runCovTest(ExecMode.SINGLE_NODE); }
+	public void testCovCP() { runCovTest(ExecMode.SINGLE_NODE, false); }
 
-	private void runCovTest(ExecMode execMode) {
+	@Test
+	public void testAlignedCovCP() { runCovTest(ExecMode.SINGLE_NODE, true); }
+
+	private void runCovTest(ExecMode execMode, boolean alignedFedInput) {
 		boolean sparkConfigOld = DMLScript.USE_LOCAL_SPARK_CONFIG;
 		ExecMode platformOld = rtplatform;
 
 		if(rtplatform == ExecMode.SPARK)
 			DMLScript.USE_LOCAL_SPARK_CONFIG = true;
 
+		String TEST_NAME = alignedFedInput ? TEST_NAME2 : TEST_NAME1;
+
 		getAndLoadTestConfiguration(TEST_NAME);
 		String HOME = SCRIPT_DIR + TEST_DIR;
 
 		// write input matrices
-		int r = rows;
-		int c = cols / 4;
-		if(rowPartitioned) {
-			r = rows / 4;
-			c = cols;
-		}
-
+		int r = r = rows / 4;
+		int c = cols;
 
 		// empty script name because we don't execute any script, just start the worker
 		fullDMLScriptName = "";
-
 
 		double[][] X1 = getRandomMatrix(r, c, 1, 5, 1, 3);
 		double[][] X2 = getRandomMatrix(r, c, 1, 5, 1, 7);
 		double[][] X3 = getRandomMatrix(r, c, 1, 5, 1, 8);
 		double[][] X4 = getRandomMatrix(r, c, 1, 5, 1, 9);
 
-		double[][] Y = getRandomMatrix(rows, c, 1, 5, 1, 3);
-
-		//		double[][] Y1 = getRandomMatrix(r, c, 1, 5, 1, 3);
-		//		double[][] Y2 = getRandomMatrix(r, c, 1, 5, 1, 7);
-		//		double[][] Y3 = getRandomMatrix(r, c, 1, 5, 1, 8);
-		//		double[][] Y4 = getRandomMatrix(r, c, 1, 5, 1, 9);
-
 		MatrixCharacteristics mc = new MatrixCharacteristics(r, c, blocksize, r * c);
 		writeInputMatrixWithMTD("X1", X1, false, mc);
 		writeInputMatrixWithMTD("X2", X2, false, mc);
 		writeInputMatrixWithMTD("X3", X3, false, mc);
 		writeInputMatrixWithMTD("X4", X4, false, mc);
-
-		writeInputMatrixWithMTD("Y", Y, false, new MatrixCharacteristics(rows, cols, blocksize, r*c));
-		//		writeInputMatrixWithMTD("Y1", Y1, false, mc);
-		//		writeInputMatrixWithMTD("Y2", Y2, false, mc);
-		//		writeInputMatrixWithMTD("Y3", Y3, false, mc);
-		//		writeInputMatrixWithMTD("Y4", Y4, false, mc);
 
 		// empty script name because we don't execute any script, just start the worker
 		fullDMLScriptName = "";
@@ -134,28 +119,58 @@ public class FederatedCovarianceTest extends AutomatedTestBase {
 		TestConfiguration config = availableTestConfigurations.get(TEST_NAME);
 		loadTestConfiguration(config);
 
-		// Run reference dml script with normal matrix
-		fullDMLScriptName = HOME + TEST_NAME + "Reference.dml";
-		programArgs = new String[] {"-stats", "100", "-args", input("X1"), input("X2"), input("X3"), input("X4"),
-			input("Y"),
-			Boolean.toString(rowPartitioned).toUpperCase(), expected("S")};
+		if(alignedFedInput) {
+			double[][] Y1 = getRandomMatrix(r, c, 1, 5, 1, 3);
+			double[][] Y2 = getRandomMatrix(r, c, 1, 5, 1, 7);
+			double[][] Y3 = getRandomMatrix(r, c, 1, 5, 1, 8);
+			double[][] Y4 = getRandomMatrix(r, c, 1, 5, 1, 9);
 
-		runTest(null);
+			writeInputMatrixWithMTD("Y1", Y1, false, mc);
+			writeInputMatrixWithMTD("Y2", Y2, false, mc);
+			writeInputMatrixWithMTD("Y3", Y3, false, mc);
+			writeInputMatrixWithMTD("Y4", Y4, false, mc);
 
-		fullDMLScriptName = HOME + TEST_NAME + ".dml";
-		programArgs = new String[] {"-stats", "100", "-nvargs",
-			"in_X1=" + TestUtils.federatedAddress(port1, input("X1")),
-			"in_X2=" + TestUtils.federatedAddress(port2, input("X2")),
-			"in_X3=" + TestUtils.federatedAddress(port3, input("X3")),
-			"in_X4=" + TestUtils.federatedAddress(port4, input("X4")),
-			"Y=" + input("Y"),
-			"rows=" + rows, "cols=" + cols,
-			"rP=" + Boolean.toString(rowPartitioned).toUpperCase(), "out_S=" + output("S")};
+			// Run reference dml script with normal matrix
+			fullDMLScriptName = HOME + TEST_NAME + "Reference.dml";
+			programArgs = new String[] {"-stats", "100", "-args", input("X1"), input("X2"), input("X3"), input("X4"),
+				input("Y1"), input("Y2"), input("Y3"), input("Y4"), expected("S")};
+			runTest(null);
 
-		runTest(null);
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[] {"-stats", "100", "-nvargs",
+				"in_X1=" + TestUtils.federatedAddress(port1, input("X1")),
+				"in_X2=" + TestUtils.federatedAddress(port2, input("X2")),
+				"in_X3=" + TestUtils.federatedAddress(port3, input("X3")),
+				"in_X4=" + TestUtils.federatedAddress(port4, input("X4")),
+				"in_Y1=" + TestUtils.federatedAddress(port1, input("Y1")),
+				"in_Y2=" + TestUtils.federatedAddress(port2, input("Y2")),
+				"in_Y3=" + TestUtils.federatedAddress(port3, input("Y3")),
+				"in_Y4=" + TestUtils.federatedAddress(port4, input("Y4")),
+				"rows=" + rows, "cols=" + cols, "out_S=" + output("S")};
+			runTest(null);
+
+		} else {
+			double[][] Y = getRandomMatrix(rows, c, 1, 5, 1, 3);
+			writeInputMatrixWithMTD("Y", Y, false, new MatrixCharacteristics(rows, cols, blocksize, r*c));
+
+			// Run reference dml script with normal matrix
+			fullDMLScriptName = HOME + TEST_NAME + "Reference.dml";
+					programArgs = new String[] {"-stats", "100", "-args", input("X1"), input("X2"), input("X3"), input("X4"),
+						input("Y"), expected("S"),};
+			runTest(null);
+
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[] {"-stats", "100", "-nvargs",
+				"in_X1=" + TestUtils.federatedAddress(port1, input("X1")),
+				"in_X2=" + TestUtils.federatedAddress(port2, input("X2")),
+				"in_X3=" + TestUtils.federatedAddress(port3, input("X3")),
+				"in_X4=" + TestUtils.federatedAddress(port4, input("X4")),
+				"Y=" + input("Y"), "rows=" + rows, "cols=" + cols, "out_S=" + output("S")};
+			runTest(null);
+		}
 
 		// compare via files
-		compareResults(1e-9);
+		compareResults(1e-2);
 		Assert.assertTrue(heavyHittersContainsString("fed_cov"));
 
 		TestUtils.shutdownThreads(t1, t2, t3, t4);

4df2d5699bf8346d5f47aa07528dd9277d1a3d46
Vanessa Vieira <vanessa.soaresvieira@student.tugraz.at>
2021-02-23 22:56:00 +0100

    Generate random input data for testing

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinCoxTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinCoxTest.java
@@ -19,9 +19,12 @@
 
 package org.apache.sysds.test.functions.builtin;
 
+import org.apache.sysds.common.Types;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.junit.Test;
+
+import static org.apache.sysds.test.TestUtils.*;
 
 public class BuiltinCoxTest extends AutomatedTestBase
 {
@@ -29,22 +32,21 @@ public class BuiltinCoxTest extends AutomatedTestBase
 	private final static String TEST_DIR = "functions/builtin/";
 	private final static String TEST_CLASS_DIR = TEST_DIR + BuiltinCoxTest.class.getSimpleName() + "/";
 
-	private final static double eps = 1e-3;
-	private final static int rows = 1765;
 	private final static double spDense = 0.99;
 
 	@Override
 	public void setUp() {
-		addTestConfiguration(TEST_NAME,new TestConfiguration(TEST_CLASS_DIR, TEST_NAME,new String[]{"B"}));
+		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME,new String[]{"B"}));
 	}
 
 	@Test
-	void testFunction() {
-		runCoxTest(0.05, 100, 0);
+	public void testFunction() {
+		runCoxTest(1000, 2.0, 1.5, 0.8, 100, 0.05, 100, 0);
 	}
 	
-	public void runCoxTest(double alpha, int moi, int mii)
-	{
+	public void runCoxTest(int numRecords, double scaleWeibull, double shapeWeibull, double prob,
+						   int numFeatures, double alpha, int moi, int mii) {
+		Types.ExecMode platformOld = setExecMode(Types.ExecMode.SPARK);
 		loadTestConfiguration(getTestConfiguration(TEST_NAME));
 		String HOME = SCRIPT_DIR + TEST_DIR;
 		fullDMLScriptName = HOME + TEST_NAME + ".dml";
@@ -52,21 +54,66 @@ public class BuiltinCoxTest extends AutomatedTestBase
 
 		programArgs = new String[]{
 				"-nvargs", "X=" + input("X"), "TE=" + input("TE"), "F=" + input("F"),
-				"R=" + input("R"), "M=" + output("M"), "S=" + output("S"), "T=" + output("T"),
+				"M=" + output("M"), "S=" + output("S"), "T=" + output("T"),
 				"COV=" + output("COV"), "RT=" + output("RT"), "XO=" + output("XO"), "MF=" + output("MF"),
 				"alpha=" + alpha, "moi=" + moi, "mii=" + mii};
 
-		double[][] X = getRandomMatrix(rows, , , , spDense, seed);
+		double[][] X = getRandomMatrix(numRecords, numFeatures, 1, 5, spDense, seed);
 		writeInputMatrixWithMTD("X", X, false);
 
-		double[][] TE = getRandomMatrix(rows, , , , spDense, seed);
+		double[][] B = getRandomMatrix (numFeatures, 1, -1.0, 1.0, spDense, seed);
+		double[][] U = getRandomMatrix(numRecords, 1, 0.000000001, 1, spDense, seed);
+		double[][] TE = new double[numRecords][2];
+
+		double probCensor = 1 - prob;
+		double[][] event = floor(getRandomMatrix(numRecords, 1, 1 - probCensor, 1 + prob, spDense, seed));
+		double nTime = sum(event, numRecords, 1);
+
+		for(int i = 0; i < numRecords; i++) {
+			TE[i][1] = event[i][0];
+		}
+
+		double[][] T = new double[numRecords][1];
+		double max_T = 0;
+		double min_T = 0;
+
+		double[][] multipliedMatrices = performMatrixMultiplication(X, B);
+		for(int i = 0; i < numRecords; i++) {
+			max_T = Double.MIN_VALUE;
+			min_T = Double.MAX_VALUE;
+
+			T[i][0] = Math.pow(- Math.log(U[i][0])/ (scaleWeibull * Math.exp(multipliedMatrices[i][0])), (1/shapeWeibull));
+
+			if (T[i][0] > max_T) {
+				max_T = T[i][0];
+			}
+			if (T[i][0] < min_T) {
+				min_T = T[i][0];
+			}
+		}
+
+		double len = max_T - min_T;
+		double numBins = len / nTime;
+		for (int i = 0; i < numRecords; i++) {
+			T[i][0] = T[i][0] / numBins;
+		}
+		ceil(T);
+
+		for(int i = 0; i < numRecords; i++) {
+			TE[i][0] = T[i][0];
+		}
+
 		writeInputMatrixWithMTD("TE", TE, false);
 
-		double[][] F = getRandomMatrix(rows, , , , spDense, seed);
-		writeInputMatrixWithMTD("F", F, false);
+		if (numFeatures > 0) {
+			double[][] F = new double[numFeatures][1];
 
-		double[][] R = getRandomMatrix(rows, , , , spDense, seed);
-		writeInputMatrixWithMTD("R", R, false);
+			for (int i = 0; i < numFeatures; i++) {
+				F[i][0] = i+1;
+			}
+
+			writeInputMatrixWithMTD("F", F, false);
+		}
 
 		runTest(true, false, null, -1);
 
--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinKmTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinKmTest.java
@@ -19,10 +19,14 @@
 
 package org.apache.sysds.test.functions.builtin;
 
+import org.apache.sysds.common.Types;
+import org.apache.sysds.lops.LopProperties;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.apache.sysds.test.TestUtils;
 import org.junit.Test;
+
+import static org.apache.sysds.test.TestUtils.*;
 
 public class BuiltinKmTest extends AutomatedTestBase
 {
@@ -30,45 +34,78 @@ public class BuiltinKmTest extends AutomatedTestBase
 	private final static String TEST_DIR = "functions/builtin/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinKmTest.class.getSimpleName() + "/";
 
-	private final static double eps = 1e-3;
-	private final static int rows = 1765;
 	private final static double spDense = 0.99;
 
 	@Override
 	public void setUp() {
 		TestUtils.clearAssertionInformation();
-		addTestConfiguration(TEST_NAME,new TestConfiguration(TEST_CLASS_DIR, TEST_NAME,new String[]{"C"}));
+		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME,new String[]{"B"}));
 	}
 
 	@Test
 	public void testFunction() {
-		runKmTest(0.05,"greenwood", "log", "none");
+		runKmTest(1000, 2.0, 1.5, 0.8, 2,
+				1, 10, 0.05,"greenwood", "log", "none");
 	}
 
-	private void runKmTest(Double alpha, String err_type,
+	private void runKmTest(int numRecords, double scaleWeibull, double shapeWeibull, double prob,
+						   int numCatFeaturesGroup, int numCatFeaturesStrat, int maxNumLevels, double alpha, String err_type,
 						   String conf_type, String test_type) {
+		Types.ExecMode platformOld = setExecMode(LopProperties.ExecType.SPARK);
 		loadTestConfiguration(getTestConfiguration(TEST_NAME));
 		String HOME = SCRIPT_DIR + TEST_DIR;
 		fullDMLScriptName = HOME + TEST_NAME + ".dml";
 		int seed = 11;
 
 		programArgs = new String[]{
-				"-nvargs", "X=" + input("X"), "TE=" + input("TE"), "GI=" + input("GI"),
-				"SI=" + input("SI"), "O=" + output("O"), "M=" + output("M"), "T=" + output("T"),
+				"-nvargs", "X=" + input("X"), "TE=" + input("TE"), "O=" + output("O"), "M=" + output("M"), "T=" + output("T"),
 				"alpha=" + alpha, "err_type=" + err_type, "err_type=" + err_type,
-				"conf_type=" + conf_type, "test_type" + test_type};
+				"conf_type=" + conf_type, "test_type=" + test_type};
 
-		double[][] X = getRandomMatrix(rows, , , , spDense, seed);
+		double[][] X = ceil(getRandomMatrix(numRecords, numCatFeaturesGroup + numCatFeaturesStrat,
+				0.000000001, maxNumLevels - 0.000000001, spDense, seed));
 		writeInputMatrixWithMTD("X", X, false);
 
-		double[][] TE = getRandomMatrix(rows, , , , spDense, seed);
+		double[][] U = getRandomMatrix(numRecords, 1, 0.000000001, 1, spDense, seed);
+
+		double[][] TE = new double[numRecords][2];
+		double probCensor = 1 - prob;
+		double[][] event = floor(getRandomMatrix(numRecords, 1, 1 - probCensor, 1 + prob, spDense, seed));
+		double nTime = sum(event, numRecords, 1);
+
+		for(int i = 0; i < numRecords; i++) {
+			TE[i][1] = event[i][0];
+		}
+
+		double[][] T = new double[numRecords][1];
+		double max_T = 0;
+		double min_T = 0;
+		for(int i = 0; i < numRecords; i++) {
+			max_T = Double.MIN_VALUE;
+			min_T = Double.MAX_VALUE;
+
+			T[i][0] = Math.pow((- Math.log(U[i][0])/ (scaleWeibull)), (1/shapeWeibull));
+
+			if (T[i][0] > max_T) {
+				max_T = T[i][0];
+			}
+			if (T[i][0] < min_T) {
+				min_T = T[i][0];
+			}
+		}
+
+		double len = max_T - min_T;
+		double numBins = len / nTime;
+		for (int i = 0; i < numRecords; i++) {
+			T[i][0] = T[i][0] / numBins;
+		}
+		ceil(T);
+
+		for(int i = 0; i < numRecords; i++) {
+			TE[i][0] = T[i][0];
+		}
+
 		writeInputMatrixWithMTD("TE", TE, false);
-
-		double[][] GI = getRandomMatrix(rows, , , , spDense, seed);
-		writeInputMatrixWithMTD("GI", GI, false);
-
-		double[][] SI = getRandomMatrix(rows, , , , spDense, seed);
-		writeInputMatrixWithMTD("SI", SI, false);
 
 		runTest(true, false, null, -1);
 	}

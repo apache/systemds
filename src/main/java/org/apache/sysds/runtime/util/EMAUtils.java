534f763801e373dd03fd134417dc5d1102b612dc
Maximilian Theiner <maximilian.theiner@student.tugraz.at>
2021-02-26 17:47:31 +0100

    fixed nan issues

--- src/main/java/org/apache/sysds/runtime/util/EMAUtils.java
+++ src/main/java/org/apache/sysds/runtime/util/EMAUtils.java
@@ -68,15 +68,7 @@ class LinearRegression {
 public class EMAUtils {
 
 	public static FrameBlock exponentialMovingAverageImputation(FrameBlock data2, int search_iterations, String mode, int freq) {
-		Double[] data= new Double[]{.112,.118,.132,.129, null,.135,.148,.148,null,.119,.104,.118,.115,.126,.141,.135,.125,.149,.170,.170,null,.133,null,.140,.145,.150,.178,.163,.172,.178,.199,.199,.184,.162,.146,.166,.171,.180,.193,.181,.183,.218,.230,.242,.209,.191,.172,.194,.196,.196,.236,.235,.229,.243,.264,.272,.237,.211,.180,.201,.204,.188,.235,.227,.234,null,.302,.293,.259,.229,.203,.229,.242,.233,.267,.269,.270,.315,.364,.347,.312,.274,.237,.278,.284,.277,null,null,null,.374,.413,.405,.355,.306,.271,.306,.315,.301,.356,.348,.355,null,.465,.467,.404,.347,null,.336,.340,.318,null,.348,.363,.435,.491,.505,.404,.359,.310,.337,.360,.342,.406,.396,.420,.472,.548,.559,.463,.407,.362,null,.417,.391,.419,.461,null,.535,.622,.606,.508,.461,.390,.432};
-
-		data = new Double[]{41.7275, 24.0418, 32.3281, 37.3287, 46.2132, 29.3463, 36.4829, 42.9777, 48.9015, 31.1802, 37.7179, 40.4202,
-				51.2069, 31.8872, 40.9783, 43.7725, 55.5586, 33.8509, 42.0764, 45.6423, 59.7668, 35.1919, 44.3197, 47.9137};
-		int n = data.length;
-
-		double best_alpha = .0;
-		double best_beta = .0;
-		double best_gamma = .0;
+		Double[] data= new Double[]{.112,.118,.132,.129, .131,.135,.148,.148,.149,.119,.104,.118,.115,.126,.141,.135,.125,.149,.170,.170,Double.NaN,.133,Double.NaN,.140,.145,.150,.178,.163,.172,.178,.199,.199,.184,.162,.146,.166,.171,.180,.193,.181,.183,.218,.230,.242,.209,.191,.172,.194,.196,.196,.236,.235,.229,.243,.264,.272,.237,.211,.180,.201,.204,.188,.235,.227,.234,Double.NaN,.302,.293,.259,.229,.203,.229,.242,.233,.267,.269,.270,.315,.364,.347,.312,.274,.237,.278,.284,.277,Double.NaN,Double.NaN,Double.NaN,.374,.413,.405,.355,.306,.271,.306,.315,.301,.356,.348,.355,Double.NaN,.465,.467,.404,.347,Double.NaN,.336,.340,.318,Double.NaN,.348,.363,.435,.491,.505,.404,.359,.310,.337,.360,.342,.406,.396,.420,.472,.548,.559,.463,.407,.362,Double.NaN,.417,.391,.419,.461,Double.NaN,.535,.622,.606,.508,.461,.390,.432};
 		Container best_cont = new Container(new Double[]{.0}, 1000);
 
 		Random rand = new Random();
@@ -86,8 +78,6 @@ public class EMAUtils {
 			Double alpha = rand.nextDouble();
 			Double beta = rand.nextDouble();
 			Double gamma = rand.nextDouble();
-
-			mode = "single";
 
 			if (mode.equals("single")) {
 				lst = single_exponential_smoothing(data, alpha);
@@ -99,9 +89,6 @@ public class EMAUtils {
 
 			if (i == 0 || lst.rsme < best_cont.rsme) {
 				best_cont = lst;
-				best_alpha = alpha;
-				best_beta = beta;
-				best_gamma = gamma;
 			}
 
 		}
@@ -131,7 +118,7 @@ public class EMAUtils {
 		int n_size = 0;
 
 		for (int i = 1; i < n; i++) {
-			if (data[i] == null) {
+			if (Double.isNaN(data[i])) {
 				val = pred[i - 1];
 			} else {
 				val = data[i];
@@ -141,7 +128,7 @@ public class EMAUtils {
 		}
 
 		for (int i = 0; i < data.length; i++) {
-			if (data[i] != null) {
+			if (!Double.isNaN(data[i])) {
 				not_missing.add(data[i]);
 				not_missing_pred.add(pred[i]);
 				n_size++;
@@ -176,7 +163,7 @@ public class EMAUtils {
 		int n_size = 0;
 
 		for (int i = 1; i < n-1; i++) {
-			if (data[i+1] == null) {
+			if (Double.isNaN(data[i + 1])) {
 				val = pred.get(i - 1);
 			} else {
 				val = data[i+1];
@@ -190,7 +177,7 @@ public class EMAUtils {
 		pred.add(0, data[0]);
 
 		for (int i = 0; i < data.length; i++) {
-			if (data[i] != null) {
+			if (!Double.isNaN(data[i])) {
 				not_missing.add(data[i]);
 				not_missing_pred.add(pred.get(i));
 				n_size++;
@@ -238,7 +225,7 @@ public class EMAUtils {
 		for (int i = 0; i < l - trend_len; i++) {
 			double sum = 0;
 			for (int j = i; j < i + filt.size(); j++) {
-				sum += data[j] * filt.get(j - i);
+				sum += start_data.get(j) * filt.get(j - i);
 			}
 
 			trend.add(sum);
@@ -311,7 +298,7 @@ public class EMAUtils {
 		int n_size = 0;
 
 		for (int i = 1; i < n - freq; i++) {
-			if (data[i + freq - 1] == null) {
+			if (Double.isNaN(data[i + freq - 1] )) {
 				val = pred.get(i - 1);
 			} else {
 				val = data[i + freq - 1];
@@ -329,7 +316,7 @@ public class EMAUtils {
 		}
 
 		for (int i = 0; i < data.length; i++) {
-			if (data[i] != null) {
+			if (!Double.isNaN(data[i])) {
 				not_missing.add(data[i]);
 				not_missing_pred.add(pred.get(i));
 				n_size++;

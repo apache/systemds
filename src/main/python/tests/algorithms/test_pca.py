217d7620404031f8d03da0dece7e7628ca3c613d
TMaddox <github@mailexpress.cc>
2021-04-20 23:57:52 +0200

    Added two testing functions to test the behaviour of the OutputNode2 in a scenario where a node with multiple outputs is part of the DAG while not being the root node

--- src/main/python/tests/algorithms/test_pca.py
+++ src/main/python/tests/algorithms/test_pca.py
@@ -25,6 +25,8 @@ import numpy as np
 from systemds.context import SystemDSContext
 from systemds.matrix import Matrix
 from systemds.operator.algorithm import pca
+from systemds.operator import OperationNode2
+from systemds.script_building.dag import OutputType
 
 
 class TestPCA(unittest.TestCase):
@@ -49,9 +51,37 @@ class TestPCA(unittest.TestCase):
         m1 = self.generate_matrices_for_pca(30, seed=1304)
         X = Matrix(self.sds, m1)
         # print(features)
-        [res, model, _, _ ] = pca(X, K=1, scale="FALSE", center="FALSE").compute()
+        [res, model, _, _] = pca(X, K=1, scale="FALSE", center="FALSE").compute()
         for (x, y) in zip(m1, res):
             self.assertTrue((x[0] > 0 and y > 0) or (x[0] < 0 and y < 0))
+
+    def test_500x2b(self):
+        """
+        This test constructs a line of values in 2d space. 
+        That if fit correctly maps perfectly to 1d space.
+        The check is simply if the input value was positive
+        then the output value should be similar.
+        """
+        m1 = self.generate_matrices_for_pca(30, seed=1304)
+        X = Matrix(self.sds, m1)
+        # print(features)
+        X._check_matrix_op()
+        params_dict = {'X': X, 'K': 1, 'scale': "FALSE", 'center': "FALSE"}
+        nodeC = OperationNode2(self.sds, 'pca', named_input_nodes=params_dict,
+                               outputs=[("res", OutputType.MATRIX), ("model", OutputType.MATRIX), ("scale", OutputType.MATRIX), ("center", OutputType.MATRIX)])
+        nodeD = OperationNode2(self.sds, 'abs', unnamed_input_nodes=[nodeC.output_nodes["model"]])
+        nodeD_res = nodeD.compute(verbose=True)
+
+    def test_multiple_outputs(self):
+        # Added a second test function because test_500x2b doesn't account for the case where multiple outputs of a node which provides
+        # multiple outputs are used
+        x = Matrix(self.sds, np.array([1,2,3,4,5,6,7,8,9]))
+        y = Matrix(self.sds, np.array([10,20,30,40,50,60,70,80,90]))
+        params_dict = {'X': x, 'Y': y}
+        M2 = OperationNode2(self.sds, 'split', named_input_nodes=params_dict,
+                            outputs=[("X_train", OutputType.MATRIX), ("X_test", OutputType.MATRIX), ("Y_train", OutputType.MATRIX), ("Y_test", OutputType.MATRIX)])
+        M3 = M2.output_nodes["X_train"] + M2.output_nodes["Y_train"]
+        res = M3.compute(verbose=True)
 
     def test_simple(self):
         """

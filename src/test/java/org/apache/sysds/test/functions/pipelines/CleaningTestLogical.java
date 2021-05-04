1e84af8d8ecc61e86ef443dffa34448801872ba3
Shafaq Siddiqi <shafaq.siddiqi@tugraz.at>
2021-05-04 11:16:40 +0200

    [SYSTEMDS-2962] Initial implementation of Logical Pipelines optimizer   Optimizer is based on evolutionary algorithm with constrained crossover and mutation   this commit also contains some minor formatting cleanups in pipelines package Closes #1249.

--- src/test/java/org/apache/sysds/test/functions/pipelines/CleaningTestLogical.java
+++ src/test/java/org/apache/sysds/test/functions/pipelines/CleaningTestLogical.java
@@ -27,10 +27,9 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class CleaningTestClassification extends AutomatedTestBase {
-	private final static String TEST_NAME1 = "testClassification";
-	private final static String TEST_NAME2 = "compareAccuracy";
-	private final static String TEST_CLASS_DIR = SCRIPT_DIR + CleaningTestClassification.class.getSimpleName() + "/";
+public class CleaningTestLogical extends AutomatedTestBase {
+	private final static String TEST_NAME = "testLogical";
+	private final static String TEST_CLASS_DIR = SCRIPT_DIR + CleaningTestLogical.class.getSimpleName() + "/";
 
 	private static final String RESOURCE = SCRIPT_DIR+"functions/pipelines/";
 	private static final String DATA_DIR = DATASET_DIR+ "pipelines/";
@@ -38,7 +37,6 @@ public class CleaningTestClassification extends AutomatedTestBase {
 	private final static String DIRTY = DATA_DIR+ "dirty.csv";
 	private final static String CLEAN = DATA_DIR+ "clean.csv";
 	private final static String META = RESOURCE+ "meta/meta_census.csv";
-	private final static String OUTPUT = RESOURCE+"intermediates/";
 
 	private static final String PARAM_DIR = "./scripts/pipelines/properties/";
 	private final static String PARAM = PARAM_DIR + "param.csv";
@@ -46,55 +44,34 @@ public class CleaningTestClassification extends AutomatedTestBase {
 
 	@Override
 	public void setUp() {
-		addTestConfiguration(TEST_NAME1,new TestConfiguration(TEST_CLASS_DIR, TEST_NAME1,new String[]{"R"}));
-		addTestConfiguration(TEST_NAME2,new TestConfiguration(TEST_CLASS_DIR, TEST_NAME2,new String[]{"R"}));
-	}
-
-	@Ignore
-	public void testFindBestPipeline() {
-		runFindPipelineTest(0.1, 5,10, 2,
-			true, "classification", Types.ExecMode.SINGLE_NODE);
+		addTestConfiguration(TEST_NAME,new TestConfiguration(TEST_CLASS_DIR, TEST_NAME,new String[]{"R"}));
 	}
 
 	@Test
-	public void testCompareRepairs() {
-		runCleanAndCompareTest( Types.ExecMode.SINGLE_NODE);
+	public void testLogical1() {
+		runTestLogical(2, 10, 2, 2, 2, 2,
+			"classification", Types.ExecMode.SINGLE_NODE);
 	}
 
-	private void runFindPipelineTest(Double sample, int topk, int resources, int crossfold,
-		boolean weightedAccuracy, String target, Types.ExecMode et) {
+	@Ignore
+	public void testLogicalSP() {
+		runTestLogical(3, 10, 3, 2, 2, 4,
+			"classification", Types.ExecMode.SPARK);
+	}
 
-		setOutputBuffering(true);
+	private void runTestLogical(int max_iter, int pipelineLength, int crossfold,
+		int num_inst, int num_exec, int n_pop, String target, Types.ExecMode et) {
+
+		//		setOutputBuffering(true);
 		String HOME = SCRIPT_DIR+"functions/pipelines/" ;
 		Types.ExecMode modeOld = setExecMode(et);
 		try {
-			loadTestConfiguration(getTestConfiguration(TEST_NAME1));
-			fullDMLScriptName = HOME + TEST_NAME1 + ".dml";
+			loadTestConfiguration(getTestConfiguration(TEST_NAME));
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
 			programArgs = new String[] {"-stats", "-exec", "singlenode", "-nvargs", "dirtyData="+DIRTY,
-				"metaData="+META, "primitives="+PRIMITIVES, "parameters="+PARAM, "sampleSize="+ sample,
-				"topk="+ topk, "rv="+ resources, "cv="+ crossfold, "weighted="+ weightedAccuracy,
-				"output="+OUTPUT, "target="+target, "cleanData="+CLEAN, "O="+output("O")};
-
-			runTest(true, EXCEPTION_NOT_EXPECTED, null, -1);
-
-			//expected loss smaller than default invocation
-			Assert.assertTrue(TestUtils.readDMLBoolean(output("O")));
-		}
-		finally {
-			resetExecMode(modeOld);
-		}
-	}
-
-	private void runCleanAndCompareTest( Types.ExecMode et) {
-		setOutputBuffering(true);
-		String HOME = SCRIPT_DIR+"functions/pipelines/";
-		Types.ExecMode modeOld = setExecMode(et);
-		try {
-			loadTestConfiguration(getTestConfiguration(TEST_NAME2));
-			fullDMLScriptName = HOME + TEST_NAME2 + ".dml";
-
-			programArgs = new String[] {"-stats", "-exec",
-				"singlenode", "-args", DIRTY, CLEAN, META, OUTPUT, output("O")};
+				"metaData="+META, "primitives="+PRIMITIVES, "parameters="+PARAM, "max_iter="+ max_iter,
+				 "pipLength="+ pipelineLength, "cv="+ crossfold, "num_inst="+ num_inst, "num_exec="+ num_exec,
+				"n_pop="+ n_pop,"target="+target, "cleanData="+CLEAN, "O="+output("O")};
 
 			runTest(true, EXCEPTION_NOT_EXPECTED, null, -1);
 

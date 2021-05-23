dd2a8767e924cb33a0a4ca1060f2f36ebd9418e6
arnabp <arnab.phani@tugraz.at>
2021-05-23 10:36:58 +0200

    [SYSTEMDS-2980] Add statistics for lineage cache in GPU
    This patch adds a initial set of statistics for reuse
    and eviction of GPU intermediates.
    e.g. LinCache GPU (Hit/Async/Sync): 	38/26/25

--- src/main/java/org/apache/sysds/utils/Statistics.java
+++ src/main/java/org/apache/sysds/utils/Statistics.java
@@ -1024,6 +1024,7 @@ public class Statistics
 			if (DMLScript.LINEAGE && !ReuseCacheType.isNone()) {
 				sb.append("LinCache hits (Mem/FS/Del): \t" + LineageCacheStatistics.displayHits() + ".\n");
 				sb.append("LinCache MultiLevel (Ins/SB/Fn):" + LineageCacheStatistics.displayMultiLevelHits() + ".\n");
+				sb.append("LinCache GPU (Hit/Async/Sync): \t" + LineageCacheStatistics.displayGpuStats() + ".\n");
 				sb.append("LinCache writes (Mem/FS/Del): \t" + LineageCacheStatistics.displayWtrites() + ".\n");
 				sb.append("LinCache FStimes (Rd/Wr): \t" + LineageCacheStatistics.displayFSTime() + " sec.\n");
 				sb.append("LinCache Computetime (S/M): \t" + LineageCacheStatistics.displayComputeTime() + " sec.\n");

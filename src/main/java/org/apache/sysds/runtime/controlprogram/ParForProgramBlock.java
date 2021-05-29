c4115fcc172bb4994fd70e8e746eb46c35007f59
Matthias Boehm <mboehm7@gmail.com>
2021-05-30 00:16:55 +0200

    [SYSTEMDS-2989] Fix thread contention in parfor eval function calls
    In parfor, individual functions are deep copied per worker in order to
    prevent thread contention on shared HOP DAGs during recompilation.
    However, if functions are called through eval (second-order functions),
    this setup cannot be done. For that reason, gridSearch with functions
    that do not benefit from recompile-once function compilation (e.g.,
    MLogreg) showed large overhead.
    
    This patch fixes this thread contention issue by also deep-copying the
    functions called through eval, but on-demand during eval function calls.
    On a scenario of gridSearch with a single node (112 vcores), the Adult
    dataset, 3003 hyper-parameter configurations and MLogreg, this patch
    improved end-to-end performance by 3x (from 1,911s to 654s) and
    recompilation times by >15x (from 147,624s to 9,318s).

--- src/main/java/org/apache/sysds/runtime/controlprogram/ParForProgramBlock.java
+++ src/main/java/org/apache/sysds/runtime/controlprogram/ParForProgramBlock.java
@@ -299,6 +299,7 @@ public class ParForProgramBlock extends ForProgramBlock
 	public static final boolean LIVEVAR_AWARE_EXPORT        = true; // export only read variables according to live variable analysis
 	public static final boolean RESET_RECOMPILATION_FLAGs   = true;
 	public static       boolean ALLOW_BROADCAST_INPUTS      = true; // enables to broadcast inputs for remote_spark
+	public static final boolean COPY_EVAL_FUNCTIONS         = true; // copy eval functions similar to normal parfor functions
 	
 	public static final String PARFOR_FNAME_PREFIX          = "/parfor/"; 
 	public static final String PARFOR_MR_TASKS_TMP_FNAME    = PARFOR_FNAME_PREFIX + "%ID%_MR_taskfile"; 

7d01dc9417a717bc6c9739ffe413b7c7d6f94d23
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2021-06-07 15:35:21 +0200

    deleted library

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinShortestPathTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinShortestPathTest.java
@@ -26,7 +26,6 @@ import org.junit.Test;
 
 import org.apache.sysds.common.Types;
 import org.apache.sysds.lops.LopProperties;
-import org.apache.sysds.lops.LopProperties.ExecType;
 import org.apache.sysds.runtime.matrix.data.MatrixValue;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;

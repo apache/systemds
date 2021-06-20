4186b9c16af386aaf8a84251c1fd482f421328a7
baunsgaard <baunsgaard@tugraz.at>
2021-06-20 20:05:33 +0200

    add license

--- src/test/java/org/apache/sysds/test/component/compress/colgroup/SDCConstructionTest.java
+++ src/test/java/org/apache/sysds/test/component/compress/colgroup/SDCConstructionTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sysds.test.component.compress.colgroup;
 
 import org.apache.sysds.runtime.compress.CompressionSettings;
@@ -123,24 +141,24 @@ public class SDCConstructionTest {
 		// ABitmap ubm = BitmapEncoder.extractBitmap(colIndexes, null, compSettings.transposed);
 		CompressionSettings cs = new CompressionSettingsBuilder().create();
 		ABitmap ubm = new Bitmap(gen(offsets), values, 1000);
-			try{
+		try {
 
-				ColGroupFactory.compress(colIndexes, 1000, ubm, CompressionType.SDC, cs, null, 1.0);
-			}
-			catch(Exception e){
-				e.printStackTrace();
-				throw e;
-			}
+			ColGroupFactory.compress(colIndexes, 1000, ubm, CompressionType.SDC, cs, null, 1.0);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	public IntArrayList[] gen(int[][] in) {
 		IntArrayList[] res = new IntArrayList[in.length];
 		int idx = 0;
 		int totalLen = 0;
-		for(int[] i : in){
+		for(int[] i : in) {
 			totalLen += i.length;
 			res[idx++] = new IntArrayList(i);
-		} 
+		}
 		System.out.println(totalLen);
 		return res;
 	}

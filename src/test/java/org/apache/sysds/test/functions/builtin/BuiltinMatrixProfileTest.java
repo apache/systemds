4cdce621a55f031f5f268b39fece9d363e5b6700
Alexander Ertl <ertl@student.tugraz.at>
2021-07-15 20:09:25 +0200

    update test

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinMatrixProfileTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinMatrixProfileTest.java
@@ -20,13 +20,11 @@
 package org.apache.sysds.test.functions.builtin;
 
 import org.apache.sysds.common.Types.ExecMode;
-// import org.apache.sysds.common.Types.FileFormat;
 import org.apache.sysds.common.Types.ExecType;
 import org.apache.sysds.runtime.matrix.data.MatrixValue.CellIndex;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -41,89 +39,78 @@ import java.util.Map;
 
 public class BuiltinMatrixProfileTest extends AutomatedTestBase 
 {
-	private final static String TEST_NAME = "matrix_profile";
-	private final static String TEST_DIR = "functions/builtin/";
-	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinMatrixProfileTest.class.getSimpleName() + "/";
-	
+  private final static String TEST_NAME = "matrix_profile";
+  private final static String TEST_DIR = "functions/builtin/";
+  private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinMatrixProfileTest.class.getSimpleName() + "/";
+  
   private static Random generator;
   private final static int seed = 42;
+  
+  @Override
+  public void setUp() {
+    addTestConfiguration(TEST_NAME,new TestConfiguration(TEST_CLASS_DIR, TEST_NAME,new String[]{"B"})); 
+  }
 
-	@Override
-	public void setUp() {
-		addTestConfiguration(TEST_NAME,new TestConfiguration(TEST_CLASS_DIR, TEST_NAME,new String[]{"B"})); 
-	}
+  @Test
+  public void testMatrixProfileCP() throws IOException {
+    runMatrixProfileTest(4, 1.0, "TRUE", ExecType.CP);
+  }
 
-	 @Test
-	 public void testMatrixProfileCP() throws IOException {
-		 runMatrixProfileTest(4, 1.0, "TRUE", ExecType.CP);
-	 }
+  @Test
+  public void testMatrixProfileApproxCP() throws IOException {
+    runMatrixProfileTest(4, 0.6, "TRUE", ExecType.CP);
+  }
 
-	 @Test
-	 public void testMatrixProfileApproxCP() throws IOException {
-		 runMatrixProfileTest(4, 0.6, "TRUE", ExecType.CP);
-	 }
+  // @Test
+  // public void testMatrixProfileSPARK() throws IOException {
+    // runMatrixProfileTest(4, 1.0, "FALSE", ExecType.SPARK);
+  // }
 
-   // @Test
-	 // public void testMatrixProfileSPARK() throws IOException {
-		 // runMatrixProfileTest(4, 1.0, "TRUE", ExecType.SPARK);
-	 // }
+  
+  private void runMatrixProfileTest(Integer window_size, Double sample_percent, String is_verbose, ExecType instType) throws IOException
+  {
+    ExecMode platformOld = setExecMode(instType);
 
-	
-	private void runMatrixProfileTest(Integer window_size, Double sample_percent, String is_verbose, ExecType instType) throws IOException
-	{
-		ExecMode platformOld = setExecMode(instType);
-
-		System.out.println("Begin MatrixProfileTest");
+    System.out.println("Begin MatrixProfileTest");
     
-		try
-		{
-			loadTestConfiguration(getTestConfiguration(TEST_NAME));
+    try
+    {
+      loadTestConfiguration(getTestConfiguration(TEST_NAME));
 
-			String HOME = SCRIPT_DIR + TEST_DIR;
-			fullDMLScriptName = HOME + TEST_NAME + ".dml";
-			programArgs = new String[]{
-				"-nvargs", "TS=" + input("TS"), "MP=" + output("MP"),
+      String HOME = SCRIPT_DIR + TEST_DIR;
+      fullDMLScriptName = HOME + TEST_NAME + ".dml";
+      programArgs = new String[]{
+        "-nvargs", "TS=" + input("TS"), "MP=" + output("MP"),
         "MPI=" + output("MPI"),
-				"window_size=" + window_size,
+        "window_size=" + window_size,
         "sample_percent=" + sample_percent,
-				"is_verbose=" + is_verbose};
+        "is_verbose=" + is_verbose};
 
-			generator = new Random(seed);
+      generator = new Random(seed);
 
-      int len = 50;
-      // double[][] ts = initTimeSeries(10);
+      int len = 100;
       double[][] ts = genSineWave(len, 0.05, 1, 1);
       int[] noise_idxs = addNoise(1, len, ts);
       writeInputMatrixWithMTD("TS", ts, false);
 
-			System.out.println("Run test");
-			runTest(true, false, null, -1);
-			System.out.println("DONE");
+      System.out.println("Run test");
+      runTest(true, false, null, -1);
 
-			HashMap<CellIndex, Double> MP = readDMLMatrixFromOutputDir("MP");
-			HashMap<CellIndex, Double> MPI = readDMLMatrixFromOutputDir("MPI");
+      HashMap<CellIndex, Double> MP = readDMLMatrixFromOutputDir("MP");
+      HashMap<CellIndex, Double> MPI = readDMLMatrixFromOutputDir("MPI");
 
       List sortedList = sortByValues(MP);
-			// System.out.println(sortedList);
       Map.Entry entry = (Map.Entry)sortedList.get(0);
-      System.out.println(entry);
       int highest_dist_idx = ((CellIndex)entry.getKey()).row;
-      System.out.println(highest_dist_idx);
-
       int noise_idx = noise_idxs[0];
-      Assert.assertTrue(highest_dist_idx>noise_idx-window_size || highest_dist_idx<noise_idx+window_size);
-		}
-		finally {
-			rtplatform = platformOld;
-		}
-	}
 
-  private static double[][] initTimeSeries(Integer n) {
-    double[][] ts = new double[n][1];
-    for (int i=0; i<n; ++i) {
-      ts[i][0] = i;
+      System.out.println("Detected anomaly around idx " + highest_dist_idx);
+      System.out.println("Noise idx: " + noise_idx);
+      Assert.assertTrue(highest_dist_idx>noise_idx-window_size && highest_dist_idx<noise_idx+window_size);
     }
-    return ts;
+    finally {
+      rtplatform = platformOld;
+    }
   }
 
   private static double[][] genSineWave(Integer n, double sampling_rate, float p, float amp) {
@@ -143,12 +130,6 @@ public class BuiltinMatrixProfileTest extends AutomatedTestBase
       idxs[i] = idx;
     }
     return idxs;
-  }
-
-  private static boolean verifyResult(double[][] matrix_profile, 
-      int[][] matrix_profile_index, int[] perturbation_idxs) {
-
-    return true;
   }
 
   private static List sortByValues(HashMap map) {

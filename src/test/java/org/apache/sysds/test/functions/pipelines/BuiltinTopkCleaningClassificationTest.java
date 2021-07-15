405e500a396ce403bf89aad79e23cfd1bb8ae3c7
Shafaq Siddiqi <shafaq.siddiqi@tugraz.at>
2021-07-15 17:02:55 +0200

    adding missing headers and removing prints

--- src/test/java/org/apache/sysds/test/functions/pipelines/BuiltinTopkCleaningClassificationTest.java
+++ src/test/java/org/apache/sysds/test/functions/pipelines/BuiltinTopkCleaningClassificationTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sysds.test.functions.pipelines;
 
 import org.apache.sysds.common.Types;
@@ -35,7 +53,7 @@ public class BuiltinTopkCleaningClassificationTest extends AutomatedTestBase {
 
 	private void runtopkCleaning(Double sample, int topk, int resources, String testName, Types.ExecMode et) {
 
-		//		setOutputBuffering(true);
+		setOutputBuffering(true);
 		Types.ExecMode modeOld = setExecMode(et);
 		String HOME = SCRIPT_DIR + TEST_DIR;
 		try {

553f2e92e00711344c55ef8667dd9dbd951a49bf
baunsgaard <baunsgaard@tugraz.at>
2021-07-15 13:56:29 +0200

    [SYSTEMDS-3057, SYSTEMDS-3058] Transpose Bug Fix for nCells > integer & Transpose to CSR
    This commit fixes a bug that forced transpose to execute single threaded
    because of an inter overflow when multiplying nRows with nCol of a matrix.
    
    Also contained in this commit is sparse transpose to CSR layout, if
    number of non zero values is less than Integer.MAX_VALUE. this give
    10-20% faster transpose of sparse to sparse matrices
    
    Parallelization over columns in sparse transpose is increased to a minimum
    of 32 columns is parallelized. In cases with many threads and not
    many columns (MNIST for instance) the previous minimum of 8 columns
    lead to inefficient parallelization, since the larger number of columns
    each threads have to process the better the utilization of cache.
    increasing it above 32 reduced the performance because of the reduced
    number of cores used.

--- src/test/java/org/apache/sysds/test/component/matrix/TransposeInplaceTest.java
+++ src/test/java/org/apache/sysds/test/component/matrix/TransposeInplaceTest.java
@@ -24,7 +24,6 @@ import java.util.Collection;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.sysds.runtime.controlprogram.parfor.stat.Timing;
 import org.apache.sysds.runtime.functionobjects.SwapIndex;
 import org.apache.sysds.runtime.matrix.data.LibMatrixReorg;
 import org.apache.sysds.runtime.matrix.data.MatrixBlock;
@@ -44,28 +43,13 @@ public class TransposeInplaceTest {
 	@Parameters
 	public static Collection<Object[]> data() {
 		ArrayList<Object[]> tests = new ArrayList<>();
-		
-		tests.add(genIncMatrix(4, 8));
-		tests.add(genIncMatrix(8, 4));
-		tests.add(genIncMatrix(4, 12));
-		tests.add(genIncMatrix(4, 16));
-		tests.add(genIncMatrix(12, 4));
-		tests.add(genIncMatrix(12, 56));
-		// tests.add(genIncMatrix(3, 6));
-		// tests.add(genIncMatrix(2, 5));
-		// tests.add(genIncMatrix(2, 10));
-		// tests.add(genIncMatrix(4, 6));
-		// tests.add(genIncMatrix(13, 51));
-		// tests.add(genIncMatrix(30000, 290));
-		
-		// tests.add(genIncMatrix(80, 40));
-		// tests.add(genIncMatrix(40, 20));
-		// tests.add(genIncMatrix(30000, 30));
-		// tests.add(genIncMatrix(30000, 300));
-		// tests.add(genIncMatrix(100000, 5000));
-		// tests.add(genIncMatrix(300, 28));
-		// tests.add(genIncMatrix(30, 300000));
-		
+
+		tests.add(genIncMatrix(4, 8, 1));
+		tests.add(genIncMatrix(8, 4, 1));
+		tests.add(genIncMatrix(4, 12, 4));
+		tests.add(genIncMatrix(4, 16, 4));
+		tests.add(genIncMatrix(12, 4, 4));
+		tests.add(genIncMatrix(12, 56, 4));
 
 		return tests;
 	}
@@ -74,59 +58,39 @@ public class TransposeInplaceTest {
 	public MatrixBlock in;
 	@Parameterized.Parameter(1)
 	public int[][] org;
-
-	private static ReorgOperator opP = new ReorgOperator(SwapIndex.getSwapIndexFnObject(), 16);
-	//private static ReorgOperator op = new ReorgOperator(SwapIndex.getSwapIndexFnObject(), 1);
+	@Parameterized.Parameter(2)
+	public int threads;
 
 	private int rep = 2;
+
 	@Test
 	public void testEstimation() {
-		// for (int i = 0; i < rep; i++){
-
-		//	 LibMatrixReorg.reorgInPlace(in, op);
-		//	 Assert.assertEquals(in.quickGetValue(i%2, 1- i%2), org[1][0], 0.00001);
-		// }
-		// for (int i = 0; i < rep; i++){
-		//	 in = LibMatrixReorg.reorg(in,new MatrixBlock(in.getNumColumns(), in.getNumRows(), false), op);
-		// }
-		try{
-			
-			Timing time = new Timing(true);
-			// for (int i = 0; i < rep; i++){
-			//	 in = LibMatrixReorg.reorg(in,new MatrixBlock(in.getNumColumns(), in.getNumRows(), false), opP);
-			// }
-	
-			// System.out.println("duplicate avg: "+(time.stop() / rep)+" ms.");
-			// time = new Timing(true);
-			for (int i = 0; i < rep; i++){
+		ReorgOperator opP = new ReorgOperator(SwapIndex.getSwapIndexFnObject(), threads);
+		
+		try {
+			// very small test only verifying one cell...
+			for(int i = 0; i < rep; i++) {
 				LibMatrixReorg.reorgInPlace(in, opP);
-				// LOG.error(in);
-				// LOG.error(in.slice(0, 3, 0, 3, null));
-				Assert.assertEquals(org[1][0],in.quickGetValue(i%2, 1- i%2), 0.00001);
+				Assert.assertEquals(org[1][0], in.quickGetValue(i % 2, 1 - i % 2), 0.00001);
 			}
-			
-			System.out.println("in place avg: "+(time.stop() / rep)+" ms.");
-	
-		} catch(Exception e){
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}
-		// very small test only verifying one cell...
 	}
-
-	
 
 	private static MatrixBlock gen(int[][] v) {
 		return DataConverter.convertToMatrixBlock(v);
 	}
 
-	private static Object[] genIncMatrix(int rows, int cols ){
+	private static Object[] genIncMatrix(int rows, int cols, int threads) {
 		int[][] ret = new int[rows][cols];
 		int x = 0;
-		for (int i = 0; i< rows; i ++){
-			for (int j = 0; j < cols; j++){
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
 				ret[i][j] = x++;
 			}
 		}
-		return new Object[]{gen(ret), ret};
+		return new Object[] {gen(ret), ret, threads};
 	}
 }

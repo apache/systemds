4a7e693c010a49592d92c5a504e43c4dbf72b048
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2021-07-28 16:16:54 +0300

    Fix Binary instruction

--- src/main/java/org/apache/sysds/runtime/instructions/fed/BinaryMatrixMatrixFEDInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/fed/BinaryMatrixMatrixFEDInstruction.java
@@ -51,20 +51,24 @@ public class BinaryMatrixMatrixFEDInstruction extends BinaryFEDInstruction
 			mo2 = ec.getMatrixObject(input1);
 		}
 
+		if(mo1.isFederated(FType.PART)) {
+			FederationUtils.checkFedMapType(mo1);
+		}
+		if(mo2.isFederated(FType.PART)) {
+			FederationUtils.checkFedMapType(mo2);
+		}
+
 		//execute federated operation on mo1 or mo2
 		FederatedRequest fr2 = null;
 		if( mo2.isFederated() ) {
 			if((mo1.isFederated() && mo1.getFedMapping().isAligned(mo2.getFedMapping(),
-					mo1.isFederated(FType.ROW) ? AlignType.ROW : AlignType.COL))
-				|| (mo1.getFedMapping().getType() == FType.PART && mo2.getFedMapping().getType() == FType.PART &&
-				mo1.getFedMapping().isAligned(mo2.getFedMapping(), false))) {
+					mo1.isFederated(FType.ROW) ? AlignType.ROW : AlignType.COL))) {
 				fr2 = FederationUtils.callInstruction(instString, output,
 					new CPOperand[]{input1, input2},
 					new long[]{mo1.getFedMapping().getID(), mo2.getFedMapping().getID()}, true);
 				mo1.getFedMapping().execute(getTID(), true, fr2);
 			}
 			else if ( !mo1.isFederated() || (mo1.isFederated(FType.PART) && !mo1.isDataEmpty())){
-				System.out.println(mo1.getFedMapping().isAligned(mo2.getFedMapping(), false));
 				FederatedRequest[] fr1 = mo2.getFedMapping().broadcastSliced(mo1, false, mo2.getUniqueID());
 				fr2 = FederationUtils.callInstruction(instString, output,
 					new CPOperand[]{input1, input2},

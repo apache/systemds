cd27ea47ea02ff3b569d2189404fc0568fe732cf
Christof Jahn <c.jahn19@gmx.at>
2021-08-01 21:05:02 +0200

    Added all required improvements stated by Prof Dr. Boehm

--- scripts/builtin/mark_down_creator.py
+++ scripts/builtin/mark_down_creator.py
@@ -1,37 +1,47 @@
-import re
-import os
-import sys
-from mdutils.mdutils import MdUtils
+#-------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#-------------------------------------------------------------
 
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------
 # This python script does take all files within the current directory and creates a corresponding markdown file out of the existing headers
 #   If there are specific parts of the header missing a warning within the terminal will be displayed
 #   The finished markdown file is then placed in the same directory
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------
 # OUTPUTS
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------
 #   Name                            Type    Default     Meaning
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
-#   SystemDs_Builtin_Markdown       .dm     -------     A .dm file of all files with a parsable header
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
-# Additional Information
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
-# If the output within the terminal does not look correct because the os isnt supporting colored output,
-#   please swap the comment prefix in line 300 and 301
+#-------------------------------------------------------------
+#   SystemDs_Builtin_Markdown       .md    -------     A .md file of all files with a parsable header
+#-------------------------------------------------------------
 
+import re
+import os
+from mdutils.mdutils import MdUtils
 
+file_data_array = [] #Contains all valid header information
 
+incorrect_file_data = [] #Contains all file names with invalid header
 
-#This Array contains all valid information
-file_data_array = []
+ALLOW_DIFFERENT_SIZED_HEADERS = False #Global FLAG to identify if Headers with only 3 arguments are valid or not
 
-#This Array contains all invalid file names with corresponding missing values
-incorrect_file_data = []
-
-#Global FLAG to identify if Headers with only 3 arguments are valid or not!
-ALLOW_DIFFERENT_SIZED_HEADERS = False
-
+THROW_EXCEPTION = False #Global FLAG to throw Exception on existing warnings - If set True no md file will be created unless all files have valid headers!
 
 
 #This class contains all necessary variables for the complete representation of the markdown file
@@ -49,16 +59,13 @@ class File_data:
         self.additional = additional
         self.fileName = fileName
 
-#class to make a pretty output possible
+
+#class to make colored output possible
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
 
@@ -74,6 +81,7 @@ def callWithAbsoluteParameters():
     cleaned_array = cleanFileDataArray()
     createMarkDownFile(cleaned_array)
 
+
 #Function to start parsing the file, it skips the lincence agreement and then forwards the rest to the parse Information Function
 def parseFile(lines, fileName):
     fileName = os.path.basename(fileName)
@@ -81,6 +89,7 @@ def parseFile(lines, fileName):
 
     EMPTY_LINES = True
     markDownArray = []
+    license_break_count = 0
 
     lineBreakRegex = re.compile('#.[\-]+')  # Match all lines which look like #---- or # ---
     emptyLineRegex = re.compile('^\s*$') # Matches all empty lines within the header
@@ -88,10 +97,8 @@ def parseFile(lines, fileName):
     commentLineRegex = re.compile('^#.*$') #Matches any line with a # in front
     functionStartRegex = re.compile('^[A-Za-z_]*\s*=+') #Because we know that the start of the function name must be within the range of a-Z we can simply search for it to find the start of the program
 
-    license_break_count = 0
-
     for line in lines:
-        if lineBreakRegex.match(line) and license_break_count < 2:  #First we skip the apache license info
+        if lineBreakRegex.match(line) and license_break_count < 2:  #Skip Apache License info
             license_break_count += 1
             continue
         elif(license_break_count < 2):   #skip all lines within the apache license header
@@ -106,73 +113,55 @@ def parseFile(lines, fileName):
             EMPTY_LINES = False
             markDownArray.append(line)
 
-    #If we don´t have anything in our markDownArray we don´t need to continue
-    if len(markDownArray) > 0:
+    if len(markDownArray) > 0: #If our markDownArray is empty we append the name to the incorrect file_data_array
         parseInformation(markDownArray, fileName)
     else:
         incorrect_file_data.append(fileName)
 
+
 def parseInformation(markDownArray, fileName):
     markDownArray = [e[1:] for e in markDownArray]  # First I cut the first character of all lines because it is simply a # character
 
-    #Then I start to set some global flags to identify the status of my program
-    #As soon as INPUT_FINISHED is True I know that I already parsed some inputStringRegex parameters
-    INPUT = False
+    INPUT = False #Then I start to set some global flags to identify the status of my program
     INPUT_FINISHED = False
-
     OUTPUT = False
     OUTPUT_FINISHED = False
-
-    #When I start parsing the File, the first line(s) should ALWAYS contain the description therefore I start with True
-    DESCRIPTION = True
-
-    #To track how many comment_lines where found inside the headerRegex
-    commentLineCount = 0
-
-    #If there are information which do not match the syntax or are not identifyable I put it inside this array
-    additionalInfos = []
-
-    #contains the description of the corresponding file
-    description = []
-
-    #Number of parameters and array of parameters
+    DESCRIPTION = True  #When I start parsing the File, the first line(s) should ALWAYS contain the description therefore I start with True
+    commentLineCount = 0 #To track how many comment_lines where found inside the headerRegex
+    additionalInfos = [] #If there are information which do not match the syntax or are not identifyable it is put inside this array
+    description = [] #contains the description of the corresponding file
     I_DESCRIPTION_MISSING = False
     I_HEADER_MISSING = False
     inputParameterCount = 0
     inputParameters = []
-
-    # Number of outputs and array of parameters
     O_DESCRIPTION_MISSING = False
     O_HEADER_MISSING = False
     outputParameterCount = 0
     outputParameters = []
 
-
     commentLineRegex = re.compile('^#\s{0,}$')
     emptyLineRegex = re.compile('^\s*$')
     headerRegex = re.compile('\s{1,}[A-Za-z]{2,}')
-    inputStringRegex = re.compile('^#*\s{1,}input', flags=re.I)  #\s{1,}.*input  <-- other possibility to find all input fields, problem here that it matches all lines with input in the test
+    inputStringRegex = re.compile('^#*\s{1,}input', flags=re.I)
     outputStringRegex = re.compile('^#*\s{0,}(output|return)', flags=re.I)
 
     for line in markDownArray:
         if INPUT:
             inputParameterCount, INPUT, INPUT_FINISHED, I_HEADER_MISSING, commentLineCount = parseParam(inputParameters, inputParameterCount, INPUT, INPUT_FINISHED, I_HEADER_MISSING, commentLineCount, additionalInfos, line)
 
-
         if OUTPUT:
             outputParameterCount, OUTPUT, OUTPUT_FINISHED, O_HEADER_MISSING, commentLineCount = parseParam(outputParameters, outputParameterCount, OUTPUT, OUTPUT_FINISHED, O_HEADER_MISSING, commentLineCount, additionalInfos, line)
 
-        #First we care about the headerRegex, if everything worked out only the headerRegex information should be left
         if headerRegex.match(line) and DESCRIPTION:
             if inputStringRegex.match(line):
                 DESCRIPTION = False
                 INPUT = True
                 continue
+
             description.append(line)
         else:
             DESCRIPTION = False
 
-        #Within the parsing process I want to skip all empty lines or simple comment_lines
         if emptyLineRegex.match(line) or commentLineRegex.match(line):
             continue
 
@@ -182,8 +171,7 @@ def parseInformation(markDownArray, fileName):
         if outputStringRegex.match(line) and OUTPUT_FINISHED is False:
             OUTPUT = True
 
-    file_data = File_data(inputParameterCount, inputParameters, I_HEADER_MISSING, I_DESCRIPTION_MISSING, outputParameterCount, outputParameters, O_HEADER_MISSING, O_DESCRIPTION_MISSING, description, additionalInfos, fileName)
-    file_data_array.append(file_data)
+    file_data_array.append(File_data(inputParameterCount, inputParameters, I_HEADER_MISSING, I_DESCRIPTION_MISSING, outputParameterCount, outputParameters, O_HEADER_MISSING, O_DESCRIPTION_MISSING, description, additionalInfos, fileName))
 
 
 def createMarkDownFile(file_array):
@@ -191,7 +179,6 @@ def createMarkDownFile(file_array):
     mdFile.new_header(level=1, title='Overview')  # style is set 'atx' format by default.
 
     for entry in file_array:
-        #First I gather all Parameters from the Entry
         inputParameters = entry.param
         outputParameters = entry.output
         inputParameterCount = entry.param_count
@@ -200,7 +187,6 @@ def createMarkDownFile(file_array):
         additionalInfos = entry.additional
         fileName = entry.fileName
 
-
         #First I strip all the newline characters from the end of each string and replace the newlines within strings wit spaces
         inputParameters = replaceNewlineCharacter(inputParameters)
         outputParameters = replaceNewlineCharacter(outputParameters)
@@ -208,12 +194,11 @@ def createMarkDownFile(file_array):
 
         titleString = fileName + "-Function"
         mdFile.new_header(level=2, title=titleString)  # style is set 'atx' format by default.
+
         for line in description:
             mdFile.new_paragraph(line)
-        #usage would come next
-        mdFile.new_header(level=3, title="Usage")
 
-        #Creating the UsageString
+        mdFile.new_header(level=3, title="Usage")
         usage = fileName + "("
 
         for argument in inputParameters[4::4]:
@@ -225,17 +210,14 @@ def createMarkDownFile(file_array):
             usage = usage + ')'
 
         mdFile.insert_code(usage, language='python')
-
-        #arguments
         mdFile.new_header(level=3, title="Arguments")
 
         if inputParameterCount > 0:
             rows = int(len(inputParameters)/inputParameterCount)
             mdFile.new_table(columns=inputParameterCount, rows=rows, text=inputParameters, text_align='center')
 
-
-        #Returns
         mdFile.new_header(level=3, title="Returns")
+
         if outputParameterCount > 0:
             rows = int(len(outputParameters) / outputParameterCount)
             mdFile.new_table(columns=outputParameterCount, rows=rows, text=outputParameters, text_align='center')
@@ -249,10 +231,10 @@ def createMarkDownFile(file_array):
 def cleanFileDataArray():
     #Here I go through all elements of the file_data_array check if everything is correct and if not create a file  or meaningfull output
     new_file_data_array = []
+    missingValues = False
     for entry in file_data_array:
-        missingValues = False
+        missingParts = False
         missingParameterString = ''
-
         inputParameterCount = entry.param_count
         I_HEADER_MISSING = entry.input_header_missing
         I_DESCRIPTION_MISSING = entry.input_description_missing
@@ -262,47 +244,54 @@ def cleanFileDataArray():
         description = entry.description
         fileName = entry.fileName
 
-
         if len(description) == 0:
-            missingValues = True
+            missingParts = True
             missingParameterString += "There is no description of the function available!\n"
 
         if I_HEADER_MISSING is True:
-            missingValues = True
+            missingParts = True
             missingParameterString += "There is either no input parameter header given or the syntax is incorrect!\n"
 
         if I_DESCRIPTION_MISSING is True:
-            missingValues = True
+            missingParts = True
             missingParameterString += "There is either no dedicated Input section header or syntax is incorrect!\n"
 
         if inputParameterCount == 0:
-            missingValues = True
+            missingParts = True
             missingParameterString += "There are either no parameters given within the file or syntax is incorrect!\n"
 
         if O_DESCRIPTION_MISSING is True:
-            missingValues = True
+            missingParts = True
             missingParameterString += "There is either no dedicated Output section header or syntax is incorrect!\n"
 
         if O_HEADER_MISSING is True:
-            missingValues = True
+            missingParts = True
             missingParameterString += "There is either no output parameter header given or the syntax is incorrect!\n"
 
         if outputParameterCount == 0:
-            missingValues = True
+            os.system('color')
+            missingParts = True
             missingParameterString += "There are either no output parameters given within the file or syntax is incorrect!\n"
 
-        if missingValues:
-            print('\033[93m' + "For the File: " + '\033[4m\033[92m' + fileName + '\033[0m' + '\033[93m' + " following errors occured:" + '\033[0m')
+        if missingParts:
+            missingValues = True
+            os.system('color')
+            print(f'{bcolors.WARNING}For the File: {bcolors.UNDERLINE}{bcolors.OKGREEN}' +  fileName + f'{bcolors.ENDC}{bcolors.WARNING} following errors occured: {bcolors.ENDC}')
             print(missingParameterString)
         else:
             new_file_data_array.append(entry)
 
     for entry in incorrect_file_data:
-        print(f'{bcolors.WARNING}For the File: {bcolors.UNDERLINE}{bcolors.OKGREEN}' + entry + f'{bcolors.ENDC}{bcolors.WARNING} NO HEADER WAS FOUND AT ALL:{bcolors.ENDC}')
-        #print('For the File: ' + entry + ' NO HEADER WAS FOUND AT ALL') # Print without color
+        os.system('color')
+        print(f'{bcolors.WARNING}For the File: {bcolors.UNDERLINE}{bcolors.OKGREEN}' + entry + f'{bcolors.ENDC}{bcolors.WARNING} no header was found at all!{bcolors.ENDC}')
+
+    if len(incorrect_file_data) > 0:
+        missingValues = True
+
+    if missingValues and THROW_EXCEPTION is True:
+        raise Exception(f'{bcolors.FAIL}At least one file does not fit the required Syntax!{bcolors.ENDC}')
 
     return new_file_data_array
-
 
 
 def parseParam(param_array, param_count, FLAG, FLAG_FIN, FLAG_HEADER_MISSING, comment_line_count, additional_infos, line):
@@ -314,7 +303,6 @@ def parseParam(param_array, param_count, FLAG, FLAG_FIN, FLAG_HEADER_MISSING, co
     continuedParamDescribtionRegex = re.compile('^#+\s{10,}')  # if my parameter input matches this i obviously have a continuation of the param describtion
     arbritraryNumberOfSpacesRegex = re.compile('\s{1,}')
 
-
     if lineBreakRegex.match(line):
         comment_line_count += 1
         if comment_line_count > 2:
@@ -323,7 +311,6 @@ def parseParam(param_array, param_count, FLAG, FLAG_FIN, FLAG_HEADER_MISSING, co
         return param_count, FLAG, FLAG_FIN, FLAG_HEADER_MISSING, comment_line_count
 
     if comment_line_count == 1:
-        # case parameter name if there are some
         # current line should only contain parameter names
         # In case there are no parameter names given but only the parameters, we set the parameter header missing Flag
         if len(arbritraryNumberOfSpacesRegex.split(line)) >= 7:
@@ -343,7 +330,6 @@ def parseParam(param_array, param_count, FLAG, FLAG_FIN, FLAG_HEADER_MISSING, co
             return param_count, FLAG, FLAG_FIN, FLAG_HEADER_MISSING, comment_line_count
 
     if comment_line_count == 2:
-        # now I have to parse my parameters
         if continuedParamDescribtionRegex.match(line):
             splitted_line = continuedParamDescribtionRegex.split(line)
             if (len(splitted_line)) == 2:
@@ -382,4 +368,3 @@ def replaceNewlineCharacter(array):
 
 if __name__ == '__main__':
     callWithAbsoluteParameters()
-

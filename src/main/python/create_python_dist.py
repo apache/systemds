2c90f9e7009d766ed0cb887d4f319c54c322e410
baunsgaard <baunsgaard@tugraz.at>
2021-11-15 18:46:02 +0100

    [SYSTEMDS-3217] PythonAPI build release cleanup
    This commit cleanup the python release artifact to
    
    not include:
    the generator scripts, tests files, compiled pyc files and tutorial files.
    
    include:
    java dependencies cleanly with only the BIN release java files, similar to
    our java BIN release for consistency.
    
    The dev version is also changed to fit to pip specification so instead
    of being called SNAPSHOT it is now dev.

--- src/main/python/create_python_dist.py
+++ src/main/python/create_python_dist.py
@@ -21,8 +21,8 @@
 #-------------------------------------------------------------
 
 import subprocess
+
+f = open("generator.log","w")
+subprocess.run("python3 generator/generator.py",shell=True, check=True, stdout =f, stderr=f)
 subprocess.run("python3 pre_setup.py",shell=True, check=True)
-subprocess.run("python3 generator/generator.py",shell=True, check=True)
 subprocess.run("python3 setup.py sdist bdist_wheel",shell=True, check=True)
-# post_setup.py moves the files from dist to target which we probably don't want for uploading them to pypi
-#subprocess.run(["python3", "post_setup.py"]).check_returncode()

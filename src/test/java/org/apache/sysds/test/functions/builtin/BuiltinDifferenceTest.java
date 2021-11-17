427673f5a7951ea44dbf6ab714d955beb9931c57
Joachim Dunkel <dunkel@student.tugraz.at>
2021-11-17 10:35:39 +0100

    cleaned up differenence test setup

--- src/test/java/org/apache/sysds/test/functions/builtin/BuiltinDifferenceTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/BuiltinDifferenceTest.java
@@ -1,6 +1,5 @@
 package org.apache.sysds.test.functions.builtin;
 
-import org.apache.spark.sql.catalyst.expressions.Ascending;
 import org.apache.sysds.common.Types;
 import org.apache.sysds.runtime.matrix.data.MatrixValue;
 import org.apache.sysds.test.AutomatedTestBase;
@@ -8,15 +7,35 @@ import org.apache.sysds.test.TestConfiguration;
 import org.apache.sysds.test.TestUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 
+
+@RunWith(Parameterized.class)
 public class BuiltinDifferenceTest extends AutomatedTestBase {
     private final static String TEST_NAME = "difference";
     private final static String TEST_DIR = "functions/builtin/";
     private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinDifferenceTest.class.getSimpleName() + "/";
+
+    private final Types.ExecType execType;
+
+    public BuiltinDifferenceTest(Types.ExecType execType){
+
+        this.execType = execType;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> types(){
+        return Arrays.asList(new Object[][]{
+                {Types.ExecType.CP},
+                {Types.ExecType.SPARK}
+        });
+    }
 
     @Override
     public void setUp() {
@@ -25,22 +44,15 @@ public class BuiltinDifferenceTest extends AutomatedTestBase {
     }
 
     @Test
-    public void testDifference1CP() {
-        double[][] X = {{1}, {2}, {3}};
-        double[][] Y = {{2}, {3}, {4}};
-        runUnionTests(X, Y, Types.ExecType.CP);
-    }
-
-    @Test
-    public void testDifference1SP() {
+    public void testPosNumbersAscending() {
         double[][] X = {{1}, {2}, {3}};
         double[][] Y = {{2}, {3}, {4}};
 
-        runUnionTests(X, Y, Types.ExecType.SPARK);
+        runUnionTests(X, Y, execType);
     }
 
     @Test
-    public void testDifference2CP() {
+    public void testPosNumbersRandomOrder() {
         double[][] X = {{9}, {2}, {3}};
         double[][] Y = {{2}, {3}, {4}};
 
@@ -48,39 +60,52 @@ public class BuiltinDifferenceTest extends AutomatedTestBase {
     }
 
     @Test
-    public void testDifference2SP() {
-        double[][] X = {{9}, {2}, {3}};
-        double[][] Y = {{2}, {3}, {4}};
-
-        runUnionTests(X, Y, Types.ExecType.SPARK);
-    }
-
-    @Test
-    public void testDifference3CP() {
+    public void testComplexPosNumbers() {
         double[][] X =  {{12},{22},{13},{4},{6},{7},{8},{9},{12},{12}};
         double[][] Y = {{1},{2},{11},{12},{13},{18},{20},{21},{12}};
         runUnionTests(X, Y, Types.ExecType.CP);
     }
 
     @Test
-    public void testDifference3SP() {
-        double[][] X = {{12},{22},{13},{4},{6},{7},{8},{9},{12},{12}};
-        double[][] Y = {{1},{2},{11},{12},{13},{18},{20},{21},{12}};
-        runUnionTests(X, Y, Types.ExecType.SPARK);
+    public void testNegNumbers() {
+        double[][] X = {{-10},{-5},{2}};
+        double[][] Y = {{2},{-3}};
+        runUnionTests(X, Y, Types.ExecType.CP);
     }
 
     @Test
-    public void testDifference4CP() { //fails because element order in R is wrong
+    public void testFloatingPNumbers() { //floating point numbers do not work with table()...
+        double[][] X = {{2},{2.5},{4}};
+        double[][] Y = {{2.4},{2}};
+        runUnionTests(X, Y, Types.ExecType.CP);
+    }
+
+    @Test
+    public void testNegAndFloating() {
         double[][] X =  {{1.4}, {-1.3}, {10}, {4}};
         double[][] Y = {{1.3},{-1.4},{10},{9}};
         runUnionTests(X, Y, Types.ExecType.CP);
     }
 
     @Test
-    public void testDifference4Spark() {
-        double[][] X =  {{1.4}, {-1.3}, {10}, {4}};
-        double[][] Y = {{1.3},{-1.4},{10},{9}};
-        runUnionTests(X, Y, Types.ExecType.SPARK);
+    public void testMaxValue() {
+        double[][] X =  {{Double.MAX_VALUE}, {2},{4}}; //Max value does not work. because of x/y row size hack probably.
+        double[][] Y = {{2},{15}};
+        runUnionTests(X, Y, Types.ExecType.CP);
+    }
+
+    @Test
+    public void testMinValue() {
+        double[][] X =  {{Double.MIN_VALUE}, {2},{4}};
+        double[][] Y = {{2},{15}};
+        runUnionTests(X, Y, Types.ExecType.CP);
+    }
+
+    @Test
+    public void testCombined() {
+        double[][] X =  {{Double.MIN_VALUE}, {-Double.MAX_VALUE},{4}, {-1.3}, {10}, {4}};
+        double[][] Y = {{Double.MIN_VALUE},{15},{-1.2},{-25.3}};
+        runUnionTests(X, Y, Types.ExecType.CP);
     }
 
     private void runUnionTests(double[][] X, double[][]Y, Types.ExecType instType) {

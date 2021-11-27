39ad98bc8bb012cdc1b6fdcf535b61921e4a1125
Palmisano Alexander <alexander.palmisano@student.tugraz.at>
2021-11-27 12:08:37 +0100

    added Lanczos, WIP

--- src/main/java/org/apache/sysds/runtime/matrix/data/EigenDecompOurs.java
+++ src/main/java/org/apache/sysds/runtime/matrix/data/EigenDecompOurs.java
@@ -1,13 +1,12 @@
 package org.apache.sysds.runtime.matrix.data;
 
 import org.apache.sysds.runtime.DMLRuntimeException;
+import org.apache.sysds.runtime.functionobjects.*;
+import org.apache.sysds.runtime.instructions.InstructionUtils;
+import org.apache.sysds.runtime.matrix.operators.*;
 import org.apache.sysds.runtime.util.DataConverter;
 
-import java.io.Console;
-import java.util.Arrays;
-
 // TODO: EigenDecompostition entirely in SystemDS matrix-operators
-//		 (find out what the computeEigenRegularized function does and when it is needed)
 
 // NOTES: what does org.apache.commons.math3.linear.EigenDecomposition use?
 //			- only for real matrices (symmetric and non-symmetric)
@@ -24,12 +23,14 @@ import java.util.Arrays;
 //			- othes (p.349) (or dirhes, elmhes) -> hqr2 (p. 383)
 //
 //        Make sure to return the EVec and EVal sorted by values of Eval descending order
+//
+//        Look at power iterations
+//        Use Matrixblock and parallelism
+//        One QL and one for e.g. symmetric
+//        scripts/staging/lanczos
 
 public class EigenDecompOurs {
     private double[][] m;
-    private double[][] tred2_z;
-    private double[] tred2_e;
-    private double[] tred2_d;
 
     public EigenDecompOurs(MatrixBlock in) {
         if ( in.getNumRows() != in.getNumColumns() ) {
@@ -38,9 +39,7 @@ public class EigenDecompOurs {
         }
         this.m= DataConverter.convertToArray2DRowRealMatrix(in).getData();
         if(isSym(this.m)) {
-            tred2(this.m);
-            printMatrix(this.tred2_z);
-            tql2();
+            Lanczos(in);
         }
 
     }
@@ -56,103 +55,58 @@ public class EigenDecompOurs {
         return true;
     }
 
-    // "Handbook for Automatic Computation" (1971)
-    // tred2 on page 217
-    private void tred2(double[][] m) {
-        double tol = 1e-7;
 
-        int n = m.length;
-        double[][] z = new double[n][n];
-        double[] e = new double[n];
-        double[] d = new double[n];
+    private void Lanczos(MatrixBlock A) {
+        int num_Threads = 1;
 
-        for(int i = 0; i < n; i++) {
-            e[i] = 0;
-            d[i] = 0;
-            for(int j = 0; j < n; j++) {
-                if (j < i)
-                    z[i][j] = m[i][j];
-                else
-                    z[i][j] = 0.0;
+        int m = A.getNumRows();
+        MatrixBlock v0 = new MatrixBlock(m, 1, 0.0);
+        MatrixBlock v1 = MatrixBlock.randOperations(m, 1, 1.0, 0.0, 1.0, "UNIFORM", 0xC0FFEE);
+
+        // normalize v1
+        double v1_sum = v1.sum();
+        RightScalarOperator op_div_scalar = new RightScalarOperator(Divide.getDivideFnObject(), v1_sum, num_Threads);
+        v1 = v1.scalarOperations(op_div_scalar, new MatrixBlock());
+        UnaryOperator op_sqrt = new UnaryOperator(Builtin.getBuiltinFnObject(Builtin.BuiltinCode.SQRT), num_Threads, true);
+        v1 = v1.unaryOperations(op_sqrt, new MatrixBlock());
+        assert v1.sumSq() == 1.0 : "v1 not correctly normalized";
+
+        MatrixBlock T = new MatrixBlock(m, m, 0.0);
+        MatrixBlock TV = new MatrixBlock(m, m, 0.0);
+        MatrixBlock w1;
+
+        BinaryOperator op_mul = new BinaryOperator(Multiply.getMultiplyFnObject(), num_Threads);
+        ReorgOperator op_t = new ReorgOperator(SwapIndex.getSwapIndexFnObject(), num_Threads);
+        TernaryOperator op_minus_mul = new TernaryOperator(MinusMultiply.getFnObject(), num_Threads);
+        AggregateBinaryOperator op_mul_agg = InstructionUtils.getMatMultOperator(num_Threads);
+
+        MatrixBlock beta = new MatrixBlock(1, 1, 0.0);
+        for(int i = 0; i < m; i++) {
+            w1 = A.aggregateBinaryOperations(A, v1, op_mul_agg);
+            MatrixBlock w1_t = w1.reorgOperations(op_t,new MatrixBlock(), 0, 0, m);
+            MatrixBlock alpha = w1_t.aggregateBinaryOperations(w1_t, v1, op_mul_agg);
+            if(i < m-1) {
+                w1 = w1.ternaryOperations(op_minus_mul, v1, alpha, new MatrixBlock());
+                w1 = w1.ternaryOperations(op_minus_mul, v0, beta, new MatrixBlock());
+                beta.setValue(0, 0, Math.sqrt(w1.sumSq()));
+                v0 = v1;
+                op_div_scalar.setConstant(beta.getDouble(0, 0));
+                v1 = w1.scalarOperations(op_div_scalar, new MatrixBlock());
+
+                T.setValue(i+1, i, beta.getValue(0, 0));
+                T.setValue(i, i+1, beta.getValue(0, 0));
+            }
+            T.setValue(i, i, alpha.getValue(0, 0));
+
+            // TODO: TV[,i] = v1; with Matrixblock operation
+            for(int j = 0; j < m; j++) {
+                TV.setValue(j,i, v1.getValue(j,0));
             }
         }
-        for(int i = n-1; i > 0; i--) {
-            int l = i-2;
-            double f = z[i][i-1];
-            double g = 0.0;
-            for(int k = 0; k < l; k++) {
-                g += (z[i][k] * z[i][k]);
-            }
-            double h = g + f * f;
-            if (g <= tol) {
-                e[i] = f;
-                d[i] = 0.0;
-                continue;
-            }
-            l = l+1;
-            g = f >= 0 ? (-1 * Math.sqrt(h)) : Math.sqrt(h);
-            e[i] = g;
-            h = h - f * g;
-            z[i][i-1] = f - g;
-            f = 0;
-            for(int j = 0; j < l; j++) {
-                z[j][i] = z[i][j] / h;
-                g = 0;
-                for(int k = 0; k < j; k++) {
-                    g += (z[j][k] * z[i][k]);
-                }
-                for(int k = j; k < l; k++) {
-                    g += (z[k][j] * z[i][k]);
-                }
-                e[j] = g / h;
-                f += (g * z[j][i]);
-            }
-            double hh = f / (h + h);
-            for(int j = 0; j < l; j++) {
-                f = z[i][j];
-                e[j] -= (hh * f);
-                g = e[j];
-                for(int k = 0; k < j; k++) {
-                    z[j][k] -= (f * e[k] + g * z[i][k]);
-                }
-            }
-            d[i] = h;
-        }
-        d[0] = 0.0;
-        e[0] = 0.0;
-        for(int i = 0; i < n; i++) {
-            int l = i - 1;
-            if (d[i] != 0.0) {
-                for(int j = 0; j < l; j++) {
-                    double g = 0.0;
-                    for(int k = 0; k < l; k++) {
-                        g += (z[i][k] * z[k][j]);
-                    }
-                    for(int k = 0; k < l; k++) {
-                        z[k][j] -= (g * z[k][i]);
-                    }
-                }
-            }
-            d[i] = z[i][i];
-            z[i][i] = 1.0;
-            for(int j = 0; j < l; j++) {
-                z[i][j] = 0.0;
-                z[j][i] = 0.0;
-            }
-        }
-        this.tred2_d = d;
-        this.tred2_e = e;
-        this.tred2_z = z;
-    }
 
-    private MatrixBlock tql2() {
-        return null;
-    }
-
-    private void printMatrix(double[][] m) {
-        for(double[] a : m){
-            System.out.println(Arrays.toString(a));
-        }
+        // TEST
+        MatrixBlock[] a = LibCommonsMath.multiReturnOperations(A, "eigen");
+        MatrixBlock[] b = LibCommonsMath.multiReturnOperations(T, "eigen");
     }
 
     public MatrixBlock getV() {

aff7020c70dd136dd6b15b2f9b7e302b16f2ff0a
baunsgaard <baunsgaard@tugraz.at>
2021-12-01 11:38:30 +0100

    [MINOR] Add error message log in case of incorrect paramserv functions

--- src/main/java/org/apache/sysds/hops/rewrite/HopRewriteUtils.java
+++ src/main/java/org/apache/sysds/hops/rewrite/HopRewriteUtils.java
@@ -20,6 +20,8 @@
 package org.apache.sysds.hops.rewrite;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.sysds.api.DMLScript;
 import org.apache.sysds.common.Types.DataType;
 import org.apache.sysds.common.Types.ExecMode;
@@ -87,8 +89,8 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-public class HopRewriteUtils 
-{
+public class HopRewriteUtils {
+	private static final Log LOG = LogFactory.getLog(HopRewriteUtils.class.getName());
 
 	public static boolean isValueTypeCast( OpOp1 op ) {
 		return op == OpOp1.CAST_AS_BOOLEAN
@@ -1637,7 +1639,10 @@ public class HopRewriteUtils
 				&& prog.getFunctionStatementBlock(sagg) != null;
 		}
 		catch(Exception ex) {
-			//robustness invalid function keys
+			// If the function keys are incorrect this exception is caught for robustness in error messages for users.
+			// Intensionally only catching the exception!
+			// For debugging if for some reason the error we encountered was something else we LOG the error.
+			LOG.error(ex);
 			return false;
 		}
 	}

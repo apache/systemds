2af99a6a361642959da0bb1ee6b92b290c41a7bf
ywcb00 <ywcb00@ywcb.org>
2021-12-31 01:11:01 +0100

    feat(FederatedReadCache.java): remove the FederatedReadCache since it will be handled by the lineage from now on
    feat(FederatedWorker.java): set the lineage config to the specified flags when creating a worker
    
    feat(FederatedWorkerHandler.java): check if the file which is requested to read is available in cache before performing an actual read
    	put the matrix object to cache after the read of a file
    
    feat(LineageCache.java): create methods reuseRead() and putReadObject() to manage the lineage cache for read matrix object
    
    chore(FederatedRequest.java): initialize the checksums member of the federated request inside the getChecksum call if it is null
    
    chore(FederatedStatistics.java): add the overloaded method incrementFedReadCacheBytesCount with the parameter of a cacheblock

--- src/main/java/org/apache/sysds/runtime/controlprogram/federated/FederatedWorkerHandler.java
+++ src/main/java/org/apache/sysds/runtime/controlprogram/federated/FederatedWorkerHandler.java
@@ -90,7 +90,6 @@ public class FederatedWorkerHandler extends ChannelInboundHandlerAdapter {
 	 * separate execution contexts at the federated sites too
 	 * 
 	 * @param flt The Federated Lookup Table of the current Federated Worker.
-	 * @param frc read cache shared by all worker handlers
 	 */
 	public FederatedWorkerHandler(FederatedLookupTable flt, FederatedReadCache frc) {
 		_flt = flt;
@@ -278,18 +277,23 @@ public class FederatedWorkerHandler extends ChannelInboundHandlerAdapter {
 					_frc.setInvalid(filename);
 				throw ex;
 			}
+
+			if(DMLScript.LINEAGE)
+				// create a literal type lineage item with the file name
+				ec.getLineage().set(String.valueOf(id), linItem);
 		}
 
-		if(DMLScript.LINEAGE)
-			// create a literal type lineage item with the file name
-			ec.getLineage().set(String.valueOf(id), linItem);
-
 		if(dataType == Types.DataType.FRAME) {
-			FrameObject frameObject = (FrameObject) cd;
-			frameObject.acquireRead();
-			frameObject.refreshMetaData(); // get block schema
-			frameObject.release();
-			return new FederatedResponse(ResponseType.SUCCESS, new Object[] {id, frameObject.getSchema(), mc});
+			try {
+				FrameObject frameObject = (FrameObject) cd;
+				frameObject.acquireRead();
+				frameObject.refreshMetaData(); // get block schema
+				frameObject.release();
+				return new FederatedResponse(ResponseType.SUCCESS, new Object[] {id, frameObject.getSchema(), mc});
+			} catch(NullPointerException npe) {
+				throw new FederatedWorkerHandlerException("Data should never be null at this point. "
+					+ "Either set by lineage or read from fs", npe);
+			}
 		}
 		return new FederatedResponse(ResponseType.SUCCESS, new Object[] {id, mc});
 	}

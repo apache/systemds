d144db7aa4f6ce82f0f0c755feb06218cc07e5bf
Shafaq Siddiqi <shafaq.siddiqi@tugraz.at>
2022-01-12 16:36:58 +0100

    [MINOR] Minor changes in logical enumeration   - This commit introduce the default parameter values for the cleaning primitives   - The logical enumeration now only use the default parameter values to evaluate the      primitives and iteratively add the categories to logical pipelines until converge.   - The output is a single best logical pipeline

--- src/test/java/org/apache/sysds/test/functions/pipelines/BuiltinTopkEvaluateTest.java
+++ src/test/java/org/apache/sysds/test/functions/pipelines/BuiltinTopkEvaluateTest.java
@@ -25,6 +25,7 @@ import org.apache.sysds.test.TestConfiguration;
 import org.apache.sysds.test.TestUtils;
 import org.junit.Assert;
 import org.junit.Ignore;
+import org.junit.Test;
 
 public class BuiltinTopkEvaluateTest extends AutomatedTestBase {
 	//	private final static String TEST_NAME1 = "prioritized";

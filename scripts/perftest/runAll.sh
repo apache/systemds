c2d751fc1809e8a2896f6a373595926a26407dba
baunsgaard <baunsgaard@tugraz.at>
2022-05-17 17:00:26 +0200

    incremental update

--- scripts/perftest/runAll.sh
+++ scripts/perftest/runAll.sh
@@ -41,50 +41,61 @@ MAXMEM=${MAXMEM%"MB"}; MAXMEM=${MAXMEM/GB/"000"}
 export LOG4JPROP='conf/log4j-off.properties'
 export SYSDS_QUIET=1
 export SYSDS_EXEC_MODE="hybrid"
-export SYSDS_DISTRIBUTED=1
-# export SYSTEMDS_STANDALONE_OPTS="-Xmx10g -Xms10g -Xmn2000m"
+export SYSDS_EXEC_MODE="singlenode"
+# export SYSDS_DISTRIBUTED=1
+export SYSTEMDS_STANDALONE_OPTS="-Xmx10g -Xms10g -Xmn2000m"
 
 # Possible lines to initialize Intel MKL, depending on version and install location
+if [ -d ~/intel ] && [ -d ~/intel/bin ] && [ -f ~/intel/bin/compilervars.sh ]; then
+    . ~/intel/bin/compilervars.sh intel64
+else
+    . /opt/intel/bin/compilervars.sh intel64
+fi
 #    . ~/intel/bin/compilervars.sh intel64
 #    . ~/intel/oneapi/setvars.sh intel64
 #    . /opt/intel/bin/compilervars.sh intel64
 
+# make dirs if not exsisting
+mkdir -p logs 
+mkdir -p results 
+mkdir -p temp
+
 # init time measurement
-if [ ! -d logs ]; then mkdir -p logs ; fi
-if [ ! -d results ]; then mkdir -p results ; fi
-if [ ! -d temp ]; then mkdir -p temp ; fi
-date >> results/times.txt
+
+rm -f results/times.txt
+date +"%Y-%m-%d-%T" >> results/times.txt
+echo -e "\n\n" >> results/times.txt
 
 ### Data Generation
-echo "-- Generating binomial data..." >> results/times.txt;
-./genBinomialData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genBinomialData.out
-echo "-- Generating multinomial data..." >> results/times.txt;
-./genMultinomialData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genMultinomialData.out
-echo "-- Generating stats data..." >> results/times.txt;
-./genDescriptiveStatisticsData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genStatsData.out
-./genStratStatisticsData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genStratStatsData.out
-echo "-- Generating clustering data..." >> results/times.txt;
-./genClusteringData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genClusteringData.out
-echo "-- Generating Dimension Reduction data." >> results/times.txt;
-./genDimensionReductionData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genDimensionReductionData.out
-echo "-- Generating ALS data." >> results/times.txt;
-./genALSData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genALSData.out
+# echo "-- Generating binomial data..." >> results/times.txt;
+# ./genBinomialData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genBinomialData.out
+# echo "-- Generating multinomial data..." >> results/times.txt;
+# ./genMultinomialData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genMultinomialData.out
+# echo "-- Generating stats data..." >> results/times.txt;
+# ./genDescriptiveStatisticsData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genStatsData.out
+# ./genStratStatisticsData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genStratStatsData.out
+# echo "-- Generating clustering data..." >> results/times.txt;
+# ./genClusteringData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genClusteringData.out
+# echo "-- Generating Dimension Reduction data." >> results/times.txt;
+# ./genDimensionReductionData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genDimensionReductionData.out
+# echo "-- Generating ALS data." >> results/times.txt;
+# ./genALSData.sh ${CMD} ${TEMPFOLDER} ${MAXMEM} &> logs/genALSData.out
 
 ### Micro Benchmarks:
 ./MatrixMult.sh ${CMD}
-./MatrixTranspose.sh ${CMD}
+# ./MatrixTranspose.sh ${CMD}
 
 # Federate benchmark
 #./fed/runAllFed.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
 
 ### Algorithms Benchmarks:
-./runAllBinomial.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
-./runAllMultinomial.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
-./runAllRegression.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
-./runAllStats.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
-./runAllClustering.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
-./runAllDimensionReduction.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
-./runAllALS.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
+# ./runAllBinomial.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
+# ./runAllMultinomial.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
+# ./runAllRegression.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
+# ./runAllStats.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
+# ./runAllClustering.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
+# ./runAllDimensionReduction.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
+# ./runAllALS.sh ${CMD} ${TEMPFOLDER} ${MAXMEM}
 
 # TODO The following benchmarks have yet to be written. The decision tree algorithms additionally need to be fixed.
 # add stepwise Linear 
@@ -95,3 +106,5 @@ echo "-- Generating ALS data." >> results/times.txt;
 #./runAllSurvival.sh $CMD $TEMPFOLDER
 #KaplanMeier
 #Cox
+
+cp results/times.txt "results/times-$(date +"%Y-%m-%d-%T").txt"

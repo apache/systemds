81d2c99543db1963000719f7be3a95692c11b4f7
Arnab Phani <phaniarnab@gmail.com>
2022-12-16 23:30:18 +0100

    [SYSTEMDS-3479] Persist and reuse of Spark RDDs
    This patch extends lineage cache to store the RDD objects
    which are checkpointed. This addition allows the compiler
    to place a chkpoint after a potentially redundant operator.
    During runtime, we then persist the RDD, save the RDD in the
    lineage cache, and reuse if the instruction repeats. It is a
    bit tricky to cache a RDD for a lineage trace of a previous
    instruction. A better way would be to be able to mark any
    instruction to persist the result RDD and skip the chkpoint
    instruction.
    Hyperparameter tuning of LmDS with 2.5k columns improves by
    4x by caching the cpmm results in the executors.

--- src/main/java/org/apache/sysds/runtime/instructions/spark/CheckpointSPInstruction.java
+++ src/main/java/org/apache/sysds/runtime/instructions/spark/CheckpointSPInstruction.java
@@ -77,10 +77,10 @@ public class CheckpointSPInstruction extends UnarySPInstruction {
 		SparkExecutionContext sec = (SparkExecutionContext)ec;
 
 		// Asynchronously trigger count() and persist this RDD
-		// TODO: Synchronize. Avoid double execution
 		if (getOpcode().equals("chkpoint_e")) {  //eager checkpoint
 			// Inplace replace output matrix object with the input matrix object
 			// We will never use the output of the Spark count call
+			// TODO: Synchronize. Avoid double execution
 			ec.setVariable(output.getName(), ec.getCacheableData(input1));
 
 			if (CommonThreadPool.triggerRemoteOPsPool == null)
@@ -98,6 +98,13 @@ public class CheckpointSPInstruction extends UnarySPInstruction {
 			//add a dummy entry to the input, which will be immediately overwritten by the null output.
 			sec.setVariable( input1.getName(), new BooleanObject(false));
 			sec.setVariable( output.getName(), new BooleanObject(false));
+			return;
+		}
+
+		if (sec.getCacheableData(input1).getRDDHandle().isCheckpointRDD()) {
+			// Do nothing if the RDD is already checkpointed
+			sec.setVariable(output.getName(), sec.getCacheableData(input1.getName()));
+			Statistics.decrementNoOfExecutedSPInst();
 			return;
 		}
 		//-------

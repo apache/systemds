e8091f1823eed8b4b91b6cdeb7e76423619378c1
Matthias Boehm <mboehm7@gmail.com>
2023-04-14 16:36:20 +0200

    [MINOR] More decisionTree/randomForest tests, and validation checks

--- src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinDecisionTreeRealDataTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinDecisionTreeRealDataTest.java
@@ -35,53 +35,68 @@ public class BuiltinDecisionTreeRealDataTest extends AutomatedTestBase {
 
 	private final static String TITANIC_DATA = DATASET_DIR + "titanic/titanic.csv";
 	private final static String TITANIC_TFSPEC = DATASET_DIR + "titanic/tfspec.json";
+	private final static String WINE_DATA = DATASET_DIR + "wine/winequality-red-white.csv";
+	private final static String WINE_TFSPEC = DATASET_DIR + "wine/tfspec.json";
 
+	
 	@Override
 	public void setUp() {
-		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {"R"}));
+		for(int i=1; i<=2; i++)
+			addTestConfiguration(TEST_NAME+i, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {"R"}));
 	}
 
 	@Test
 	public void testDecisionTreeTitanic_MaxV1() {
-		runDecisionTree(TITANIC_DATA, TITANIC_TFSPEC, 0.875, 1, 1.0, ExecType.CP);
+		runDecisionTree(1, TITANIC_DATA, TITANIC_TFSPEC, 0.875, 1, 1.0, ExecType.CP);
 	}
 	
 	@Test
 	public void testRandomForestTitanic1_MaxV1() {
 		//one tree with sample_frac=1 should be equivalent to decision tree
-		runDecisionTree(TITANIC_DATA, TITANIC_TFSPEC, 0.875, 2, 1.0, ExecType.CP);
+		runDecisionTree(1, TITANIC_DATA, TITANIC_TFSPEC, 0.875, 2, 1.0, ExecType.CP);
 	}
 	
 	@Test
 	public void testRandomForestTitanic8_MaxV1() {
 		//8 trees with sample fraction 0.125 each, accuracy 0.785 due to randomness
-		runDecisionTree(TITANIC_DATA, TITANIC_TFSPEC, 0.793, 9, 1.0, ExecType.CP);
+		runDecisionTree(1, TITANIC_DATA, TITANIC_TFSPEC, 0.793, 9, 1.0, ExecType.CP);
 	}
 	
 	@Test
 	public void testDecisionTreeTitanic_MaxV06() {
-		runDecisionTree(TITANIC_DATA, TITANIC_TFSPEC, 0.871, 1, 0.6, ExecType.CP);
+		runDecisionTree(1, TITANIC_DATA, TITANIC_TFSPEC, 0.871, 1, 0.6, ExecType.CP);
 	}
 	
 	@Test
 	public void testRandomForestTitanic1_MaxV06() {
 		//one tree with sample_frac=1 should be equivalent to decision tree
-		runDecisionTree(TITANIC_DATA, TITANIC_TFSPEC, 0.871, 2, 0.6, ExecType.CP);
+		runDecisionTree(1, TITANIC_DATA, TITANIC_TFSPEC, 0.871, 2, 0.6, ExecType.CP);
 	}
 	
 	@Test
 	public void testRandomForestTitanic8_MaxV06() {
 		//8 trees with sample fraction 0.125 each, accuracy 0.785 due to randomness
-		runDecisionTree(TITANIC_DATA, TITANIC_TFSPEC, 0.793, 9, 0.6, ExecType.CP);
+		runDecisionTree(1, TITANIC_DATA, TITANIC_TFSPEC, 0.793, 9, 0.6, ExecType.CP);
+	}
+	
+	@Test
+	public void testDecisionTreeWine_MaxV1() {
+		runDecisionTree(2, WINE_DATA, WINE_TFSPEC, 0.989, 1, 1.0, ExecType.CP);
+	}
+	
+	@Test
+	public void testRandomForestWine_MaxV1() {
+		//one tree with sample_frac=1 should be equivalent to decision tree
+		runDecisionTree(2, WINE_DATA, WINE_TFSPEC, 0.989, 2, 1.0, ExecType.CP);
 	}
 
-	private void runDecisionTree(String data, String tfspec, double minAcc, int dt, double maxV, ExecType instType) {
+	private void runDecisionTree(int test, String data, String tfspec, double minAcc, int dt, double maxV, ExecType instType) {
 		Types.ExecMode platformOld = setExecMode(instType);
 		try {
-			loadTestConfiguration(getTestConfiguration(TEST_NAME));
+			loadTestConfiguration(getTestConfiguration(TEST_NAME+test));
 
 			String HOME = SCRIPT_DIR + TEST_DIR;
-			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			fullDMLScriptName = HOME + (TEST_NAME+test) + ".dml";
 			programArgs = new String[] {"-stats",
 				"-args", data, tfspec, String.valueOf(dt), String.valueOf(maxV), output("R")};
 

361b340420ebf520b6f41c7717862c27c77b88dc
Sheypex <sheypex@gmail.com>
2023-06-22 18:09:49 +0200

    got datagen for nn tests running

--- scripts/perftest/datagen/genNNData.sh
+++ scripts/perftest/datagen/genNNData.sh
@@ -29,40 +29,39 @@ CMD=$1
 DATADIR=$2/nn
 MAXMEM=$3
 
-FORMAT="text" # can be csv, mm, text, binary
-DENSE_SP=0.9
-SPARSE_SP=0.01
+FORMAT="csv" # can be csv, mm, text, binary
 
 echo "-- Generating NN data." >> results/times.txt;
-
+# the scaling of nr and nf is to just multiply them by 3 each .. since sqrt(10) is about 3 and the data size should scale by a factor of 10 ..... needs to be tested for applicability
+# for now only t=1 and t=5 are generated for regression and classification respectively .. may want to add more variety
 #generate XS scenarios (80MB)
 if [ $MAXMEM -ge 80 ]; then
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X10k_1k_dense rows=10000 cols=1000 rank=10 nnz=`echo "scale=0; 10000 * 1000 * $DENSE_SP" | bc` sigma=0.01 fmt=$FORMAT &
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X10k_1k_sparse rows=10000 cols=1000 rank=10 nnz=`echo "scale=0; 10000 * 1000 * $SPARSE_SP" | bc` sigma=0.01 fmt=$FORMAT &
+  ${CMD} -f ../datagen/genRandData4NNRegression.dml --nvargs X=${DATADIR}/X1024_100_1_reg Y=${DATADIR}/Y1024_100_1_reg nr=1024 nf=100 nt=1 fmt=$FORMAT &
+  ${CMD} -f ../datagen/genRandData4NNClassification.dml --nvargs X=${DATADIR}/X1024_100_1_class Y=${DATADIR}/Y1024_100_1_class nr=1024 nf=100 nt=5 fmt=$FORMAT &
 fi
 
 #generate S scenarios (800MB)
 if [ $MAXMEM -ge 800 ]; then
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X100k_1k_dense rows=100000 cols=1000 rank=10 nnz=`echo "scale=0; 100000 * 1000 * $DENSE_SP" | bc` sigma=0.01 fmt=$FORMAT &
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X100k_1k_sparse rows=100000 cols=1000 rank=10 nnz=`echo "scale=0; 100000 * 1000 * $SPARSE_SP" | bc` sigma=0.01 fmt=$FORMAT &
+  ${CMD} -f ../datagen/genRandData4NNRegression.dml --nvargs X=${DATADIR}/X3072_300_1_reg Y=${DATADIR}/Y3072_300_1_reg=3072 nf=300 nt=1 fmt=$FORMAT &
+  ${CMD} -f ../datagen/genRandData4NNClassification.dml --nvargs X=${DATADIR}/X3072_300_1_class Y=${DATADIR}/Y3072_300_1_class=3072 nf=300 nt=5 fmt=$FORMAT &
 fi
 
 #generate M scenarios (8GB)
 if [ $MAXMEM -ge 8000 ]; then
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X1M_1k_dense rows=1000000 cols=1000 rank=10 nnz=`echo "scale=0; 1000000 * 1000 * $DENSE_SP" | bc` sigma=0.01 fmt=$FORMAT &
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X1M_1k_sparse rows=1000000 cols=1000 rank=10 nnz=`echo "scale=0; 1000000 * 1000 * $SPARSE_SP" | bc` sigma=0.01 fmt=$FORMAT &
+  ${CMD} -f ../datagen/genRandData4NNRegression.dml --nvargs X=${DATADIR}/X9216_900_1_reg Y=${DATADIR}/Y9216_900_1_reg=9216 nf=900 nt=1 fmt=$FORMAT &
+  ${CMD} -f ../datagen/genRandData4NNClassification.dml --nvargs X=${DATADIR}/X9216_900_1_class Y=${DATADIR}/Y9216_900_1_class=9216 nf=900 nt=5 fmt=$FORMAT &
 fi
 
 #generate L scenarios (80GB)
 if [ $MAXMEM -ge 80000 ]; then
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X10M_1k_dense rows=10000000 cols=1000 rank=10 nnz=`echo "scale=0; 10000000 * 1000 * $DENSE_SP" | bc` sigma=0.01 fmt=$FORMAT
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X10M_1k_sparse rows=10000000 cols=1000 rank=10 nnz=`echo "scale=0; 10000000 * 1000 * $SPARSE_SP" | bc` sigma=0.01 fmt=$FORMAT
+  ${CMD} -f ../datagen/genRandData4NNRegression.dml --nvargs X=${DATADIR}/X27648_2700_1_reg Y=${DATADIR}/Y27648_2700_1_reg=27648 nf=2700 nt=1 fmt=$FORMAT &
+  ${CMD} -f ../datagen/genRandData4NNClassification.dml --nvargs X=${DATADIR}/X27648_2700_1_class Y=${DATADIR}/Y27648_2700_1_class=27648 nf=2700 nt=5 fmt=$FORMAT &
 fi
 
 #generate XL scenarios (800GB)
 if [ $MAXMEM -ge 800000 ]; then
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X100M_1k_dense rows=100000000 cols=1000 rank=10 nnz=`echo "scale=0; 100000000 * 1000 * $DENSE_SP" | bc` sigma=0.01 fmt=$FORMAT
-  ${CMD} -f ../datagen/genRandData4ALS.dml --nvargs X=${DATADIR}/X100M_1k_sparse rows=100000000 cols=1000 rank=10 nnz=`echo "scale=0; 100000000 * 1000 * $SPARSE_SP" | bc` sigma=0.01 fmt=$FORMAT
+  ${CMD} -f ../datagen/genRandData4NNRegression.dml --nvargs X=${DATADIR}/X82944_8200_1_reg Y=${DATADIR}/Y82944_8200_1_reg=82944 nf=8200 nt=1 fmt=$FORMAT &
+  ${CMD} -f ../datagen/genRandData4NNClassification.dml --nvargs X=${DATADIR}/X82944_8200_1_class Y=${DATADIR}/Y82944_8200_1_class=82944 nf=8200 nt=5 fmt=$FORMAT &
 fi
 
 wait

87f58b0dfa03c552bf7432b5b872c1525bc822b6
Matthias Boehm <mboehm7@gmail.com>
2023-07-21 16:51:50 +0200

    [SYSTEMDS-3597] Fix race conditions in double buffering output streams
    This patch fixes race conditions in the new double buffering, where
    the lambda function is not immediately executed leading to false
    orderings of array references. We now pass dedicated write tasks to
    the thread pool.

--- src/main/java/org/apache/sysds/runtime/util/FastBufferedDataOutputStream.java
+++ src/main/java/org/apache/sysds/runtime/util/FastBufferedDataOutputStream.java
@@ -229,7 +229,7 @@ public class FastBufferedDataOutputStream extends FilterOutputStream implements 
 				long tmp = Double.doubleToRawLongBits(varr[i+j]);
 				longToBa(tmp, _buff, _count);
 				_count += 8;
-			}	
+			}
 			
 			//flush buffer for current block
 			flushBuffer(); //based on count
@@ -258,7 +258,7 @@ public class FastBufferedDataOutputStream extends FilterOutputStream implements 
 				if( alen2 < _bufflen )
 				{
 					if (_count+alen2 > _bufflen) 
-					    flushBuffer();
+						flushBuffer();
 					
 					for( int j=apos; j<apos+alen; j++ )
 					{
@@ -274,7 +274,7 @@ public class FastBufferedDataOutputStream extends FilterOutputStream implements 
 					for( int j=apos; j<apos+alen; j++ )
 					{
 						if (_count+12 > _bufflen) 
-						    flushBuffer();
+							flushBuffer();
 						
 						long tmp2 = Double.doubleToRawLongBits(avals[j]);
 						intToBa(aix[j], _buff, _count);

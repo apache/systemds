8390ed73648723544321f2c1bd012202b5525931
regaleo605 <matthewjs257@gmail.com>
2023-08-16 11:07:04 +0200

    fixed some spacing

--- src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinImputeKNNTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinImputeKNNTest.java
@@ -19,16 +19,6 @@
 
 package org.apache.sysds.test.functions.builtin.part1;
 
-import org.apache.sysds.api.DMLScript;
-import org.apache.sysds.common.Types;
-import org.apache.sysds.runtime.matrix.data.MatrixValue;
-import org.apache.sysds.test.AutomatedTestBase;
-import org.apache.sysds.test.TestConfiguration;
-import org.apache.sysds.test.TestUtils;
-import org.junit.Test;
-
-import java.io.IOException;
-import java.util.HashMap;
 import org.apache.sysds.common.Types;
 import org.apache.sysds.common.Types.ExecMode;
 import org.apache.sysds.common.Types.ExecType;
@@ -40,7 +30,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-
 
 public class BuiltinImputeKNNTest extends AutomatedTestBase {
 
@@ -65,30 +54,23 @@ public class BuiltinImputeKNNTest extends AutomatedTestBase {
         runImputeKNN(true, Types.ExecType.SPARK);
     }
 
-
     private void runImputeKNN(boolean defaultProb, ExecType instType) throws IOException {
         ExecMode platform_old = setExecMode(instType);
-
         try {
             loadTestConfiguration(getTestConfiguration(TEST_NAME));
             String HOME = SCRIPT_DIR + TEST_DIR;
             fullDMLScriptName = HOME + TEST_NAME + ".dml";
-
             programArgs = new String[] {"-args", DATASET_DIR+"Salaries.csv", 
             	"dist", "dist_missing", output("B"), output("B2")};
-
 
             runTest(true, false, null, -1);
 
             //Compare matrices, check if the sum of the imputed value is roughly the same
-
-
             double sum1 = readDMLMatrixFromOutputDir("B").get(new CellIndex(1,1));
             double sum2 = readDMLMatrixFromOutputDir("B2").get(new CellIndex(1,1));
             Assert.assertEquals(sum1, sum2, eps);
         }
         finally {
-
             rtplatform = platform_old;
         }
     }

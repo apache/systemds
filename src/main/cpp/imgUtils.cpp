6fc5bad0754626e6f46d072f2edbcab1a68f170f
Anish Sapkota <anishsapkota054@gmail.com>
2023-09-15 11:16:39 +0000

    added exception if non square images

--- src/main/cpp/imgUtils.cpp
+++ src/main/cpp/imgUtils.cpp
@@ -20,6 +20,7 @@
 #include <iostream>
 #include <cmath>
 #include <cstring>
+#include <stdexcept>
 #include "common.h"
 
 using namespace std;
@@ -94,6 +95,11 @@ void img_transform(const double* img_in, int orig_w, int orig_h, int out_w, int 
 }
 
 void imageRotate(double* img_in, int rows, int cols, double radians, double fill_value, double* img_out) {
+    
+    if (rows != cols) {
+        throw std::invalid_argument("Input image must be square");
+    }
+
     // Translation matrix for moving the origin to the center of the image
     double t1_data[] = {
             1, 0, static_cast<double>(-cols)/2,

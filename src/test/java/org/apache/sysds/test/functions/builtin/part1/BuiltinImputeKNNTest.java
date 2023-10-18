4fa8b122eddefffd4d291bcea2011fdcc8c485f1
Christina Dionysio <christina.dionysio@student.tugraz.at>
2023-10-18 13:44:07 +0200

    [SYSTEMDS-3153] Fix KNN
    fixes the sampling method for missing value imputation using KNN
    
    Closes #1925

--- src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinImputeKNNTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinImputeKNNTest.java
@@ -41,34 +41,36 @@ public class BuiltinImputeKNNTest extends AutomatedTestBase {
     @Override
     public void setUp() {
         TestUtils.clearAssertionInformation();
-        addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {"B","B2"}));
+        addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {"B","B2","B3"}));
     }
 
     @Test
     public void testDefaultCP()throws IOException{
-        runImputeKNN(true, Types.ExecType.CP);
+        runImputeKNN(Types.ExecType.CP);
     }
 
     @Test
     public void testDefaultSpark()throws IOException{
-        runImputeKNN(true, Types.ExecType.SPARK);
+        runImputeKNN(Types.ExecType.SPARK);
     }
 
-    private void runImputeKNN(boolean defaultProb, ExecType instType) throws IOException {
+    private void runImputeKNN(ExecType instType) throws IOException {
         ExecMode platform_old = setExecMode(instType);
         try {
             loadTestConfiguration(getTestConfiguration(TEST_NAME));
             String HOME = SCRIPT_DIR + TEST_DIR;
             fullDMLScriptName = HOME + TEST_NAME + ".dml";
             programArgs = new String[] {"-args", DATASET_DIR+"Salaries.csv", 
-            	"dist", "dist_missing", output("B"), output("B2")};
+            	"dist", "dist_missing", "dist_sample", "42", "0.9", output("B"), output("B2"), output("B3")};
 
             runTest(true, false, null, -1);
 
             //Compare matrices, check if the sum of the imputed value is roughly the same
             double sum1 = readDMLMatrixFromOutputDir("B").get(new CellIndex(1,1));
             double sum2 = readDMLMatrixFromOutputDir("B2").get(new CellIndex(1,1));
+            double sum3 = readDMLMatrixFromOutputDir("B3").get(new CellIndex(1,1));
             Assert.assertEquals(sum1, sum2, eps);
+            Assert.assertEquals(sum2, sum3, eps);
         }
         finally {
             rtplatform = platform_old;

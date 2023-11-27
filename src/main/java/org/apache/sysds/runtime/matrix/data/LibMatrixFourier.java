189208c04f5449136efc203f1138cda7b293dae7
psej <jessica.priebe@web.de>
2023-11-27 18:09:34 +0100

    added pow for ComplexDouble, updated fft

--- src/main/java/org/apache/sysds/runtime/matrix/data/LibMatrixFourier.java
+++ src/main/java/org/apache/sysds/runtime/matrix/data/LibMatrixFourier.java
@@ -1,2 +1,88 @@
-package org.apache.sysds.runtime.matrix.data;public class LibMatrixFourier {
+package org.apache.sysds.runtime.matrix.data;
+
+import org.apache.sysds.runtime.matrix.data.sketch.ComplexDouble;
+
+public class LibMatrixFourier {
+
+    /**
+     * Function to perform Fast Fourier Transformation on a given array.
+     *
+     * @param in array of ComplexDoubles
+     * @return array of ComplexDoubles
+     */
+    public ComplexDouble[] fft(ComplexDouble[] in){
+
+        in = fillToPowerOfTwo(in);
+        int n = in.length;
+        if(n == 1){
+            return in;
+        }
+
+        double alpha = 2*Math.PI/n;
+        ComplexDouble omega = new ComplexDouble(Math.cos(alpha), Math.sin(alpha));
+
+        ComplexDouble[] even = new ComplexDouble[n/2];
+        ComplexDouble[] odd = new ComplexDouble[n/2];
+        for(int i=0; i < n/2; i++){
+            even[i] = in[i*2];
+            odd[i] = in[i*2+1];
+        }
+        ComplexDouble[] resEven = fft(even);
+        ComplexDouble[] resOdd = fft(odd);
+
+        ComplexDouble[] res = new ComplexDouble[n];
+        for(int j=0; j < n/2; j++){
+            res[j] = resEven[j].add(omega.pow(j).mul(resOdd[j]));
+            res[j+n/2] = resEven[j].sub(omega.pow(j).mul(resOdd[j]));
+        }
+        return res;
+    }
+
+    /**
+     * Function to perform Fast Fourier Transformation on a given array.
+     *
+     * @param in array of doubles
+     * @return array of ComplexDoubles
+     */
+    public ComplexDouble[] fft(double[] in){
+        ComplexDouble[] complex = new ComplexDouble[in.length];
+        for(int i=0; i<in.length; i++){
+            complex[i] = new ComplexDouble(in[i],0);
+        }
+        return fft(complex);
+    }
+
+    /**
+     * Function to fill a given array of ComplexDoubles with 0-ComplexDoubles, so that the length is a power of two.
+     * Needed for FastFourierTransformation
+     *
+     * @param in array of ComplexDoubles
+     * @return array of ComplexDoubles
+     */
+    private ComplexDouble[] fillToPowerOfTwo(ComplexDouble[] in){
+        int missing = nextPowerOfTwo(in.length);
+        ComplexDouble[] res = new ComplexDouble[in.length+missing];
+        for(int i=0; i<in.length; i++){
+            res[i] = in[i];
+        }
+        for(int j=0; j<missing; j++){
+            res[in.length+j] = new ComplexDouble(0,0);
+        }
+        return res;
+    }
+
+    /**
+     * Function for calculating the next larger int which is a power of two
+     *
+     * @param n integer
+     * @return next larger int which is a power of two
+     */
+    private int nextPowerOfTwo(int n){
+        int res = 1;
+        while (res < n){
+            res = res << 1;
+        }
+        return res;
+    }
+
 }
--- src/main/java/org/apache/sysds/runtime/matrix/data/sketch/ComplexDouble.java
+++ src/main/java/org/apache/sysds/runtime/matrix/data/sketch/ComplexDouble.java
@@ -1,2 +1,39 @@
-package org.apache.sysds.runtime.matrix.data.sketch;public class ComplexDouble {
+package org.apache.sysds.runtime.matrix.data.sketch;
+
+public class ComplexDouble {
+    double re;
+    double im;
+
+    public ComplexDouble(double re, double im){
+        this.re = re;
+        this.im = im;
+    }
+
+    public ComplexDouble add(ComplexDouble other){
+        return new ComplexDouble(this.re + other.re, this.im + other.im);
+    }
+
+    public ComplexDouble sub(ComplexDouble other){
+        return new ComplexDouble(this.re - other.re, this.im - other.im);
+    }
+    public ComplexDouble mul(ComplexDouble other){
+        return new ComplexDouble(this.re * other.re - this.im * other.im, this.im * other.re + this.re * other.im);
+    }
+
+    /**
+     * Powering of a complex double.
+     * First, the polar form is calculated and then De Moivre's theorem is applied.
+     *
+     * @param n exponent
+     * @return the n-th power of the complex double
+     */
+    public ComplexDouble pow(int n){
+        // compute polar form
+        double dist = Math.sqrt(Math.pow(this.re,2) + Math.pow(this.im,2));
+        double angle = Math.acos(this.re/dist);
+
+        // de moivre’s theorem
+        return new ComplexDouble(Math.pow(dist,n) * Math.cos(n*angle),Math.pow(dist,n) * Math.sin(n*angle));
+    }
+
 }

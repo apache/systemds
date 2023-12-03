a2aea092a8dc4da55b29c6b1a3a7fbd253e04f8f
Sebastian Baunsgaard <baunsgaard@apache.org>
2023-12-03 14:47:23 +0100

    [SYSTEMDS-3659] Federated GitHub Actions Fail
    The federated tests fails after an assumed update to
    GitHub Actions runners. This commit addresses this change
    by modifying all our primitives federated tests to run in a more
    conservative setup with separated workers on different
    processes. This setup makes the federated tests slower,
    and therefore we split the tests to different action workers
    to provide more parallel execution of the tests.
    
    The old tests that currently still pass run in a single node setup.
    
    Closes #1953

---
+++ src/test/java/org/apache/sysds/test/functions/federated/primitives/part5/FederatedMMChainTest.java
@@ -0,0 +1,190 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.test.functions.federated.primitives.part5;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.sysds.api.DMLScript;
+import org.apache.sysds.common.Types.ExecMode;
+import org.apache.sysds.runtime.meta.MatrixCharacteristics;
+import org.apache.sysds.runtime.util.HDFSTool;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+import org.apache.sysds.test.TestUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(value = Parameterized.class)
+@net.jcip.annotations.NotThreadSafe
+public class FederatedMMChainTest extends AutomatedTestBase {
+
+	private final static String TEST_NAME1 = "FederatedMMChainTest";
+	private final static String TEST_NAME2 = "FederatedMMChainWeightsTest";
+	private final static String TEST_NAME3 = "FederatedMMChainWeights2Test";
+
+	private final static String TEST_DIR = "functions/federated/";
+	private static final String TEST_CLASS_DIR = TEST_DIR + FederatedMMChainTest.class.getSimpleName() + "/";
+
+	private final static int blocksize = 1024;
+	@Parameterized.Parameter()
+	public int rows;
+	@Parameterized.Parameter(1)
+	public int cols;
+	@Parameterized.Parameter(2)
+	public boolean rowPartitioned;
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {{1000, 100, true}, {100, 1000, false}});
+	}
+
+	@Override
+	public void setUp() {
+		TestUtils.clearAssertionInformation();
+		addTestConfiguration(TEST_NAME1, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME1, new String[] {"S"}));
+		addTestConfiguration(TEST_NAME2, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME2, new String[] {"S"}));
+		addTestConfiguration(TEST_NAME3, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME3, new String[] {"S"}));
+	}
+
+	@Test
+	public void testMMChainCP() {
+		runMMChainTest(ExecMode.SINGLE_NODE, TEST_NAME1);
+	}
+
+	@Test
+	public void testMMChainWeightsCP() {
+		runMMChainTest(ExecMode.SINGLE_NODE, TEST_NAME2);
+	}
+
+	@Test
+	public void testMMChainWeights2CP() {
+		runMMChainTest(ExecMode.SINGLE_NODE, TEST_NAME3);
+	}
+
+	@Test
+	public void testMMChainSP() {
+		runMMChainTest(ExecMode.SPARK, TEST_NAME1);
+	}
+
+	@Test
+	public void testMMChainWeightsSP() {
+		runMMChainTest(ExecMode.SPARK, TEST_NAME2);
+	}
+
+	@Test
+	public void testMMChainWeights2SP() {
+		runMMChainTest(ExecMode.SPARK, TEST_NAME3);
+	}
+
+	private void runMMChainTest(ExecMode execMode, String TEST_NAME) {
+		boolean sparkConfigOld = DMLScript.USE_LOCAL_SPARK_CONFIG;
+		ExecMode platformOld = rtplatform;
+
+		if(rtplatform == ExecMode.SPARK)
+			DMLScript.USE_LOCAL_SPARK_CONFIG = true;
+
+		getAndLoadTestConfiguration(TEST_NAME);
+		String HOME = SCRIPT_DIR + TEST_DIR;
+
+		// write input matrices
+		int r = rows;
+		int c = cols / 4;
+		if(rowPartitioned) {
+			r = rows / 4;
+			c = cols;
+		}
+
+		double[][] X1 = getRandomMatrix(r, c, 1, 5, 1, 3);
+		double[][] X2 = getRandomMatrix(r, c, 1, 5, 1, 7);
+		double[][] X3 = getRandomMatrix(r, c, 1, 5, 1, 8);
+		double[][] X4 = getRandomMatrix(r, c, 1, 5, 1, 9);
+
+		MatrixCharacteristics mc = new MatrixCharacteristics(r, c, blocksize, r * c);
+		writeInputMatrixWithMTD("X1", X1, false, mc);
+		writeInputMatrixWithMTD("X2", X2, false, mc);
+		writeInputMatrixWithMTD("X3", X3, false, mc);
+		writeInputMatrixWithMTD("X4", X4, false, mc);
+
+		double[][] v = getRandomMatrix(cols, 1, 0, 1, 0.7, 3);
+		writeInputMatrixWithMTD("v", v, true);
+		if(!TEST_NAME.equals(TEST_NAME1)) {
+			double[][] w = getRandomMatrix(rows, 1, 0, 1, 0.7, 10);
+			writeInputMatrixWithMTD("w", w, true);
+		}
+
+		// empty script name because we don't execute any script, just start the worker
+		fullDMLScriptName = "";
+		int port1 = getRandomAvailablePort();
+		int port2 = getRandomAvailablePort();
+		int port3 = getRandomAvailablePort();
+		int port4 = getRandomAvailablePort();
+		Process t1 = startLocalFedWorker(port1, FED_WORKER_WAIT_S);
+		Process t2 = startLocalFedWorker(port2, FED_WORKER_WAIT_S);
+		Process t3 = startLocalFedWorker(port3, FED_WORKER_WAIT_S);
+		Process t4 = startLocalFedWorker(port4);
+
+		
+		try {
+			if(!isAlive(t1, t2, t3, t4))
+				throw new RuntimeException("Failed starting federated worker");
+
+			rtplatform = execMode;
+			if(rtplatform == ExecMode.SPARK) {
+				DMLScript.USE_LOCAL_SPARK_CONFIG = true;
+			}
+			TestConfiguration config = availableTestConfigurations.get(TEST_NAME);
+			loadTestConfiguration(config);
+
+			// Run reference dml script with normal matrix
+			fullDMLScriptName = HOME + TEST_NAME + "Reference.dml";
+			programArgs = new String[] {"-stats", "100", "-args", input("X1"), input("X2"), input("X3"), input("X4"),
+				Boolean.toString(rowPartitioned).toUpperCase(), input("v"), input("w"), expected("S")};
+			runTest(null);
+
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[] {"-stats", "100", "-nvargs",
+				"in_X1=" + TestUtils.federatedAddress(port1, input("X1")),
+				"in_X2=" + TestUtils.federatedAddress(port2, input("X2")),
+				"in_X3=" + TestUtils.federatedAddress(port3, input("X3")),
+				"in_X4=" + TestUtils.federatedAddress(port4, input("X4")), "rows=" + rows, "cols=" + cols,
+				"rP=" + Boolean.toString(rowPartitioned).toUpperCase(), "in_v=" + input("v"), "in_w=" + input("w"),
+				"out_S=" + output("S")};
+			runTest(null);
+
+			// compare via files
+			compareResults(1e-9, "Stat-DML1", "Stat-DML2");
+
+			// check that federated input files are still existing
+			Assert.assertTrue(HDFSTool.existsFileOnHDFS(input("X1")));
+			Assert.assertTrue(HDFSTool.existsFileOnHDFS(input("X2")));
+			Assert.assertTrue(HDFSTool.existsFileOnHDFS(input("X3")));
+			Assert.assertTrue(HDFSTool.existsFileOnHDFS(input("X4")));
+		}
+		finally {
+			TestUtils.shutdownThreads(t1, t2, t3, t4);
+
+			rtplatform = platformOld;
+			DMLScript.USE_LOCAL_SPARK_CONFIG = sparkConfigOld;
+		}
+	}
+}

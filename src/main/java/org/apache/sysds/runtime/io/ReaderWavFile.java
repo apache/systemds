f4a651ada27b5f4cb9331851643f15c5f180ce70
Jessica Priebe <jessica.priebe@web.de>
2024-01-18 14:37:40 +0100

    tab indentation new files

--- src/main/java/org/apache/sysds/runtime/io/ReaderWavFile.java
+++ src/main/java/org/apache/sysds/runtime/io/ReaderWavFile.java
@@ -7,70 +7,70 @@ import java.io.InputStream;
 
 public class ReaderWavFile {
 
-    public static double[] readMonoFromWavFile(String filePath) {
-        try {
-            // open audio file
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath));
-            double[] audioValues = readMonoFromWavFile(audioInputStream);
-            audioInputStream.close();
-            return audioValues;
+	public static double[] readMonoFromWavFile(String filePath) {
+		try {
+			// open audio file
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath));
+			double[] audioValues = readMonoFromWavFile(audioInputStream);
+			audioInputStream.close();
+			return audioValues;
 
-        } catch (UnsupportedAudioFileException | IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+		} catch (UnsupportedAudioFileException | IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    public static double[] readMonoFromWavFile(InputStream inputStream) {
+	public static double[] readMonoFromWavFile(InputStream inputStream) {
 
-        try {
-            // open audio file
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+		try {
+			// open audio file
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
 
-            // collapse channels to mono channel
-            int channels = 1;
-            AudioFormat monoAudioFormat = new AudioFormat(
-                    audioInputStream.getFormat().getSampleRate(),
-                    audioInputStream.getFormat().getSampleSizeInBits(),
-                    channels,
-                    true,
-                    false
-            );
-            AudioInputStream monoAudioInputStream = AudioSystem.getAudioInputStream(monoAudioFormat, audioInputStream);
+			// collapse channels to mono channel
+			int channels = 1;
+			AudioFormat monoAudioFormat = new AudioFormat(
+					audioInputStream.getFormat().getSampleRate(),
+					audioInputStream.getFormat().getSampleSizeInBits(),
+					channels,
+					true,
+					false
+			);
+			AudioInputStream monoAudioInputStream = AudioSystem.getAudioInputStream(monoAudioFormat, audioInputStream);
 
-            // curation of audio
-            int numFrames = (int) monoAudioInputStream.getFrameLength();
-            // size of one frame in bytes
-            int frameSize = monoAudioInputStream.getFormat().getFrameSize();
+			// curation of audio
+			int numFrames = (int) monoAudioInputStream.getFrameLength();
+			// size of one frame in bytes
+			int frameSize = monoAudioInputStream.getFormat().getFrameSize();
 
-            // read audio into buffer
-            byte[] audioData = new byte[numFrames * frameSize];
-            int bytesRead = audioInputStream.read(audioData);
+			// read audio into buffer
+			byte[] audioData = new byte[numFrames * frameSize];
+			int bytesRead = audioInputStream.read(audioData);
 
-            // read operation failed
-            if (bytesRead == -1) {
-                return null;
-            }
+			// read operation failed
+			if (bytesRead == -1) {
+				return null;
+			}
 
-            // convert byte array to double array
-            double[] audioValues = new double[numFrames];
-            for (int i = 0, frameIndex = 0; i < bytesRead; i += frameSize, frameIndex++) {
-                // 16-bit PCM encoding
-                // combine two bytes into a 16-bit integer (short)
-                short sampleValue = (short) ((audioData[i + 1] << 8) | (audioData[i] & 0xFF));
-                // audio ranges from -32768 to 32767, normalize to range -1 to 1
-                audioValues[frameIndex] = sampleValue / 32768.0;
-            }
+			// convert byte array to double array
+			double[] audioValues = new double[numFrames];
+			for (int i = 0, frameIndex = 0; i < bytesRead; i += frameSize, frameIndex++) {
+				// 16-bit PCM encoding
+				// combine two bytes into a 16-bit integer (short)
+				short sampleValue = (short) ((audioData[i + 1] << 8) | (audioData[i] & 0xFF));
+				// audio ranges from -32768 to 32767, normalize to range -1 to 1
+				audioValues[frameIndex] = sampleValue / 32768.0;
+			}
 
-            // close audio streams
-            monoAudioInputStream.close();
-            audioInputStream.close();
+			// close audio streams
+			monoAudioInputStream.close();
+			audioInputStream.close();
 
-            return audioValues;
+			return audioValues;
 
-        } catch (UnsupportedAudioFileException | IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+		} catch (UnsupportedAudioFileException | IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
--- src/main/java/org/apache/sysds/runtime/matrix/data/LibMatrixKeywordSpotting.java
+++ src/main/java/org/apache/sysds/runtime/matrix/data/LibMatrixKeywordSpotting.java
@@ -10,143 +10,143 @@ import java.util.zip.ZipInputStream;
 
 public class LibMatrixKeywordSpotting {
 
-    List<double[]> samples = new ArrayList<>();
-    List<String> labels = new ArrayList<>();
+	List<double[]> samples = new ArrayList<>();
+	List<String> labels = new ArrayList<>();
 
 
 
-    public LibMatrixKeywordSpotting() {
+	public LibMatrixKeywordSpotting() {
 
-        // load all data
-        // data: http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip
-        // zip contains command folders which contain corresponding .wav files
-        // maybe change label to int?
-        loadAllData();
+		// load all data
+		// data: http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip
+		// zip contains command folders which contain corresponding .wav files
+		// maybe change label to int?
+		loadAllData();
 
-        // convert waveforms to magnitudes of spectrogram
-        // uses stft
-        for (int i = 0; i < samples.size(); i++){
-            double[] wave = samples.get(i);
-            double[] magnitudes = convertWaveToMagnitudesSpectrogram(wave);
-            samples.set(i, magnitudes);
-        }
+		// convert waveforms to magnitudes of spectrogram
+		// uses stft
+		for (int i = 0; i < samples.size(); i++){
+			double[] wave = samples.get(i);
+			double[] magnitudes = convertWaveToMagnitudesSpectrogram(wave);
+			samples.set(i, magnitudes);
+		}
 
-        // TODO:
-        // train model
-        // use gaussianClassifier/CNN???
+		// TODO:
+		// train model
+		// use gaussianClassifier/CNN???
 
-        // [prior, means, covs, det] = gaussianClassifier(D=X, C=y, varSmoothing=$2);
-        // use global variables for classifier
-    }
+		// [prior, means, covs, det] = gaussianClassifier(D=X, C=y, varSmoothing=$2);
+		// use global variables for classifier
+	}
 
 
-    private double[] convertWaveToMagnitudesSpectrogram(double[] wave){
-        // length=255, overlap=128
-        double[][] spectrogram = LibMatrixSTFT.one_dim_stft(wave, 255, 128);
+	private double[] convertWaveToMagnitudesSpectrogram(double[] wave){
+		// length=255, overlap=128
+		double[][] spectrogram = LibMatrixSTFT.one_dim_stft(wave, 255, 128);
 
-        int cols = spectrogram[0].length;
-        double[] magnitudes = new double[cols];
-        for (int i = 0; i < cols; i++){
-            magnitudes[i] = Math.sqrt(Math.pow(spectrogram[0][i], 2) + Math.pow(spectrogram[0][i], 2));
-        }
-        return magnitudes;
-    }
+		int cols = spectrogram[0].length;
+		double[] magnitudes = new double[cols];
+		for (int i = 0; i < cols; i++){
+			magnitudes[i] = Math.sqrt(Math.pow(spectrogram[0][i], 2) + Math.pow(spectrogram[0][i], 2));
+		}
+		return magnitudes;
+	}
 
-    public String getCommandForFile(String filePath){
+	public String getCommandForFile(String filePath){
 
-        // read wave file
-        double[] wave = ReaderWavFile.readMonoFromWavFile(filePath);
+		// read wave file
+		double[] wave = ReaderWavFile.readMonoFromWavFile(filePath);
 
-        // convert waveforms to spectrogram
-        double[] magnitudes = convertWaveToMagnitudesSpectrogram(wave);
+		// convert waveforms to spectrogram
+		double[] magnitudes = convertWaveToMagnitudesSpectrogram(wave);
 
-        // use global variables for classifier
-        // TODO
+		// use global variables for classifier
+		// TODO
 
-        return null;
-    }
+		return null;
+	}
 
-    private void loadAllData(){
+	private void loadAllData(){
 
-        // doesn't work for url
-        // String url = "http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip";
-        // Set<String> dirs = Set.of("yes", "no");
+		// doesn't work for url
+		// String url = "http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip";
+		// Set<String> dirs = Set.of("yes", "no");
 
-        String zipFilePath = "/Users/jessica/desktop/mini_speech_commands.zip";
+		String zipFilePath = "/Users/jessica/desktop/mini_speech_commands.zip";
 
-        try {
-            // get zip data
-            byte[] zipData = getZipData(new FileInputStream(zipFilePath));
+		try {
+			// get zip data
+			byte[] zipData = getZipData(new FileInputStream(zipFilePath));
 
-            // get folder names
-            Set<String> dirs = getDirectories(zipData);
+			// get folder names
+			Set<String> dirs = getDirectories(zipData);
 
-            for (String dir : dirs) {
-                readWavFilesDirectory(zipData, dir);
-            }
+			for (String dir : dirs) {
+				readWavFilesDirectory(zipData, dir);
+			}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    private Set<String> getDirectories(byte[] zipData) throws IOException {
-        Set<String> dirs = new HashSet<>();
-        ZipInputStream stream = new ZipInputStream(new ByteArrayInputStream(zipData));
+	private Set<String> getDirectories(byte[] zipData) throws IOException {
+		Set<String> dirs = new HashSet<>();
+		ZipInputStream stream = new ZipInputStream(new ByteArrayInputStream(zipData));
 
-        // exclude main directory
-        ZipEntry entry = stream.getNextEntry();
-        int mainDirLength = entry.getName().length();
+		// exclude main directory
+		ZipEntry entry = stream.getNextEntry();
+		int mainDirLength = entry.getName().length();
 
-        while ((entry = stream.getNextEntry()) != null) {
-            if (entry.isDirectory()) {
-                String dir = entry.getName();
-                // remove / at the end
-                dirs.add(dir.substring(mainDirLength, dir.length() - 1));
-            }
-        }
-        return dirs;
-    }
+		while ((entry = stream.getNextEntry()) != null) {
+			if (entry.isDirectory()) {
+				String dir = entry.getName();
+				// remove / at the end
+				dirs.add(dir.substring(mainDirLength, dir.length() - 1));
+			}
+		}
+		return dirs;
+	}
 
-    private void readWavFilesDirectory(byte[] zipData, String dir) throws IOException {
-        ZipInputStream stream = new ZipInputStream(new ByteArrayInputStream(zipData));
-        ZipEntry entry;
-        while ((entry = stream.getNextEntry()) != null) {
-            if (entry.getName().startsWith(dir) && entry.isDirectory()) {
-                readWavFilesDirectory(stream, dir);
-                // dont read next dir
-                break;
-            }
-        }
-    }
+	private void readWavFilesDirectory(byte[] zipData, String dir) throws IOException {
+		ZipInputStream stream = new ZipInputStream(new ByteArrayInputStream(zipData));
+		ZipEntry entry;
+		while ((entry = stream.getNextEntry()) != null) {
+			if (entry.getName().startsWith(dir) && entry.isDirectory()) {
+				readWavFilesDirectory(stream, dir);
+				// dont read next dir
+				break;
+			}
+		}
+	}
 
-    private void readWavFilesDirectory(ZipInputStream stream, String dir) throws IOException {
-        ZipEntry entry;
-        while ((entry = stream.getNextEntry()) != null && !entry.isDirectory() && entry.getName().endsWith(".wav")) {
-            readWavFile(entry, dir);
-        }
-    }
+	private void readWavFilesDirectory(ZipInputStream stream, String dir) throws IOException {
+		ZipEntry entry;
+		while ((entry = stream.getNextEntry()) != null && !entry.isDirectory() && entry.getName().endsWith(".wav")) {
+			readWavFile(entry, dir);
+		}
+	}
 
-    private void readWavFile(ZipEntry entry, String dir) {
-        InputStream stream = new ByteArrayInputStream(entry.getExtra());
-        double[] data = ReaderWavFile.readMonoFromWavFile(stream);
-        samples.add(data);
-        labels.add(dir);
-    }
+	private void readWavFile(ZipEntry entry, String dir) {
+		InputStream stream = new ByteArrayInputStream(entry.getExtra());
+		double[] data = ReaderWavFile.readMonoFromWavFile(stream);
+		samples.add(data);
+		labels.add(dir);
+	}
 
-    private byte[] getZipData(InputStream in) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        byte[] dataBuffer = new byte[1024];
-        int bytesRead;
-        while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-            out.write(dataBuffer, 0, bytesRead);
-        }
-        return out.toByteArray();
-    }
+	private byte[] getZipData(InputStream in) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] dataBuffer = new byte[1024];
+		int bytesRead;
+		while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+			out.write(dataBuffer, 0, bytesRead);
+		}
+		return out.toByteArray();
+	}
 
-    private byte[] getZipData(URL url) throws IOException {
-        InputStream in = new BufferedInputStream(url.openStream());
-        return getZipData(in);
-    }
+	private byte[] getZipData(URL url) throws IOException {
+		InputStream in = new BufferedInputStream(url.openStream());
+		return getZipData(in);
+	}
 }

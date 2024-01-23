ef95552b59580ada5260a39f6b3c552f9cece4ac
Jaybit0 <jannik.lindemann3@gmail.com>
2024-01-23 18:19:29 +0100

    Add performance tests

---
+++ src/test/java/org/apache/sysds/performance/matrix/MatrixMulPerformance.java
@@ -0,0 +1,163 @@
+package org.apache.sysds.performance.matrix;
+
+import org.apache.sysds.runtime.data.*;
+import org.apache.sysds.runtime.instructions.InstructionUtils;
+import org.apache.sysds.runtime.matrix.data.MatrixBlock;
+import org.apache.sysds.runtime.util.DataConverter;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class MatrixMulPerformance extends AutomatedTestBase {
+
+    private static final int _rl = 1024;
+    private static final int _cl = 1024;
+
+    private static final int warmupRuns = 15;
+    private static final int repetitions = 50;
+    private static final int resolution = 18;
+    private static final float resolutionDivisor = 2f;
+
+    @Override
+    public void setUp() {
+        TestUtils.clearAssertionInformation();
+    }
+
+    static float[] sparsityProvider() {
+        float[] sparsities = new float[resolution];
+        float currentValue = 0.2f;
+
+        for (int i = 0; i < resolution; i++) {
+            sparsities[i] = currentValue;
+            currentValue /= resolutionDivisor;
+        }
+
+        return sparsities;
+    }
+
+    static String printAsPythonList(float[] list) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (float el : list)
+            sb.append(el + ",");
+
+        if (list.length > 0)
+            sb.deleteCharAt(sb.length() - 1);
+
+        sb.append("]");
+        return sb.toString();
+    }
+
+
+    static String printAsPythonList(double[] list) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (double el : list)
+            sb.append(el + ",");
+
+        if (list.length > 0)
+            sb.deleteCharAt(sb.length() - 1);
+
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Test
+    public void testCSR2CSR() {
+        testSparseFormat(SparseBlock.Type.CSR, SparseBlock.Type.CSR);
+    }
+
+    @Test
+    public void testDCSR2DCSR() {
+        testSparseFormat(SparseBlock.Type.DCSR, SparseBlock.Type.DCSR);
+    }
+
+    @Test
+    public void testMCSR2MCSR() {
+        testSparseFormat(SparseBlock.Type.MCSR, SparseBlock.Type.MCSR);
+    }
+
+    @Test
+    public void testCOO2COO() {
+        testSparseFormat(SparseBlock.Type.COO, SparseBlock.Type.COO);
+    }
+
+    private void testSparseFormat(SparseBlock.Type btype1, SparseBlock.Type btype2) {
+        float[] sparsities = MatrixMulPerformance.sparsityProvider();
+        double[] avgNanosPerSparsity = new double[sparsities.length];
+        long[] results = new long[repetitions];
+        for (int sparsityIndex = 0; sparsityIndex < sparsities.length; sparsityIndex++) {
+            // Warmup the JVM
+            for (int i = 0; i < warmupRuns; i++)
+                runSparsityEstimateTest(btype1, btype2, sparsities[sparsityIndex]);
+
+            for (int i = 0; i < repetitions; i++)
+                results[i] = runSparsityEstimateTest(btype1, btype2, sparsities[sparsityIndex]);
+
+            avgNanosPerSparsity[sparsityIndex] = Arrays.stream(results).average().getAsDouble();
+        }
+
+        System.out.println("sparsities" + btype1.name() + " = " + printAsPythonList(sparsities));
+        System.out.println("avgNanos" + btype2.name() + " =  " + printAsPythonList(avgNanosPerSparsity));
+    }
+
+    private long runSparsityEstimateTest(SparseBlock.Type btype1, SparseBlock.Type btype2, float sparsity) {
+        double[][] A = getRandomMatrix(_rl, _cl, -10, 10, sparsity, 7654321);
+        double[][] B = getRandomMatrix(_rl, _cl, -10, 10, sparsity, 7654322);
+
+        MatrixBlock mbtmp1 = DataConverter.convertToMatrixBlock(A);
+        MatrixBlock mbtmp2 = DataConverter.convertToMatrixBlock(B);
+        SparseBlock srtmp1 = mbtmp1.getSparseBlock();
+        SparseBlock srtmp2 = mbtmp2.getSparseBlock();
+        SparseBlock sblock1;
+        SparseBlock sblock2;
+
+        switch (btype1) {
+            case MCSR:
+                sblock1 = new SparseBlockMCSR(srtmp1);
+                break;
+            case CSR:
+                sblock1 = new SparseBlockCSR(srtmp1);
+                break;
+            case COO:
+                sblock1 = new SparseBlockCOO(srtmp1);
+                break;
+            case DCSR:
+                sblock1 = new SparseBlockDCSR(srtmp1);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown sparse block type");
+        }
+
+        switch (btype2) {
+            case MCSR:
+                sblock2 = new SparseBlockMCSR(srtmp2);
+                break;
+            case CSR:
+                sblock2 = new SparseBlockCSR(srtmp2);
+                break;
+            case COO:
+                sblock2 = new SparseBlockCOO(srtmp2);
+                break;
+            case DCSR:
+                sblock2 = new SparseBlockDCSR(srtmp2);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown sparse block type");
+        }
+
+        MatrixBlock m1 = new MatrixBlock(_rl, _cl, sblock1.size(), sblock1);
+        MatrixBlock m2 = new MatrixBlock(_rl, _cl, sblock2.size(), sblock2);
+
+        long nanos = System.nanoTime();
+
+        MatrixBlock m3 = m1.aggregateBinaryOperations(m1, m2,
+                new MatrixBlock(), InstructionUtils.getMatMultOperator(1));
+
+        return System.nanoTime() - nanos;
+    }
+}
---
+++ src/test/java/org/apache/sysds/performance/matrix/MatrixStorage.java
@@ -0,0 +1,122 @@
+package org.apache.sysds.performance.matrix;
+
+import org.apache.sysds.runtime.data.*;
+import org.apache.sysds.runtime.matrix.data.MatrixBlock;
+import org.apache.sysds.runtime.util.DataConverter;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestUtils;
+import org.junit.Test;
+
+public class MatrixStorage extends AutomatedTestBase {
+
+    private static final int resolution = 18;
+    private static final float resolutionDivisor = 2f;
+
+    static float[] sparsityProvider() {
+        float[] sparsities = new float[resolution];
+        float currentValue = 0.4f;
+
+        for (int i = 0; i < resolution; i++) {
+            sparsities[i] = currentValue;
+            currentValue /= resolutionDivisor;
+        }
+
+        return sparsities;
+    }
+
+    static String printAsPythonList(float[] list) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (float el : list)
+            sb.append(el + ",");
+
+        if (list.length > 0)
+            sb.deleteCharAt(sb.length() - 1);
+
+        sb.append("]");
+        return sb.toString();
+    }
+
+    static String printAsPythonList(long[] list) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (long el : list)
+            sb.append(el + ",");
+
+        if (list.length > 0)
+            sb.deleteCharAt(sb.length() - 1);
+
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public void setUp() {
+        TestUtils.clearAssertionInformation();
+    }
+
+    @Test
+    public void testMCSR() {
+        testSparseFormat(SparseBlock.Type.MCSR, 1024, 1024);
+    }
+
+    @Test
+    public void testCSR() {
+        testSparseFormat(SparseBlock.Type.CSR, 1024, 1024);
+    }
+
+    @Test
+    public void testCOO() {
+        testSparseFormat(SparseBlock.Type.COO, 1024, 1024);
+    }
+
+    @Test
+    public void testDCSR() {
+        testSparseFormat(SparseBlock.Type.DCSR, 1024, 1024);
+    }
+
+    private void testSparseFormat(SparseBlock.Type btype, int rl, int cl) {
+        float[] sparsities = MatrixMulPerformance.sparsityProvider();
+        long[] results = new long[sparsities.length];
+        for (int sparsityIndex = 0; sparsityIndex < sparsities.length; sparsityIndex++)
+            results[sparsityIndex] = evaluateMemoryConsumption(btype, sparsities[sparsityIndex], rl, cl);
+
+        System.out.println("sparsities" + btype.name() + " = " + printAsPythonList(sparsities));
+        System.out.println("memory" + btype.name() + " =  " + printAsPythonList(results));
+    }
+
+    private long evaluateMemoryConsumption(SparseBlock.Type btype, double sparsity, int rl, int cl) {
+        try
+        {
+            double[][] A = getRandomMatrix(rl, cl, -10, 10, sparsity, 7654321);
+
+            if (btype == null)
+                return ((long)(A.length))*A[0].length*8;
+
+            MatrixBlock mbtmp = DataConverter.convertToMatrixBlock(A);
+            SparseBlock srtmp = mbtmp.getSparseBlock();
+            switch (btype) {
+                case MCSR:
+                    SparseBlockMCSR mcsr = new SparseBlockMCSR(srtmp);
+                    return mcsr.getExactSizeInMemory();
+                case CSR:
+                    SparseBlockCSR csr = new SparseBlockCSR(srtmp);
+                    return csr.getExactSizeInMemory();
+                case COO:
+                    SparseBlockCOO coo = new SparseBlockCOO(srtmp);
+                    return coo.getExactSizeInMemory();
+                case DCSR:
+                    SparseBlockDCSR dcsr = new SparseBlockDCSR(srtmp);
+                    return dcsr.getExactSizeInMemory();
+            }
+
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        throw new IllegalArgumentException();
+    }
+}

6fc914eefbfe4ab797bba2148df20434eb9958e4
Joshua <joshua_dreger@tu-berlin.de>
2024-02-13 12:52:10 +0100

    created hmmFit tests

--- src/test/scripts/functions/builtin/hmmFit.R
+++ src/test/scripts/functions/builtin/hmmFit.R
@@ -18,3 +18,25 @@
 # under the License.
 #
 #-------------------------------------------------------------
+
+args <- commandArgs(TRUE)
+library("Matrix")
+library(HMM)
+
+observation_sequence <- as.matrix(readMM(paste(args[1], "observation_sequence.mtx", sep = ""))) # nolint: line_length_linter.
+start_prob <- as.matrix(readMM(paste(args[1], "start_prob.mtx", sep = "")))
+transition_prob <- as.matrix(readMM(paste(args[1], "transition_prob.mtx", sep = ""))) # nolint: line_length_linter.
+emission_prob <- as.matrix(readMM(paste(args[1], "emission_prob.mtx", sep = ""))) # nolint: line_length_linter.
+num_iterations <- as.integer(args[2])
+observation_count <- as.integer(args[3])
+hiddenstates_count <- as.integer(args[4])
+
+states <- 1:hiddenstates_count
+symbols <- 1:observation_count
+hmm <- list(States = states, Symbols = symbols, startProbs = start_prob, transProbs = transition_prob, emissionProbs = emission_prob) # nolint: line_length_linter.
+
+c(hmm_updated, diff) <- baumWelch(hmm = hmm, observation = observation, maxIterations = num_iterations, delta = 0) # nolint: line_length_linter.
+
+writeMM(as(hmm_updated$transProbs, "transition_prob"), paste(args[5], "transition_prob", sep = "")) # nolint: line_length_linter.
+writeMM(as(hmm_updated$emissionProbs, "emission_prob"), paste(args[5], "emission_prob", sep = "")) # nolint: line_length_linter.
+writeMM(as(diff, "diff_array"), paste(args[5], "diff_array", sep = "")) # nolint: line_length_linter.

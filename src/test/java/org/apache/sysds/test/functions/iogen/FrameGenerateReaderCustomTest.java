7e2b5608aa5f7a56c98eed5022d1dbb0a49db8f7
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-02-16 20:47:39 -0500

    Fix GIO Test Issue

--- src/test/java/org/apache/sysds/test/functions/iogen/FrameGenerateReaderCustomTest.java
+++ src/test/java/org/apache/sysds/test/functions/iogen/FrameGenerateReaderCustomTest.java
@@ -23,7 +23,7 @@ import org.junit.Test;
 
 public class FrameGenerateReaderCustomTest extends GenerateReaderFrameTest {
 
-	private final static String TEST_NAME = "FrameGenerateReaderCSVTest";
+	private final static String TEST_NAME = "FrameGenerateReaderCustomTest";
 
 	@Override
 	protected String getTestName() {

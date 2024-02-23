41285444cb532c9b553597d18fce0d370523ec92
louislepage <l.lepage@campus.tu-berlin.de>
2024-02-23 13:23:49 +0100

    [SYSTEMDS-3669] add testscripts for permutation runtime

---
+++ scripts/staging/shapley_values/examples/test_runtimes_permutation.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#-------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#-------------------------------------------------------------
+#
+#Runs systemds multiple times and stores resulting runtimes and sample sizes in file
+
+data_file="../data/runtimes_permutation_3000_3perm.csv"
+permutations=3
+samples=100
+
+echo "Outputfile systemds: $data_file"
+
+echo "instances,runtime_python,runtime_row,runtime_permutation,runtime_legacy" | tee "$data_file"
+for instances in $(seq 3000 500 10000); do
+    #set to 1 on first run
+    [[ $instances -eq 0 ]] && instances=1
+
+    #take three samples per size
+    for j in {1..3}; do
+#        echo -n "${instances}," | tee -a "$data_file"
+	
+        #python
+        runtime_python=$(python ./shap-permutation.py --n-permutations=${permutations} --n-instances=${instances} --silent --just-print-t)
+        echo -n "${instances},${runtime_python}," | tee -a "$data_file"
+
+        #by-row
+        runtime_r=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+        echo -n "${runtime_r}," | tee -a "$data_file"
+
+        #by-permutation
+        runtime_p=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-permutation 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+        echo -n "${runtime_p}," | tee -a "$data_file"
+
+        #legacy (only works for up to 2750 instances and crashes afterwards)
+        #runtime_l=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=legacy 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+        #echo -n "${runtime_l}" | tee -a "$data_file"
+
+        #newline
+	      echo "" | tee -a "$data_file"
+    done
+done

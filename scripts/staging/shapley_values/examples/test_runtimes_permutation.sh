0a46bbddc3902540c4759f922d94f06199d9fa20
louislepage <l.lepage@campus.tu-berlin.de>
2024-04-16 11:58:10 +0200

    [SYSTEMDS-3669] add to permutation experiments script

--- scripts/staging/shapley_values/examples/test_runtimes_permutation.sh
+++ scripts/staging/shapley_values/examples/test_runtimes_permutation.sh
@@ -22,14 +22,14 @@
 #
 #Runs systemds multiple times and stores resulting runtimes and sample sizes in file
 
-data_file="../data/runtimes_permutation_test.csv"
-permutations=10
+data_file="${1:-../data/runtimes_permutation_test.csv}"
+permutations=3
 samples=100
 
-echo "Outputfile systemds: $data_file"
+echo "Outputfile: $data_file"
 
-echo "instances,runtime_python,runtime_row,runtime_row_non_var,runtime_permutation,runtime_legacy,runtime_legacy_iterative" | tee "$data_file"
-for instances in $(seq 0 250 2000); do
+echo "instances,runtime_python,runtime_row,runtime_row_non_var,runtime_row_partitioned,runtime_permutation,runtime_legacy,runtime_legacy_iterative" | tee "$data_file"
+for instances in $(seq 0 500 20000); do
     #set to 1 on first run
     [[ $instances -eq 0 ]] && instances=1
 
@@ -49,16 +49,23 @@ for instances in $(seq 0 250 2000); do
         runtime_r_non_var=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs remove_non_var=1 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
         echo -n "${runtime_r_non_var}," | tee -a "$data_file"
 
+        #by-row partitioned
+        runtime_r_partitioned=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs remove_non_var=0 use_partitions=1 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row  2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+        echo -n "${runtime_r_partitioned}," | tee -a "$data_file"
+
         #by-permutation
-        runtime_p=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-permutation 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+        [[ $instances -le 10000 ]] && runtime_p=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-permutation 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
         echo -n "${runtime_p}," | tee -a "$data_file"
+        unset runtime_p
 
         #legacy (only works for up to 2750 instances and crashes afterwards)
-        runtime_l=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=legacy 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+        [[ $instances -le 2500 ]] && runtime_l=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=legacy 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
         echo -n "${runtime_l}," | tee -a "$data_file"
+        unset runtime_l
 
-        runtime_l_i=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=legacy-iterative 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+        [[ $instances -le 2500 ]] && runtime_l_i=$(systemds ./shapley-multiLogReg-permutation-multirow.dml -stats 1 -nvargs n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=legacy-iterative 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
         echo -n "${runtime_l_i}" | tee -a "$data_file"
+        unset runtime_l_i
 
         #newline
 	      echo "" | tee -a "$data_file"

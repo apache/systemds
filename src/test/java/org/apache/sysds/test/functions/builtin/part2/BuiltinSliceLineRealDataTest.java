bfb79f015c3f2f541e40f359a49412060bcb36d5
Matthias Boehm <mboehm7@gmail.com>
2024-05-20 20:15:35 +0200

    [SYSTEMDS-3696] New sliceLineDebug built-in function for usability
    This patch adds a new sliceLineDebug function to present the top-k
    worst-slides returned from sliceLine (slicefinder) in a human
    readable format. This is the output for the Salaries dataset:
    
    sliceLineDebug:
    -- Slice #1: score=0.4041683676825298, size=248.0
    ---- avg error=6.558681888351787E8, max error=8.524558818262574E9
    ---- predicate: "rank" = "Prof" AND "sex" = "Male"
    -- Slice #2: score=0.3731763935666855, size=42.0
    ---- avg error=8.271958572009121E8, max error=4.553584116646141E9
    ---- predicate: "rank" = "Prof" AND "yrs.since.phd" = 31.25
    -- Slice #3: score=0.3675193573989536, size=125.0
    ---- avg error=6.758211389786526E8, max error=8.524558818262574E9
    ---- predicate: "rank" = "Prof" AND "discipline" = "B" AND "sex" =
    "Male"
    -- Slice #4: score=0.35652331744984933, size=266.0
    ---- avg error=6.307265846260264E8, max error=8.524558818262574E9
    ---- predicate: "rank" = "Prof"

---
+++ src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinSliceLineRealDataTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.test.functions.builtin.part2;
+
+import org.apache.sysds.common.Types;
+import org.apache.sysds.common.Types.ExecType;
+import org.apache.sysds.runtime.matrix.data.MatrixValue.CellIndex;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+import org.apache.sysds.utils.Statistics;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BuiltinSliceLineRealDataTest extends AutomatedTestBase {
+	private final static String TEST_NAME = "sliceLineRealData";
+	private final static String TEST_DIR = "functions/builtin/";
+	private final static String TEST_CLASS_DIR = TEST_DIR + BuiltinSliceLineRealDataTest.class.getSimpleName() + "/";
+
+	private final static String SALARIES_DATA = DATASET_DIR + "Salaries.csv";
+	private final static String SALARIES_TFSPEC = DATASET_DIR + "Salaries_tfspec.json";
+
+	@Override
+	public void setUp() {
+		for(int i=1; i<=4; i++)
+			addTestConfiguration(TEST_NAME+i, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {"R"}));
+	}
+
+	@Test
+	public void testSliceLineSalaries() {
+		runSliceLine(1, SALARIES_DATA, SALARIES_TFSPEC, 0.5, ExecType.CP);
+	}
+
+	private void runSliceLine(int test, String data, String tfspec, double minAcc, ExecType instType) {
+		Types.ExecMode platformOld = setExecMode(instType);
+		try {
+			loadTestConfiguration(getTestConfiguration(TEST_NAME+test));
+			
+			String HOME = SCRIPT_DIR + TEST_DIR;
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[] {"-stats",
+				"-args", data, tfspec, output("R")};
+
+			runTest(true, false, null, -1);
+
+			double acc = readDMLMatrixFromOutputDir("R").get(new CellIndex(1,1));
+			Assert.assertTrue(acc >= minAcc);
+			Assert.assertEquals(0, Statistics.getNoOfExecutedSPInst());
+		}
+		finally {
+			rtplatform = platformOld;
+		}
+	}
+}

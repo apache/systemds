8e1e53bacbf444625272462c90e1e9c2bfaf206a
Nakroma <tarackobarack@gmail.com>
2024-05-28 10:40:44 +0200

    [SYSTEMDS-3694] Python NN Sequence and layer interface
    This commit:
    - Adds a Layer interface for the Python API.
    - Affine and ReLU classes are changed to extend this interface.
    - Fixes fixes some small formatting issues in the modified classes.
    - Adds a Sequential primitive to the nn Python API.
      It is able to combine multiple nn layers into one sequential module.
    - fix in the python MultiReturn so outputs of the instance can be properly accessed.
    - Adds the backwards pass to the Sequential primitives.
    - Variations to Sequential testing involving MultiReturns.
    - Test if the input gradient is set correctly on the backwards pass
      and Fixes a bug where this was not the case on the affine layer.
    - Testing to verify that the layer gets updated correctly during forward and backward pass.
    
    AMLS project SoSe'24
    
    Closes #2025

---
+++ src/main/python/tests/nn/test_layer.py
@@ -0,0 +1,80 @@
+# -------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# -------------------------------------------------------------
+
+import unittest
+
+from systemds.context import SystemDSContext
+from systemds.operator.nn.layer import Layer
+
+
+class TestLayer(unittest.TestCase):
+    sds: SystemDSContext = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sds = SystemDSContext()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.sds.close()
+
+    def test_init(self):
+        """
+        Test that the source is created correctly from dml_script param when layer is initialized
+        """
+        _ = Layer(self.sds, "relu.dml")
+        self.assertIsNotNone(Layer._source)
+        self.assertTrue(Layer._source.operation.endswith('relu.dml"'))
+        self.assertEqual(Layer._source._Source__name, "relu")
+
+    def test_notimplemented(self):
+        """
+        Test that NotImplementedError is raised
+        """
+
+        class TestLayerImpl(Layer):
+            pass
+
+        layer = TestLayerImpl(self.sds, "relu.dml")
+        with self.assertRaises(NotImplementedError):
+            layer.forward(None)
+        with self.assertRaises(NotImplementedError):
+            layer.backward(None)
+        with self.assertRaises(NotImplementedError):
+            TestLayerImpl.forward(None)
+        with self.assertRaises(NotImplementedError):
+            TestLayerImpl.backward(None)
+
+    def test_class_source_assignments(self):
+        """
+        Test that the source is not shared between interface and implementation class
+        """
+
+        class TestLayerImpl(Layer):
+            @classmethod
+            def _create_source(cls, sds_context: SystemDSContext, dml_script: str):
+                cls._source = "test"
+
+        _ = Layer(self.sds, "relu.dml")
+        _ = TestLayerImpl(self.sds, "relu.dml")
+
+        self.assertNotEqual(Layer._source, "test")
+        self.assertEqual(TestLayerImpl._source, "test")

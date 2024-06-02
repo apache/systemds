1c712621f5b3dae6ee47c8cf7f265c54206f161d
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2024-06-02 15:15:49 +0200

    Add put and get methods to the cacheMap

--- src/main/java/org/apache/sysds/runtime/transform/encode/EncodeBuildCache.java
+++ src/main/java/org/apache/sysds/runtime/transform/encode/EncodeBuildCache.java
@@ -1,21 +1,33 @@
 package org.apache.sysds.runtime.transform.encode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class EncodeBuildCache {
 
-    private HashMap<Integer, ArrayList<ColumnEncoder>> cacheMap;
+    // The integer key is the column ID, and the value is the ColumnEncoder for the given columnID
+    private HashMap<Integer, ColumnEncoder> cacheMap;
+    private Integer max = 20;
 
     public EncodeBuildCache() {
-        this.cacheMap = new HashMap<Integer, ArrayList<ColumnEncoder>>();
+        this.cacheMap = new HashMap<Integer, ColumnEncoder>();
     }
 
-    public HashMap<Integer, ArrayList<ColumnEncoder>> getCacheMap() {
+    public HashMap<Integer, ColumnEncoder> getCacheMap() {
         return cacheMap;
     }
 
-    public void setCacheMap(HashMap<Integer, ArrayList<ColumnEncoder>> cacheMap) {
+    public void setCacheMap(HashMap<Integer, ColumnEncoder> cacheMap) {
         this.cacheMap = cacheMap;
     }
+
+    public void put(Integer columnID, ColumnEncoder encoder) {
+        // TODO: if cachemap.size is at max, evict the least recently used column
+        cacheMap.put(columnID, encoder);
+    }
+
+    public ColumnEncoder get(Integer columnID) {
+        return cacheMap.get(columnID);
+    }
+
+    
 }

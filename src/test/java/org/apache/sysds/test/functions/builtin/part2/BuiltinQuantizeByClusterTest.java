9ff9e1635fcc76a1e0fc21cfa4f6aefba1e64326
Can Abdulla <canabdulla@outlook.de>
2024-06-04 10:10:43 +0200

    [SYSTEMDS-3213] New builtin for cluster-based quantization
    LDE SoSe'24 project
    Closes #2030.

---
+++ src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinQuantizeByClusterTest.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.test.functions.builtin.part2;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.sysds.runtime.matrix.data.MatrixValue;
+import org.apache.sysds.runtime.meta.MatrixCharacteristics;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+
+@RunWith(Parameterized.class)
+public class BuiltinQuantizeByClusterTest extends AutomatedTestBase {
+
+	@Parameter
+	public String test_case;
+	@Parameter(1)
+	public int rows;
+	@Parameter(2)
+	public int cols;
+	@Parameter(3)
+	public int clusters;
+	@Parameter(4)
+	public int subspaces;
+	@Parameter(5)
+	public int k;
+	@Parameter(6)
+	public int vectors_per_cluster;
+	@Parameter(7)
+	public boolean quantize_separately;
+
+	private final static String TEST_NAME = "quantizeByCluster";
+	private final static String TEST_DIR = "functions/builtin/";
+	private final static String TEST_CLASS_DIR = TEST_DIR + BuiltinQuantizeByClusterTest.class.getSimpleName() + "/";
+	private final static double eps = 1e-10;
+	private final static int runs = 3;
+	private final static int max_iter = 1000;
+//	private final static double cluster_offset = 0.1;
+
+	@Parameterized.Parameters(name = "{0}: rows={1}, cols={2}, c={3}, subspaces={4}, k={5}, v_per_c={6}, sep={7}")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][]{
+			{"sub_cluster", 1024, 64, 12, 8, 12, 40, true},  {"sub_cluster", 1024, 64, 12, 4, 12, 40, true},  {"sub_cluster", 1024, 64, 12, 2, 12, 40, true},
+			{"sub_cluster", 1024, 64, 12, 8, 12, 40, false}, {"sub_cluster", 1024, 64, 12, 4, 12, 40, false}, {"sub_cluster", 1024, 64, 12, 2, 12, 40, false},
+			{"cluster", 1024, 64, 12, 8, 12, 40, true},  {"cluster", 1024, 64, 12, 4, 12, 40, true},  {"cluster", 1024, 64, 12, 2, 12, 40, true},
+			{"cluster", 1024, 64, 20, 8, 12, 40, false}, {"cluster", 1024, 64, 12, 4, 12, 40, false}, {"cluster", 1024, 64, 12, 2, 12, 40, false},
+			{"uniform", 1024, 64, 12, 8, 12, 40, true},  {"uniform", 1024, 64, 12, 4, 12, 40, true},  {"uniform", 1024, 64, 12, 2, 12, 40, true},
+			{"uniform", 1024, 64, 12, 8, 12, 40, false}, {"uniform", 1024, 64, 12, 4, 12, 40, false}, {"uniform", 1024, 64, 12, 2, 12, 40, false},
+			{"normal",  1024, 64, 12, 8, 12, 40, true},  {"normal",  1024, 64, 12, 4, 12, 40, true},  {"normal",  1024, 64, 12, 2, 12, 40, true},
+			{"normal",  1024, 64, 12, 8, 12, 40, false}, {"normal",  1024, 64, 12, 4, 12, 40, false}, {"normal",  1024, 64, 12, 2, 12, 40, false},
+		});
+	}
+
+	@Override
+	public void setUp() {
+		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME));
+	}
+
+	@Test
+	public void basicTest() {
+		runQuantizeByClusterTest();
+	}
+
+	/*The tests use kmeans clustering as a baseline and check whether the distortion is within
+	a certain threshold.*/
+	private void runQuantizeByClusterTest() {
+		loadTestConfiguration(getTestConfiguration(TEST_NAME));
+		String HOME = SCRIPT_DIR + TEST_DIR;
+		fullDMLScriptName = HOME + TEST_NAME + ".dml";
+		programArgs = new String[]{"-nvargs", "codes=" + output("codes"), "codebook=" + output("codebook"),
+				"pq_distortion=" + output("pq_distortion"), "k_distortion=" + output("k_distortion"),
+				"clusters=" + clusters, "test_case=" + test_case, "rows=" + rows,
+				"cols=" + cols, "subspaces=" + subspaces, "k=" + k, "runs=" + runs, "max_iter=" + max_iter,
+				"eps=" + eps, "vectors_per_cluster=" + vectors_per_cluster, "sep=" + quantize_separately};
+
+		runTest(true, EXCEPTION_NOT_EXPECTED, null, -1);
+
+		// check if output dimensions are correct
+		MatrixCharacteristics meta_codes = readDMLMetaDataFile("codes");
+		MatrixCharacteristics meta_codebook = readDMLMetaDataFile("codebook");
+		Assert.assertTrue("Matrix dimensions should be equal to expected dimensions",
+			meta_codes.getRows() == (long) clusters * vectors_per_cluster 
+			&& meta_codes.getCols() == subspaces);
+		Assert.assertEquals("Centroid dimensions should be equal to expected dimensions",
+			cols / subspaces, meta_codebook.getCols());
+
+		double pq_distortion = readDMLMatrixFromOutputDir("pq_distortion").get(new MatrixValue.CellIndex(1, 1));
+		double k_distortion = readDMLMatrixFromOutputDir("k_distortion").get(new MatrixValue.CellIndex(1, 1));
+
+		//check if distortion is within a threshold
+		if (!test_case.equals("cluster")) {
+			Assert.assertTrue(pq_distortion < 1.2 * k_distortion + 0.1);
+		} else {
+			Assert.assertTrue(pq_distortion < 2 * k_distortion + 0.1);
+		}
+	}
+}

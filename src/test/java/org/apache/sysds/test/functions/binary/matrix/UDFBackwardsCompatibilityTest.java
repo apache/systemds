5015f63a7980f36e832bdffcbebba575cf8ddd62
Matthias Boehm <mboehm7@gmail.com>
2024-06-07 09:44:45 +0200

    [SYSTEMDS-3709] Additional tests for UDF backwards compatibility
    This patch adds tests for the old SystemML UDF MultiInputCbind,
    ensuring the related DML script is properly compiled to an nary cbind
    and if the inputs are vectors and are reshaped to vectors, we also
    eliminate the unnecessary reshape.

--- src/test/java/org/apache/sysds/test/functions/binary/matrix/UDFBackwardsCompatibilityTest.java
+++ src/test/java/org/apache/sysds/test/functions/binary/matrix/UDFBackwardsCompatibilityTest.java
@@ -19,16 +19,20 @@
 
 package org.apache.sysds.test.functions.binary.matrix;
 
+import org.junit.Assert;
 import org.junit.Test;
+
 import org.apache.sysds.common.Types.ExecMode;
 import org.apache.sysds.common.Types.ExecType;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.apache.sysds.test.TestUtils;
+import org.apache.sysds.utils.Statistics;
 
 public class UDFBackwardsCompatibilityTest extends AutomatedTestBase 
 {
 	private final static String TEST_NAME1 = "RowClassMeetTest";
+	private final static String TEST_NAME2 = "MultiInputCbindTest";
 	private final static String TEST_DIR = "functions/binary/matrix/";
 	private final static String TEST_CLASS_DIR = TEST_DIR + 
 		UDFBackwardsCompatibilityTest.class.getSimpleName() + "/";
@@ -44,29 +48,46 @@ public class UDFBackwardsCompatibilityTest extends AutomatedTestBase
 	public void setUp() {
 		addTestConfiguration( TEST_NAME1,
 			new TestConfiguration(TEST_CLASS_DIR, TEST_NAME1, new String[] { "C" }) );
+		addTestConfiguration( TEST_NAME2,
+			new TestConfiguration(TEST_CLASS_DIR, TEST_NAME2, new String[] { "C" }) );
 	}
 
 	@Test
 	public void testRowClassMeetDenseDense() {
-		runUDFTest(TEST_NAME1, false, false, ExecType.CP);
+		runUDFTest(TEST_NAME1, false, false, false, false, ExecType.CP);
 	}
 	
 	@Test
 	public void testRowClassMeetDenseSparse() {
-		runUDFTest(TEST_NAME1, false, true, ExecType.CP);
+		runUDFTest(TEST_NAME1, false, true, false, false, ExecType.CP);
 	}
 	
 	@Test
 	public void testRowClassMeetSparseDense() {
-		runUDFTest(TEST_NAME1, true, false, ExecType.CP);
+		runUDFTest(TEST_NAME1, true, false, false, false, ExecType.CP);
 	}
 	
 	@Test
 	public void testRowClassMeetSparseSparse() {
-		runUDFTest(TEST_NAME1, true, true, ExecType.CP);
+		runUDFTest(TEST_NAME1, true, true, false, false, ExecType.CP);
 	}
 	
-	private void runUDFTest(String testname, boolean sparseM1, boolean sparseM2, ExecType instType)
+	@Test
+	public void testMultiInputCBindDenseDenseMatrixMatrix() {
+		runUDFTest(TEST_NAME2, false, false, false, false, ExecType.CP);
+	}
+	
+	@Test
+	public void testMultiInputCBindDenseDenseMatrixVector() {
+		runUDFTest(TEST_NAME2, false, false, false, true, ExecType.CP);
+	}
+	
+	@Test
+	public void testMultiInputCBindDenseDenseVectorVector() {
+		runUDFTest(TEST_NAME2, false, false, true, true, ExecType.CP);
+	}
+	
+	private void runUDFTest(String testname, boolean sparseM1, boolean sparseM2, boolean vectorData, boolean vectorize, ExecType instType)
 	{
 		ExecMode platformOld = setExecMode(instType);
 		String TEST_NAME = testname;
@@ -76,18 +97,27 @@ public class UDFBackwardsCompatibilityTest extends AutomatedTestBase
 			
 			String HOME = SCRIPT_DIR + TEST_DIR;
 			fullDMLScriptName = HOME + TEST_NAME + ".dml";
-			programArgs = new String[]{"-explain","-args", input("A"), input("B"), output("C")};
+			programArgs = new String[]{"-stats", "-explain","-args",
+				input("A"), input("B"), String.valueOf(vectorize).toUpperCase(), output("C")};
 			
 			//generate actual dataset
+			int nr = vectorData ? rows*cols : rows;
+			int nc = vectorData ? 1 : cols;
+			
 			double[][] A = TestUtils.round(
-				getRandomMatrix(rows, cols, 0, 10, sparseM1?sparsity2:sparsity1, 7));
+				getRandomMatrix(nr, nc, 0, 10, sparseM1?sparsity2:sparsity1, 7));
 			writeInputMatrixWithMTD("A", A, false);
 			double[][] B = TestUtils.round(
-				getRandomMatrix(rows, cols, 0, 10, sparseM2?sparsity2:sparsity1, 3));
+				getRandomMatrix(nr, nc, 0, 10, sparseM2?sparsity2:sparsity1, 3));
 			writeInputMatrixWithMTD("B", B, false);
 	
 			//run test case
-			runTest(true, false, null, -1); 
+			runTest(true, false, null, -1);
+			
+			if( TEST_NAME.equals(TEST_NAME2) ) //check nary cbind
+				Assert.assertEquals(1, Statistics.getCPHeavyHitterCount("cbind"));
+			if( vectorData && vectorize ) //check eliminated reshape
+				Assert.assertFalse(heavyHittersContainsString("rshape"));
 		}
 		finally {
 			rtplatform = platformOld;

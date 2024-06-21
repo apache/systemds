a55c1e979c022f39338a06b6afc1b7ab1b4f32d6
lachezar-n <lachezar.nikolov99@gmail.com>
2024-06-21 14:09:24 +0200

    not-relevant code changes removal

--- src/main/java/org/apache/sysds/conf/CompilerConfig.java
+++ src/main/java/org/apache/sysds/conf/CompilerConfig.java
@@ -78,9 +78,7 @@ public class CompilerConfig
 		CODEGEN_ENABLED,
 		
 		//federated runtime conversion
-		FEDERATED_RUNTIME,
-		// resource optimization mode
-		RESOURCE_OPTIMIZATION;
+		FEDERATED_RUNTIME;
 	}
 	
 	//default flags (exposed for testing purposes only)
@@ -109,7 +107,6 @@ public class CompilerConfig
 		_bmap.put(ConfigType.REJECT_READ_WRITE_UNKNOWNS, true);
 		_bmap.put(ConfigType.MLCONTEXT, false);
 		_bmap.put(ConfigType.CODEGEN_ENABLED, false);
-		_bmap.put(ConfigType.RESOURCE_OPTIMIZATION, false);
 		
 		_imap = new HashMap<>();
 		_imap.put(ConfigType.BLOCK_SIZE, OptimizerUtils.DEFAULT_BLOCKSIZE);

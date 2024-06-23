f21d4bd60cb70e3349937037ddb9a24347c2c951
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2024-06-24 01:01:31 +0200

    Extend CacheKey to include binMethod so we can cache build result based on binMethod

--- src/main/java/org/apache/sysds/runtime/transform/encode/CacheKey.java
+++ src/main/java/org/apache/sysds/runtime/transform/encode/CacheKey.java
@@ -2,13 +2,30 @@ package org.apache.sysds.runtime.transform.encode;
 
 import java.util.Objects;
 
+import org.apache.sysds.runtime.transform.encode.ColumnEncoderBin.BinMethod;
+
 public class CacheKey {
     private final int columnId;
     private final EncoderType encoderType;
 
+    private final BinMethod binMethod;
+
     public CacheKey(int columnId, EncoderType encoderType) {
         this.columnId = columnId;
         this.encoderType = encoderType;
+        if (encoderType == EncoderType.Bin) {
+            // throw exception
+            this.binMethod = null;
+        }
+        else {
+            this.binMethod = null;
+        }
+    }
+    
+    public CacheKey(int columnId, EncoderType encoderType, BinMethod binMethod) {
+        this.columnId = columnId;
+        this.encoderType = encoderType;
+        this.binMethod = binMethod;
     }
 
     public int getColumnId() {
@@ -24,12 +41,12 @@ public class CacheKey {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CacheKey cacheKey = (CacheKey) o;
-        return columnId == cacheKey.columnId && encoderType == cacheKey.encoderType;
+        return columnId == cacheKey.columnId && encoderType == cacheKey.encoderType && binMethod == cacheKey.binMethod;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(columnId, encoderType);
+        return Objects.hash(columnId, encoderType, binMethod);
     }
 
     @Override
@@ -37,6 +54,7 @@ public class CacheKey {
         return "CacheKey{" +
                 "columnId=" + columnId +
                 ", encoderType=" + encoderType +
+                ", binMethod=" + binMethod +
                 '}';
     }
 }

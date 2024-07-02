e8558d4ba35c8375b649c6149c932d9f921c5354
lynn-janzen007 <lynn.janzen@campus.tu-berlin.de>
2024-07-02 12:42:51 +0200

    [SYSTEMDS-3534] - added size computation

--- src/main/java/org/apache/sysds/runtime/transform/encode/BinMinsMaxs.java
+++ src/main/java/org/apache/sysds/runtime/transform/encode/BinMinsMaxs.java
@@ -17,5 +17,16 @@ public class BinMinsMaxs {
     public double[] get_binMins() {
         return _binMins;
     }
-    
+
+    public long getSize(){
+        // object header: 16 bytes, reference to _binMins, _binMaxs: each 4 bytes,
+        // array header: each 12 bytes, storage of array length: each 4 bytes
+        // the constant object overhead is a minimum estimation as exact storage behaviour cannot be predicted
+        int size = 56;
+        if (_binMins != null && _binMaxs != null) {
+            size += 2 *  8 * _binMins.length; // size of double: 8 bytes
+        }
+        return size;
+    }
+
 }

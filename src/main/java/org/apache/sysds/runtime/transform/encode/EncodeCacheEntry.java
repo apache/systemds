0d4d32a28cb4f11fe0adae73f5194d3c31c10f8b
lynn-janzen007 <lynn.janzen@campus.tu-berlin.de>
2024-07-03 14:09:34 +0200

    [SYSTEMDS-3534] - removed unused code

--- src/main/java/org/apache/sysds/runtime/transform/encode/EncodeCacheEntry.java
+++ src/main/java/org/apache/sysds/runtime/transform/encode/EncodeCacheEntry.java
@@ -19,27 +19,16 @@
 
 package org.apache.sysds.runtime.transform.encode;
 
-import org.apache.sysds.runtime.transform.encode.EncodeCacheConfig.EncodeCacheStatus;
-
-import java.util.HashMap;
-
 public class EncodeCacheEntry<T> { // uses generic to store any kind of object
 
-    protected final EncodeCacheKey _key; //TODO: do we need it in here too?
+    protected final EncodeCacheKey _key;
     protected T _value; // generic, can be an RCDMap or a BinBoundries object
     protected long _timestamp = 0;
-    protected EncodeCacheConfig.EncodeCacheStatus _status; //TODO: do we need this?
 
     public EncodeCacheEntry(EncodeCacheKey key, T value) {
         _key = key;
         _value = value;
         _timestamp = System.currentTimeMillis();
-        _status = isEmpty() ? EncodeCacheStatus.EMPTY : EncodeCacheStatus.CACHED;
-    }
-
-    //TODO: not sure about the signature here
-    protected synchronized void setCacheStatus(EncodeCacheStatus st) {
-        _status = st;
     }
 
     protected synchronized void updateTimestamp(){
@@ -49,23 +38,6 @@ public class EncodeCacheEntry<T> { // uses generic to store any kind of object
     public synchronized Object getValue() {
         return _value;
     }
-
-    //TODO: Copied the following from the lineageCacheEntry but not clear whether we need them
-
-    /*public synchronized void setValue(T val) {
-        _value = val;
-        _status = isEmpty() ? EncodeCacheStatus.EMPTY : EncodeCacheStatus.CACHED;
-        //resume all threads waiting for val
-        notifyAll();
-    }*/
-
-    /*protected synchronized void removeAndNotify() {
-        //Set the status to NOTCACHED (not cached anymore) and wake up the sleeping threads
-        if (_status != EncodeCacheStatus.EMPTY)
-            return;
-        _status = EncodeCacheConfig.EncodeCacheStatus.NOTCACHED;
-        notifyAll();
-    }*/
 
     public long getSize() {
         if (_value instanceof BinMinsMaxs){ return ((BinMinsMaxs) _value).getSize(); }

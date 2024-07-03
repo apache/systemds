40dfc315137761533c391a4a7b384361bccf25c9
lynn-janzen007 <lynn.janzen@campus.tu-berlin.de>
2024-07-03 23:23:37 +0200

    [SYSTEMDS-3534] - moved evition logic out of the get method

--- src/main/java/org/apache/sysds/runtime/transform/encode/EncodeBuildCache.java
+++ src/main/java/org/apache/sysds/runtime/transform/encode/EncodeBuildCache.java
@@ -3,7 +3,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.sysds.runtime.controlprogram.parfor.stat.InfrastructureAnalyzer;
 
-
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,17 +21,15 @@ public class EncodeBuildCache {
     private static long _usedCacheMemory;
     // Note: we omitted maintaining a timestamp since and ordered data structure is sufficient for LRU
 
-
     private EncodeBuildCache() {
         _cache = new ConcurrentHashMap<>();
         _cachePolicy = EncodeCacheConfig._cachepolicy;
         _evictionQueue = new LinkedList<>();
         _usedCacheMemory = 0;
         _config = EncodeCacheConfig.create(); // reads values from ConfigurationManager or uses default ones
-        _cacheLimit = setCacheLimit(_config.getCacheMemoryFraction()); //5%
-
-
+        setCacheLimit(_config.getCacheMemoryFraction()); //5%
     }
+
     // we chose the singleton pattern instead of making the cache a static class because it is lazy loaded
     public static EncodeBuildCache getEncodeBuildCache() {
         if (_instance == null) {
@@ -46,59 +43,92 @@ public class EncodeBuildCache {
     }
 
     public synchronized void put(EncodeCacheKey key, EncodeCacheEntry buildResult) {
+        long entrySize = buildResult.getSize();
+        long freeMemory = _cacheLimit - _usedCacheMemory;
+        LOG.debug(String.format("Free memory in cache: %d", freeMemory));
+
+        if (entrySize > _cacheLimit) {
+            throw new RuntimeException(String.format("Size of build result exceeds cache limit. Size: %d ", buildResult.getSize()));
+        }
+
+        if (freeMemory < entrySize) {
+            evict(key, buildResult);
+        }
+        _cache.put(key, buildResult);
+        _evictionQueue.add(key);
+        _usedCacheMemory += buildResult.getSize();
+        LOG.debug(String.format("Putting entry with size %d in cache: %s\n", buildResult.getSize(), buildResult));
+    }
+
+    public synchronized EncodeCacheEntry get(EncodeCacheKey key) {
+        try {
+            EncodeCacheEntry entry = _cache.get(key);
+
+            if (entry != null) {
+                LOG.debug(String.format("Getting %s from the cache\n", key));
+
+                switch (_cachePolicy) {
+                    case LRU:
+                        updateQueueLRU(key);
+                        break;
+                }
+                return entry;
+            }
+
+        } catch (Exception e) {
+            LOG.error("Unexpected error occurred: " + e.getMessage(), e);
+        }
+        return null;
+    }
+
+    private synchronized void evict(EncodeCacheKey key, EncodeCacheEntry buildResult) {
         switch (_cachePolicy) {
             case LRU:
                 evictLRU(key, buildResult);
                 break;
-            default:
-                throw new IllegalArgumentException("Unknown cache policy: " + _cachePolicy);
         }
     }
 
-    public synchronized EncodeCacheEntry get(EncodeCacheKey key) {
-        if (_cache.get(key) != null){
-            LOG.debug(String.format("Getting %s from the cache\n", key));
-        }
-
-        switch (_cachePolicy) {
-            case LRU:
-                updateQueueLRU(key);
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown cache policy: " + _cachePolicy);
-        }
-        return _cache.get(key);
-    }
-
-    private synchronized void evictLRU(EncodeCacheKey key, EncodeCacheEntry buildResult){
+    private synchronized void evictLRU(EncodeCacheKey key, EncodeCacheEntry buildResult) {
         long entrySize = buildResult.getSize();
+
         long freeMemory = _cacheLimit - _usedCacheMemory;
 
         while (freeMemory < entrySize) {
-            EncodeCacheKey evictedKey = _evictionQueue.remove();
-            EncodeCacheEntry<Object> evictedEntry = _cache.get(evictedKey);
-            _cache.remove(evictedKey);
-            _usedCacheMemory -= evictedEntry.getSize();
-            freeMemory = _cacheLimit - _usedCacheMemory;
+            try {
+                System.out.println("Evicting using LRU policy..");
+                EncodeCacheKey evictedKey = _evictionQueue.remove();
+                EncodeCacheEntry<Object> evictedEntry = _cache.get(evictedKey);
+                _cache.remove(evictedKey);
+                _usedCacheMemory -= evictedEntry.getSize();
+                LOG.debug(String.format("Used memory: %d", _usedCacheMemory));
+                freeMemory = _cacheLimit - _usedCacheMemory;
+
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
         }
-
-        _cache.put(key, buildResult);
-        _evictionQueue.add(key);
-        _usedCacheMemory += buildResult.getSize();
-
-        LOG.debug(String.format("Putting %s in the cache\n", key));
     }
 
-    private synchronized void updateQueueLRU(EncodeCacheKey key){
+    private synchronized void updateQueueLRU(EncodeCacheKey key) {
         _evictionQueue.remove(key);
         _evictionQueue.add(key);
     }
 
-    protected static long setCacheLimit(double fraction) {
+    public static void setCacheLimit(double fraction) { // this is void now for manipulating cache limit in the test
         long maxMem = InfrastructureAnalyzer.getLocalMaxMemory();
-        long limit = (long)(fraction * maxMem);
-        LOG.debug("Cache limit: "+ limit);
-        return limit;
+        long limit = (long) (fraction * maxMem);
+        LOG.debug("Set cache limit to: " + limit);
+        _cacheLimit = limit;
     }
 
+    // ------ methods used for testing --------
+
+    public static long get_cacheLimit() {
+        return _cacheLimit;
+    }
+
+    public static long get_usedCacheMemory() {
+        return _usedCacheMemory;
+    }
 }

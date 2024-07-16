99d5c01107fa8b62b2c819ed0383b3d211e2aefa
WDRshadow <i@wdr.im>
2024-07-16 02:03:16 +0200

    update: MultiGPUTest (draft)

---
+++ src/test/java/org/apache/sysds/test/gpu/multigpu/MultiGPUTest.java
@@ -0,0 +1,59 @@
+package org.apache.sysds.test.gpu.multigpu;
+
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+import org.junit.Test;
+
+public class MultiGPUTest extends AutomatedTestBase {
+    private static final String TEST_DIR = "gpu/";
+    private static final String TEST_CLASS_DIR = TEST_DIR + MultiGPUTest.class.getSimpleName() +  "/";
+    private static final String SINGLE_GPU_TEST = "SingleGPUTest";
+    private static final String MULTI_GPUS_TEST = "MultiGPUsTest";
+    private static final String TEST_NAME = "GPUTest";
+    private static final String DATA_SET = DATASET_DIR + "MNIST/mnist_test.csv";
+
+    @Override
+    public void setUp() {
+        TEST_GPU = true;
+        VERBOSE_STATS = true;
+        addTestConfiguration(SINGLE_GPU_TEST,
+                new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[]{"R"}));
+        addTestConfiguration(MULTI_GPUS_TEST,
+                new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[]{"R"}));
+        TestConfiguration singleConfig = availableTestConfigurations.get(SINGLE_GPU_TEST);
+        singleConfig.addVariable("sysds.gpu.availableGPUs", 0);
+        TestConfiguration multiConfig = availableTestConfigurations.get(MULTI_GPUS_TEST);
+        multiConfig.addVariable("sysds.gpu.availableGPUs", -1);
+    }
+
+    @Test
+    public void CPUTest() {
+        TEST_GPU = false;
+        runMultiGPUsTest(true);
+    }
+    @Test
+    public void SingleGPUTest() {
+        runMultiGPUsTest(false);
+    }
+
+    @Test
+    public void MultiGPUsTest() {
+        runMultiGPUsTest(true);
+    }
+
+    /**
+     * Run the test with multiple GPUs
+     * @param multiGPUs whether to run the test with multiple GPUs
+     */
+    private void runMultiGPUsTest(boolean multiGPUs) {
+        getAndLoadTestConfiguration(multiGPUs ? MULTI_GPUS_TEST : SINGLE_GPU_TEST);
+
+        String HOME = SCRIPT_DIR + TEST_DIR;
+        fullDMLScriptName = HOME + TEST_NAME + ".dml";
+        programArgs = new String[]{"-args", DATA_SET, output("R") };
+        fullRScriptName = HOME + TEST_NAME + ".R";
+        rCmd = null;
+
+        runTest(true, false, null, -1);
+    }
+}

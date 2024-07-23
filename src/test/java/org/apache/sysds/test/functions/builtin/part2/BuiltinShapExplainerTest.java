abf1b8ae311353f414a169f13b12b60d7cd55233
louislepage <l.lepage@campus.tu-berlin.de>
2024-07-23 17:07:12 +0200

    [SYSTEMDS-3669] add unit tests and component test with dummy data

--- src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinShapExplainerTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinShapExplainerTest.java
@@ -20,7 +20,6 @@
 package org.apache.sysds.test.functions.builtin.part2;
 
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -48,6 +47,51 @@ public class BuiltinShapExplainerTest extends AutomatedTestBase
         runShapExplainerUnitTest("prepare_mask_for_permutation");
     }
 
+    @Test
+    public void testPrepareMaskForPartialPermutation() {
+        runShapExplainerUnitTest("prepare_mask_for_partial_permutation");
+    }
+
+    @Test
+    public void testPrepareMaskForPartitionedPermutation() {
+        runShapExplainerUnitTest("prepare_mask_for_partitioned_permutation");
+    }
+
+    @Test
+    public void testComputeMeansFromPredictions() {
+        runShapExplainerUnitTest("compute_means_from_predictions");
+    }
+
+    @Test
+    public void testComputePhisFromPredictionMeans() {
+        runShapExplainerUnitTest("compute_phis_from_prediction_means");
+    }
+
+    @Test
+    public void testComputePhisFromPredictionMeansNonVars() {
+        runShapExplainerUnitTest("compute_phis_from_prediction_means_non_vars");
+    }
+
+    @Test
+    public void testPrepareFullMask() {
+        runShapExplainerUnitTest("prepare_full_mask");
+    }
+
+    @Test
+    public void testPrepareMaskedXBg() {
+        runShapExplainerUnitTest("prepare_masked_X_bg");
+    }
+
+    @Test
+    public void testPrepareMaskedXBgIndependentPerms() {
+        runShapExplainerUnitTest("prepare_masked_X_bg_independent_perms");
+    }
+
+    @Test
+    public void testApplyFullMask() {
+        runShapExplainerUnitTest("apply_full_mask");
+    }
+
     private void runShapExplainerUnitTest(String testType) {
         ExecMode platformOld = setExecMode(ExecMode.SINGLE_NODE);
         loadTestConfiguration(getTestConfiguration(TEST_NAME));
@@ -67,6 +111,42 @@ public class BuiltinShapExplainerTest extends AutomatedTestBase
 
             TestUtils.compareMatrices(result, result_expected, 1e-3, testType+"_result", testType+"_expected");
 
+        }
+        finally {
+            rtplatform = platformOld;
+        }
+    }
+
+    @Test
+    public void testShapExplainerDummyData(){
+        runShapExplainerComponentTest(false);
+    }
+    //TODO add test with real data
+
+    private void runShapExplainerComponentTest(Boolean useRealData) {
+        ExecMode platformOld = setExecMode(ExecMode.SINGLE_NODE);
+        loadTestConfiguration(getTestConfiguration(TEST_NAME));
+        String HOME = SCRIPT_DIR + TEST_DIR;
+
+        try {
+            loadTestConfiguration(getTestConfiguration(TEST_NAME));
+
+            //execute given unit test
+            fullDMLScriptName = HOME + TEST_NAME + "Component.dml";
+            programArgs = new String[]{"-args", output("R"), output("R_expected")};
+            runTest(true, false, null, -1);
+
+            //compare to expected phis
+            HashMap<CellIndex, Double> result = readDMLMatrixFromOutputDir("R_phis");
+            HashMap<CellIndex, Double> result_expected = readDMLMatrixFromOutputDir("R_expected_phis");
+
+            TestUtils.compareMatrices(result, result_expected, 1e-3, "explainer_result_phis", "explainer_expected_phis");
+
+            //compare to expected value of model
+            HashMap<CellIndex, Double> result_e = readDMLMatrixFromOutputDir("R_e");
+            HashMap<CellIndex, Double> result_expected_e = readDMLMatrixFromOutputDir("R_expected_e");
+
+            TestUtils.compareMatrices(result_e, result_expected_e, 1e-3, "explainer_result_e", "explainer_expected_e");
         }
         finally {
             rtplatform = platformOld;

e35ea845c18459bdf8b2acb1587f647922141e6e
mark-p4 <mark.paranskij@gmail.com>
2024-07-28 17:51:15 +0200

    added approriate licences to new dml and java files

--- src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinImageTransformMatrixTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinImageTransformMatrixTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.sysds.test.functions.builtin.part1;
 
 import org.apache.sysds.test.AutomatedTestBase;
@@ -40,10 +59,12 @@ public class BuiltinImageTransformMatrixTest extends AutomatedTestBase {
     private static final double [][] d7 = new double[][] {{1920, 1080},{3840, 2160}};
     private static final double [][] t8 = new double[][] {{2,0,0},{0,1,0},{0,0,1}};
     private static final double [][] d8 = new double[][] {{3840, 2160},{1980, 1080}};
+    private static final double [][] t9 = new double[][] {{2,0,0},{0,1,0},{0,0,1}};
+    private static final double [][] d9 = new double[][] {{6000, 4000},{6000, 4000}};
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {{t1, d1},{t2, d2},{t3, d3},{t4, d4},{t5, d5},{t6, d6},{t7, d7},{t8, d8}});
+        return Arrays.asList(new Object[][] {{t1, d1},{t2, d2},{t3, d3},{t4, d4},{t5, d5},{t6, d6},{t7, d7},{t8, d8},{t9, d9}});
     }
 
     @Override

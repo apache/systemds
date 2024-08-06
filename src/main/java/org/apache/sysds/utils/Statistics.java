d85363eb062f62aec4bb7b4b58e06e68c3ab0ee1
Jaybit0 <jannik.lindemann3@gmail.com>
2024-08-06 11:05:18 +0200

    Minor bugfix to prevent issues with multithreading

--- src/main/java/org/apache/sysds/utils/Statistics.java
+++ src/main/java/org/apache/sysds/utils/Statistics.java
@@ -335,6 +335,8 @@ public class Statistics
 		FederatedStatistics.reset();
 
 		_instStatsNGram.clear();
+		_instStatsLineageTracker.clear();
+		_instStats.clear();
 	}
 
 	public static void resetJITCompileTime(){
@@ -449,21 +451,21 @@ public class Statistics
 		return item == null ? Optional.empty() : Optional.of(item);
 	}
 
-	public static void clearNGramRecording() {
+	public static synchronized void clearNGramRecording() {
 		NGramBuilder<String, NGramStats>[] bl = _instStatsNGram.get(Thread.currentThread().getName());
 		for (NGramBuilder<String, NGramStats> b : bl)
 			b.clearCurrentRecording();
 	}
 
-	public static void extendLineageItem(LineageItem li, LineageNGramExtension ext) {
+	public static synchronized void extendLineageItem(LineageItem li, LineageNGramExtension ext) {
 		_lineageExtensions.put(li, ext);
 	}
 
-	public static LineageNGramExtension getExtendedLineage(LineageItem li) {
+	public static synchronized LineageNGramExtension getExtendedLineage(LineageItem li) {
 		return _lineageExtensions.get(li);
 	}
 
-	public static void maintainNGramsFromLineage(LineageItem li) {
+	public static synchronized void maintainNGramsFromLineage(LineageItem li) {
 		NGramBuilder<String, NGramStats>[] tmp = _instStatsNGram.computeIfAbsent(Thread.currentThread().getName(), k -> {
 			NGramBuilder<String, NGramStats>[] threadEntry = new NGramBuilder[DMLScript.STATISTICS_NGRAM_SIZES.length];
 			for (int i = 0; i < threadEntry.length; i++) {
@@ -472,26 +474,6 @@ public class Statistics
 			return threadEntry;
 		});
 		addLineagePaths(li, new ArrayList<>(), new ArrayList<>(), tmp);
-	}
-
-	/**
-	 * Count the number of paths from the current lineage item with a minimum size
-	 * @param li the root item
-	 * @param size the minimum size of the path
-	 * @return
-	 */
-	private static int countLineageLinesOfSize(LineageItem li, int size) {
-		if (li.getInputs() == null)
-			return 0;
-
-		if (size == 1)
-			return li.getInputs().length;
-
-		int lines = 0;
-		for (LineageItem in : li.getInputs())
-			lines += countLineageLinesOfSize(in, size-1);
-
-		return lines;
 	}
 
 	/**

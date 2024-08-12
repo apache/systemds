b2fa20d6b304b0242855d54ac80e9fa56ec98bef
Rene Enjilian <enjilianrene@gmail.com>
2024-08-12 21:43:28 +0200

    added testing for simplifyConstantSort in static rewrites

--- src/test/java/org/apache/sysds/test/functions/rewrite/RewriteSimplifyConstantSortTest.java
+++ src/test/java/org/apache/sysds/test/functions/rewrite/RewriteSimplifyConstantSortTest.java
@@ -19,7 +19,83 @@
 
 package org.apache.sysds.test.functions.rewrite;
 
+import org.apache.sysds.hops.OptimizerUtils;
+import org.apache.sysds.runtime.matrix.data.MatrixValue;
 import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+import org.apache.sysds.test.TestUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
 
 public class RewriteSimplifyConstantSortTest extends AutomatedTestBase {
+
+	private static final String TEST_NAME = "RewriteSimplifyConstantSort";
+	private static final String TEST_DIR = "functions/rewrite/";
+	private static final String TEST_CLASS_DIR = TEST_DIR + RewriteSimplifyConstantSortTest.class.getSimpleName() + "/";
+	private static final double eps = Math.pow(10, -10);
+
+	@Override
+	public void setUp() {
+		TestUtils.clearAssertionInformation();
+		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {"R"}));
+	}
+
+	@Test
+	public void testConstantSortDataNoRewrite() {
+		testRewriteSimplifyConstantSort(1, false);
+	}
+
+	@Test
+	public void testConstantSortDataRewrite() {		//pattern: order(matrix(7), indexreturn=FALSE) -> matrix(7)
+		testRewriteSimplifyConstantSort(1, true);
+	}
+
+	@Test
+	public void testConstantSortIndexNoRewrite() {
+		testRewriteSimplifyConstantSort(2, false);
+	}
+
+	@Test
+	public void testConstantSortIndexRewrite() {	 //pattern: order(matrix(7), indexreturn=TRUE) -> seq(1,nrow(X),1)
+		testRewriteSimplifyConstantSort(2, true);
+	}
+
+	private void testRewriteSimplifyConstantSort(int ID, boolean rewrites) {
+		boolean oldFlag = OptimizerUtils.ALLOW_ALGEBRAIC_SIMPLIFICATION;
+		try {
+			TestConfiguration config = getTestConfiguration(TEST_NAME);
+			loadTestConfiguration(config);
+
+			String HOME = SCRIPT_DIR + TEST_DIR;
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[] {"-stats", "-args", String.valueOf(ID), output("R")};
+			fullRScriptName = HOME + TEST_NAME + ".R";
+			rCmd = getRCmd(String.valueOf(ID), expectedDir());
+
+			OptimizerUtils.ALLOW_ALGEBRAIC_SIMPLIFICATION = rewrites;
+
+			runTest(true, false, null, -1);
+			runRScript(true);
+
+			//compare matrices
+			HashMap<MatrixValue.CellIndex, Double> dmlfile = readDMLMatrixFromOutputDir("R");
+			HashMap<MatrixValue.CellIndex, Double> rfile = readRMatrixFromExpectedDir("R");
+			TestUtils.compareMatrices(dmlfile, rfile, eps, "Stat-DML", "Stat-R");
+
+			if(rewrites) {
+				Assert.assertFalse(heavyHittersContainsString("rsort"));
+				if(ID == 2)
+					Assert.assertTrue(heavyHittersContainsString("seq"));
+			}
+			else
+				Assert.assertTrue(heavyHittersContainsString("rsort"));
+
+		}
+		finally {
+			OptimizerUtils.ALLOW_ALGEBRAIC_SIMPLIFICATION = oldFlag;
+		}
+
+	}
 }

ec1bac3391039323cd062aa393eb7a9bbcde9361
Rene Enjilian <enjilianrene@gmail.com>
2024-08-15 20:47:56 +0200

    added testing for simplifyLowerTriExtraction  in dynamic rewrites

--- src/test/scripts/functions/rewrite/RewriteSimplifyLowerTriExtraction.R
+++ src/test/scripts/functions/rewrite/RewriteSimplifyLowerTriExtraction.R
@@ -19,12 +19,31 @@
 #
 #-------------------------------------------------------------
 
+# Read command line arguments
+args <- commandArgs(TRUE)
 
-# Load matrix X
-X = read($1)
+# Set options for numeric precision
+options(digits=22)
 
-# Perform the operation
-R = sum(diag(X))
+# Load required libraries
+library("Matrix")
+library("matrixStats")
+
+# Read matrix X from Matrix Market format files
+X = as.matrix(readMM(paste(args[1], "X.mtx", sep="")))
+
+# Create a column vector of ones with the same number of rows as X
+ones_vector = matrix(1, nrow(X), 1)
+
+# Create a diagonal matrix from the ones_vector
+diag_matrix = diag(as.vector(ones_vector))
+
+# Compute the column-wise cumulative sum (matching DML behavior)
+cumsum_matrix = apply(diag_matrix, 2, cumsum)
+
+# Perform the element-wise multiplication of X with the cumsum_matrix
+R = X * cumsum_matrix
 
 # Write the result R
-write(R, $2)
+writeMM(as(R, "CsparseMatrix"), paste(args[2], "R", sep=""))
+

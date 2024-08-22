6b35eb332255a004b86f7cac871d4e939a6779dc
e-strauss <lathancer@gmx.de>
2024-08-22 10:06:10 +0200

    This commit updates the GEMM strategy for evaluating decision tree to the new decision tree layout. Additionally, this patch adds three new test cases for decision tree predict. I kept for now the "<=" [instead of "<"] because the DecisionTreeRealDataTest was failing, and added a comment in the code.

--- src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinDecisionTreePredictTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinDecisionTreePredictTest.java
@@ -43,28 +43,58 @@ public class BuiltinDecisionTreePredictTest extends AutomatedTestBase {
 	}
 
 	@Test
-	public void testDecisionTreeTTPredictDefaultCP() {
-		runDecisionTreePredict(true, ExecType.CP, "TT");
+	public void testDecisionTreeTTPredictDefaultCP1() {
+		runDecisionTreePredict(true, ExecType.CP, "TT", 1);
 	}
+
+	@Test
+	public void testDecisionTreeTTPredictDefaultCP2() {
+		runDecisionTreePredict(true, ExecType.CP, "TT", 2);
+	}
+
+	@Test
+	public void testDecisionTreeTTPredictDefaultCP3() {
+		runDecisionTreePredict(true, ExecType.CP, "TT", 3);
+	}
+
+	@Test
+	public void testDecisionTreeTTPredictDefaultCP4() {
+		runDecisionTreePredict(true, ExecType.CP, "TT", 4);
+	}
+
 
 	@Test
 	public void testDecisionTreeTTPredictSP() {
-		runDecisionTreePredict(true, ExecType.SPARK, "TT");
+		runDecisionTreePredict(true, ExecType.SPARK, "TT", 1);
 	}
 	
 	@Test
-	@Ignore
-	public void testDecisionTreeGEMMPredictDefaultCP() {
-		runDecisionTreePredict(true, ExecType.CP, "GEMM");
+	public void testDecisionTreeGEMMPredictDefaultCP1() {
+		runDecisionTreePredict(true, ExecType.CP, "GEMM", 1);
 	}
 
 	@Test
-	@Ignore
-	public void testDecisionTreeGEMMPredictSP() {
-		runDecisionTreePredict(true, ExecType.SPARK, "GEMM");
+	public void testDecisionTreeGEMMPredictDefaultCP2() {
+		runDecisionTreePredict(true, ExecType.CP, "GEMM", 2);
 	}
 
-	private void runDecisionTreePredict(boolean defaultProb, ExecType instType, String strategy) {
+	@Test
+	public void testDecisionTreeGEMMPredictDefaultCP3() {
+		runDecisionTreePredict(true, ExecType.CP, "GEMM", 3);
+	}
+
+	@Test
+	public void testDecisionTreeGEMMPredictDefaultCP4() {
+		runDecisionTreePredict(true, ExecType.CP, "GEMM", 4);
+	}
+
+
+	@Test
+	public void testDecisionTreeGEMMPredictSP() {
+		runDecisionTreePredict(true, ExecType.SPARK, "GEMM", 1);
+	}
+
+	private void runDecisionTreePredict(boolean defaultProb, ExecType instType, String strategy, int test_case) {
 		Types.ExecMode platformOld = setExecMode(instType);
 		try {
 			loadTestConfiguration(getTestConfiguration(TEST_NAME));
@@ -74,20 +104,102 @@ public class BuiltinDecisionTreePredictTest extends AutomatedTestBase {
 			programArgs = new String[] {"-args", input("M"), input("X"), strategy, output("Y")};
 
 			//data and model consistent with decision tree test
-			double[][] X = {
-					{3, 1, 2, 1, 5}, 
-					{2, 1, 2, 2, 4}, 
-					{1, 1, 1, 3, 3},
-					{4, 2, 1, 4, 2}, 
-					{2, 2, 1, 5, 1},};
-			double[][] M = {{1.0, 2.0, 0.0, 1.0, 0.0, 2.0}};
-			
+			double[][] X = null;
+			double[][] M = null;
+
 			HashMap<MatrixValue.CellIndex, Double> expected_Y = new HashMap<>();
-			expected_Y.put(new MatrixValue.CellIndex(1, 1), 2.0);
-			expected_Y.put(new MatrixValue.CellIndex(2, 1), 1.0);
-			expected_Y.put(new MatrixValue.CellIndex(3, 1), 1.0);
-			expected_Y.put(new MatrixValue.CellIndex(4, 1), 2.0);
-			expected_Y.put(new MatrixValue.CellIndex(5, 1), 1.0);
+			switch(test_case){
+				case 1:
+					double[][] X1 = {
+							{3, 1, 2, 1, 5},
+							{2, 1, 2, 2, 4},
+							{1, 1, 1, 3, 3},
+							{4, 2, 1, 4, 2},
+							{2, 2, 1, 5, 1},};
+					double[][] M1 = {{1.0, 2.0, 0.0, 1.0, 0.0, 2.0}};
+
+					expected_Y.put(new MatrixValue.CellIndex(1, 1), 2.0);
+					expected_Y.put(new MatrixValue.CellIndex(2, 1), 1.0);
+					expected_Y.put(new MatrixValue.CellIndex(3, 1), 1.0);
+					expected_Y.put(new MatrixValue.CellIndex(4, 1), 2.0);
+					expected_Y.put(new MatrixValue.CellIndex(5, 1), 1.0);
+					X = X1;
+					M = M1;
+					break;
+				case 2:
+					double[][] X2 = {
+							{3, 1, 2, 1},
+							{2, 1, 2, 6},
+							{1, 1, 1, 3},
+							{9, 2, 1, 7},
+							{2, 2, 1, 1},};
+					double[][] M2 = {{4, 5, 0, 2, 1, 7, 0, 0, 0, 0, 0, 2, 0, 1}};
+
+					expected_Y.put(new MatrixValue.CellIndex(1, 1), 2.0);
+					expected_Y.put(new MatrixValue.CellIndex(2, 1), 2.0);
+					expected_Y.put(new MatrixValue.CellIndex(3, 1), 2.0);
+					expected_Y.put(new MatrixValue.CellIndex(4, 1), 1.0);
+					expected_Y.put(new MatrixValue.CellIndex(5, 1), 2.0);
+					X = X2;
+					M = M2;
+					break;
+				case 3:
+					double[][] X3 = {
+							{1, 1, 1},
+							{1, 1, 7,},
+							{1, 5, 1},
+							{1, 5, 7,},};
+					double[][] M3 = {{1, 5, 2, 4, 2, 4, 3, 6, 3, 6, 3, 6, 3, 6, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8}};
+
+					expected_Y.put(new MatrixValue.CellIndex(1, 1), 1.0);
+					expected_Y.put(new MatrixValue.CellIndex(2, 1), 2.0);
+					expected_Y.put(new MatrixValue.CellIndex(3, 1), 3.0);
+					expected_Y.put(new MatrixValue.CellIndex(4, 1), 4.0);
+					X = X3;
+					M = M3;
+					break;
+				case 4:
+					double[][] X4 = {
+							{1, 1, 1, 1},
+							{4, 1, 1, 1},
+							{1, 1, 7, 1},
+							{4, 1, 7, 1},
+							{1, 5, 1, 1},
+							{4, 5, 1, 1},
+							{1, 5, 7, 1},
+							{4, 5, 7, 1},
+							{1, 1, 1, 6},
+							{4, 1, 1, 6},
+							{1, 1, 7, 6},
+							{4, 1, 7, 6},
+							{1, 5, 1, 6},
+							{4, 5, 1, 6},
+							{1, 5, 7, 6},
+							{4, 5, 7, 6},};
+					double[][] M4 = {{4, 5, 2, 4, 2, 4, 3, 6, 3, 6, 3, 6, 3, 6, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1,
+							3, 1, 3, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10, 0, 11, 0, 12, 0, 13,
+							0, 14, 0, 15, 0, 16}};
+
+					expected_Y.put(new MatrixValue.CellIndex(1, 1), 1.0);
+					expected_Y.put(new MatrixValue.CellIndex(2, 1), 2.0);
+					expected_Y.put(new MatrixValue.CellIndex(3, 1), 3.0);
+					expected_Y.put(new MatrixValue.CellIndex(4, 1), 4.0);
+					expected_Y.put(new MatrixValue.CellIndex(5, 1), 5.0);
+					expected_Y.put(new MatrixValue.CellIndex(6, 1), 6.0);
+					expected_Y.put(new MatrixValue.CellIndex(7, 1), 7.0);
+					expected_Y.put(new MatrixValue.CellIndex(8, 1), 8.0);
+					expected_Y.put(new MatrixValue.CellIndex(9, 1), 9.0);
+					expected_Y.put(new MatrixValue.CellIndex(10, 1), 10.0);
+					expected_Y.put(new MatrixValue.CellIndex(11, 1), 11.0);
+					expected_Y.put(new MatrixValue.CellIndex(12, 1), 12.0);
+					expected_Y.put(new MatrixValue.CellIndex(13, 1), 13.0);
+					expected_Y.put(new MatrixValue.CellIndex(14, 1), 14.0);
+					expected_Y.put(new MatrixValue.CellIndex(15, 1), 15.0);
+					expected_Y.put(new MatrixValue.CellIndex(16, 1), 16.0);
+					X = X4;
+					M = M4;
+					break;
+			}
 
 			writeInputMatrixWithMTD("M", M, true);
 			writeInputMatrixWithMTD("X", X, true);

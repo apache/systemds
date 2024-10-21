c8c704ecefb817f891fb2b85f85a365600b91fbd
Sebastian Baunsgaard <baunsgaard@apache.org>
2024-10-21 18:26:27 +0200

    less parallelizm

--- src/main/java/org/apache/sysds/runtime/io/FrameReaderTextCSVParallel.java
+++ src/main/java/org/apache/sysds/runtime/io/FrameReaderTextCSVParallel.java
@@ -57,7 +57,7 @@ public class FrameReaderTextCSVParallel extends FrameReaderTextCSV
 		throws IOException
 	{
 		Timing time = new Timing(true);
-		final int numThreads = InfrastructureAnalyzer.getLocalParallelism();
+		final int numThreads = InfrastructureAnalyzer.getLocalParallelism() / 2;
 		//  OptimizerUtils.getParallelTextReadParallelism();
 		
 		TextInputFormat informat = new TextInputFormat();

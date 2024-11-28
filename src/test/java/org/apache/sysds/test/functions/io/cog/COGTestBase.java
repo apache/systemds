5bbddb4dc8242b7e8cc2f9212e40347adc6b9a3e
fietenoer <fietenoer@users.noreply.github.com>
2024-11-28 15:04:08 +0100

    Added first COG test structure (always true, passing) without actual example

---
+++ src/test/java/org/apache/sysds/test/functions/io/cog/COGTestBase.java
@@ -0,0 +1,22 @@
+package org.apache.sysds.test.functions.io.cog;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+
+public abstract class COGTestBase extends AutomatedTestBase {
+    protected final static String TEST_DIR = "functions/io/cog/";
+    protected static final Log LOG = LogFactory.getLog(COGTestBase.class.getName());
+    protected final static double eps = 1e-9;
+
+    protected abstract String getTestClassDir();
+
+    protected abstract String getTestName();
+
+    @Override
+    public void setUp() {
+        addTestConfiguration(getTestName(),
+                new TestConfiguration(getTestClassDir(), getTestName(), new String[] {"Rout"}));
+    }
+}
---
+++ src/test/java/org/apache/sysds/test/functions/io/cog/ReadCOGTest1.java
@@ -0,0 +1,18 @@
+package org.apache.sysds.test.functions.io.cog;
+
+public class ReadCOGTest1 extends ReadCOGTest {
+    private final static String TEST_NAME = "ReadCOGTest";
+    public final static String TEST_CLASS_DIR = TEST_DIR + ReadCOGTest1.class.getSimpleName() + "/";
+
+    protected String getTestName() {
+        return TEST_NAME;
+    }
+
+    protected String getTestClassDir() {
+        return TEST_CLASS_DIR;
+    }
+
+    protected int getId() {
+        return 1;
+    }
+}

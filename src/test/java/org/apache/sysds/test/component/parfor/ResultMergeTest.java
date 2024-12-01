58490d3acf70baab18db2c37b083f589a08288eb
Matthias Boehm <mboehm7@gmail.com>
2024-12-01 17:46:18 +0100

    [SYSTEMDS-3799] Fix parfor result merge (all combinations)
    This patch fixes recently discovered (see code coverage) issues of
    parfor result merge for combinations of different result merge
    implementations, dense/sparse inputs, with compare dense/sparse blocks,
    and most importantly += accumulation into the output.

--- src/test/java/org/apache/sysds/test/component/parfor/ResultMergeTest.java
+++ src/test/java/org/apache/sysds/test/component/parfor/ResultMergeTest.java
@@ -30,7 +30,9 @@ import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
 import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
 import org.apache.sysds.runtime.controlprogram.context.ExecutionContextFactory;
 import org.apache.sysds.runtime.controlprogram.parfor.ResultMerge;
+import org.apache.sysds.runtime.instructions.InstructionUtils;
 import org.apache.sysds.runtime.matrix.data.MatrixBlock;
+import org.apache.sysds.runtime.matrix.operators.BinaryOperator;
 import org.apache.sysds.runtime.meta.MetaDataFormat;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
@@ -48,38 +50,43 @@ public class ResultMergeTest extends AutomatedTestBase{
 	}
 	
 	@Test
-	public void testLocalMem() {
-		testResultMergeAll(PResultMerge.LOCAL_MEM);
+	public void testLocalMemDenseCompare() {
+		testResultMergeAll(PResultMerge.LOCAL_MEM, false);
 	}
 	
 	@Test
-	public void testLocalFile() {
-		testResultMergeAll(PResultMerge.LOCAL_FILE);
+	public void testLocalFileDenseCompare() {
+		testResultMergeAll(PResultMerge.LOCAL_FILE, false);
 	}
 	
 	@Test
-	public void testLocalAutomatic() {
-		testResultMergeAll(PResultMerge.LOCAL_AUTOMATIC);
+	public void testLocalAutomaticDenseCompare() {
+		testResultMergeAll(PResultMerge.LOCAL_AUTOMATIC, false);
 	}
 	
-	private void testResultMergeAll(PResultMerge mtype) {
-		testResultMerge(false, false, false, false, mtype);
-		testResultMerge(false, true, false, false, mtype);
-		testResultMerge(true, false, false, false, mtype);
-		testResultMerge(false, false, true, false, mtype);
-		if( mtype != PResultMerge.LOCAL_FILE ) //FIXME
-			testResultMerge(false, true, true, false, mtype);
-		testResultMerge(true, false, true, false, mtype);
+	@Test
+	public void testLocalMemSparseCompare() {
+		testResultMergeAll(PResultMerge.LOCAL_MEM, true);
+	}
+	
+	@Test
+	public void testLocalFileSparseCompare() {
+		testResultMergeAll(PResultMerge.LOCAL_FILE, true);
+	}
+	
+	@Test
+	public void testLocalAutomaticSparseCompare() {
+		testResultMergeAll(PResultMerge.LOCAL_AUTOMATIC, true);
+	}
+	
+	private void testResultMergeAll(PResultMerge mtype, boolean sparseCompare) {
+		testResultMerge(false, false, false, sparseCompare, mtype);
+		testResultMerge(false, true, false, sparseCompare, mtype);
+		testResultMerge(true, false, false, sparseCompare, mtype);
+		testResultMerge(false, false, true, sparseCompare, mtype);
+		testResultMerge(false, true, true, sparseCompare, mtype);
+		testResultMerge(true, false, true, sparseCompare, mtype);
 		//testResultMerge(true, true, false, false, mtype); invalid
-	
-		/* FIXME sparse compare
-		testResultMerge(false, false, false, true, mtype);
-		testResultMerge(false, true, false, true, mtype);
-		testResultMerge(true, false, false, true, mtype);
-		testResultMerge(false, false, true, true, mtype);
-		testResultMerge(false, true, true, true, mtype);
-		testResultMerge(true, false, true, true, mtype);
-		*/
 	}
 	
 	private void testResultMerge(boolean par, boolean accum, boolean compare, boolean sparseCompare, PResultMerge mtype) {
@@ -87,19 +94,29 @@ public class ResultMergeTest extends AutomatedTestBase{
 			loadTestConfiguration(getTestConfiguration(TEST_NAME));
 	
 			//create input and output objects
-			MatrixBlock A = MatrixBlock.randOperations(1200, 1100, 0.1);
-			CacheableData<?> Cobj = compare ?
-				toMatrixObject(new MatrixBlock(1200,1100,1d), output("C")) :
-				toMatrixObject(new MatrixBlock(1200,1100,true), output("C"));
+			int rows = 1200, cols = 1100;
+			MatrixBlock A = MatrixBlock.randOperations(rows,cols,0.1,0,1,"uniform",7);
+			A.checkSparseRows();
 			MatrixBlock rest = compare ? 
-				new MatrixBlock(400,1100,sparseCompare?0.2:1.0) : //constant
-				new MatrixBlock(400,1100,true); //empty (also sparse)
-			MatrixObject[] Bobj = new MatrixObject[4];
-			Bobj[0] = toMatrixObject(A.slice(0,399).rbind(rest).rbind(rest), output("B0"));
-			Bobj[1] = toMatrixObject(rest.rbind(A.slice(400,799)).rbind(rest), output("B1"));
-			Bobj[2] = toMatrixObject(rest.rbind(rest).rbind(A.slice(800,1199)), output("B2"));
-			Bobj[3] = toMatrixObject(rest.rbind(rest).rbind(rest), output("B3"));
-	
+				MatrixBlock.randOperations(rows/3,cols,sparseCompare?0.2:1.0,1,1,"uniform",3): //constant
+				new MatrixBlock(rows/3,cols,true); //empty (also sparse)
+			CacheableData<?> Cobj = compare ?
+				toMatrixObject(rest.rbind(rest).rbind(rest), output("C")) :
+				toMatrixObject(new MatrixBlock(rows,cols,true), output("C"));
+			MatrixObject[] Bobj = new MatrixObject[3];
+			Bobj[0] = toMatrixObject(A.slice(0,rows/3-1).rbind(rest).rbind(rest), output("B0"));
+			Bobj[1] = toMatrixObject(rest.rbind(A.slice(rows/3,2*rows/3-1)).rbind(rest), output("B1"));
+			Bobj[2] = toMatrixObject(rest.rbind(rest).rbind(A.slice(2*rows/3,rows-1)), output("B2"));
+			BinaryOperator PLUS = InstructionUtils.parseBinaryOperator("+");
+			BinaryOperator MINUS = InstructionUtils.parseBinaryOperator("-");
+			MatrixBlock aggAll = ((MatrixBlock)Cobj.acquireReadAndRelease())
+				.binaryOperations(PLUS, Bobj[0].acquireReadAndRelease())
+				.binaryOperations(PLUS, Bobj[1].acquireReadAndRelease())
+				.binaryOperations(PLUS, Bobj[2].acquireReadAndRelease())
+				.binaryOperations(MINUS, (MatrixBlock)Cobj.acquireReadAndRelease())
+				.binaryOperations(MINUS, (MatrixBlock)Cobj.acquireReadAndRelease())
+				.binaryOperations(MINUS, (MatrixBlock)Cobj.acquireReadAndRelease());
+			
 			//create result merge
 			ExecutionContext ec = ExecutionContextFactory.createContext();
 			int numThreads = 4;
@@ -113,8 +130,11 @@ public class ResultMergeTest extends AutomatedTestBase{
 				Cobj = rm.executeSerialMerge();
 			
 			//check results
-			TestUtils.compareMatrices(A, 
-				(MatrixBlock)Cobj.acquireReadAndRelease(), 1e-14);
+			MatrixBlock out = (MatrixBlock)Cobj.acquireReadAndRelease();
+			if(!accum)
+				TestUtils.compareMatrices(A, out, 1e-14);
+			else
+				TestUtils.compareMatrices(aggAll, out, 1e-14);
 		}
 		catch(Exception e){
 			e.printStackTrace();

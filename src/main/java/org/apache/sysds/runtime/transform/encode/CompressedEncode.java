4758bce650228da734a439063c2cc375ceedef07
Sebastian Baunsgaard <baunsgaard@apache.org>
2025-01-14 21:06:22 +0100

    only add nnz if combined

--- src/main/java/org/apache/sysds/runtime/transform/encode/CompressedEncode.java
+++ src/main/java/org/apache/sysds/runtime/transform/encode/CompressedEncode.java
@@ -401,7 +401,7 @@ public class CompressedEncode {
 
 		if(a.getValueType() != ValueType.BOOLEAN // if not booleans
 			&& (stats == null || !stats.shouldCompress || stats.valueType != a.getValueType())) {
-			return new ColGroupUncompressedArray(a, c._colID - 1,colIndexes);
+			return new ColGroupUncompressedArray(a, c._colID - 1, colIndexes);
 		}
 		else {
 			boolean containsNull = a.containsNull();
@@ -616,9 +616,10 @@ public class CompressedEncode {
 			else
 				ret.add(g);
 		}
-		if(ucg.size() > 0)
+		if(ucg.size() > 0){
 			ret.add(combine(ucg));
-		nnz.addAndGet(ret.get(ret.size()-1).getNumberNonZeros(in.getNumRows()));
+			nnz.addAndGet(ret.get(ret.size()-1).getNumberNonZeros(in.getNumRows()));
+		}
 		mb.allocateColGroupList(ret);
 	}
 

4cc40fe737b564cf232ccf83008eb28fbd40060f
Sebastian Baunsgaard <baunsgaard@apache.org>
2025-02-03 15:28:37 +0100

    [MINOR] CLA Factory logging cleanup

--- src/main/java/org/apache/sysds/runtime/compress/CompressedMatrixBlockFactory.java
+++ src/main/java/org/apache/sysds/runtime/compress/CompressedMatrixBlockFactory.java
@@ -37,6 +37,7 @@ import org.apache.sysds.runtime.compress.colgroup.ColGroupEmpty;
 import org.apache.sysds.runtime.compress.colgroup.ColGroupFactory;
 import org.apache.sysds.runtime.compress.colgroup.ColGroupUncompressed;
 import org.apache.sysds.runtime.compress.cost.ACostEstimate;
+import org.apache.sysds.runtime.compress.cost.ComputationCostEstimator;
 import org.apache.sysds.runtime.compress.cost.CostEstimatorBuilder;
 import org.apache.sysds.runtime.compress.cost.CostEstimatorFactory;
 import org.apache.sysds.runtime.compress.cost.InstructionTypeCounter;
@@ -159,7 +160,7 @@ public class CompressedMatrixBlockFactory {
 		return compress(mb, k, compSettings, (WTreeRoot) null);
 	}
 
-	public static Future<Void>  compressAsync(ExecutionContext ec, String varName) {
+	public static Future<Void> compressAsync(ExecutionContext ec, String varName) {
 		return compressAsync(ec, varName, null);
 	}
 
@@ -168,7 +169,7 @@ public class CompressedMatrixBlockFactory {
 		final ExecutorService pool = CommonThreadPool.get(); // We have to guarantee that a thread pool is allocated.
 		return CompletableFuture.runAsync(() -> {
 			// method call or code to be async
-			try{
+			try {
 				CacheableData<?> data = ec.getCacheableData(varName);
 				if(data instanceof MatrixObject) {
 					MatrixObject mo = (MatrixObject) data;
@@ -178,10 +179,11 @@ public class CompressedMatrixBlockFactory {
 						ExecutionContext.createCacheableData(mb);
 						mo.acquireModify(mbc);
 						mo.release();
+						mbc.sum(); // calculate sum to forcefully materialize counts
 					}
 				}
 			}
-			finally{
+			finally {
 				pool.shutdown();
 			}
 		}, pool);
@@ -288,11 +290,16 @@ public class CompressedMatrixBlockFactory {
 		_stats.originalSize = mb.getInMemorySize();
 		_stats.originalCost = costEstimator.getCost(mb);
 
+		final double orgSum;
+		if(CompressedMatrixBlock.debug)
+			orgSum = mb.sum(k).getDouble(0, 0);
+		else
+			orgSum = 0;
 		if(mb.isEmpty()) // empty input return empty compression
 			return createEmpty();
 
 		res = new CompressedMatrixBlock(mb); // copy metadata and allocate soft reference
-
+		logInit();
 		classifyPhase();
 		if(compressionGroups == null)
 			return abortCompression();
@@ -307,6 +314,12 @@ public class CompressedMatrixBlockFactory {
 
 		if(res == null)
 			return abortCompression();
+
+		if(CompressedMatrixBlock.debug) {
+			final double afterComp = mb.sum(k).getDouble(0, 0);
+			final double deltaSum = Math.abs(orgSum - afterComp);
+			LOG.debug("compression Sum: Before:" + orgSum + " after: " + afterComp + " |delta|: " + deltaSum);
+		}
 
 		return new ImmutablePair<>(res, _stats);
 	}
@@ -334,7 +347,8 @@ public class CompressedMatrixBlockFactory {
 		final double scale = Math.sqrt(nCols);
 		final double threshold = _stats.estimatedCostCols / scale;
 
-		if(threshold < _stats.originalCost) {
+		if(threshold < _stats.originalCost *
+			((costEstimator instanceof ComputationCostEstimator) && !(mb instanceof CompressedMatrixBlock) ? 15 : 0.8)) {
 			if(nCols > 1)
 				coCodePhase();
 			else // LOG a short cocode phase (since there is one column we don't cocode)
@@ -406,7 +420,7 @@ public class CompressedMatrixBlockFactory {
 				compSettings.transposed = false;
 				break;
 			default:
-				compSettings.transposed = transposeHeuristics(compressionGroups.getNumberColGroups() , mb);
+				compSettings.transposed = transposeHeuristics(compressionGroups.getNumberColGroups(), mb);
 		}
 	}
 
@@ -442,20 +456,20 @@ public class CompressedMatrixBlockFactory {
 
 		_stats.compressedSize = res.getInMemorySize();
 		_stats.compressedCost = costEstimator.getCost(res.getColGroups(), res.getNumRows());
-
-		final double ratio = _stats.getRatio();
-		final double denseRatio = _stats.getDenseRatio();
-
 		_stats.setColGroupsCounts(res.getColGroups());
-		if(ratio < 1 && denseRatio < 100.0) {
+
+		if(_stats.compressedCost > _stats.originalCost) {
 			LOG.info("--dense size:        " + _stats.denseSize);
 			LOG.info("--original size:     " + _stats.originalSize);
 			LOG.info("--compressed size:   " + _stats.compressedSize);
-			LOG.info("--compression ratio: " + ratio);
+			LOG.info("--compression ratio: " + _stats.getRatio());
+			LOG.info("--original Cost:     " + _stats.originalCost);
+			LOG.info("--Compressed Cost:   " + _stats.compressedCost);
+			LOG.info("--Cost Ratio:        " + _stats.getCostRatio());
 			LOG.debug("--col groups types   " + _stats.getGroupsTypesString());
 			LOG.debug("--col groups sizes   " + _stats.getGroupsSizesString());
 			logLengths();
-			LOG.info("Abort block compression because compression ratio is less than 1.");
+			LOG.info("Abort block compression because cost ratio is less than 1. ");
 			res = null;
 			setNextTimePhase(time.stop());
 			DMLCompressionStatistics.addCompressionTime(getLastTimePhase(), phase);
@@ -472,7 +486,21 @@ public class CompressedMatrixBlockFactory {
 
 	private Pair<MatrixBlock, CompressionStatistics> abortCompression() {
 		LOG.warn("Compression aborted at phase: " + phase);
+		if(mb instanceof CompressedMatrixBlock) {
+			MatrixBlock ucmb = ((CompressedMatrixBlock) mb).getUncompressed("Decompressing for abort: ", k);
+			return new ImmutablePair<>(ucmb, _stats);
+		}
 		return new ImmutablePair<>(mb, _stats);
+	}
+
+	private void logInit() {
+		if(LOG.isDebugEnabled()) {
+			LOG.debug("--Seed used for comp : " + compSettings.seed);
+			LOG.debug(String.format("--number columns to compress: %10d", mb.getNumColumns()));
+			LOG.debug(String.format("--number rows to compress   : %10d", mb.getNumRows()));
+			LOG.debug(String.format("--sparsity                  : %10.5f", mb.getSparsity()));
+			LOG.debug(String.format("--nonZeros                  : %10d", mb.getNonZeros()));
+		}
 	}
 
 	private void logPhase() {
@@ -486,7 +514,6 @@ public class CompressedMatrixBlockFactory {
 			else {
 				switch(phase) {
 					case 0:
-						LOG.debug("--Seed used for comp : " + compSettings.seed);
 						LOG.debug("--compression phase " + phase + " Classify  : " + getLastTimePhase());
 						LOG.debug("--Individual Columns Estimated Compression: " + _stats.estimatedSizeCols);
 						if(mb instanceof CompressedMatrixBlock) {

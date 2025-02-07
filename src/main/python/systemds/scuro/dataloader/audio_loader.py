4dd5ab3e2585baef339a870f181a4bba1ff8e9dd
Christina Dionysio <dionysio@tu-berlin.de>
2025-02-07 18:03:03 +0100

    [SYSTEMDS-3830] Add join operator to Scuro
    This patch adds a new join operator to Scuro. The join operation takes
    two modalities as well as a join condition as input and joins the two
    modalities on their common dimension (temporal for now). This includes
    two new modalities and the ability to apply new representations on top
    of a joined modality. In the future the join operator will also serve
    as a simple alignment operator by joining two modalities by a given
    offset.
    
    Closes #2220

--- src/main/python/systemds/scuro/dataloader/audio_loader.py
+++ src/main/python/systemds/scuro/dataloader/audio_loader.py
@@ -18,10 +18,11 @@
 # under the License.
 #
 # -------------------------------------------------------------
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import librosa
 from systemds.scuro.dataloader.base_loader import BaseLoader
+from systemds.scuro.utils.schema_helpers import create_timestamps
 
 
 class AudioLoader(BaseLoader):
@@ -33,7 +34,11 @@ class AudioLoader(BaseLoader):
     ):
         super().__init__(source_path, indices, chunk_size)
 
-    def extract(self, file: str):
+    def extract(self, file: str, index: Optional[Union[str, List[str]]] = None):
         self.file_sanity_check(file)
         audio, sr = librosa.load(file)
+        self.metadata[file] = {"sample_rate": sr, "length": audio.shape[0]}
+        self.metadata[file]["timestamp"] = create_timestamps(
+            self.metadata[file]["sample_rate"], self.metadata[file]["length"]
+        )
         self.data.append(audio)
--- src/main/python/tests/scuro/test_unimodal_representations.py
+++ src/main/python/tests/scuro/test_unimodal_representations.py
@@ -22,6 +22,10 @@
 import os
 import shutil
 import unittest
+
+from systemds.scuro.representations.bow import BoW
+from systemds.scuro.representations.word2vec import W2V
+from systemds.scuro.representations.tfidf import TfIdf
 from systemds.scuro.modality.unimodal_modality import UnimodalModality
 from systemds.scuro.representations.bert import Bert
 from systemds.scuro.representations.mel_spectrogram import MelSpectrogram
@@ -34,7 +38,7 @@ from systemds.scuro.dataloader.text_loader import TextLoader
 from systemds.scuro.modality.type import ModalityType
 
 
-class TestDataLoaders(unittest.TestCase):
+class TestUnimodalRepresentations(unittest.TestCase):
     test_file_path = None
     mods = None
     text = None
@@ -42,70 +46,74 @@ class TestDataLoaders(unittest.TestCase):
     video = None
     data_generator = None
     num_instances = 0
+    indizes = []
 
     @classmethod
     def setUpClass(cls):
-        cls.test_file_path = "test_data"
-        cls.num_instances = 2
+        cls.test_file_path = "unimodal_test_data"
+
+        cls.num_instances = 4
         cls.mods = [ModalityType.VIDEO, ModalityType.AUDIO, ModalityType.TEXT]
+
         cls.data_generator = setup_data(cls.mods, cls.num_instances, cls.test_file_path)
-
         os.makedirs(f"{cls.test_file_path}/embeddings")
-
-        cls.text_ref = cls.data_generator.modalities_by_type[
-            ModalityType.TEXT
-        ].apply_representation(Bert())
-        cls.audio_ref = cls.data_generator.modalities_by_type[
-            ModalityType.AUDIO
-        ].apply_representation(MelSpectrogram())
-        cls.video_ref = cls.data_generator.modalities_by_type[
-            ModalityType.VIDEO
-        ].apply_representation(ResNet())
 
     @classmethod
     def tearDownClass(cls):
         print("Cleaning up test data")
         shutil.rmtree(cls.test_file_path)
 
-    def test_load_audio_data_from_file(self):
+    def test_audio_representations(self):
+        audio_representations = [MelSpectrogram()]  # TODO: add FFT, TFN, 1DCNN
         audio_data_loader = AudioLoader(
             self.data_generator.get_modality_path(ModalityType.AUDIO),
             self.data_generator.indices,
         )
-        audio = UnimodalModality(
-            audio_data_loader, ModalityType.AUDIO
-        ).apply_representation(MelSpectrogram())
+        audio = UnimodalModality(audio_data_loader, ModalityType.AUDIO)
 
-        for i in range(0, self.num_instances):
-            assert round(sum(sum(self.audio_ref.data[i])), 4) == round(
-                sum(sum(audio.data[i])), 4
-            )
+        for representation in audio_representations:
+            r = audio.apply_representation(representation)
+            assert r.data is not None
+            assert len(r.data) == self.num_instances
 
-    def test_load_video_data_from_file(self):
+    def test_video_representations(self):
+        video_representations = [ResNet()]  # Todo: add other video representations
         video_data_loader = VideoLoader(
             self.data_generator.get_modality_path(ModalityType.VIDEO),
             self.data_generator.indices,
         )
-        video = UnimodalModality(
-            video_data_loader, ModalityType.VIDEO
-        ).apply_representation(ResNet())
+        video = UnimodalModality(video_data_loader, ModalityType.VIDEO)
+        for representation in video_representations:
+            r = video.apply_representation(representation)
+            assert r.data is not None
+            assert len(r.data) == self.num_instances
 
-        for i in range(0, self.num_instances):
-            assert round(sum(sum(self.video_ref.data[i])), 4) == round(
-                sum(sum(video.data[i])), 4
-            )
-
-    def test_load_text_data_from_file(self):
+    def test_text_representations(self):
+        # TODO: check params fro BOW, W2V, TfIdf
+        test_representations = [BoW(2, 2), W2V(5, 2, 2), TfIdf(2), Bert()]
         text_data_loader = TextLoader(
             self.data_generator.get_modality_path(ModalityType.TEXT),
             self.data_generator.indices,
         )
-        text = UnimodalModality(
-            text_data_loader, ModalityType.TEXT
-        ).apply_representation(Bert())
+        text = UnimodalModality(text_data_loader, ModalityType.TEXT)
 
-        for i in range(0, self.num_instances):
-            assert round(sum(self.text_ref.data[i]), 4) == round(sum(text.data[i]), 4)
+        for representation in test_representations:
+            r = text.apply_representation(representation)
+            assert r.data is not None
+            assert len(r.data) == self.num_instances
+
+    def test_chunked_video_representations(self):
+        video_representations = [ResNet()]
+        video_data_loader = VideoLoader(
+            self.data_generator.get_modality_path(ModalityType.VIDEO),
+            self.data_generator.indices,
+            chunk_size=2,
+        )
+        video = UnimodalModality(video_data_loader, ModalityType.VIDEO)
+        for representation in video_representations:
+            r = video.apply_representation(representation)
+            assert r.data is not None
+            assert len(r.data) == self.num_instances
 
 
 if __name__ == "__main__":

865daf0dcc8958abf47dcdf61d32e9fb025e4cab
Frxms <tomoki.mengel@gmail.com>
2025-02-20 19:13:57 +0100

    implement colProd and rowProd

--- src/test/java/org/apache/sysds/test/functions/codegen/CellwiseTmplTest.java
+++ src/test/java/org/apache/sysds/test/functions/codegen/CellwiseTmplTest.java
@@ -67,6 +67,8 @@ public class CellwiseTmplTest extends AutomatedTestBase
 	private static final String TEST_NAME25 = TEST_NAME+25; //bias_add
 	private static final String TEST_NAME26 = TEST_NAME+26; //bias_mult
 	private static final String TEST_NAME27 = TEST_NAME+27; //outer < +7 negative
+		private static final String TEST_NAME28 = TEST_NAME+28; //colProd(X)
+	private static final String TEST_NAME29 = TEST_NAME+29; //rowProd(X)
 
 	private static final String TEST_DIR = "functions/codegen/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + CellwiseTmplTest.class.getSimpleName() + "/";
@@ -79,7 +81,7 @@ public class CellwiseTmplTest extends AutomatedTestBase
 	@Override
 	public void setUp() {
 		TestUtils.clearAssertionInformation();
-		for( int i=1; i<=27; i++ ) {
+		for( int i=1; i<=29; i++ ) {
 			addTestConfiguration( TEST_NAME+i, new TestConfiguration(
 				TEST_CLASS_DIR, TEST_NAME+i, new String[] {String.valueOf(i)}) );
 		}
@@ -459,6 +461,16 @@ public class CellwiseTmplTest extends AutomatedTestBase
 		testCodegenIntegration( TEST_NAME27, true, ExecType.SPARK );
 	}
 
+	@Test
+	public void testCodegenCellwise28() {
+		testCodegenIntegration( TEST_NAME28, false, ExecType.CP );
+	}
+
+	@Test
+	public void testCodegenCellwise29() {
+		testCodegenIntegration( TEST_NAME29, false, ExecType.CP );
+	}
+
 	private void testCodegenIntegration( String testname, boolean rewrites, ExecType instType )
 	{
 		boolean oldRewrites = OptimizerUtils.ALLOW_ALGEBRAIC_SIMPLIFICATION;
@@ -475,7 +487,7 @@ public class CellwiseTmplTest extends AutomatedTestBase
 			
 			String HOME = SCRIPT_DIR + TEST_DIR;
 			fullDMLScriptName = HOME + testname + ".dml";
-			programArgs = new String[]{"-stats", "-args", output("S") };
+			programArgs = new String[]{"-explain", "codegen", "-stats", "-args", output("S") };
 			
 			fullRScriptName = HOME + testname + ".R";
 			rCmd = getRCmd(inputDir(), expectedDir());

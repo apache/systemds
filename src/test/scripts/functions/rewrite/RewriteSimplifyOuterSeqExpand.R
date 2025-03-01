34f21ede022a897c2ab8ba5072c6648d5c43b5d9
ReneEnjilian <enjilianrene@gmail.com>
2025-03-01 23:16:52 +0100

    added testing for simplifyOuterSeqExpand

---
+++ src/test/scripts/functions/rewrite/RewriteSimplifyOuterSeqExpand.R
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#-------------------------------------------------------------
+
+args <- commandArgs(TRUE)
+
+# Set options for numeric precision
+options(digits=22)
+
+# Load required libraries
+library("Matrix")
+library("matrixStats")
+
+# Read operation type
+type = as.integer(args[2])
+m = 5
+v = matrix(1, 6, 1)
+
+# Perform operations
+if(type==1){
+    R = outer(v, 1:m, "==")
+} else if(type==2){
+    R = outer(1:m, as.vector(v), "==")
+
+}
+
+#Write result matrix R
+writeMM(as(R, "CsparseMatrix"), paste(args[3], "R", sep=""));

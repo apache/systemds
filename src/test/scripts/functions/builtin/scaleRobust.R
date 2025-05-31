9f7ffee993c4a71090f6b7ca2104ba25aa2df745
KilianBat <kilian.batliner@gmail.com>
2025-05-31 11:45:58 +0200

    R file has now the same workflow as the DML file

--- src/test/scripts/functions/builtin/scaleRobust.R
+++ src/test/scripts/functions/builtin/scaleRobust.R
@@ -8,9 +8,15 @@ colnames(X) = colnames(X, do.NULL=FALSE, prefix="C")
 # Robust scaling: subtract median, divide by IQR
 Y = X
 for (j in 1:ncol(X)) {
-  med = median(X[, j])
-  iqr = IQR(X[, j])
-  if (iqr == 0) iqr = 1
-  Y[, j] = (X[, j] - med) / iqr
+  col = X[, j]
+  med = quantile(col, probs=0.5, type=7, names=FALSE, na.rm=FALSE)
+  q1  = quantile(col, probs=0.25, type=7, names=FALSE, na.rm=FALSE)
+  q3  = quantile(col, probs=0.75, type=7, names=FALSE, na.rm=FALSE)
+  iqr = q3 - q1
+
+  if (iqr == 0 || is.nan(iqr)) iqr = 1
+
+  Y[, j] = (col - med) / iqr
 }
+
 writeMM(as(Y, "CsparseMatrix"), paste(args[2], "B", sep=""))

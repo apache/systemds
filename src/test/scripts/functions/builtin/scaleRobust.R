640b6bbf3bf1f0648e4bf99861407c0494a4f233
KilianBat <kilian.batliner@gmail.com>
2025-06-12 17:48:54 +0200

    R version now shows memory usage

--- src/test/scripts/functions/builtin/scaleRobust.R
+++ src/test/scripts/functions/builtin/scaleRobust.R
@@ -1,42 +1,31 @@
+library("Matrix")
+library("peakRAM")
+
 args <- commandArgs(TRUE)
 options(digits=22)
-library("Matrix")
 
-X = as.matrix(readMM(paste(args[1], "A.mtx", sep="")))
-colnames(X) = colnames(X, do.NULL=FALSE, prefix="C")
+result <- peakRAM({
+  X = as.matrix(readMM(paste(args[1], "A.mtx", sep="")))
+  colnames(X) = colnames(X, do.NULL=FALSE, prefix="C")
+  Y = X
 
-Y = X
+  start_time <- Sys.time()
 
+  for (j in 1:ncol(X)) {
+    col = X[, j]
+    med = quantile(col, probs=0.5, type=1, names=FALSE, na.rm=FALSE)
+    q1  = quantile(col, probs=0.25, type=1, names=FALSE, na.rm=FALSE)
+    q3  = quantile(col, probs=0.75, type=1, names=FALSE, na.rm=FALSE)
+    iqr = q3 - q1
+    if (iqr == 0 || is.nan(iqr)) iqr = 1
+    Y[, j] = (col - med) / iqr
+  }
 
-start_time <- Sys.time()
+  end_time <- Sys.time()
+  print("R overall time (s):")
+  print(end_time - start_time)
 
-for (j in 1:ncol(X)) {
-  col = X[, j]
-  med = quantile(col, probs=0.5, type=1, names=FALSE, na.rm=FALSE)
-  q1  = quantile(col, probs=0.25, type=1, names=FALSE, na.rm=FALSE)
-  q3  = quantile(col, probs=0.75, type=1, names=FALSE, na.rm=FALSE)
+  writeMM(as(Y, "CsparseMatrix"), paste(args[2], "B", sep=""))
+})
 
-
-
-
-  # Print debug values for first column
-  #if (j == 1) {
-  #  cat("R column 1:\n")
-  #  cat("Q1: ", q1, "\n")
-  #  cat("Median: ", med, "\n")
-  #  cat("Q3: ", q3, "\n")
-  #}
-
-
-
-  iqr = q3 - q1
-  if (iqr == 0 || is.nan(iqr)) iqr = 1
-  Y[, j] = (col - med) / iqr
-
-}
-
-end_time <- Sys.time()
-print(paste("R overall time (ms):", as.numeric(difftime(end_time, start_time, units="secs")) * 1000))
-
-
-writeMM(as(Y, "CsparseMatrix"), paste(args[2], "B", sep=""))
+print(result[, c("Elapsed_Time_sec", "Total_RAM_Used_MiB", "Peak_RAM_Used_MiB")])

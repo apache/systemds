584ed0e0b56c35e7429ac08b31ac33d5c9bd417b
Hubert Krawczyk <hubert0krawczyk@gmail.com>
2025-06-30 20:14:39 +0200

    moved einsum information extraction to EinsumContext function

--- src/main/java/org/apache/sysds/runtime/instructions/cp/EinsumContext.java
+++ src/main/java/org/apache/sysds/runtime/instructions/cp/EinsumContext.java
@@ -29,185 +29,143 @@ import java.util.Iterator;
 public class EinsumContext {
     public Integer outRows;
     public Integer outCols;
+    public Character outChar1;
+    public Character outChar2;
     public HashMap<Character, Integer> charToDimensionSizeInt;
     public String equationString;
-    public Integer[] contractDims;
-    public Integer[] summingDims;
+    public boolean[] diagonalInputs;
     public HashSet<Character> summingChars;
     public HashSet<Character> contractDimsSet;
+    public static final int CONTRACT_LEFT = 1;
+    public static final int CONTRACT_RIGHT = 2;
+    public static final int CONTRACT_BOTH = 3;
+    public int[] contractDims;
+    public ArrayList<String> newEquationStringSplit;
+    public HashMap<Character, ArrayList<Integer>> partsCharactersToIndices; // for each character, this tells in which inputs it appears
 
+    private EinsumContext(){};
     public static EinsumContext getEinsumContext(String eqStr, ArrayList<MatrixBlock> inputs){
         EinsumContext res = new EinsumContext();
 
         res.equationString = eqStr;
-        int i = 0;
         res.charToDimensionSizeInt  = new HashMap<Character, Integer>();
+        HashSet<Character> summingChars = new HashSet<>();
+        int[] contractDims = new int[inputs.size()];
+        boolean[] diagonalInputs = new boolean[inputs.size()]; // all false by default
+        HashSet<Character> contractDimsSet = new HashSet();
+        HashMap<Character, ArrayList<Integer>> partsCharactersToIndices = new HashMap<>();
+        ArrayList<String> newEquationStringSplit = new ArrayList();
+
         Iterator<MatrixBlock> it = inputs.iterator();
         MatrixBlock curArr = it.next();
         int arrSizeIterator=0;
-        HashSet<Character> summingChars = new HashSet<>();
-        Integer[] contractDims = new Integer[inputs.size()];//0==nothing, 1 = right, 2=left, 3 = both
-        Integer[] summingDims = new Integer[inputs.size()];//0/null==nothing, 1 = right, 2=left, 3 = both
-        HashSet<Character> contractDimsSet = new HashSet();
-
-
-        int arrIt = 0;
-        for (i = 0; true; i++){
+        int arrayIterator = 0;
+        int i;
+        for (i = 0; true; i++) {
             char c = eqStr.charAt(i);
-            if(c=='-'){
+            if(c == '-'){
                 i+=2;
                 break;
             }
-            if(c==','){
-
-                arrIt++;
+            if(c == ','){
+                arrayIterator++;
                 curArr = it.next();
                 arrSizeIterator = 0;
             }
-
             else{
-                if (res.charToDimensionSizeInt.containsKey(c)){
-                    // just check if dims match!
-                    if(arrSizeIterator==0)
-                        assert (res.charToDimensionSizeInt.get(c) == curArr.getNumRows());
-                    else if(arrSizeIterator==1)
-                        assert (res.charToDimensionSizeInt.get(c) == curArr.getNumColumns());
-
+                if (res.charToDimensionSizeInt.containsKey(c)) { // sanity check if dims match, this is already checked at validation
+                    if(arrSizeIterator == 0 && res.charToDimensionSizeInt.get(c) != curArr.getNumRows())
+                        throw new RuntimeException("Einsum: character "+c+" has multiple conflicting sizes");
+                    else if(arrSizeIterator == 1 && res.charToDimensionSizeInt.get(c) != curArr.getNumColumns())
+                        throw new RuntimeException("Einsum: character "+c+" has multiple conflicting sizes");
                     summingChars.add(c);
-
-                }else{
-                    if(arrSizeIterator==0)
+                } else {
+                    if(arrSizeIterator == 0)
                         res.charToDimensionSizeInt.put(c, curArr.getNumRows());
-                    else if(arrSizeIterator==1)
+                    else if(arrSizeIterator == 1)
                         res.charToDimensionSizeInt.put(c, curArr.getNumColumns());
                 }
+
                 arrSizeIterator++;
             }
-
-            //Process char
         }
-        int rem = eqStr.length() - i;
+        int numOfRemainingChars = eqStr.length() - i;
+
+        if (numOfRemainingChars > 2)
+            throw new RuntimeException("Einsum: dim > 2 not supported");
+
         arrSizeIterator = 0;
-        if (rem ==0){
-            res.outRows=1;
-            res.outCols=1;
 
-            arrIt=0;
-            for (i = 0; true; i++) {
-                char c = eqStr.charAt(i);
-                if (c == '-') {
-                    break;
-                }
-                if (c == ',') {
-                    arrIt++;
-                    arrSizeIterator = 0;
-                    continue;
-                }
+        Character outChar1 = numOfRemainingChars > 0 ? eqStr.charAt(i) : null;
+        Character outChar2 = numOfRemainingChars > 1 ? eqStr.charAt(i+1) : null;
+        res.outRows=(numOfRemainingChars > 0 ? res.charToDimensionSizeInt.get(outChar1) : 1);
+        res.outCols=(numOfRemainingChars > 1 ? res.charToDimensionSizeInt.get(outChar2) : 1);
 
-                if(summingChars.contains(c)){
-
-                }else{
-                    contractDimsSet.add(c);
-                    if(contractDims[arrIt]==null){
-                        contractDims[arrIt] = arrSizeIterator +1;
-
-                    }else {
-                        contractDims[arrIt] += arrSizeIterator + 1;
-                    }
-                }
-                arrSizeIterator++;
-
+        arrayIterator=0;
+        for (i = 0; true; i++) {
+            char c = eqStr.charAt(i);
+            if (c == '-') {
+                break;
             }
-        }else if (rem == 1){
-            char c1= eqStr.charAt(i);
-            res.outRows=(res.charToDimensionSizeInt.get(c1));
-
-            res.outCols=1;
-            arrIt=0;
-            for (i = 0; true; i++) {
-                char c = eqStr.charAt(i);
-                if (c == '-') {
-                    break;
-                }
-                if (c == ',') {
-                    arrIt++;
-                    arrSizeIterator = 0;
-                    continue;
-                }
-
-                if(summingChars.contains(c)){
-
-                    if(summingDims[arrIt] == null){
-                        summingDims[arrIt]=arrSizeIterator +1; // it=0->add 1, it==1->add 2
-                    }else{
-                        summingDims[arrIt]+=arrSizeIterator +1; // it=0->add 1, it==1->add 2
-
-                    }
-                }else if(c==c1){
-                    // this dim is remaining
-                }else{
-                    contractDimsSet.add(c);
-
-                    if(contractDims[arrIt]==null){
-                        contractDims[arrIt]=arrSizeIterator +1;
-
-                    }else {
-                        contractDims[arrIt] += arrSizeIterator + 1;
-                    }
-
-                }
-                arrSizeIterator++;
-
+            if (c == ',') {
+                arrayIterator++;
+                arrSizeIterator = 0;
+                continue;
             }
-        }else if (rem==2){
-            char c1= eqStr.charAt(i);
-            char c2= eqStr.charAt(i+1);
-            res.outRows=(res.charToDimensionSizeInt.get(c1));
-            res.outCols=(res.charToDimensionSizeInt.get(c2));
+            String s = "";
 
-            arrIt=0;
-            for (i = 0; true; i++) {
-                char c = eqStr.charAt(i);
-                if (c == '-') {
-                    break;
-                }
-                if (c == ',') {
-                    arrIt++;
-                    arrSizeIterator = 0;
-                    continue;
-
-                }
-
-                if(summingChars.contains(c)){
-                    if(summingDims[arrIt] == null){
-                        summingDims[arrIt]=arrSizeIterator +1; // it=0->add 1, it==1->add 2
-                    }else{
-                        summingDims[arrIt]+=arrSizeIterator +1; // it=0->add 1, it==1->add 2
-
-                    }
-                }else if(c==c1 || c==c2){
-                    // this dim is remaining
-                }else{
-                    contractDimsSet.add(c);
-
-                    if(contractDims[arrIt]==null){
-                        contractDims[arrIt]=arrSizeIterator +1;
-
-                    }else {
-                        contractDims[arrIt] += arrSizeIterator + 1;
-                    }
-                }
-                arrSizeIterator++;
-
+            if(summingChars.contains(c)) {
+                s+=c;
+                if(!partsCharactersToIndices.containsKey(c))
+                    partsCharactersToIndices.put(c, new ArrayList<>());
+                partsCharactersToIndices.get(c).add(arrayIterator);
             }
-        }else{
-            throw new RuntimeException("output dim > 2 not supported for now");
+            else if((outChar1 != null && c == outChar1) || (outChar2 != null && c == outChar2)) {
+                s+=c;
+            }
+            else {
+                contractDimsSet.add(c);
+                contractDims[arrayIterator] = CONTRACT_LEFT;
+            }
+
+            if(i + 1 < eqStr.length()) { // process next character together
+                char c2 = eqStr.charAt(i + 1);
+                i++;
+                if (c2 == '-') { newEquationStringSplit.add(s); break;}
+                if (c2 == ',') { arrayIterator++; newEquationStringSplit.add(s); continue; }
+
+                if (c2 == c){
+                    diagonalInputs[arrayIterator] = true;
+                    if (contractDims[arrayIterator] == CONTRACT_LEFT) contractDims[arrayIterator] = CONTRACT_BOTH;
+                }
+                else{
+                    if(summingChars.contains(c2)) {
+                        s+=c2;
+                        if(!partsCharactersToIndices.containsKey(c2))
+                            partsCharactersToIndices.put(c2, new ArrayList<>());
+                        partsCharactersToIndices.get(c2).add(arrayIterator);
+                    }
+                    else if((outChar1 != null && c2 == outChar1) || (outChar2 != null && c2 == outChar2)) {
+                        s+=c2;
+                    }
+                    else {
+                        contractDimsSet.add(c2);
+                        contractDims[arrayIterator] += CONTRACT_RIGHT;
+                    }
+                }
+            }
+            newEquationStringSplit.add(s);
+            arrSizeIterator++;
         }
-        res.contractDims=contractDims;
-        res.contractDimsSet = contractDimsSet;
-        res.summingDims=summingDims;
 
+        res.contractDims = contractDims;
+        res.contractDimsSet = contractDimsSet;
+        res.diagonalInputs = diagonalInputs;
         res.summingChars = summingChars;
+        res.outChar1 = outChar1;
+        res.outChar2 = outChar2;
+        res.newEquationStringSplit = newEquationStringSplit;
+        res.partsCharactersToIndices = partsCharactersToIndices;
         return res;
     }
 }

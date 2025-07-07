a44425e77e3678a3c174a7e091793b980f40c423
esracosgun <esra.cosgun@gmx.de>
2025-07-07 21:16:41 +0200

    adjust BuilinDedupTest + added distributed_represntation for testing

--- src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinDedupTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinDedupTest.java
@@ -1,4 +1,23 @@
-package org.apache.sysds.test.functions.builtin.part2;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.test.functions.builtin.part1;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,23 +34,18 @@ import org.junit.runners.Parameterized;
 @RunWith(value = Parameterized.class)
 @net.jcip.annotations.NotThreadSafe
 public class BuiltinDedupTest extends AutomatedTestBase {
-	private final static String TEST_NAME = "dedup";
+	private final static String TEST_NAME = "distributed_representation";
 	private final static String TEST_DIR = "functions/builtin/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinDedupTest.class.getSimpleName() + "/";
 
 	@Parameterized.Parameter()
-	public boolean returnDuplicates;
-
-	@Parameterized.Parameter(1)
 	public String similarityMeasure;
 
 	@Parameterized.Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-			{true, "cosine"},
-			{false, "cosine"},
-			{true, "euclidean"},
-			{false, "euclidean"},
+			{"cosine"},
+			{"euclidean"}
 		});
 	}
 
@@ -60,8 +74,8 @@ public class BuiltinDedupTest extends AutomatedTestBase {
 			loadTestConfiguration(getTestConfiguration(TEST_NAME));
 			String HOME = SCRIPT_DIR + TEST_DIR;
 			fullDMLScriptName = HOME + TEST_NAME + ".dml";
-			programArgs = new String[]{"-stats", "-args", input("X"), input("gloveMatrix"), input("vocab"), similarityMeasure,
-				String.valueOf(returnDuplicates).toUpperCase(), output("Y")};
+			programArgs = new String[]{"-stats", "-args", input("X"), input("gloveMatrix"), input("vocab"), similarityMeasure, 
+				"0.8", output("Y")};
 
 			// Mock input data
 			String[][] X = new String[][]{

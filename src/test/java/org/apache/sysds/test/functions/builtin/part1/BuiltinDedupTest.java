b31237722e3455a289d0a8576b34870bae9c7d97
esracosgun <esra.cosgun@gmx.de>
2025-07-08 12:09:42 +0200

    added simple test + corrected folder path for test

--- src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinDedupTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinDedupTest.java
@@ -19,25 +19,28 @@
 
 package org.apache.sysds.test.functions.builtin.part1;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.apache.sysds.common.Types;
 import org.apache.sysds.common.Types.ExecType;
+import org.apache.sysds.common.Types.ValueType;
+import org.apache.sysds.common.Types.FileFormat;
+import org.apache.sysds.runtime.frame.data.FrameBlock;
+import org.apache.sysds.runtime.meta.MatrixCharacteristics;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.apache.sysds.test.TestUtils;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-@RunWith(value = Parameterized.class)
-@net.jcip.annotations.NotThreadSafe
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Parameterized;
+
+//@RunWith(value = Parameterized.class)
+//@net.jcip.annotations.NotThreadSafe
 public class BuiltinDedupTest extends AutomatedTestBase {
-	private final static String TEST_NAME = "distributed_representation";
+	private final static String TEST_NAME = "distributed_representation"; 
 	private final static String TEST_DIR = "functions/builtin/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinDedupTest.class.getSimpleName() + "/";
 
+	/*
 	@Parameterized.Parameter()
 	public String similarityMeasure;
 
@@ -48,6 +51,7 @@ public class BuiltinDedupTest extends AutomatedTestBase {
 			{"euclidean"}
 		});
 	}
+	*/
 
 	@Override
 	public void setUp() {
@@ -57,7 +61,7 @@ public class BuiltinDedupTest extends AutomatedTestBase {
 			setOutAndExpectedDeletionDisabled(true);
 		}
 	}
-
+	/*
 	@Test
 	public void testDedupCP() {
 		runDedupTests(ExecType.CP);
@@ -67,7 +71,66 @@ public class BuiltinDedupTest extends AutomatedTestBase {
 	public void testDedupSPARK() {
 		runDedupTests(ExecType.SPARK);
 	}
+	*/
 
+	@Test
+	public void testSimpleDedupCP() {
+		runTestCase(ExecType.CP);
+	}
+
+	private void runTestCase(ExecType execType) {
+		Types.ExecMode platformOld = setExecMode(execType);
+		try {
+			loadTestConfiguration(getTestConfiguration(TEST_NAME));
+			String HOME = SCRIPT_DIR + TEST_DIR;
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+
+			programArgs = new String[]{
+				"-stats", "-args",
+				input("X"), input("gloveMatrix"), input("vocab"),
+				"cosine", "0.8", output("Y")
+			};
+
+			// ----- Frame X -----
+			String[][] X = new String[][]{
+				{"John Doe", "New York"},
+				{"Jon Doe", "New York City"},
+				{"Jane Doe", "Boston"},
+				{"John Doe", "NY"}
+			};
+			ValueType[] schemaX = new ValueType[]{ValueType.STRING, ValueType.STRING};
+			FrameBlock fbX = new FrameBlock(schemaX);
+			for (String[] row : X) fbX.appendRow(row);
+			writeInputFrameWithMTD("X", fbX, true, new MatrixCharacteristics(X.length, X[0].length, -1, -1), schemaX, FileFormat.CSV);
+
+			// ----- Vocab -----
+			String[][] vocab = new String[][]{
+				{"john"}, {"doe"}, {"new"}, {"york"},
+				{"city"}, {"boston"}, {"ny"}, {"jane"}
+			};
+			ValueType[] schemaVocab = new ValueType[]{ValueType.STRING};
+			FrameBlock fbVocab = new FrameBlock(schemaVocab);
+			for (String[] row : vocab) fbVocab.appendRow(row);
+			writeInputFrameWithMTD("vocab", fbVocab, true, new MatrixCharacteristics(vocab.length, 1, -1, -1), schemaVocab, FileFormat.CSV);
+
+			// ----- Glove-Matrix -----
+			double[][] gloveMatrix = getRandomMatrix(vocab.length, 50, -0.5, 0.5, 1, 123);
+			writeInputMatrixWithMTD("gloveMatrix", gloveMatrix, true);
+
+			// Run
+			runTest(true, false, null, -1);
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		finally {
+			rtplatform = platformOld;
+		}
+	}
+
+	/*
 	private void runDedupTests(ExecType instType) {
 		Types.ExecMode platformOld = setExecMode(instType);
 		try {
@@ -110,4 +173,5 @@ public class BuiltinDedupTest extends AutomatedTestBase {
 			rtplatform = platformOld;
 		}
 	}
+	*/
 }

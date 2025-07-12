bdca81dd988c89e143294d96b267d0b6a5d5c69f
Jessica Priebe <jessica.priebe@web.de>
2025-07-12 02:36:42 +0200

    Initial structure for OOCInstruction and OOCInstructionParser

---
+++ src/main/java/org/apache/sysds/runtime/instructions/OOCInstructionParser.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.runtime.instructions;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.sysds.common.InstructionType;
+import org.apache.sysds.runtime.DMLRuntimeException;
+import org.apache.sysds.runtime.instructions.ooc.OOCInstruction;
+
+public class OOCInstructionParser extends InstructionParser {
+	protected static final Log LOG = LogFactory.getLog(OOCInstructionParser.class.getName());
+
+	public static OOCInstruction parseSingleInstruction(String str) {
+		if(str == null || str.isEmpty())
+			return null;
+		InstructionType ooctype = InstructionUtils.getOOCType(str);
+		if(ooctype == null)
+			throw new DMLRuntimeException("Unable derive ooctype for instruction: " + str);
+		OOCInstruction oocinst = parseSingleInstruction(ooctype, str);
+		if(oocinst == null)
+			throw new DMLRuntimeException("Unable to parse instruction: " + str);
+		return oocinst;
+	}
+
+	public static OOCInstruction parseSingleInstruction(InstructionType ooctype, String str) {
+		if(str == null || str.isEmpty())
+			return null;
+		switch(ooctype) {
+
+			// TODO:
+			case AggregateUnary:
+			case Binary:
+
+			default:
+				throw new DMLRuntimeException("Invalid OOC Instruction Type: " + ooctype);
+		}
+	}
+}
---
+++ src/main/java/org/apache/sysds/runtime/instructions/ooc/OOCInstruction.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.runtime.instructions.ooc;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.sysds.api.DMLScript;
+import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
+import org.apache.sysds.runtime.instructions.Instruction;
+import org.apache.sysds.runtime.matrix.operators.Operator;
+
+public abstract class OOCInstruction extends Instruction {
+	protected static final Log LOG = LogFactory.getLog(OOCInstruction.class.getName());
+
+	public enum OOCType {
+		AggregateUnary, Binary
+	}
+
+	protected final OOCInstruction.OOCType _ooctype;
+	protected final boolean _requiresLabelUpdate;
+
+	protected OOCInstruction(OOCInstruction.OOCType type, String opcode, String istr) {
+		this(type, null, opcode, istr);
+	}
+
+	protected OOCInstruction(OOCInstruction.OOCType type, Operator op, String opcode, String istr) {
+		super(op);
+		_ooctype = type;
+		instString = istr;
+		instOpcode = opcode;
+
+		_requiresLabelUpdate = super.requiresLabelUpdate();
+	}
+
+	@Override
+	public IType getType() {
+		return IType.OUT_OF_CORE;
+	}
+
+	public OOCInstruction.OOCType getOOCInstructionType() {
+		return _ooctype;
+	}
+
+	@Override
+	public boolean requiresLabelUpdate() {
+		return _requiresLabelUpdate;
+	}
+
+	@Override
+	public String getGraphString() {
+		return getOpcode();
+	}
+
+	@Override
+	public Instruction preprocessInstruction(ExecutionContext ec) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public abstract void processInstruction(ExecutionContext ec);
+
+	@Override
+	public void postprocessInstruction(ExecutionContext ec) {
+		if(DMLScript.LINEAGE_DEBUGGER)
+			ec.maintainLineageDebuggerInfo(this);
+	}
+}

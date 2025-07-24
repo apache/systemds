0e82e9cdf006453d09e9ec88fc8bc9c8332ffac0
Frxms <tomoki.mengel@gmail.com>
2025-07-24 22:00:55 +0200

    only one check is needed

--- src/main/java/org/apache/sysds/hops/codegen/cplan/CNodeUnary.java
+++ src/main/java/org/apache/sysds/hops/codegen/cplan/CNodeUnary.java
@@ -116,7 +116,7 @@ public class CNodeUnary extends CNode
 			((_inputs.get(0) instanceof CNodeData
 				&& _inputs.get(0).getVarname().startsWith("a")
 				&& !_inputs.get(0).isLiteral())
-				|| (DMLScript.SPARSE_INTERMEDIATE && _inputs.get(0).getVarname().startsWith("STMP")));
+				|| (_inputs.get(0).getVarname().startsWith("STMP")));
 		String var = createVarname(DMLScript.SPARSE_INTERMEDIATE && lsparse && getOutputType());
 		String tmp = getLanguageTemplateClass(this, api).getTemplate(_type, lsparse);
 		tmp = tmp.replaceAll("%TMP%", var);

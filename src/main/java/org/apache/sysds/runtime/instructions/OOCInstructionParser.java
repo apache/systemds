5f0fd56e16fb029a626cc43f0616eb0126e0c637
Janardhan Pulivarthi <j143@protonmail.com>
2025-07-26 15:51:23 +0530

    add test to invoke ooc unary op

--- src/main/java/org/apache/sysds/runtime/instructions/OOCInstructionParser.java
+++ src/main/java/org/apache/sysds/runtime/instructions/OOCInstructionParser.java
@@ -23,10 +23,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.sysds.common.InstructionType;
 import org.apache.sysds.runtime.DMLRuntimeException;
-import org.apache.sysds.runtime.instructions.ooc.AggregateUnaryOOCInstruction;
-import org.apache.sysds.runtime.instructions.ooc.BinaryOOCInstruction;
-import org.apache.sysds.runtime.instructions.ooc.OOCInstruction;
-import org.apache.sysds.runtime.instructions.ooc.ReblockOOCInstruction;
+import org.apache.sysds.runtime.instructions.ooc.*;
 
 public class OOCInstructionParser extends InstructionParser {
 	protected static final Log LOG = LogFactory.getLog(OOCInstructionParser.class.getName());
@@ -51,6 +48,8 @@ public class OOCInstructionParser extends InstructionParser {
 				return ReblockOOCInstruction.parseInstruction(str);
 			case AggregateUnary:
 				return AggregateUnaryOOCInstruction.parseInstruction(str);
+			case Unary:
+				return UnaryOOCInstruction.parseInstruction(str);
 			case Binary:
 				return BinaryOOCInstruction.parseInstruction(str);
 			
---
+++ src/main/java/org/apache/sysds/runtime/instructions/ooc/UnaryOOCInstruction.java
@@ -0,0 +1,31 @@
+package org.apache.sysds.runtime.instructions.ooc;
+
+import org.apache.sysds.common.Types;
+import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
+import org.apache.sysds.runtime.instructions.InstructionUtils;
+import org.apache.sysds.runtime.instructions.cp.CPOperand;
+import org.apache.sysds.runtime.matrix.operators.Operator;
+import org.apache.sysds.runtime.matrix.operators.UnaryOperator;
+
+public class UnaryOOCInstruction extends ComputationOOCInstruction {
+    private UnaryOperator _uop = null;
+
+    protected UnaryOOCInstruction(OOCType type, Operator op, CPOperand in1, CPOperand out, String opcode, String istr) {
+        super(type, op, in1, out, opcode, istr);
+    }
+
+    public static UnaryOOCInstruction parseInstruction(String str) {
+        String[] parts = InstructionUtils.getInstructionParts(str);
+        InstructionUtils.checkNumFields(parts, 2);
+        String opcode = parts[0];
+        CPOperand in1 = new CPOperand(parts[1]);
+        CPOperand out = new CPOperand(parts[2]);
+
+        UnaryOperator uopcode = InstructionUtils.parseUnaryOperator(opcode);
+        return new UnaryOOCInstruction(OOCType.Unary, uopcode, in1, out, str);
+    }
+
+    public void processInstruction( ExecutionContext ec ) {
+
+    }
+}

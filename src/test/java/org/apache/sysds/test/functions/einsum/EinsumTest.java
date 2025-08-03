7eb950c46130fe45917d9cc7f200f0c366476982
Hubert Krawczyk <hubert0krawczyk@gmail.com>
2025-08-03 22:56:11 +0200

    create einsum test files from configuration list, fixes in code

--- src/test/java/org/apache/sysds/test/functions/einsum/EinsumTest.java
+++ src/test/java/org/apache/sysds/test/functions/einsum/EinsumTest.java
@@ -28,36 +28,261 @@ import org.apache.sysds.runtime.matrix.data.MatrixValue.CellIndex;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.apache.sysds.test.TestUtils;
+import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
+@RunWith(Parameterized.class)
 public class EinsumTest extends AutomatedTestBase
 {
+	final private static List<Config> TEST_CONFIGS = List.of(
+			new Config("ij,jk->ik", List.of(shape(5, 600), shape(600, 10))), // mm
+			new Config("ji,jk->ik", List.of(shape(600, 5), shape(600, 10))),
+			new Config("ji,kj->ik", List.of(shape(600, 5), shape(10, 600))),
+			new Config("ij,kj->ik", List.of(shape(5, 600), shape(10, 600))),
+
+			new Config("ji,jk->i", List.of(shape(600, 5), shape(600, 10))),
+			new Config("ij,jk->i", List.of(shape(5, 600), shape(600, 10))),
+
+			new Config("ji,jk->k", List.of(shape(600, 5), shape(600, 10))),
+			new Config("ij,jk->k", List.of(shape(5, 600), shape(600, 10))),
+
+			new Config("ji,jk->j", List.of(shape(600, 5), shape(600, 10))),
+
+			new Config("ji,ji->ji", List.of(shape(600, 5), shape(600, 5))), // elemwise mult
+			new Config("ji,ij->ji", List.of(shape(600, 5), shape(5, 600))), // elemwise mult
+
+
+			new Config("ij,i->ij",   List.of(shape(1000, 50), shape(1000))), // col mult
+			new Config("ji,i->ij",   List.of(shape(50, 1000), shape(1000))), // row mult
+			new Config("ij,i->i",   List.of(shape(1000, 50), shape(1000))),
+			new Config("ij,i->j",   List.of(shape(1000, 50), shape(1000))),
+
+			new Config("i,i->",     List.of(shape(500), shape(500))),
+			new Config("i,j->",     List.of(shape(500), shape(800))),
+			new Config("i,j->ij",     List.of(shape(500), shape(800))), // outer vect mult
+			new Config("i,j->ji",     List.of(shape(500), shape(800))), // outer vect mult
+
+			new Config("ij->",     List.of(shape(1000, 50))), // sum
+			new Config("ij->i",     List.of(shape(1000, 50))), // sum(1)
+			new Config("ij->j",     List.of(shape(1000, 50))), // sum(0)
+			new Config("ij->ji",     List.of(shape(1000, 50))), // T
+
+			new Config("ab,cd->ba",     List.of(shape( 600, 10), shape(6, 5))),
+
+			new Config("ab,bc,cd,de->ae",   List.of(shape(5, 600), shape(600, 10), shape(10, 5), shape(5, 4))), // chain of mm
+
+			new Config("ji,jz,zx->ix",   List.of(shape(600, 5), shape( 600, 10), shape(10, 2))),
+			new Config("fx,fg,fz,xg->z",   List.of(shape(600, 5), shape( 600, 10), shape(600, 6), shape(5, 10))),
+			new Config("fx,fg,fz,xg,zx,zg->g", // each idx 3 times (cell tpl)
+					List.of(shape(5, 60), shape(5, 30), shape(5, 100), shape(60, 30), shape(100, 60), shape(100, 30))),
+
+			new Config("i->",     List.of(shape(1000))),
+			new Config("i->i",     List.of(shape(1000)))
+	);
+
+	private final int id;
+	private final String einsumStr;
+	private final List<int[]> shapes;
+	private final File dmlFile;
+	private final File rFile;
+	private final boolean outputScalar;
+
+	public EinsumTest(String einsumStr, List<int[]> shapes, File dmlFile, File rFile, boolean outputScalar, int id){
+		this.id = id;
+		this.einsumStr = einsumStr;
+		this.shapes = shapes;
+		this.dmlFile = dmlFile;
+		this.rFile = rFile;
+		this.outputScalar = outputScalar;
+	}
+
+	@Parameterized.Parameters(name = "{index}: einsum={0}")
+	public static Collection<Object[]> data() throws IOException {
+		List<Object[]> parameters = new ArrayList<>();
+
+		int counter = 1;
+
+		for (Config config : TEST_CONFIGS) {
+			List<File> files = new ArrayList<>();
+			String fullDMLScriptName = "SystemDS_einsum_test" + counter;
+
+			File dmlFile = File.createTempFile(fullDMLScriptName, ".dml");
+			dmlFile.deleteOnExit();
+
+			boolean outputScalar = config.einsumStr.trim().endsWith("->");
+
+			StringBuilder sb = createDmlFile(config, outputScalar);
+
+			Files.writeString(dmlFile.toPath(), sb.toString());
+
+			File rFile = File.createTempFile(fullDMLScriptName, ".R");
+			rFile.deleteOnExit();
+
+			sb = createRFile(config, outputScalar);
+
+			Files.writeString(rFile.toPath(), sb.toString());
+
+			parameters.add(new Object[]{config.einsumStr, config.shapes, dmlFile, rFile, outputScalar, counter});
+
+			counter++;
+		}
+
+		return parameters;
+	}
+
+	private static StringBuilder createDmlFile(Config config, boolean outputScalar) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < config.shapes.size(); i++) {
+			int[] dims = config.shapes.get(i);
+
+			double factor = 0.0001;
+			sb.append("A");
+			sb.append(i);
+
+			if (dims.length == 1) { // A1 = seq(1,1000) * 0.0001
+				sb.append(" = seq(1,");
+				sb.append(dims[0]);
+				sb.append(") * ");
+				sb.append(factor);
+			} else { // A0 = matrix(seq(1,50000), 1000, 50) * 0.0001
+				sb.append(" = matrix(seq(1, ");
+				sb.append(dims[0]*dims[1]);
+				sb.append("), ");
+				sb.append(dims[0]);
+				sb.append(", ");
+				sb.append(dims[1]);
+
+				sb.append(") * ");
+				sb.append(factor);
+			}
+			sb.append("\n");
+		}
+		sb.append("\n");
+
+		sb.append("R = einsum(\"");
+		sb.append(config.einsumStr);
+		sb.append("\", ");
+
+		for (int i = 0; i < config.shapes.size()-1; i++) {
+			sb.append("A");
+			sb.append(i);
+			sb.append(", ");
+		}
+		sb.append("A");
+		sb.append(config.shapes.size()-1);
+		sb.append(")");
+
+		sb.append("\n\n");
+		sb.append("write(R, $1)\n");
+		return sb;
+	}
+
+	private static StringBuilder createRFile(Config config, boolean outputScalar) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("args<-commandArgs(TRUE)\n");
+		sb.append("options(digits=22)\n");
+		sb.append("library(\"Matrix\")\n");
+		sb.append("library(\"matrixStats\")\n");
+		sb.append("library(\"einsum\")\n\n");
+
+
+		for (int i = 0; i < config.shapes.size(); i++) {
+			int[] dims = config.shapes.get(i);
+
+			double factor = 0.0001;
+			sb.append("A");
+			sb.append(i);
+
+			if (dims.length == 1) { // A1 = seq(1,1000) * 0.0001
+				sb.append(" = seq(1,");
+				sb.append(dims[0]);
+				sb.append(") * ");
+				sb.append(factor);
+			} else { // A0 = matrix(seq(1,50000), 1000, 50, byrow=TRUE) * 0.0001
+				sb.append(" = matrix(seq(1, ");
+				sb.append(dims[0]*dims[1]);
+				sb.append("), ");
+				sb.append(dims[0]);
+				sb.append(", ");
+				sb.append(dims[1]);
+
+				sb.append(", byrow=TRUE) * ");
+				sb.append(factor);
+			}
+			sb.append("\n");
+		}
+		sb.append("\n");
+
+		sb.append("R = einsum(\"");
+		sb.append(config.einsumStr);
+		sb.append("\", ");
+
+		for (int i = 0; i < config.shapes.size()-1; i++) {
+			sb.append("A");
+			sb.append(i);
+			sb.append(", ");
+		}
+		sb.append("A");
+		sb.append(config.shapes.size()-1);
+		sb.append(")");
+
+		sb.append("\n\n");
+		if(outputScalar){
+			sb.append("write(R, paste(args[2], \"S\", sep=\"\"))\n");
+		}else{
+			sb.append("writeMM(as(R, \"CsparseMatrix\"), paste(args[2], \"S\", sep=\"\"))\n");
+		}
+		return sb;
+	}
+
+	@Test
+	public void testEinsumWithFiles() {
+		System.out.println("Testing einsum: " + this.einsumStr);
+		testCodegenIntegration(TEST_NAME_EINSUM+this.id);
+	}
+	@After
+	public void cleanUp() {
+		if (this.dmlFile.exists()) {
+			boolean deleted = this.dmlFile.delete();
+			if (!deleted) {
+				System.err.println("Failed to delete temp file: " + this.dmlFile.getAbsolutePath());
+			}
+		}
+		if (this.rFile.exists()) {
+			boolean deleted = this.rFile.delete();
+			if (!deleted) {
+				System.err.println("Failed to delete temp file: " + this.rFile.getAbsolutePath());
+			}
+		}
+	}
+
+	private static class Config {
+		String einsumStr;
+		List<int[]> shapes;
+
+		Config(String einsum, List<int[]> shapes) {
+			this.einsumStr = einsum;
+			this.shapes = shapes;
+		}
+	}
+
+	private static int[] shape(int... dims) {
+		return dims;
+	}
 	private static final Log LOG = LogFactory.getLog(EinsumTest.class.getName());
 
 	private static final String TEST_NAME_EINSUM = "einsum";
-	private static final String TEST_EINSUM1 = TEST_NAME_EINSUM+"1";
-	private static final String TEST_EINSUM2 = TEST_NAME_EINSUM+"2";
-	private static final String TEST_EINSUM3 = TEST_NAME_EINSUM+"3";
-	private static final String TEST_EINSUM4 = TEST_NAME_EINSUM+"4";
-	private static final String TEST_EINSUM5 = TEST_NAME_EINSUM+"5";
-	private static final String TEST_EINSUM6 = TEST_NAME_EINSUM+"6";
-	private static final String TEST_EINSUM7 = TEST_NAME_EINSUM+"7";
-	private static final String TEST_EINSUM8 = TEST_NAME_EINSUM+"8";
-	private static final String TEST_EINSUM9 = TEST_NAME_EINSUM+"9";
-	private static final String TEST_EINSUM10 = TEST_NAME_EINSUM+"10";
-	private static final String TEST_EINSUM11 = TEST_NAME_EINSUM+"11";
-	private static final String TEST_EINSUM12 = TEST_NAME_EINSUM+"12";
-	private static final String TEST_EINSUM13 = TEST_NAME_EINSUM+"13";
-	private static final String TEST_EINSUM14 = TEST_NAME_EINSUM+"14";
-	private static final String TEST_EINSUM15 = TEST_NAME_EINSUM+"15";
-	private static final String TEST_EINSUM16 = TEST_NAME_EINSUM+"16";
-	private static final String TEST_EINSUM17 = TEST_NAME_EINSUM+"17";
-	private static final String TEST_EINSUM18 = TEST_NAME_EINSUM+"18";
-	private static final String TEST_EINSUM19 = TEST_NAME_EINSUM+"19";
-	private static final String TEST_EINSUM20 = TEST_NAME_EINSUM+"20";
 	private static final String TEST_DIR = "functions/einsum/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + EinsumTest.class.getSimpleName() + "/";
 	private final static String TEST_CONF = "SystemDS-config-codegen.xml";
@@ -68,89 +293,27 @@ public class EinsumTest extends AutomatedTestBase
 	@Override
 	public void setUp() {
 		TestUtils.clearAssertionInformation();
-		for(int i=1; i<=20; i++)
+		for(int i = 1; i<= TEST_CONFIGS.size(); i++)
 			addTestConfiguration( TEST_NAME_EINSUM+i, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME_EINSUM+i, new String[] { String.valueOf(i) }) );
 	}
-	@Test
-	public void testCodegenEinsum1CP() {
-		testCodegenIntegration( TEST_EINSUM1, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum2CP() {
-		testCodegenIntegration( TEST_EINSUM2, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum3CP() {
-		testCodegenIntegration( TEST_EINSUM3, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum4CP() {
-		testCodegenIntegration( TEST_EINSUM4, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum5CP() {
-		testCodegenIntegration( TEST_EINSUM5, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum6CP() {
-		testCodegenIntegration( TEST_EINSUM6, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum7CP() {
-		testCodegenIntegration( TEST_EINSUM7, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum8CP() { testCodegenIntegration( TEST_EINSUM8, ExecType.CP ); }
-	@Test
-	public void testCodegenEinsum9CP() {
-		testCodegenIntegration( TEST_EINSUM9, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum10CP() {
-		testCodegenIntegration( TEST_EINSUM10, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum11CP() {
-		testCodegenIntegration( TEST_EINSUM11, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum12CP() {
-		testCodegenIntegration( TEST_EINSUM12, ExecType.CP );
-	}
-	@Test
-	public void testCodegenEinsum13CP() {
-		testCodegenIntegration( TEST_EINSUM13, ExecType.CP, true );
-	}
-	@Test
-	public void testCodegenEinsum14CP() { testCodegenIntegration( TEST_EINSUM14, ExecType.CP); }
-	@Test
-	public void testCodegenEinsum15CP() { testCodegenIntegration( TEST_EINSUM15, ExecType.CP); }
-	@Test
-	public void testCodegenEinsum16CP() { testCodegenIntegration( TEST_EINSUM16, ExecType.CP); }
-	@Test
-	public void testCodegenEinsum17CP() { testCodegenIntegration( TEST_EINSUM17, ExecType.CP); }
-	@Test
-	public void testCodegenEinsum18CP() { testCodegenIntegration( TEST_EINSUM18, ExecType.CP); }
-	@Test
-	public void testCodegenEinsum19CP() { testCodegenIntegration( TEST_EINSUM19, ExecType.CP); }
-	@Test
-	public void testCodegenEinsum20CP() { testCodegenIntegration( TEST_EINSUM20, ExecType.CP); }
-	private void testCodegenIntegration( String testname, ExecType instType) { testCodegenIntegration(testname, instType, false);  }
-	private void testCodegenIntegration( String testname, ExecType instType, boolean outputScalar )
+
+	private void testCodegenIntegration( String testname)
 	{
 		boolean oldFlag = OptimizerUtils.ALLOW_ALGEBRAIC_SIMPLIFICATION;
-		ExecMode platformOld = setExecMode(instType);
+		ExecMode platformOld = setExecMode(ExecType.CP);
 
+		String testnameDml = this.dmlFile.getAbsolutePath();
+		String testnameR = this.rFile.getAbsolutePath();
 		try
 		{
 			TestConfiguration config = getTestConfiguration(testname);
 			loadTestConfiguration(config);
 
 			String HOME = SCRIPT_DIR + TEST_DIR;
-			fullDMLScriptName = HOME + testname + ".dml";
+			fullDMLScriptName = testnameDml;
 			programArgs = new String[]{"-stats", "-explain", "-args", output("S") };
 
-			fullRScriptName = HOME + testname + ".R";
+			fullRScriptName = testnameR;
 			rCmd = getRCmd(inputDir(), expectedDir());
 
 			OptimizerUtils.ALLOW_ALGEBRAIC_SIMPLIFICATION = false;
@@ -176,6 +339,7 @@ public class EinsumTest extends AutomatedTestBase
 			OptimizerUtils.ALLOW_OPERATOR_FUSION = true;
 		}
 	}
+
 
 	/**
 	 * Override default configuration with custom test configuration to ensure

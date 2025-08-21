8846b6072c758782c7745ffd72f7a9752840d88a
ReneEnjilian <enjilianrene@gmail.com>
2025-08-22 01:17:36 +0200

    add method definition in header

--- src/main/cpp/jni/common/cujava_jni_utils.hpp
+++ src/main/cpp/jni/common/cujava_jni_utils.hpp
@@ -29,6 +29,7 @@ bool init(JNIEnv* env, jclass cls, jmethodID& method, const char* name, const ch
 bool init(JNIEnv* env, jclass& globalCls, jmethodID& constructor, const char* className);
 bool initNativePointer(JNIEnv* env, jfieldID& field, const char* className);
 bool set(JNIEnv *env, jlongArray ja, int index, jlong value);
+bool set(JNIEnv *env, jintArray ja, int index, jint value);
 
 // ---- Exceptions ----
 void ThrowByName(JNIEnv* env, const char* name, const char* msg);

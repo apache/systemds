4ab7abfbdbf4f4f6f2e573ae6eef6534ff2d0ebc
ReneEnjilian <enjilianrene@gmail.com>
2025-08-25 00:52:06 +0200

    implement CUfunction

--- src/main/java/org/apache/sysds/cujava/driver/CUfunction.java
+++ src/main/java/org/apache/sysds/cujava/driver/CUfunction.java
@@ -19,5 +19,10 @@
 
 package org.apache.sysds.cujava.driver;
 
-public class CUfunction {
+import org.apache.sysds.cujava.NativePointerObject;
+
+public class CUfunction extends NativePointerObject {
+
+	public CUfunction() {
+	}
 }

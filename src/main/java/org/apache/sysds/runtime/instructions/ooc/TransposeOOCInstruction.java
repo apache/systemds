8e9aaa5e3af50a6c51a20a6709c7a24ae7475956
Janardhan Pulivarthi <j143@protonmail.com>
2025-08-28 08:12:59 +0530

    [SYSTEMDS-3911] OOC Transpose operation (#2316)
    This patch introduces the TransposeOOCInstruction, a fundamental component of the out-of-core (OOC) backend. It performs a full transpose on a matrix stream, enabling the composition of complex OOC pipelines required for algorithms like lmDS.
    
    Implementation detail:
    
    * Asynchronous Producer: The processInstruction method launches a background thread to perform the transpose operation but returns control to the main thread immediately. This allows the compiler to continue building the execution plan without blocking. The actual computation is triggered when a downstream consumer "pulls" data from the output stream.
    
    * Streaming Logic: The background thread consumes a stream of IndexedMatrixValue blocks from its input. For each block, it:
        -  Performs an in-memory transpose using the standard reorgOperations with a ReorgOperator.
        - Crucially, it also transposes the MatrixIndexes of the block (e.g., a block at (row=i, col=j) becomes a block at (row=j, col=i)).
        - Enqueues the new, transposed IndexedMatrixValue into the output stream.
    
    * Integration: The new instruction is fully integrated into the system:
        - A Reorg type has been added to the OOCType enum.
        - The OOCInstructionParser has been updated to recognize the r' opcode for OOC execution and route it to the TransposeOOCInstruction.
        - The new Reorg OOC type is now registered with the OOCInstructionParser to correctly route the r' opcode to this new instruction.

---
+++ src/main/java/org/apache/sysds/runtime/instructions/ooc/TransposeOOCInstruction.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.runtime.instructions.ooc;
+
+import org.apache.sysds.runtime.DMLRuntimeException;
+import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
+import org.apache.sysds.runtime.controlprogram.context.ExecutionContext;
+import org.apache.sysds.runtime.controlprogram.parfor.LocalTaskQueue;
+import org.apache.sysds.runtime.functionobjects.SwapIndex;
+import org.apache.sysds.runtime.instructions.InstructionUtils;
+import org.apache.sysds.runtime.instructions.cp.CPOperand;
+import org.apache.sysds.runtime.instructions.spark.data.IndexedMatrixValue;
+import org.apache.sysds.runtime.matrix.data.MatrixBlock;
+import org.apache.sysds.runtime.matrix.data.MatrixIndexes;
+import org.apache.sysds.runtime.matrix.operators.ReorgOperator;
+import org.apache.sysds.runtime.matrix.operators.UnaryOperator;
+import org.apache.sysds.runtime.util.CommonThreadPool;
+
+import java.util.concurrent.ExecutorService;
+
+public class TransposeOOCInstruction extends ComputationOOCInstruction {
+
+	protected TransposeOOCInstruction(OOCType type, ReorgOperator op, CPOperand in1, CPOperand out, String opcode, String istr) {
+		super(type, op, in1, out, opcode, istr);
+
+	}
+
+	public static TransposeOOCInstruction parseInstruction(String str) {
+		String[] parts = InstructionUtils.getInstructionPartsWithValueType(str);
+		InstructionUtils.checkNumFields(parts, 2);
+		String opcode = parts[0];
+		CPOperand in1 = new CPOperand(parts[1]);
+		CPOperand out = new CPOperand(parts[2]);
+
+		ReorgOperator reorg = new ReorgOperator(SwapIndex.getSwapIndexFnObject());
+		return new TransposeOOCInstruction(OOCType.Reorg, reorg, in1, out, opcode, str);
+	}
+
+	public void processInstruction( ExecutionContext ec ) {
+
+		// Create thread and process the transpose operation
+		MatrixObject min = ec.getMatrixObject(input1);
+		LocalTaskQueue<IndexedMatrixValue> qIn = min.getStreamHandle();
+		LocalTaskQueue<IndexedMatrixValue> qOut = new LocalTaskQueue<>();
+		ec.getMatrixObject(output).setStreamHandle(qOut);
+
+
+		ExecutorService pool = CommonThreadPool.get();
+		try {
+			pool.submit(() -> {
+				IndexedMatrixValue tmp = null;
+				try {
+					while ((tmp = qIn.dequeueTask()) != LocalTaskQueue.NO_MORE_TASKS) {
+						MatrixBlock inBlock = (MatrixBlock)tmp.getValue();
+						long oldRowIdx = tmp.getIndexes().getRowIndex();
+						long oldColIdx = tmp.getIndexes().getColumnIndex();
+
+						MatrixBlock outBlock = inBlock.reorgOperations((ReorgOperator) _optr, new MatrixBlock(), -1, -1, -1);
+						qOut.enqueueTask(new IndexedMatrixValue(new MatrixIndexes(oldColIdx, oldRowIdx), outBlock));
+					}
+					qOut.closeInput();
+				}
+				catch(Exception ex) {
+					throw new DMLRuntimeException(ex);
+				}
+			});
+		} catch (Exception ex) {
+			throw new DMLRuntimeException(ex);
+		} finally {
+			pool.shutdown();
+		}
+	}
+}

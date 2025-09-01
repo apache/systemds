79d859b750cd129f3ed78a45b346465b0524d8b6
Janardhan Pulivarthi <j143@protonmail.com>
2025-09-02 00:10:31 +0530

    implement reset, closeInput method signatures

--- src/main/java/org/apache/sysds/runtime/controlprogram/parfor/ResettableStream.java
+++ src/main/java/org/apache/sysds/runtime/controlprogram/parfor/ResettableStream.java
@@ -17,11 +17,33 @@ public class ResettableStream extends LocalTaskQueue<IndexedMatrixValue> {
 	// in-memory cache to store stream for re-play
 	private final ArrayList<IndexedMatrixValue> _cache;
 
+	// state flags
+	private boolean _cacheInProgress = true; // caching in progress, in the first pass.
+	private int _replayPosition = 0; // slider position in the stream
+
 	public ResettableStream(LocalTaskQueue<IndexedMatrixValue> source) {
 		this._source = source;
 		this._cache = new ArrayList<>();
 	}
 
+	@Override
+	public synchronized IndexedMatrixValue dequeueTask()
+					throws InterruptedException {
+		// Implement dequeTask logic
+		return null;
+	}
 
+	/**
+	 * Resets the stream to beginning to read the stream from start.
+	 * This can only be called once the stream is fully consumed once.
+	 */
+	public void reset() {
+		// Implement reset logic
+	}
 
+	@Override
+	public synchronized void closeInput() {
+
+		_source.closeInput();
+	}
 }

3fe964b15781239fee8d7599544a4b3436187100
Janardhan Pulivarthi <j143@protonmail.com>
2025-09-02 18:31:04 +0530

    implement dequeueTask and reset

--- src/main/java/org/apache/sysds/runtime/controlprogram/parfor/ResettableStream.java
+++ src/main/java/org/apache/sysds/runtime/controlprogram/parfor/ResettableStream.java
@@ -1,5 +1,7 @@
 package org.apache.sysds.runtime.controlprogram.parfor;
 
+import org.apache.hadoop.yarn.webapp.hamlet.HamletSpec;
+import org.apache.sysds.runtime.DMLRuntimeException;
 import org.apache.sysds.runtime.instructions.spark.data.IndexedMatrixValue;
 
 import java.util.ArrayList;
@@ -26,11 +28,35 @@ public class ResettableStream extends LocalTaskQueue<IndexedMatrixValue> {
 		this._cache = new ArrayList<>();
 	}
 
+	/**
+	 * Dequeues a task. If it is the first, it reads from the disk and stores in the cache.
+	 * For subsequent passes it reads from the memory.
+	 *
+	 * @return The next matrix value in the stream, or NO_MORE_TASKS
+	 * @throws InterruptedException
+	 */
 	@Override
 	public synchronized IndexedMatrixValue dequeueTask()
 					throws InterruptedException {
-		// Implement dequeTask logic
-		return null;
+		if (_cacheInProgress) {
+			// First pass: Read value from the source and cache it, and return.
+			IndexedMatrixValue task = _source.dequeueTask();
+			if (task != NO_MORE_TASKS) {
+				_cache.add(new IndexedMatrixValue(task));
+			} else {
+				_cacheInProgress = false; // caching is complete
+				_source.closeInput(); // close source stream
+			}
+			return task;
+		} else {
+			// Replay pass: read directly from in-memory cache
+			if (_replayPosition < _cache.size()) {
+				// Return a copy to ensure consumer won't modify the cache
+				return new IndexedMatrixValue(_cache.get(_replayPosition++));
+			} else {
+				return (IndexedMatrixValue) NO_MORE_TASKS;
+			}
+		}
 	}
 
 	/**
@@ -38,7 +64,10 @@ public class ResettableStream extends LocalTaskQueue<IndexedMatrixValue> {
 	 * This can only be called once the stream is fully consumed once.
 	 */
 	public void reset() {
-		// Implement reset logic
+		if (_cacheInProgress) {
+			throw new DMLRuntimeException("Attempted to reset a stream that's not been fully cached yet.");
+		}
+		_replayPosition = 0;
 	}
 
 	@Override

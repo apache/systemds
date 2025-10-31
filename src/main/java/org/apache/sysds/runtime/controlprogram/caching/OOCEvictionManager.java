3926c1c3d77b83b1d28607df07aa4805d1d6fa48
Janardhan Pulivarthi <j143@protonmail.com>
2025-10-31 18:37:34 +0530

    use indexedmatrixvalue and check with actions

--- src/main/java/org/apache/sysds/runtime/controlprogram/caching/OOCEvictionManager.java
+++ src/main/java/org/apache/sysds/runtime/controlprogram/caching/OOCEvictionManager.java
@@ -78,7 +78,7 @@ public class OOCEvictionManager {
 	private static long _size;
 
 	// Cache structures: map key -> MatrixBlock and eviction deque (head=oldest block)
-	private static final Map<String, MatrixBlock> _cache = new HashMap<>();
+	private static final Map<String, IndexedMatrixValue> _cache = new HashMap<>();
 	private static final Deque<String> _evictDeque = new ArrayDeque<>();
 
 	// Single lock for synchronization
@@ -110,10 +110,10 @@ public class OOCEvictionManager {
 		String key = streamId + "_" + blockId;
 
 		synchronized (lock) {
-			MatrixBlock old = _cache.remove(key);
+			IndexedMatrixValue old = _cache.remove(key); // remove old value
 			if (old != null) {
 				_evictDeque.remove(key);
-				_size -= estimateSerializedSize(old);
+				_size -= estimateSerializedSize((MatrixBlock) old.getValue());
 			}
 
 			try {
@@ -122,7 +122,7 @@ public class OOCEvictionManager {
 				throw new DMLRuntimeException(e);
 			}
 
-			_cache.put(key, mb);
+			_cache.put(key, value); // put new value
 			_evictDeque.addLast(key); // add to end for FIFO/LRU
 			_size += size;
 		}
@@ -132,19 +132,19 @@ public class OOCEvictionManager {
 	 * Get a block from the OOC cache (deserialize on read)
 	 */
 	public static synchronized IndexedMatrixValue get(long streamId, int blockId) {
+
 		String key = streamId + "_" + blockId;
-		MatrixBlock mb = (MatrixBlock) _cache.get(key);
+		IndexedMatrixValue imv = _cache.get(key);
 
 		synchronized (lock) {
-			if (mb != null && _policy == RPolicy.LRU) {
+			if (imv != null && _policy == RPolicy.LRU) {
 				_evictDeque.remove(key);
 				_evictDeque.addLast(key);
 			}
 		}
 
-		if (mb != null) {
-			MatrixIndexes ix = new MatrixIndexes(blockId + 1, 1);
-			return new IndexedMatrixValue(ix, mb);
+		if (imv != null) {
+			return imv;
 		} else {
 			try {
 				return loadFromDisk(streamId, blockId);
@@ -162,23 +162,23 @@ public class OOCEvictionManager {
 		while(_size + requiredSize > _limit && !_evictDeque.isEmpty()) {
 			System.out.println("_size + requiredSize: " + _size +" + "+ requiredSize + "; _limit: " + _limit);
 			String oldKey = _evictDeque.removeLast();
-			MatrixBlock mbToEvict = (MatrixBlock) _cache.remove(oldKey);
+			IndexedMatrixValue toEvict = _cache.remove(oldKey);
 
-			if(mbToEvict != null) {
+			if (toEvict == null) { continue;}
+			MatrixBlock mbToEvict = (MatrixBlock) toEvict.getValue();
 
-				// Spill to disk
-				String filename = _spillDir + "/" + oldKey;
-				File spillDirFile = new File(_spillDir);
-				if (!spillDirFile.exists()) {
-					spillDirFile.mkdirs();
-				}
-
-				LocalFileUtils.writeMatrixBlockToLocal(filename, mbToEvict);
-				System.out.println("Evicting directory: "+ filename);
-
-				long freedSize = estimateSerializedSize(mbToEvict);
-				_size -= freedSize;
+			// Spill to disk
+			String filename = _spillDir + "/" + oldKey;
+			File spillDirFile = new File(_spillDir);
+			if (!spillDirFile.exists()) {
+				spillDirFile.mkdirs();
 			}
+
+			LocalFileUtils.writeMatrixBlockToLocal(filename, mbToEvict);
+
+			long freedSize = estimateSerializedSize(mbToEvict);
+			_size -= freedSize;
+
 		}
 	}
 

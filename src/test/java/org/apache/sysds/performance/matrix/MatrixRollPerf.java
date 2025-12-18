2978d006becdfffbb4e8c752fedaa7f6f13e1459
Biranavan Parameswaran <biranavan.parameswaran@adesso.de>
2025-12-18 01:01:55 +0100

    [SYSTEMDS-3730] Add missing license

--- src/test/java/org/apache/sysds/performance/matrix/MatrixRollPerf.java
+++ src/test/java/org/apache/sysds/performance/matrix/MatrixRollPerf.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.sysds.performance.matrix;
 
 import org.apache.sysds.performance.compression.APerfTest;
@@ -89,8 +108,8 @@ public class MatrixRollPerf extends APerfTest<Object, MatrixBlock> {
 		Random rand = new Random(42);
 
 		for (int i = 0; i < iterations; i++) {
-			int rows = minRows + rand.nextInt(500);
-			int cols = minCols + rand.nextInt(500);
+			int rows = 10_000_000;
+			int cols = 10;
 			int shift = rand.nextInt((maxShift - minShift) + 1) + minShift;
 
 			MatrixBlock denseIn = TestUtils.generateTestMatrixBlock(rows, cols, -100, 100, 1.0, 42);

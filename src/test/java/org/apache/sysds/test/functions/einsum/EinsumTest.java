eac7e0eebaa33d9f306d76c7fe2cc8ab219ffb5c
Hubert Krawczyk <hubert0krawczyk@gmail.com>
2025-12-29 11:23:59 +0100

    [SYSTEMDS-3909] Final framework for einsum expressions
    Closes #2391.

--- src/test/java/org/apache/sysds/test/functions/einsum/EinsumTest.java
+++ src/test/java/org/apache/sysds/test/functions/einsum/EinsumTest.java
@@ -37,65 +37,109 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(Parameterized.class)
 public class EinsumTest extends AutomatedTestBase
 {
-	final private static List<Config> TEST_CONFIGS = List.of(
-			new Config("ij,jk->ik", List.of(shape(50, 600), shape(600, 10))), // mm
-			new Config("ji,jk->ik", List.of(shape(600, 5), shape(600, 10))),
-			new Config("ji,kj->ik", List.of(shape(600, 5), shape(10, 600))),
-			new Config("ij,kj->ik", List.of(shape(5, 600), shape(10, 600))),
+    final private static List<Config> TEST_CONFIGS = List.of(
+		new Config("ij,jk->ik", List.of(shape(5, 6), shape(6, 5))), // mm
+		new Config("ji,jk->ik", List.of(shape(6, 5), shape(6, 10))),
+		new Config("ji,kj->ik", List.of(shape(6, 5), shape(10, 6))),
+		new Config("ij,kj->ik", List.of(shape(5, 6), shape(10, 6))),
+		new Config("ij,jk->ki", List.of(shape(5, 6), shape(6, 5))), // mm t
+		new Config("ji,jk->ki", List.of(shape(6, 5), shape(6, 10))),
+		new Config("ji,kj->ki", List.of(shape(6, 5), shape(10, 6))),
+		new Config("ij,kj->ki", List.of(shape(5, 6), shape(10, 6))),
+		new Config("ij,kp,pj->ki", List.of(shape(5,6), shape(5,4), shape(4, 6))), // reordering
+		new Config("ab,bc,cd,de->ae", List.of(shape(5, 6), shape(6, 5),shape(5, 6), shape(6, 5))), // mm chain
+		new Config("de,cd,bc,ab->ae", List.of(shape(6, 5), shape(5, 6),shape(6, 5), shape(5, 6))), // mm chain
+		new Config("ab,cb,de,cd->ae", List.of(shape(5, 6), shape(5,6), shape(6, 5),shape(5, 6))), // mm chain
 
-			new Config("ji,jk->i", List.of(shape(600, 5), shape(600, 10))),
-			new Config("ij,jk->i", List.of(shape(5, 600), shape(600, 10))),
+		new Config("ji,jk->i", List.of(shape(6, 5), shape(6, 4))),
+		new Config("ij,jk->i", List.of(shape(5, 6), shape(6, 4))),
+		new Config("ji,jk->k", List.of(shape(6, 5), shape(6, 4))),
+		new Config("ij,jk->k", List.of(shape(5, 6), shape(6, 4))),
+		new Config("ji,jk->j", List.of(shape(6, 5), shape(6, 4))),
 
-			new Config("ji,jk->k", List.of(shape(600, 5), shape(600, 10))),
-			new Config("ij,jk->k", List.of(shape(5, 600), shape(600, 10))),
+		new Config("ji,ji->ji", List.of(shape(60, 5), shape(60, 5))), // elemwise mult
+		new Config("ji,ji->j", List.of(shape(60, 5), shape(60, 5))),
+		new Config("ji,ji->i", List.of(shape(60, 5), shape(60, 5))),
+		new Config("ji,ij->ji", List.of(shape(60, 5), shape(5, 60))), // elemwise mult
+		new Config("ji,ij->i", List.of(shape(60, 5), shape(5, 60))),
+		new Config("ji,ij->j", List.of(shape(60, 5), shape(5, 60))),
 
-			new Config("ji,jk->j", List.of(shape(600, 5), shape(600, 10))),
+		new Config("ij,i->ij",   List.of(shape(10, 5), shape(10))), // col mult
+		new Config("ji,i->ij",   List.of(shape(5, 10), shape(10))), // row mult
+		new Config("ij,i->i",   List.of(shape(10, 5), shape(10))),
+		new Config("ij,i->j",   List.of(shape(10, 5), shape(10))),
 
-			new Config("ji,ji->ji", List.of(shape(600, 5), shape(600, 5))), // elemwise mult
-			new Config("ji,ji,ji->ji", List.of(shape(600, 5),shape(600, 5), shape(600, 5)),
-					List.of(0.0001, 0.0005, 0.001)),
-			new Config("ji,ij->ji", List.of(shape(600, 5), shape(5, 600))), // elemwise mult
+		new Config("i,i->",     List.of(shape(5), shape(5))), // dot
+		new Config("i,j->",     List.of(shape(5), shape(80))), // sum
+		new Config("i,j->ij",     List.of(shape(5), shape(80))), // outer vect mult
+		new Config("i,j->ji",     List.of(shape(5), shape(80))), // outer vect mult
 
+		new Config("ij->",     List.of(shape(10, 5))), // sum
+		new Config("i->",     List.of(shape(10))), // sum
+		new Config("ij->i",     List.of(shape(10, 5))), // sum(1)
+		new Config("ij->j",     List.of(shape(10, 5))), // sum(0)
+		new Config("ij->ji",     List.of(shape(10, 5))), // T
+		new Config("ij->ij",     List.of(shape(10, 5))),
+		new Config("i->i",     List.of(shape(10))),
+		new Config("ii->i",     List.of(shape(10, 10))), // Diag
+		new Config("ii->",     List.of(shape(10, 10))), // Trace
+		new Config("ii,i->i",     List.of(shape(10, 10),shape(10))), // Diag*vec
 
-			new Config("ij,i->ij",   List.of(shape(100, 50), shape(100))), // col mult
-			new Config("ji,i->ij",   List.of(shape(50, 100), shape(100))), // row mult
-			new Config("ij,i->i",   List.of(shape(100, 50), shape(100))),
-			new Config("ij,i->j",   List.of(shape(100, 50), shape(100))),
+		new Config("ab,cd->ba",     List.of(shape( 6, 10), shape(6, 5))), // sum cd to scalar and multiply ab
 
-			new Config("i,i->",     List.of(shape(50), shape(50))),
-			new Config("i,j->",     List.of(shape(50), shape(80))),
-			new Config("i,j->ij",     List.of(shape(50), shape(80))), // outer vect mult
-			new Config("i,j->ji",     List.of(shape(50), shape(80))), // outer vect mult
+		new Config("fx,fg,fz,xg,zx,zg->g", // each idx 3 times (the cell tpl fallback)
+				List.of(shape(5, 6), shape(5, 3), shape(5, 10), shape(6, 3), shape(10, 6), shape(10, 3))),
 
-			new Config("ij->",     List.of(shape(100, 50))), // sum
-			new Config("ij->i",     List.of(shape(100, 50))), // sum(1)
-			new Config("ij->j",     List.of(shape(100, 50))), // sum(0)
-			new Config("ij->ji",     List.of(shape(100, 50))), // T
+		// test fused:
+		new Config("ij,ij,ji->ij",   List.of(shape(10, 5), shape(10, 5), shape(5, 10))),
+		new Config("ij,ij,ji,i,j->ij",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(10),shape(5))),
+		new Config("ij,ij,ji,i->ij",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(10))),
+		new Config("ij,ij,ji,j->ij",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(5))),
 
-			new Config("ab,cd->ba",     List.of(shape( 600, 10), shape(6, 5))),
-			new Config("ab,cd,g->ba",     List.of(shape( 600, 10), shape(6, 5), shape(3))),
+		new Config("ij,ij,ji->i",   List.of(shape(10, 5), shape(10, 5), shape(5, 10))),
+		new Config("ij,ij,ji,i,j->i",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(10),shape(5))),
+		new Config("ij,ij,ji,i->i",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(10))),
+		new Config("ij,ij,ji,j->i",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(5))),
 
-			new Config("ab,bc,cd,de->ae",   List.of(shape(5, 600), shape(600, 10), shape(10, 5), shape(5, 4))), // chain of mm
+		new Config("ij,ij,ji->j",   List.of(shape(10, 5), shape(10, 5), shape(5, 10))),
+		new Config("ij,ij,ji,i,j->j",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(10),shape(5))),
+		new Config("ij,ij,ji,i->j",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(10))),
+		new Config("ij,ij,ji,j->j",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(5))),
 
-			new Config("ji,jz,zx->ix",   List.of(shape(600, 5), shape( 600, 10), shape(10, 2))),
-			new Config("fx,fg,fz,xg->z",   List.of(shape(600, 5), shape( 600, 10), shape(600, 6), shape(5, 10))),
-			new Config("fx,fg,fz,xg,zx,zg->g", // each idx 3 times (cell tpl)
-					List.of(shape(5, 60), shape(5, 30), shape(5, 10), shape(60, 30), shape(10, 60), shape(10, 30))),
+		new Config("ij,ij,ji->",   List.of(shape(10, 5), shape(10, 5), shape(5, 10))),
+		new Config("ij,ij,ji,i,j->",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(10),shape(5))),
+		new Config("ij,ij,ji,i->",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(10))),
+		new Config("ij,ij,ji,j->",   List.of(shape(10, 5), shape(10, 5),shape(5, 10),shape(5))),
 
-			new Config("i->",     List.of(shape(100))),
-			new Config("i->i",     List.of(shape(100)))
-	);
+		new Config("ij,ij,ij,i,j,iz->z",  List.of(shape(5, 5), shape(5, 5), shape(5, 5),shape(5),shape(5),shape(5, 6))),
+		new Config("ij,ij,ij,i,j,iz,z->z",  List.of(shape(5, 5), shape(5, 5), shape(5, 5),shape(5),shape(5),shape(5, 6),shape(6))),
 
+		new Config("ij,i,j,iz->z",  List.of(shape(10, 5),shape(10),shape(5),shape(10, 51))),
+		new Config("ij,i,j,iz,iz->z",  List.of(shape(10, 5),shape(10),shape(5),shape(10, 51),shape(10, 51))),
+		new Config("ij,i,j,iz,iz->z",  List.of(shape(10, 5),shape(10),shape(5),shape(10, 4),shape(10, 4))), // order swapped because sizeof(iz) < sizeof(ij), but should still produce the same tmpl
+		new Config("ij,i,j,iz->z",  List.of(shape(20, 10),shape(20),shape(10),shape(20, 10))),
+
+		new Config("ij,ij,ji,j,i, ab,ba,ab,a,b->jb",   Map.of('i',10, 'j',5, 'a', 11, 'b', 6)),
+		new Config("ij,ij,ji,j,i,iz->jz",   Map.of('i',10, 'j',10,'z', 10)),  // // no skinny right
+		// takes 2 mins each due to R package being super slow so commented out:
+//			new Config("ij,ij,i,j,iz->jz",   Map.of('i',100000, 'j',50,'z', 40), List.of(0.0000001,0.000002, 0.000003,0.000004,0.000004)),  // skinny right with outer mm
+//			new Config("ij,ij,i,iz->jz",   Map.of('i',100000, 'j',50,'z', 40), List.of(0.0000001,0.000002, 0.000003,0.000004)),  // skinny right with outer mm
+//			new Config("ij,j,i,iz->zj",   Map.of('i',100000, 'j',50,'z', 40), List.of(0.0000001,0.000002, 0.000003,0.000004)),  // skinny right with outer mm
+		new Config("ab,ab,a,ag,gz->bz",   List.of(shape(10, 5), shape(10, 5),shape(10),shape(10,200),shape(200,7)))
+		,new Config("ab,ab,a,ag,gz->bz",   List.of(shape(10, 5), shape(10, 5),shape(10),shape(10,20),shape(20,7)))
+		,new Config("ab,ab,bc,bc->bc",   List.of(shape(10, 5), shape(10, 5),shape(5,20),shape(5,20)))
+    );
 	private final int id;
 	private final String einsumStr;
-	//private final List<int[]> shapes;
 	private final File dmlFile;
 	private final File rFile;
 	private final boolean outputScalar;
@@ -103,7 +147,6 @@ public class EinsumTest extends AutomatedTestBase
 	public EinsumTest(String einsumStr, List<int[]> shapes, File dmlFile, File rFile, boolean outputScalar, int id){
 		this.id = id;
 		this.einsumStr = einsumStr;
-		//this.shapes = shapes;
 		this.dmlFile = dmlFile;
 		this.rFile = rFile;
 		this.outputScalar = outputScalar;
@@ -116,7 +159,6 @@ public class EinsumTest extends AutomatedTestBase
 		int counter = 1;
 
 		for (Config config : TEST_CONFIGS) {
-			//List<File> files = new ArrayList<>();
 			String fullDMLScriptName = "SystemDS_einsum_test" + counter;
 
 			File dmlFile = File.createTempFile(fullDMLScriptName, ".dml");
@@ -153,12 +195,12 @@ public class EinsumTest extends AutomatedTestBase
 			sb.append("A");
 			sb.append(i);
 
-			if (dims.length == 1) { // A1 = seq(1,1000) * 0.0001
+			if (dims.length == 1) { // e.g. A1 = seq(1,100) * 0.0001
 				sb.append(" = seq(1,");
 				sb.append(dims[0]);
 				sb.append(") * ");
 				sb.append(factor);
-			} else { // A0 = matrix(seq(1,50000), 1000, 50) * 0.0001
+			} else { // e.g. A0 = matrix(seq(1,5000), 100, 5) * 0.0001
 				sb.append(" = matrix(seq(1, ");
 				sb.append(dims[0]*dims[1]);
 				sb.append("), ");
@@ -172,7 +214,6 @@ public class EinsumTest extends AutomatedTestBase
 			sb.append("\n");
 		}
 		sb.append("\n");
-
 		sb.append("R = einsum(\"");
 		sb.append(config.einsumStr);
 		sb.append("\", ");
@@ -202,17 +243,17 @@ public class EinsumTest extends AutomatedTestBase
 
 		for (int i = 0; i < config.shapes.size(); i++) {
 			int[] dims = config.shapes.get(i);
-			
+
 			double factor = config.factors != null ? config.factors.get(i) : 0.0001;
 			sb.append("A");
 			sb.append(i);
 
-			if (dims.length == 1) { // A1 = seq(1,1000) * 0.0001
+			if (dims.length == 1) { // e.g. A1 = seq(1,100) * 0.0001
 				sb.append(" = seq(1,");
 				sb.append(dims[0]);
 				sb.append(") * ");
 				sb.append(factor);
-			} else { // A0 = matrix(seq(1,50000), 1000, 50, byrow=TRUE) * 0.0001
+			} else { // e.g. A0 = matrix(seq(1,5000), 100, 5, byrow=TRUE) * 0.0001
 				sb.append(" = matrix(seq(1, ");
 				sb.append(dims[0]*dims[1]);
 				sb.append("), ");
@@ -252,7 +293,7 @@ public class EinsumTest extends AutomatedTestBase
 	@Test
 	public void testEinsumWithFiles() {
 		System.out.println("Testing einsum: " + this.einsumStr);
-		testCodegenIntegration(TEST_NAME_EINSUM+this.id);
+		test(TEST_NAME_EINSUM+this.id);
 	}
 	@After
 	public void cleanUp() {
@@ -271,19 +312,36 @@ public class EinsumTest extends AutomatedTestBase
 	}
 
 	private static class Config {
-		public List<Double> factors;
+        public List<Double> factors;
 		String einsumStr;
 		List<int[]> shapes;
 
-		Config(String einsum, List<int[]> shapes) {
-			this.einsumStr = einsum;
-			this.shapes = shapes;
-			this.factors = null;
-		}
+        Config(String einsum, List<int[]> shapes) {
+            this(einsum,shapes,null);
+        }
+        Config(String einsum, Map<Character, Integer> charToSize){
+            this(einsum, charToSize, null);
+        }
+
+        Config(String einsum, Map<Character, Integer> charToSize, List<Double> factors) {
+            this.einsumStr = einsum;
+            String leftPart = einsum.split("->")[0];
+            List<int[]> shapes = new ArrayList<>();
+            for(String op : Arrays.stream(leftPart.split(",")).map(x->x.trim()).toList()){
+                if (op.length() == 1) {
+                    shapes.add(new int[]{charToSize.get(op.charAt(0))});
+                }else{
+                    shapes.add(new int[]{charToSize.get(op.charAt(0)),charToSize.get(op.charAt(1))});
+                }
+
+            }
+            this.shapes = shapes;
+            this.factors = factors;
+        }
 		Config(String einsum, List<int[]> shapes, List<Double> factors) {
 			this.einsumStr = einsum;
 			this.shapes = shapes;
-			this.factors = factors;
+            this.factors = factors;
 		}
 	}
 
@@ -295,7 +353,7 @@ public class EinsumTest extends AutomatedTestBase
 	private static final String TEST_NAME_EINSUM = "einsum";
 	private static final String TEST_DIR = "functions/einsum/";
 	private static final String TEST_CLASS_DIR = TEST_DIR + EinsumTest.class.getSimpleName() + "/";
-	private final static String TEST_CONF = "SystemDS-config-codegen.xml";
+	private final static String TEST_CONF = "SystemDS-config-einsum.xml";
 	private final static File   TEST_CONF_FILE = new File(SCRIPT_DIR + TEST_DIR, TEST_CONF);
 
 	private static double eps = Math.pow(10, -10);
@@ -307,7 +365,7 @@ public class EinsumTest extends AutomatedTestBase
 			addTestConfiguration( TEST_NAME_EINSUM+i, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME_EINSUM+i, new String[] { String.valueOf(i) }) );
 	}
 
-	private void testCodegenIntegration( String testname)
+	private void test(String testname)
 	{
 		boolean oldFlag = OptimizerUtils.ALLOW_ALGEBRAIC_SIMPLIFICATION;
 		ExecMode platformOld = setExecMode(ExecType.CP);
@@ -329,16 +387,17 @@ public class EinsumTest extends AutomatedTestBase
 			runTest(true, false, null, -1);
 			runRScript(true);
 
+			HashMap<CellIndex, Double> dmlfile;
+			HashMap<CellIndex, Double> rfile;
 			if(outputScalar){
-				HashMap<CellIndex, Double> dmlfile = readDMLScalarFromOutputDir("S");
-				HashMap<CellIndex, Double> rfile = readRScalarFromExpectedDir("S");
-				TestUtils.compareMatrices(dmlfile, rfile, eps, "Stat-DML", "Stat-R");
+				dmlfile = readDMLScalarFromOutputDir("S");
+				rfile = readRScalarFromExpectedDir("S");
 			}else {
 				//compare matrices
-				HashMap<CellIndex, Double> dmlfile = readDMLMatrixFromOutputDir("S");
-				HashMap<CellIndex, Double> rfile = readRMatrixFromExpectedDir("S");
-				TestUtils.compareMatrices(dmlfile, rfile, eps, "Stat-DML", "Stat-R");
+				dmlfile = readDMLMatrixFromOutputDir("S");
+				rfile = readRMatrixFromExpectedDir("S");
 			}
+			TestUtils.compareMatrices(dmlfile, rfile, eps, "Stat-DML", "Stat-R");
 		}
 		finally {
 			resetExecMode(platformOld);

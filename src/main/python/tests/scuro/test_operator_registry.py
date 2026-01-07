a1ea0a9fb8effd99bb6c94bf1b8078ba52bf84cb
Christina Dionysio <dionysio@tu-berlin.de>
2026-01-07 15:55:24 +0100

    add missing context operators to test

--- src/main/python/tests/scuro/test_operator_registry.py
+++ src/main/python/tests/scuro/test_operator_registry.py
@@ -25,6 +25,10 @@ from systemds.scuro.representations.text_context import (
     SentenceBoundarySplit,
     OverlappingSplit,
 )
+from systemds.scuro.representations.text_context_with_indices import (
+    SentenceBoundarySplitIndices,
+    OverlappingSplitIndices,
+)
 from systemds.scuro.representations.covarep_audio_features import (
     ZeroCrossing,
     Spectral,
@@ -135,6 +139,8 @@ class TestOperatorRegistry(unittest.TestCase):
         assert registry.get_context_operators(ModalityType.TEXT) == [
             SentenceBoundarySplit,
             OverlappingSplit,
+            SentenceBoundarySplitIndices,
+            OverlappingSplitIndices,
         ]
 
     # def test_fusion_operator_in_registry(self):

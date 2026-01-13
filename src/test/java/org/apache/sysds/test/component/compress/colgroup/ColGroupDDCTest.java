31568637f6f7ea783b66cc924ee18dd7454fba6a
fjobs <florianjobs@gmx.de>
2026-01-13 11:27:24 +0100

    test commit

--- src/test/java/org/apache/sysds/test/component/compress/colgroup/ColGroupDDCTest.java
+++ src/test/java/org/apache/sysds/test/component/compress/colgroup/ColGroupDDCTest.java
@@ -63,11 +63,11 @@ public class ColGroupDDCTest {
 
                 // mixed noise
                 2, 1, 0, 2, 1, 0, 2, 1, 0,
-                0, 2, 1, 0, 2, 1, 0, 2, 1,1,1,1,1,1,1,
+                0, 2, 1, 0, 2, 1, 0, 2, 1, 1, 1, 1, 1, 1, 1,
 
                 // repeating tail (tests dictionary reuse)
                 2, 0, 2, 1, 0, 2, 1, 0, 2,
-                2, 0, 2, 1, 0, 2, 1, 0, 2,0,0,0,0,0,1
+                2, 0, 2, 1, 0, 2, 1, 0, 2, 0, 0, 0, 0, 0, 1
         };
 
         final int nRows = src.length;

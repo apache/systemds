eac21f5423e5c7351767a36295c5cb39a7f0f924
AdityaPandey2612 <apande27@uic.edu>
2026-01-18 16:09:37 +0100

    "done"

--- src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinAutoencoderGeneralizedBasicTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part1/BuiltinAutoencoderGeneralizedBasicTest.java
@@ -16,24 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
 package org.apache.sysds.test.functions.builtin.part1;
 
@@ -42,18 +24,11 @@ import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
-package org.apache.sysds.test.functions.builtin.part1;
 
-import org.apache.sysds.runtime.meta.MatrixCharacteristics;
-import org.apache.sysds.test.AutomatedTestBase;
-import org.apache.sysds.test.TestConfiguration;
-import org.junit.Assert;
-import org.junit.Test;
-
-public class BuiltinAutoencoderGeneralizedTest extends AutomatedTestBase {
+public class BuiltinAutoencoderGeneralizedBasicTest extends AutomatedTestBase {
     private static final String TEST_NAME = "autoencoderGeneralized";
     private static final String TEST_DIR = "functions/builtin/";
-    private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinAutoencoderGeneralizedTest.class.getSimpleName() + "/";
+    private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinAutoencoderGeneralizedBasicTest.class.getSimpleName() + "/";
 
     private static final int ROWS = 128;
     private static final int COLS = 64;

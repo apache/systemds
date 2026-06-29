ad8e2bb2b1ad67e6a5e0e06617537be932888d2a
ywcb00 <ywcb00@ywcb.org>
2026-06-29 13:11:30 +0200

    fix(RewriteMatrixMultChainOptimizationSparse.java): replace the mnc estimator by the basic average estimator for matrix multiplication chain rewrites 	NOTE: Missing information about the matrix histograms of input data caused certain limitations for the implementation with the mnc estimator.

--- src/main/java/org/apache/sysds/hops/rewrite/RewriteMatrixMultChainOptimizationSparse.java
+++ src/main/java/org/apache/sysds/hops/rewrite/RewriteMatrixMultChainOptimizationSparse.java
@@ -22,18 +22,12 @@ package org.apache.sysds.hops.rewrite;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.mutable.MutableInt;
 import org.apache.sysds.hops.Hop;
-import org.apache.sysds.hops.HopsException;
+import org.apache.sysds.hops.OptimizerUtils;
 import org.apache.sysds.hops.estim.MMNode;
-import org.apache.sysds.hops.estim.EstimatorMatrixHistogram;
-import org.apache.sysds.hops.estim.EstimatorMatrixHistogram.MatrixHistogram;
+import org.apache.sysds.hops.estim.EstimatorBasicAvg;
 import org.apache.sysds.hops.estim.SparsityEstimator.OpCode;
-import org.apache.sysds.runtime.controlprogram.LocalVariableMap;
-import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
-import org.apache.sysds.runtime.instructions.cp.Data;
-import org.apache.sysds.runtime.matrix.data.MatrixBlock;
 
 /**
  * Rule: Determine the optimal order of execution for a chain of
@@ -53,9 +47,8 @@ public class RewriteMatrixMultChainOptimizationSparse extends RewriteMatrixMultC
 		double[] dimsArray = new double[mmChain.size() + 1];
 		boolean dimsKnown = getDimsArray( hop, mmChain, dimsArray );
 		MMNode[] sketchArray = new MMNode[mmChain.size() + 1];
-		boolean inputsAvail = getInputMatrices(hop, mmChain, sketchArray, state);
-		
-		if( dimsKnown && inputsAvail ) {
+		boolean inputMetaAvail = getInputMatrixCharacteristics(hop, mmChain, sketchArray, state);
+		if(dimsKnown && inputMetaAvail) {
 			// Step 3: clear the links among Hops within the identified chain
 			clearLinksWithinChain ( hop, mmOperators );
 			
@@ -92,7 +85,7 @@ public class RewriteMatrixMultChainOptimizationSparse extends RewriteMatrixMultC
 		}
 
 		//compute cost-optimal chains for increasing chain sizes 
-		EstimatorMatrixHistogram estim = new EstimatorMatrixHistogram(true);
+		EstimatorBasicAvg estim = new EstimatorBasicAvg();
 		for( int l = 2; l <= size; l++ ) { // chain length
 			for( int i = 0; i < size - l + 1; i++ ) {
 				int j = i + l - 1;
@@ -102,14 +95,14 @@ public class RewriteMatrixMultChainOptimizationSparse extends RewriteMatrixMultC
 				{
 					//construct estimation nodes (w/ lazy propagation and memoization)
 					MMNode tmp = new MMNode(dpMatrixS[i][k], dpMatrixS[k+1][j], OpCode.MM);
-					estim.estim(tmp, false);
-					MatrixHistogram lhs = (MatrixHistogram) dpMatrixS[i][k].getSynopsis();
-					MatrixHistogram rhs = (MatrixHistogram) dpMatrixS[k+1][j].getSynopsis();
-					
+					estim.estim(tmp);
+
 					//recursive cost computation
-					double cost = dpMatrix[i][k] + dpMatrix[k + 1][j] 
-						+ dotProduct(lhs.getColCounts(), rhs.getRowCounts());
-					
+					double cost = dpMatrix[i][k] + dpMatrix[k+1][j] +
+						OptimizerUtils.getSparsity(tmp.getLeft().getDataCharacteristics()) *
+							OptimizerUtils.getSparsity(tmp.getRight().getDataCharacteristics()) *
+							tmp.getLeft().getRows() * tmp.getLeft().getCols() * tmp.getRight().getCols();
+
 					//prune suboptimal
 					if( cost < dpMatrix[i][j] ) {
 						dpMatrix[i][j] = cost;
@@ -119,29 +112,30 @@ public class RewriteMatrixMultChainOptimizationSparse extends RewriteMatrixMultC
 				}
 
 				if( LOG.isTraceEnabled() ){
-					LOG.trace("mmchainopt [i="+(i+1)+",j="+(j+1)+"]: costs = "+dpMatrix[i][j]+", split = "+(split[i][j]+1));
+					LOG.trace("mmchainoptsparse [i="+(i+1)+",j="+(j+1)+"]: costs = "+dpMatrix[i][j]+", split = "+(split[i][j]+1));
+					System.out.println("mmchainoptsparse [i="+(i+1)+",j="+(j+1)+"]: costs = "+dpMatrix[i][j]+", split = "+(split[i][j]+1));
 				}
 			}
 		}
 
 		return split;
 	}
-	
-	private static boolean getInputMatrices(Hop hop, List<Hop> chain, MMNode[] sketchArray, ProgramRewriteStatus state) {
-		throw new NotImplementedException("Not implemeneted yet. Prior implementation did not work.");
-	}
-	
-	private static MatrixBlock getMatrix(String name, LocalVariableMap vars) {
-		Data dat = vars.get(name);
-		if( !(dat instanceof MatrixObject) )
-			throw new HopsException("Input '"+name+"' not a matrix: "+dat.getDataType());
-		return ((MatrixObject)dat).acquireReadAndRelease();
-	}
-	
-	private static double dotProduct(int[] h1cNnz, int[] h2rNnz) {
-		long fp = 0;
-		for( int j=0; j<h1cNnz.length; j++ )
-			fp += (long)h1cNnz[j] * h2rNnz[j];
-		return fp;
+
+	private static boolean getInputMatrixCharacteristics(Hop hop, List<Hop> chain, MMNode[] sketchArray, ProgramRewriteStatus state) {
+		boolean inputMetaAvail = true;
+
+		for(int counter = 0; counter < chain.size(); counter++ ) {
+			Hop currentHop = chain.get(counter);
+			inputMetaAvail &= currentHop.isMatrix();
+			inputMetaAvail &= !currentHop.isFederated();
+			inputMetaAvail &= (currentHop.getDataCharacteristics().getNonZeros() != -1);
+			if(inputMetaAvail) {
+				sketchArray[counter] = new MMNode(currentHop.getDataCharacteristics());
+			}
+			else
+				break;
+		}
+
+		return inputMetaAvail;
 	}
 }

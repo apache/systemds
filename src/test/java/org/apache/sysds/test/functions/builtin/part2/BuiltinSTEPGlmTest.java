b7e64b83cf0c88545f833b4ac1fe911aed34039b
bruno <bruno.ignaz.rothschild@gmail.com>
2026-07-02 07:08:53 +0200

    format BuiltinSTEPGlmTest.java via jdtls

--- src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinSTEPGlmTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinSTEPGlmTest.java
@@ -25,44 +25,42 @@ import org.apache.sysds.common.Types.ExecType;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 
-public class BuiltinSTEPGlmTest extends AutomatedTestBase 
-{
-  private final static String TEST_NAME = "stepGLM";
-  private final static String TEST_DIR = "functions/builtin/";
-  private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinSTEPGlmTest.class.getSimpleName() + "/";
+public class BuiltinSTEPGlmTest extends AutomatedTestBase {
+	private final static String TEST_NAME = "stepGLM";
+	private final static String TEST_DIR = "functions/builtin/";
+	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinSTEPGlmTest.class.getSimpleName() + "/";
 
-  @Override
-  public void setUp() {
-	addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[]{}));
-  }
-
-  @Test
-  public void testLmMatrixDenseCPlm() {
-	runSTEPGlmTest(ExecType.CP);
-  }
-
-  @Test
-  public void testLmMatrixSparseSPlm() {
-	runSTEPGlmTest(ExecType.SPARK);
-  }
-
-  private void runSTEPGlmTest(ExecType instType) {
-	ExecMode platformOld = setExecMode(instType);
-
-	try {
-	  loadTestConfiguration(getTestConfiguration(TEST_NAME));
-
-	  String HOME = SCRIPT_DIR + TEST_DIR;
-
-	  // Pointing to the generated validation DML script
-	  fullDMLScriptName = HOME + TEST_NAME + ".dml";
-	  programArgs = new String[]{};
-
-	  // runTest executes the script; fails if the DML script invokes stop()
-	  runTest(true, false, null, -1);
+	@Override
+	public void setUp() {
+		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {}));
 	}
-	finally {
-	  rtplatform = platformOld;
+
+	@Test
+	public void testLmMatrixDenseCPlm() {
+		runSTEPGlmTest(ExecType.CP);
 	}
-  }
+
+	@Test
+	public void testLmMatrixSparseSPlm() {
+		runSTEPGlmTest(ExecType.SPARK);
+	}
+
+	private void runSTEPGlmTest(ExecType instType) {
+		ExecMode platformOld = setExecMode(instType);
+
+		try {
+			loadTestConfiguration(getTestConfiguration(TEST_NAME));
+
+			String HOME = SCRIPT_DIR + TEST_DIR;
+
+			// Pointing to the generated validation DML script
+			fullDMLScriptName = HOME + TEST_NAME + ".dml";
+			programArgs = new String[] {};
+
+			// runTest executes the script; fails if the DML script invokes stop()
+			runTest(true, false, null, -1);
+		} finally {
+			rtplatform = platformOld;
+		}
+	}
 }

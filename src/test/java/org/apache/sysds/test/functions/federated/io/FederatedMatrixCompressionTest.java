c347a5f293ec4c7c539e9a5a639052a141063ec5
Charansurya Udaysingh Jhurree <charansurya.udaysingh.jhurree@informatik.hu-berlin.de>
2026-07-10 17:16:48 +0200

    Add federated matrix compression integration test

---
+++ src/test/java/org/apache/sysds/test/functions/federated/io/FederatedMatrixCompressionTest.java
@@ -0,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sysds.test.functions.federated.io;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Random;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.sysds.api.DMLScript;
+import org.apache.sysds.common.Types;
+import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
+import org.apache.sysds.runtime.controlprogram.federated.compression.CompressionType;
+import org.apache.sysds.runtime.matrix.data.MatrixValue;
+import org.apache.sysds.runtime.meta.MatrixCharacteristics;
+import org.apache.sysds.test.AutomatedTestBase;
+import org.apache.sysds.test.TestConfiguration;
+import org.apache.sysds.test.TestUtils;
+import org.apache.sysds.test.functions.federated.FederatedTestObjectConstructor;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(value = Parameterized.class)
+@net.jcip.annotations.NotThreadSafe
+public class FederatedMatrixCompressionTest extends AutomatedTestBase {
+
+	private static final Log LOG = LogFactory.getLog(FederatedMatrixCompressionTest.class.getName());
+	private final static String TEST_DIR = "functions/federated/io/";
+	private final static String TEST_NAME = "FederatedMatrixCompressionTest";
+	private final static String TEST_CLASS_DIR = TEST_DIR + FederatedMatrixCompressionTest.class.getSimpleName() + "/";
+	private final static int blocksize = 1024;
+	private final static String OUTPUT_NAME = "Z";
+
+	@Parameterized.Parameter()
+	public CompressionType compressionType;
+	@Parameterized.Parameter(1)
+	public int rows;
+	@Parameterized.Parameter(2)
+	public int cols;
+
+	@Override
+	public void setUp() {
+		TestUtils.clearAssertionInformation();
+		addTestConfiguration(TEST_NAME, new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[] {OUTPUT_NAME}));
+	}
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+			// {compressionType, rows, cols}
+			{CompressionType.TOPK, 5, 5},
+			{CompressionType.PROBABILISTIC_QUANTIZATION, 5, 5},
+		});
+	}
+
+	@Test
+	public void testFederatedMatrixCompression() {
+		federatedMatrixCompression();
+	}
+
+	public void federatedMatrixCompression() {
+		Types.ExecMode oldPlatform = setExecMode(Types.ExecType.CP);
+		getAndLoadTestConfiguration(TEST_NAME);
+
+		LOG.debug("Current test configuration: compressionType = " + compressionType
+			+ ", rows = " + rows + ", cols = " + cols);
+
+		fullDMLScriptName = "";
+		int port1 = getRandomAvailablePort();
+		Thread t1 = startLocalFedWorkerThread(port1);
+		String host = "localhost";
+
+		DMLScript.FEDERATED_COMPRESSION = true;
+		DMLScript.FEDERATED_COMPRESSION_TYPE = compressionType;
+
+		try {
+			double[][] X1 = createRandomMatrix(rows, cols);
+			MatrixCharacteristics mc = new MatrixCharacteristics(rows, cols, blocksize, (long) rows * cols);
+			writeCSVMatrix("X1", X1, false, mc);
+
+			long[][] begins = new long[][] {new long[] {0, 0}};
+			long[][] ends = new long[][] {new long[] {rows, cols}};
+			MatrixObject fed = FederatedTestObjectConstructor.constructFederatedInput(rows, cols, blocksize, host,
+				begins, ends, new int[] {port1}, new String[] {input("X1")}, input("X.json"));
+			writeInputFederatedWithMTD("X.json", fed);
+
+			// Run reference DML with normal matrix multiply
+			fullDMLScriptName = SCRIPT_DIR + TEST_DIR + TEST_NAME + "Reference.dml";
+			programArgs = new String[] {"-nvargs",
+				"in_X1=" + input("X1"),
+				"rows=" + rows,
+				"cols=" + cols,
+				"out=" + expected(OUTPUT_NAME)};
+			runTest(null);
+
+			// Run federated DML with compression enabled
+			fullDMLScriptName = SCRIPT_DIR + TEST_DIR + TEST_NAME + ".dml";
+			programArgs = new String[] {"-nvargs",
+				"in_X1=" + input("X.json"),
+				"rows=" + rows,
+				"cols=" + cols,
+				"out=" + output(OUTPUT_NAME)};
+			runTest(null);
+
+			HashMap<MatrixValue.CellIndex, Double> refResults = readDMLMatrixFromExpectedDir(OUTPUT_NAME);
+			HashMap<MatrixValue.CellIndex, Double> fedResults = readDMLMatrixFromOutputDir(OUTPUT_NAME);
+			// Use tolerance of 1.0 since TopK and probabilistic quantization are lossy
+			TestUtils.compareMatrices(fedResults, refResults, 1.0, "Fed", "Ref");
+		}
+		catch(Exception e) {
+			LOG.warn("Failed to run test with compressionType = " + compressionType
+				+ ", rows = " + rows + ", cols = " + cols);
+			e.printStackTrace();
+			Assert.assertTrue(false);
+		}
+		finally {
+			DMLScript.FEDERATED_COMPRESSION = false;
+			resetExecMode(oldPlatform);
+		}
+
+		TestUtils.shutdownThreads(t1);
+	}
+
+	public double[][] createRandomMatrix(int width, int height) {
+		Random rd = new Random();
+		double[][] matrix = new double[height][];
+
+		for(int i = 0; i < height; i++)
+			matrix[i] = rd.doubles(width).toArray();
+
+		return matrix;
+	}
+}
